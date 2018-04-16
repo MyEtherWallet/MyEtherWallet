ce011c1b95f581d027446c68fb0ef4fafcaf814f
David Hong <hsurf22@gmail.com>
2018-04-16 16:32:09 -0700

    Several page components added

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "Footer.scss";
+</style>
---
+++ src/components/Header/TopMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="top-menu">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "TopMenu.scss";
+</style>
