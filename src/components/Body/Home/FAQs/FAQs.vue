0c54645cf8c031fe5bcd69fb64a4281e70ce161d
David Hong <hsurf22@gmail.com>
2018-04-18 11:47:49 -0700

    FAQs page added

---
+++ src/components/Body/Home/FAQs/FAQs.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about-mew">
+    <div class="wrap">
+      <h2>FAQs</h2>
+      <h5>Have not found the question that you are looking for?  See More…</h5>
+    </div><!-- .wrap -->
+  </div><!-- .about-mew -->
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "FAQs.scss";
+</style>
