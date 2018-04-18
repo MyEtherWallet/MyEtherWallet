d5b80f4cfeaba8f509ea1753febf92e0212d29a7
David Hong <hsurf22@gmail.com>
2018-04-17 17:04:18 -0700

    About MEW page update

--- src/components/Body/Home/AboutMEW/AboutMEW.vue
+++ src/components/Body/Home/AboutMEW/AboutMEW.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="about-mew">
     <div class="wrap">
+      <div class="circle">
+        <img src="~@/assets/images/home/circle.svg">
+      </div>
 
-      <div>
-        hi
+      <div class="star-background">
+        <div class="page-container">
+          <div class="flex-col-1-1-vertical-center content">
+            <div class="text-content">
+              <h2>About MEW</h2>
+              <p>
+                Free, open-source, client-side interface for generating
+                Ethereum wallets & more. Interact with the Ethereum blockchain
+                easily & securely. Double-check the URL ( myetherwallet.com )
+                before unlocking your wallet.
+              </p>
+              <div class="watch-video">
+                <p>Watch Video <i class="fa fa-caret-right" aria-hidden="true"></i></p>
+              </div>
+            </div>
+            <div class="image-content">
+              <img class="spaceman" src="~@/assets/images/home/spaceman.png">
+              <img class="mew-icon" src="~@/assets/images/home/mew-icon.png">
+            </div>
+          </div>
+        </div><!-- .page-container -->
       </div>
 
     </div>
