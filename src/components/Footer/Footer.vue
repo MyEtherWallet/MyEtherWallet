0ab7ceb9d04ca574ec5b10a41aaf6306b97ea0aa
David Hong <hsurf22@gmail.com>
2018-04-19 17:11:35 -0700

    Footer is added

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,7 +2,80 @@
   <div class="footer">
     <div class="wrap">
       <div class="page-container">
-        Footer
+        <div class="grid-col-1-1-1-2 footer-contents">
+          <div>
+            <h3 class="lite">Discover</h3>
+            <router-link to="/"><p>Units</p></router-link>
+            <router-link to="/"><p>TX Status</p></router-link>
+            <router-link to="/"><p>Debugs</p></router-link>
+            <router-link to="/"><p>Extantions</p></router-link>
+            <router-link to="/"><p>Others</p></router-link>
+          </div>
+          <div>
+            <h3 class="lite">Affiliates</h3>
+            <router-link to="/"><p>Ledger Wallet</p></router-link>
+            <router-link to="/"><p>Digital Bitbox</p></router-link>
+            <router-link to="/"><p>Ether Card</p></router-link>
+            <router-link to="/"><p>Trezor</p></router-link>
+            <router-link to="/"><p>Bity</p></router-link>
+          </div>
+          <div>
+            <h3 class="lite">MEW</h3>
+            <router-link to="/"><p>About Us</p></router-link>
+            <router-link to="/"><p>Team</p></router-link>
+            <router-link to="/"><p>FAQs</p></router-link>
+            <router-link to="/"><p>Customer Support</p></router-link>
+          </div>
+          <div class="donate-us">
+            <h3 class="lite">
+              Donate Us
+              <img src="~@/assets/images/icons/heart.svg">
+            </h3>
+            <p class="no-margin">BTC:</p>
+            <router-link to="/">
+              <p>1DECAF2uSpFTP4L1fAHR8GCLrPqdwdLse9</p>
+            </router-link>
+            <p class="no-margin">ETH: mewtopia.eth</p>
+            <router-link to="/">
+              <p>0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D</p>
+            </router-link>
+          </div>
+        </div><!-- .footer-contents -->
+        <div class="flex-space-between foot-note">
+          <div class="links">
+            <router-link to="/"><span>Feedback</span></router-link>
+            <router-link to="/"><span>Privacy</span></router-link>
+            <router-link to="/"><span>Terms</span></router-link>
+          </div>
+          <div class="copyright">
+            <p>
+              Copyright 2018 All right reserved by MyEtherWallet, Inc.
+            </p>
+          </div>
+          <div class="social">
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-reddit-alien" aria-hidden="true"></i>
+            </a>
+            <a href="https://facebook.com" target="_blank">
+              <i class="fa fa-medium" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div><!-- .foot-note -->
       </div>
     </div>
   </div>
