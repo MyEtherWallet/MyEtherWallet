6f264ec61d40c7d8439ec7e08d6b24eb274fc706
David Hong <hsurf22@gmail.com>
2018-04-25 14:15:19 -0700

    Resizing menu added

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="header">
     <div class="wrap">
-      <div class="page-container">
-        <div class="header-container">
-          <div class="top-logo">
-            <img src="~@/assets/images/logo.png">
-          </div>
-          <div class="top-menu">
-            <ul>
-              <li><router-link to="/">Home</router-link></li>
-              <li><a href="#pagelink-about-mew">About</a></li>
-              <li><a href="#pagelink-faqs">FAQs</a></li>
-              <li><a href="#pagelink-news">News</a></li>
-              <li>
-                <img class="country-flag" src="~@/assets/images/flags/gb.svg">
-                <router-link to="/">English</router-link>
-                <i class="fa fa-sort-desc" aria-hidden="true"></i>
-              </li>
-            </ul>
-          </div>
-          <div class="mobile-menu">
-            <div class="mobile-menu-button">
-              <div class="bar-1"></div>
-              <div class="bar-2"></div>
-              <div class="bar-3"></div>
+      <div class="fixed-header">
+        <div class="page-container">
+          <div class="header-container">
+            <div class="top-logo">
+              <img class="logo-large" src="~@/assets/images/logo.png">
             </div>
-          </div>
-        </div><!-- .header-container -->
-      </div><!-- .page-container -->
+            <div class="top-menu">
+              <ul>
+                <li><router-link to="/">Home</router-link></li>
+                <li><a href="#pagelink-about-mew">About</a></li>
+                <li><a href="#pagelink-faqs">FAQs</a></li>
+                <li><a href="#pagelink-news">News</a></li>
+                <li>
+                  <img class="country-flag" src="~@/assets/images/flags/gb.svg">
+                  <router-link to="/">English</router-link>
+                  <i class="fa fa-sort-desc" aria-hidden="true"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="mobile-menu">
+              <div class="mobile-menu-button">
+                <div class="bar-1"></div>
+                <div class="bar-2"></div>
+                <div class="bar-3"></div>
+              </div>
+            </div>
+          </div><!-- .header-container -->
+        </div><!-- .page-container -->
+      </div><!-- .fixed-header -->
     </div><!-- .wrap -->
   </div><!-- .header -->
 </template>
@@ -36,6 +38,22 @@
 export default {
   data () {
     return {
+    }
+  },
+  mounted () {
+    // Check if user scrolled window, then change header style.
+    window.onscroll = function (e) {
+      var element = document.querySelector('body')
+      var header = document.querySelector('.fixed-header')
+      var logoLarge = document.querySelector('.logo-large')
+      var topPos = element.getBoundingClientRect().top
+      if (topPos < -150) {
+        header.classList.add('tiny-header')
+        logoLarge.classList.add('logo-small')
+      } else {
+        header.classList.remove('tiny-header')
+        logoLarge.classList.remove('logo-small')
+      }
     }
   }
 }
