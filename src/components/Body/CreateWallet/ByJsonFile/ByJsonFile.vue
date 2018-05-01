22beda1e54a2cd35f9d01de1841360283f47f830
David Hong <hsurf22@gmail.com>
2018-04-30 18:10:47 -0700

    Create Wallet page added

---
+++ src/components/Body/CreateWallet/ByJsonFile/ByJsonFile.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="by-json-file">
+    <vue-header></vue-header>
+    <by-json-page-title></by-json-page-title>
+    <by-json-your-password></by-json-your-password>
+    <by-json-page-footer></by-json-page-footer>
+    <vue-footer></vue-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "ByJsonFile.scss";
+</style>
---
+++ src/components/Body/CreateWallet/ByJsonFile/PageFooter/PageFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page-footer">
+    <div class="wrap">
+      <div class="page-container">
+        <p>Learn more about Blockchain or MEW? Please go to <router-link to="/">FAQs</router-link></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "PageFooter.scss";
+</style>
