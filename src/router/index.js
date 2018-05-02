30512eb2d1e0cdf2e0503189dc8e987e2bd493c5
David Hong <hsurf22@gmail.com>
2018-05-02 15:14:56 -0700

    By JSON File page added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Body/Home/Home'
 import CreateWallet from '@/components/Body/CreateWallet/ByJsonOrMnemonic/YourPassword/YourPassword'
+import ByJsonFile from '@/components/Body/CreateWallet/ByJsonOrMnemonic/ByJsonFile/ByJsonFile'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/create-wallet',
       name: 'CreateWallet',
       component: CreateWallet
+    },
+    {
+      path: '/by-json-file',
+      name: 'ByJsonFile',
+      component: ByJsonFile
     }
   ]
 })
