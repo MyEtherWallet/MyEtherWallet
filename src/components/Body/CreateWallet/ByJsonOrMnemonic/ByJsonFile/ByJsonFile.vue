f31ee27fb199186a6baa495807ebd6586cc86897
David Hong <hsurf22@gmail.com>
2018-05-02 16:14:39 -0700

    Create Wallet - By JSON file page update

--- src/components/Body/CreateWallet/ByJsonOrMnemonic/ByJsonFile/ByJsonFile.vue
+++ src/components/Body/CreateWallet/ByJsonOrMnemonic/ByJsonFile/ByJsonFile.vue
@@ -8,7 +8,12 @@
           <b-tabs>
             <div class="progress-bar"></div>
             <b-tab title="By JSON File" active>
-              <h3>Save My Keystore File</h3>
+              <h3>
+                Save My Keystore File
+                <span class="tooltip-icon">
+                  <b-btn v-b-tooltip.hover title="Unfortunately, we encounter these phishing scams/attacks on a daily basis. Typically, the reason for these incidents is entering your private key on a malicious website. Sometimes it is a fake MyEtherWallet site (e.g. myethreawllet[.]com) and other times it is a fake ICO site or airdrop site or EthZero site. If you enter your private key (or keystore file, mnemonic phrase, passwords) on a malicious website, they have complete access to your funds. Because of the nature of the blockchain and cryptography, there is no way to reverse transactions or reset your passwords. Once a transaction is on the blockchain, it's final. For this reason, you need to move your funds as soon as possible and discontinue use of this wallet. We do actively attempt to track down the people behind these sites and do what we can to warn others, take down the site, or whatever we can given the particular situation.">?</b-btn>
+                </span>
+              </h3>
               <div class="contents">
                 <div class="content-block">
                   <img class="icon" src="~@/assets/images/icons/no-lose.svg">
