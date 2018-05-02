b8cf60cffd787d5f09e8a49ec4a132ae6207af4c
David Hong <hsurf22@gmail.com>
2018-05-02 16:55:05 -0700

    By Mnemonic Phrase page added

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '@/components/Body/Home/Home'
 import CreateWallet from '@/components/Body/CreateWallet/ByJsonOrMnemonic/YourPassword/YourPassword'
 import ByJsonFile from '@/components/Body/CreateWallet/ByJsonOrMnemonic/ByJsonFile/ByJsonFile'
+import ByMnemonic from '@/components/Body/CreateWallet/ByJsonOrMnemonic/ByMnemonic/ByMnemonic'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/by-json-file',
       name: 'ByJsonFile',
       component: ByJsonFile
+    },
+    {
+      path: '/by-mnemonic-phrase',
+      name: 'ByMnemonic',
+      component: ByMnemonic
     }
   ]
 })
