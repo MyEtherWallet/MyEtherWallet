17a0399d5ab066bd3154fa93a77037c165963bd8
David Hong <hsurf22@gmail.com>
2018-05-10 15:12:26 -0700

    FAQ update

--- src/components/Body/Home/TopBanner/TopBanner.vue
+++ src/components/Body/Home/TopBanner/TopBanner.vue
@@ -6,10 +6,11 @@
           <div class="banner-text">
             <h1>Your Security</h1>
             <h1>Ethereum Wallet.</h1>
-            <h2>
-              Here is subtitle, Here is subtitle,
-              Here is subtitle, Here is subtitle.
-            </h2>
+            <p>
+              MEW helps you to generate Ethereum wallets, and interact with
+              the Ethereum blockchain easily and securely.
+              MEW is a free and open-source, client-side interface.
+            </p>
           </div>
           <div class="banner-image">
             <img src="~@/assets/images/home/top-banner.jpg">
@@ -17,7 +18,7 @@
         </div><!-- .banner-image -->
         <div class="grid-col-2 promo-cards">
           <router-link to="/">
-            <div class="card create-wallet">
+            <div class="card-block create-wallet">
               <div class="flex-col-vertical-center">
                 <div class="card-image">
                   <img src="~@/assets/images/home/create-wallet.png">
@@ -37,7 +38,7 @@
             </div>
           </router-link>
           <router-link to="/">
-            <div class="card unlock-wallet">
+            <div class="card-block unlock-wallet">
               <div class="flex-col-vertical-center">
                 <div class="card-image">
                   <img src="~@/assets/images/home/unlock-wallet.png">
