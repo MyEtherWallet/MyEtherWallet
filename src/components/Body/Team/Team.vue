c762da48c36a4da0d98b4c381ad9b273a377328d
David Hong <hsurf22@gmail.com>
2018-05-21 17:03:12 -0700

    Team page in progress

--- src/components/Body/Team/Team.vue
+++ src/components/Body/Team/Team.vue
@@ -4,37 +4,18 @@
 
     <div class="wrap">
       <div class="page-container">
-        <div class="title">
-          <h2>Unlock Wallet</h2>
-          <h5>
-            Do not have a wallet?
-            <router-link to="/">Create Wallet</router-link>
-          </h5>
+
+        <div class="page-header">
+          <h1>Meet Our Team</h1>
+          <p>
+            MyEtherWallet is comprised of talented, inspiring, and hardworking
+            individuals from around the world. We share the passion to code,
+            create, and ultimately build an open, accessible and fair financial
+            future, one piece of software at a time. Together, we work hard,
+            brainstorm nonstop, and brew endless coffee.
+          </p>
         </div>
-        <div class="blocks">
-          <ul>
-            <li>
-              <h6>Hardware</h6>
-              <p>Ledger wallet; Trezor; Digital bitbox; Secalot</p>
-            </li>
-            <li>
-              <h6>JSON File</h6>
-              <p>Update ordrop your keystore file here</p>
-            </li>
-            <li>
-              <h6>Mnemonic Phase</h6>
-              <p>Enter your 12-bit or 14-bit mnemonic phrases</p>
-            </li>
-            <li>
-              <h6>MetaMask</h6>
-              <p>Use extention to unlock your wallet</p>
-            </li>
-            <li>
-              <h6>Private Key</h6>
-              <p>Enter your private key to unlock wallet</p>
-            </li>
-          </ul>
-        </div>
+
       </div><!-- .page-container -->
     </div><!-- .wrap -->
 
