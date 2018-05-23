bbe60dfab0d4289e57130904a54c9f2356e1d1ec
David Hong <hsurf22@gmail.com>
2018-05-22 17:08:44 -0700

    Access My Wallet is done

---
+++ src/components/Body/AccessMyWallet/AccessMyWalletOptions/AccessMyWalletOptions.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="access-my-wallet-options">
+
+    <div class="wrap">
+      <div class="page-container">
+        <div class="title">
+          <h2>Access My Wallet</h2>
+          <h5>
+            Do not have a wallet?
+            <router-link to="/">Get a free wallet</router-link>
+          </h5>
+        </div>
+        <div class="buttons-container">
+          <div class="button-block active">
+            <div class="button-image">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </div>
+            <h3>MEW Connect</h3>
+            <p>Use MEW Connect to access my wallet</p>
+          </div>
+          <div class="button-block">
+            <div class="button-image">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </div>
+            <h3>Hardware</h3>
+            <p>Ledger wallet; Trezor; Digital bitbox; Secalot</p>
+          </div>
+          <div class="button-block">
+            <div class="button-image">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </div>
+            <h3>MetaMask</h3>
+            <p>Use extention to access my wallet</p>
+          </div>
+          <div class="button-block">
+            <div class="button-image">
+              <i class="fa fa-telegram" aria-hidden="true"></i>
+            </div>
+            <h3>Software</h3>
+            <p>JSON file, Private key, Mnemonic phrase</p>
+            <p class="small-note">Not recommended</p>
+          </div>
+        </div>
+      </div><!-- .page-container -->
+    </div><!-- .wrap -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "AccessMyWalletOptions.scss";
+</style>
