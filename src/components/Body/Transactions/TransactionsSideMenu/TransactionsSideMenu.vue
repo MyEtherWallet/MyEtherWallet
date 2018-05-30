87130d6b606b959b8f31ea4bea7ce4020ad8dc7a
David Hong <hsurf22@gmail.com>
2018-05-29 18:17:19 -0700

    Send ETH & Tokens page update

--- src/components/Body/Transactions/TransactionsSideMenu/TransactionsSideMenu.vue
+++ src/components/Body/Transactions/TransactionsSideMenu/TransactionsSideMenu.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="transactions-side-menu">
-    <ul>
-      <li>
-        Send
-        <ul>
-          <li>Send ETH and Tokens</li>
-          <li>Send Offline</li>
-        </ul>
-      </li>
-      <li>Swap</li>
-      <li>Dapps</li>
-      <li>
-        Contract
-        <ul>
-          <li>Interact With Contract</li>
-          <li>Deploy Contract</li>
-        </ul>
-      </li>
-    </ul>
+    <div class="side-menu">
+      <ul>
+        <li>
+          <p class="active">Send <i class="fa fa-angle-up" aria-hidden="true"></i></p>
+          <ul>
+            <li>Send ETH and Tokens</li>
+            <li class="active">Send Offline</li>
+          </ul>
+        </li>
+        <li><p>Swap</p></li>
+        <li><p>Dapps</p></li>
+        <li>
+          <p>Contract <i class="fa fa-angle-down" aria-hidden="true"></i></p>
+          <ul>
+            <li>Interact With Contract</li>
+            <li>Deploy Contract</li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
