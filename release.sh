85f607143e59e6f6fdc1b476e137678fcb6d9050
gamalielhere <gamalielgad1234@gmail.com>
2018-05-31 14:33:36 -0700

    Run build and update the shell command

--- release.sh
+++ release.sh
@@ -1,10 +1,11 @@
+npm run build;
 # openssl aes-256-cbc -K $encrypted_85fecf66b314_key -iv $encrypted_85fecf66b314_iv -in mew.gpg.enc -out mew.gpg -d;
 sudo mkdir deploy;
 # gpg --allow-secret-key-import --import mew.gpg;
 
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]');
 
-zip -r ./deploy/release-"$PACKAGE_VERSION".zip ./dist;
+sudo zip -r ./deploy/release-"$PACKAGE_VERSION".zip ./dist;
 
 # for f in deploy/*; do
 #   gpg --output $f.sig --detach-sig $f
