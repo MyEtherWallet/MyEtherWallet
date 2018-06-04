fe21f5c2ef0aacce59a2bfd023ff8db79fa9ee15
gamalielhere <gamalielgad1234@gmail.com>
2018-06-03 23:52:13 -0700

    Cleanup deploy script, remove release script

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,3 @@ echo "Remove gitignore";
 rm .gitignore;
 echo "Rebuild app";
 npm run build;
-# # openssl aes-256-cbc -K $encrypted_85fecf66b314_key -iv $encrypted_85fecf66b314_iv -in mew.gpg.enc -out mew.gpg -d;
-# sudo mkdir deploy;
-# # gpg --allow-secret-key-import --import mew.gpg;
-#
-# PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]');
-#
-# sudo zip -r ./deploy/release-"$PACKAGE_VERSION".zip ./dist;
-#
-# # for f in deploy/*; do
-# #   gpg --output $f.sig --detach-sig $f
-# # done;x
-#
-# git tag v$PACKAGE_VERSION; git push --tags;
