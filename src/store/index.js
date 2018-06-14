57dad03234277f8574a37df35e04196d22980c94
David Hong <hsurf22@gmail.com>
2018-06-14 16:32:15 -0700

    Vuex setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import State from './state'
+import Store from './store'
 import Getters from './getters'
 import Mutations from './mutations'
 import Actions from './actions'
@@ -9,6 +10,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: State,
+  store: Store,
   mutations: Mutations,
   getters: Getters,
   actions: Actions,
