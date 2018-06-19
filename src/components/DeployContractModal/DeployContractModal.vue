f46cd363d5f0f26651f0490d69fc2b22e862a3f5
gamalielhere <gamalielgad1234@gmail.com>
2018-06-19 14:37:43 -0700

    Finish breakdown

---
+++ src/components/DeployContractModal/DeployContractModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-modal ref="success" centered hide-footer hide-header class="bootstrap-modal">
+    <div class="d-block text-center">
+      <i class="check-icon fa fa-check" aria-hidden="true"></i>
+      <h2 class="title">Succeed</h2>
+    </div>
+    <div class="button-container">
+      <router-link to="/interact-with-contract">
+        <b-btn class="mid-round-button-green-filled close-button" @click="showModal = false">
+          Ok
+        </b-btn>
+      </router-link>
+    </div>
+  </b-modal>
+</template>
+
+<style lang="scss" scoped>
+  @import "DeployContractModal.scss";
+</style>
--- src/components/InterfaceSideMenu/InterfaceSideMenu.vue
+++ src/components/InterfaceSideMenu/InterfaceSideMenu.vue
@@ -14,8 +14,8 @@
         <li>
           <p @click.prevent="toggle('openContract')" :class="selectedTab === 'interactC' || selectedTab === 'deployC' ? 'active' : ''">{{ $t("txSideMenu.contract") }} <i :class="['fa', showContract ? 'fa-angle-up':'fa-angle-down']" aria-hidden="true"></i></p>
           <ul v-show="showContract">
-            <li @click.prevent="switchTabs('interactC')" :class="selectedTab === 'interactC'? 'active': ''">{{ $t("txSideMenu.interactWcontract") }}</li>
-            <li @click.prevent="switchTabs('deployC')" :class="selectedTab === 'deployC'? 'active': ''">{{ $t("txSideMenu.depContract") }}</li>
+            <li @click.prevent="switchTabs('interactC')" :class="selectedTab === 'interactC'? 'active': ''">{{ $t("reused.interactWcontract") }}</li>
+            <li @click.prevent="switchTabs('deployC')" :class="selectedTab === 'deployC'? 'active': ''">{{ $t("reused.depContract") }}</li>
           </ul>
         </li>
       </ul>
