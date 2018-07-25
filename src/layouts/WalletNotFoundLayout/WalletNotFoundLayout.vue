97b23849d31c30ded48336cbb873002799085274
David Hong <hsurf22@gmail.com>
2018-07-25 13:38:10 -0700

    Wallet not found page added

---
+++ src/layouts/WalletNotFoundLayout/WalletNotFoundLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wallet-not-found">
+    <div class="wrap">
+      <div class="page-container">
+
+        <div class="content-block">
+          <div class="block-title">Oops! Wallet not found :(</div>
+          <div class="buttons">
+            <div class="mid-round-button-green-filled-green-border">Access My Wallet</div>
+          </div>
+          <div class="spaceman">
+            <img src="~@/assets/images/home/spaceman.png">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "WalletNotFoundLayout.scss";
+</style>
