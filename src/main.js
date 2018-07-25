0051fe115ecc353d42b7e89b0bbb2a72e2416c07
David Hong <hsurf22@gmail.com>
2018-07-25 16:07:39 -0700

    Just a merge test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import BootstrapVue from 'bootstrap-vue'
