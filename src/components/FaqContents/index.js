126bfb9212e01908676f0c323eee7c3def44ee23
David Hong <hsurf22@gmail.com>
2018-07-27 14:06:32 -0700

    FAQ items added

--- src/components/FaqContents/index.js
+++ src/components/FaqContents/index.js
@@ -1,1 +1,5 @@
-export { default } from './BalancesNotLoadingOrShowingOnMyEtherWallet'
+
+export {default as BalancesNotLoadingOrShowingOnMyEtherWallet} from './BalancesNotLoadingOrShowingOnMyEtherWallet'
+export {default as EthOrTokensSentToOrFromExchangeHaventShownUp} from './EthOrTokensSentToOrFromExchangeHaventShownUp'
+export {default as DoesMyEtherWalletSupportBitcoinOrOtherCoins} from './DoesMyEtherWalletSupportBitcoinOrOtherCoins'
+export {default as HowToBuyEthereumWithUSD} from './HowToBuyEthereumWithUSD'
