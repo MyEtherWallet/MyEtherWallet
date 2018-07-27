f2e5b5a8fb4d7aa78013e2d6da6b3bbff98be549
David Hong <hsurf22@gmail.com>
2018-07-27 14:29:56 -0700

    FAQ updated

---
+++ src/components/FaqContents/CheckTheCurrentBlockNumberOnMyEtherWallet.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    CheckTheCurrentBlockNumberOnMyEtherWallet
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "FaqContents.scss";
+</style>
