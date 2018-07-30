2327104f7e2a526d918946fc8684a7afa544ee74
David Hong <hsurf22@gmail.com>
2018-07-30 11:45:33 -0700

    More FAQs added

--- src/components/FaqContents/CheckTheCurrentBlockNumberOnMyEtherWallet.vue
+++ src/components/FaqContents/CheckTheCurrentBlockNumberOnMyEtherWallet.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    CheckTheCurrentBlockNumberOnMyEtherWallet
+    <p>If you are on <a href="https://www.myetherwallet.com/">https://www.myetherwallet.com/</a> and connected to a node that supports it, you can see the current block number in small print in footer of the page.</p>
   </div>
 </template>
 
