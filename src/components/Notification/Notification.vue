40908fbdf98fdb0ee0972d411f83cff526ef2155
gamalielhere <gamalielgad1234@gmail.com>
2018-07-30 10:09:45 -0700

    Cleanup notification, make it more reactive

--- src/components/Notification/Notification.vue
+++ src/components/Notification/Notification.vue
@@ -6,15 +6,15 @@
     </div>
     <b-modal ref="notification" hide-footer centered class="bootstrap-modal-wide nopadding" v-on:show="countUnread">
       <template slot="modal-title">
-        <h5 class="modal-title"> {{unreadCount > 1 ?'Notifications':'Notification'}} <div class="notification-count" v-show="unreadCount > 0"><span>{{ unreadCount }}</span></div></h5>
+        <h5 class="modal-title"> {{unreadCount > 1 ? 'Notifications':'Notification'}} <div class="notification-count" v-show="unreadCount > 0"><span>{{ unreadCount }}</span></div></h5>
       </template>
       <div class="notification-item-container">
-        <div v-if="notifications[$store.state.wallet.getAddressString()] !== undefined && notifications[$store.state.wallet.getAddressString()].length > 0" v-for="(notification, idx) in notifications[$store.state.wallet.getAddressString()]" class="notification-item" :key="notification.title + notification.timestamp + idx" @click="expand(idx, notification, $event)">
+        <div v-if="sortedNotifications !== undefined && sortedNotifications.length > 0" v-for="(notification, idx) in sortedNotifications" class="notification-item" :key="notification.title + notification.timestamp + idx" @click="expand(idx, notification, $event)">
           <div class="notification-header">
-            <p> {{ notification.title }} </p>
-            <p> {{ notification.timestamp }}</p>
+            <p :class="[notification.read? '': 'unread']"> {{ notification.title }} </p>
+            <p :class="[notification.read? '': 'unread']"> {{ notification.timestamp }}</p>
           </div>
-          <div class="notification-body unexpanded">
+          <div :class="[notification.expanded?'':'unexpanded', 'notification-body']">
             <code>
               {{ notification.body }}
             </code>
@@ -42,7 +42,7 @@ export default {
       const self = this
       self.unreadCount = 0
       if (self.notifications[self.$store.state.wallet.getAddressString()] !== undefined && self.notifications[self.$store.state.wallet.getAddressString()].length > 0) {
-        self.notifications[self.$store.state.wallet.getAddressString()].reverse().forEach(item => {
+        self.notifications[self.$store.state.wallet.getAddressString()].map(item => {
           if (item.read === false) {
             self.unreadCount++
           }
@@ -53,9 +53,14 @@ export default {
       this.$refs.notification.show()
     },
     expand (idx, notif, e) {
-      e.target.nextElementSibling.classList.contains('unexpanded') ? e.target.nextElementSibling.classList.remove('unexpanded') : e.target.nextElementSibling.classList.add('unexpanded')
+      // e.target.nextElementSibling.classList.contains('unexpanded') ? e.target.nextElementSibling.classList.remove('unexpanded') : e.target.nextElementSibling.classList.add('unexpanded')
       const updatedNotif = notif
-      updatedNotif.read = true
+      if (notif.expanded !== true) {
+        updatedNotif.read = true
+        updatedNotif.expanded = true
+      } else {
+        updatedNotif.expanded = false
+      }
       this.$store.dispatch('updateNotification', [this.$store.state.wallet.getAddressString(), idx, updatedNotif])
     }
   },
@@ -63,9 +68,6 @@ export default {
     this.countUnread()
   },
   watch: {
-    unreadCount (newVal) {
-      this.unreadCount = newVal
-    },
     notifications (newVal) {
       this.countUnread()
     }
@@ -73,7 +75,17 @@ export default {
   computed: {
     ...mapGetters({
       notifications: 'notifications'
-    })
+    }),
+    sortedNotifications () {
+      this.countUnread()
+      // eslint-disable-next-line
+      return this.notifications[this.$store.state.wallet.getAddressString()].sort((a, b) => {
+        a = new Date(a.timestamp)
+        b = new Date(b.timestamp)
+
+        return a > b ? -1 : a < b ? 1 : 0
+      })
+    }
   }
 }
 </script>
