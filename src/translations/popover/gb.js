8bc8d05a3fbbe36e5dac4bd36194079609e42b61
David Hong <hsurf22@gmail.com>
2018-07-31 16:15:56 -0700

    popover updates

--- src/translations/popover/gb.js
+++ src/translations/popover/gb.js
@@ -1,9 +1,14 @@
 const popoverTranslations = {
   whatIsMessageTitle: 'What is Message?',
   whatIsMessageContent: 'So what is Message??? More info needed...',
+
   whatIsSignatureTitle: 'What is Signature?',
   whatIsSignatureContent: 'So what is Signature??? More info needed...',
+
   whatIsSpeedOfTransactionTitle: 'What is Speed of Transaction?',
-  whatIsSpeedOfTransactionContent: 'So.... What is Speed of Transaction?'
+  whatIsSpeedOfTransactionContent: 'So.... What is Speed of Transaction?',
+
+  WhatIsNonceTitle: 'What is Nonce?',
+  WhatIsNonceContent: 'So... What is Nonce?'
 }
 export default popoverTranslations
