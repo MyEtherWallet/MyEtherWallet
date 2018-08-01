80fcdc626adb9d25ed934f1b0606d56297cdf6a5
Kosala Hemachandra <kvhnuke@aol.com>
2018-08-01 16:46:58 -0700

    Codecov intergration

--- test/unit/jest.conf.js
+++ test/unit/jest.conf.js
@@ -5,7 +5,8 @@ module.exports = {
   moduleFileExtensions: [
     'js',
     'json',
-    'vue'
+    'vue',
+    'node'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
@@ -19,7 +20,6 @@ module.exports = {
   ],
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   setupFiles: ['<rootDir>/test/unit/setup'],
-  mapCoverage: true,
   coverageDirectory: '<rootDir>/test/unit/coverage',
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
