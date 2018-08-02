5e30dd46da64c7c813c71b5ed0f0839d3924fdac
David Hong <hsurf22@gmail.com>
2018-08-02 13:43:32 -0700

    TX page - information block update

--- src/translations/popover/gb.js
+++ src/translations/popover/gb.js
@@ -1,9 +1,11 @@
+
 const popoverTranslations = {
   whatIsMessageContent: 'So what is Message??? More info needed...',
   whatIsSignatureContent: 'So what is Signature??? More info needed...',
   whatIsSpeedOfTransactionContent: 'So.... What is Speed of Transaction?',
   WhatIsNonceContent: 'So... What is Nonce?',
   WhatIsDataContent: 'So what is data???',
-  WhatIsGasContent: 'So.... What is Gas Limit?'
+  WhatIsGasContent: 'So.... What is Gas Limit?',
+  Print: 'Print'
 }
 export default popoverTranslations
