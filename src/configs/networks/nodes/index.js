003c6732aa5f9f67dbee39676d488a71b1452278
gamalielhere <gamalielgad1234@gmail.com>
2018-08-07 13:55:02 -0700

    Add nodes from mew

--- src/configs/networks/nodes/index.js
+++ src/configs/networks/nodes/index.js
@@ -1,6 +1,44 @@
+import clo from './clo'
+import egem from './egem'
+import ella from './ella'
+import etcepool from './etc-epool'
+import etcethereumcommonwealth from './etc-ethereum-commonwealth'
+import ethethscan from './eth-ethscan'
+import ethgiveth from './eth-giveth'
+import ethinfura from './eth-infura'
 import ethmew from './eth-mew'
+import etsc from './etsc'
+import exp from './exp'
+import kovethscan from './kov-ethscan'
+import kovinfura from './kov-infura'
+import music from './music'
+import poa from './poa'
+import rinethscan from './rin-ethscan'
+import rininfura from './rin-infura'
+import ropinfura from './rop-infura'
 import ropmew from './rop-mew'
+import tomo from './tomo'
+import ubq from './ubq'
 export {
+  clo,
+  egem,
+  ella,
+  etcepool,
+  etcethereumcommonwealth,
+  ethethscan,
+  ethgiveth,
+  ethinfura,
   ethmew,
-  ropmew
+  etsc,
+  exp,
+  kovethscan,
+  kovinfura,
+  music,
+  poa,
+  rinethscan,
+  rininfura,
+  ropinfura,
+  ropmew,
+  tomo,
+  ubq
 }
