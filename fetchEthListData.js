169147eccbfc2f41c56b1fc4d9bcbc332ad7a68f
gamalielhere <gamalielgad1234@gmail.com>
2018-08-07 11:41:04 -0700

    Add network types, fetch tokens and contracts on build

--- fetchEthListData.js
+++ fetchEthListData.js
@@ -1,18 +1,31 @@
 const fetch = require('node-fetch')
 const fs = require('fs')
+<<<<<<< HEAD
 const tokenFolder = './src/tokens'
 const contractFolder = './src/contracts'
+=======
+const tokenFolder = './tokens'
+const contractFolder = './contracts'
+>>>>>>> Add network types, fetch tokens and contracts on build
 
 async function fetchTokens () {
   if(!fs.existsSync(tokenFolder)) {
     fs.mkdirSync(tokenFolder)
   }
 
+<<<<<<< HEAD
   const tokenList = await fetch('https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/dist/tokens').then(res => res.json()).catch(err => console.log(err))
   const tokenFileURL = 'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/tokens/'
   tokenList.forEach(async (tokenFile) => {
     let tokensCollection = await fetch(`${tokenFileURL + tokenFile.name}/tokens-${tokenFile.name}.json`).then(res => res.json()).catch(err => console.log(err))
     fs.writeFileSync(`${tokenFolder}/tokens-${tokenFile.name}.json`, JSON.stringify(tokensCollection))
+=======
+  const tokenList = await fetch('https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/tokens').then(res => res.json()).catch(err => console.log(err))
+  const tokenFileURL = 'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/tokens/'
+  tokenList.forEach(async (tokenFile) => {
+    let tokensCollection = await fetch(`${tokenFileURL + tokenFile.name}`).then(res => res.json()).catch(err => console.log(err))
+    fs.writeFileSync(`${tokenFolder}/${tokenFile.name}`, JSON.stringify(tokensCollection))
+>>>>>>> Add network types, fetch tokens and contracts on build
   })
 }
 
@@ -21,11 +34,19 @@ async function fetchContracts () {
     fs.mkdirSync(contractFolder)
   }
 
+<<<<<<< HEAD
   const contractList = await fetch('https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/dist/contracts').then(res => res.json()).catch(err => console.log(err))
   const contractFileURL = 'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/contracts/'
   contractList.forEach(async (contractFile) => {
     let contractsCollection = await fetch(`${contractFileURL + contractFile.name}/contract-abi-${contractFile.name}.json`).then(res => res.json()).catch(err => console.log(err))
     fs.writeFileSync(`${contractFolder}/contract-abi-${contractFile.name}.json`, JSON.stringify(contractsCollection))
+=======
+  const contractList = await fetch('https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/contracts').then(res => res.json()).catch(err => console.log(err))
+  const contractFileURL = 'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/contracts/'
+  contractList.forEach(async (contractFile) => {
+    let contractsCollection = await fetch(`${contractFileURL + contractFile.name}`).then(res => res.text()).catch(err => console.log(err))
+    fs.writeFileSync(`${contractFolder}/${contractFile.name}`, contractsCollection)
+>>>>>>> Add network types, fetch tokens and contracts on build
   })
 }
 
