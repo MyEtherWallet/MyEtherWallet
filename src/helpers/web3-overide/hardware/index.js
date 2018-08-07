bd8ff02ba4d2c85664acdb9959f87dc73199f378
steveMieskoski <stevemieskoski@gmail.com>
2018-08-06 21:45:21 -0700

    checkpoint

---
+++ src/helpers/web3-overide/hardware/index.js
@@ -0,0 +1,15 @@
+import LedgerWallet from './ledger'
+
+export {
+  LedgerWallet
+}
+
+// const LedgerWallet = require('./ledger')
+// const MewConnect = require('./mewConnect')
+// const TrezorWallet = require('./trezor')
+//
+// module.exports = {
+//   LedgerWallet,
+//   MewConnect,
+//   TrezorWallet
+// }
