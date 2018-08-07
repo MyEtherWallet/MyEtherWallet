e135da87f798f36cbb80552dcec7fcb1abc2fc48
gamalielhere <gamalielgad1234@gmail.com>
2018-08-07 13:55:02 -0700

    Add nodes from mew

---
+++ src/configs/networks/nodes/etc-epool.js
@@ -0,0 +1,12 @@
+import {
+  ETC
+} from '../types'
+export default {
+  type: ETC,
+  service: 'epool.io',
+  url: 'https://mew.epool.io',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+}
