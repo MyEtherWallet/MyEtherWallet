c847e60e845803ac5004d64e5640cb735b1cfd77
gamalielhere <gamalielgad1234@gmail.com>
2018-08-07 13:55:02 -0700

    Add nodes from mew

---
+++ src/configs/networks/nodes/ella.js
@@ -0,0 +1,12 @@
+import {
+  ELLA
+} from '../types'
+export default {
+  type: ELLA,
+  service: 'Ellaism',
+  url: 'https://jsonrpc.ellaism.org',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+}
