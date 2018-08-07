7813475bd66ef2227e0dd0fa8f6b6bc53a2c35c5
gamalielhere <gamalielgad1234@gmail.com>
2018-08-07 13:55:02 -0700

    Add nodes from mew

---
+++ src/configs/networks/nodes/etc-ethereum-commonwealth.js
@@ -0,0 +1,12 @@
+import {
+  ETC
+} from '../types'
+export default {
+  type: ETC,
+  service: 'Ethereum Commonwealth',
+  url: 'https://etc-geth.0xinfra.com',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+}
