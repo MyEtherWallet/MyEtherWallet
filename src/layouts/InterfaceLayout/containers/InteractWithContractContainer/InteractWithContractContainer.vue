9645df3e8b9b9d2fcf5cd27248dfc2a66416650a
gamalielhere <gamalielgad1234@gmail.com>
2018-08-14 15:29:44 -0700

    Implement interacting with contract

--- src/layouts/InterfaceLayout/containers/InteractWithContractContainer/InteractWithContractContainer.vue
+++ src/layouts/InterfaceLayout/containers/InteractWithContractContainer/InteractWithContractContainer.vue
@@ -61,28 +61,52 @@
           </div>
         </div>
         <div>
-          <div class="the-form domain-name" v-if="selectedMethod.constant === true">
+          <div class="the-form domain-name" v-if="selectedMethod.constant === true && selectedMethod.inputs.length === 0">
             <input type="text" name="" v-model="result" placeholder="0x00000000000000" disabled />
           </div>
           <div class="the-form domain-name" v-for="(input, idx) in selectedMethod.inputs" :key="input.name + idx" v-else v-show="selectedMethod.inputs.length !== 0">
             <div v-if="input.type === 'bool'" class="bool-input">
               <div class="title-container">
                 <div class="title">
-                  <h4>{{ input.name | capitalize }}</h4>
+                  <p>{{ input.name | capitalize }}</p>
                 </div>
               </div>
               <div class="bool-input-container">
                 <div>
-                  <input type="radio" v-model="writeInputs[input.name]" value="true" name="true"/>
-                  <label for="true">true</label>
+                  <input type="radio" v-model="writeInputs[input.name]" :value="true" :name="input.name" :checked="writeInputs[input.name] === true? true: false"/>
+                  <label :for="input.name">true</label>
                 </div>
                 <div>
-                  <input type="radio" v-model="writeInputs[input.name]" value="false" name="false"/>
-                  <label for="false">false</label>
+                  <input type="radio" v-model="writeInputs[input.name]" :value="false" :name="input.name" :checked="writeInputs[input.name] === false? true: false"/>
+                  <label :for="input.name">false</label>
                 </div>
               </div>
             </div>
             <input :type="checkType(input.type)" name="" v-model="writeInputs[input.name]" :placeholder="input.name" v-else class="contract-inputs"/>
+          </div>
+          <div class="the-form domain-name result-container" v-show="selectedMethod.constant === false">
+            <div class="title-container">
+              <div class="title">
+                <h4>Value: </h4>
+              </div>
+            </div>
+            <input type="text" name="" v-model="value" placeholder="ETH" />
+          </div>
+          <div class="the-form domain-name result-container" v-if="result !== ''">
+            <div class="title-container">
+              <div class="title">
+                <h4>Result: </h4>
+              </div>
+            </div>
+            <div class="result-inputs">
+              <input type="text" name="" v-model="result" placeholder="0x00000000000000" disabled v-if="resType === 'string'" />
+              <div v-if="resType === 'object'"> <!-- Have to separate them since v-for still loops when v-if is in the same line getting max stack -->
+                <div v-for="(res, idx) in Object.keys(result)" :key="selectedMethod.outputs[idx].name !== ''? selectedMethod.outputs[idx].name + idx : selectedMethod.outputs[idx].type + idx">
+                  <label :name="selectedMethod.outputs[idx].name !== ''? selectedMethod.outputs[idx].name: selectedMethod.outputs[idx].type + idx"> {{selectedMethod.outputs[idx].name !== ''? selectedMethod.outputs[idx].name: selectedMethod.outputs[idx].type}}</label>
+                  <input type="text" :name="selectedMethod.outputs[idx].name !== ''? selectedMethod.outputs[idx].name: selectedMethod.outputs[idx].type + idx"  :value="result[res]" placeholder="0x00000000000000" disabled/>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -91,28 +115,42 @@
           <div class="submit-button large-round-button-green-border clickable" @click="switchView('backwards')">
             {{ $t('common.back') }}
           </div>
-          <div class="submit-button large-round-button-green-filled clickable" @click="write" v-if="selectedMethod.constant === false">
-            Write
+          <div :class="[inputsFilled? '': 'disabled', loading ? 'disabled': '','submit-button large-round-button-green-filled clickable']" @click="write">
+            <span v-show="!loading">
+              {{ selectedMethod.constant === true && selectedMethod.inputs.length !== 0 ? 'Read': 'Write'}}
+            </span>
+            <i class="fa fa-spinner fa-spin fa-lg" v-show="loading"></i>
           </div>
         </div>
         <interface-bottom-text link="/" :linkText="$t('interface.learnMore')" :question="$t('interface.haveIssues')"></interface-bottom-text>
       </div>
     </div>
+    <confirm-modal :showSuccess="showSuccessModal" :signedTx="signedTx" :fee="transactionFee" :gasPrice="$store.state.gasPrice" :from="$store.state.wallet.getAddressString()" :to="address" :value="value" :gas="gasLimit" :data="data" :nonce="nonce"></confirm-modal>
+    <success-modal message="Sending Transaction" linkMessage="Close"></success-modal>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from 'vuex'
 import CurrencyPicker from '../../components/CurrencyPicker'
 import InterfaceContainerTitle from '../../components/InterfaceContainerTitle'
 import InterfaceBottomText from '@/components/InterfaceBottomText'
-import {Misc} from '@/helpers'
+import { Misc } from '@/helpers'
+import ConfirmModal from '@/components/ConfirmModal'
+import SuccessModal from '@/components/SuccessModal'
+
+// eslint-disable-next-line
+const EthTx = require('ethereumjs-tx')
+// eslint-disable-next-line
+const unit = require("ethjs-unit");
 
 export default {
   components: {
     'interface-container-title': InterfaceContainerTitle,
     'interface-bottom-text': InterfaceBottomText,
-    'currency-picker': CurrencyPicker
+    'currency-picker': CurrencyPicker,
+    'confirm-modal': ConfirmModal,
+    'success-modal': SuccessModal
   },
   data () {
     return {
@@ -124,7 +162,17 @@ export default {
       methods: [],
       selectedMethod: {},
       result: '',
-      writeInputs: {}
+      writeInputs: {},
+      inputsFilled: false,
+      loading: false,
+      resType: 'string',
+      gasLimit: 21000,
+      value: 0,
+      data: '',
+      raw: {},
+      nonce: 0,
+      signedTx: '',
+      transactionFee: 0
     }
   },
   methods: {
@@ -137,22 +185,29 @@ export default {
       this.address = currency.address
     },
     checkType (type) {
-      if (type === 'address' || 'string') {
+      if (type === 'address' || type === 'string' || type === 'byte') {
         return 'string'
       } else {
         return 'number'
       }
     },
     selectFunction (method) {
-      const contract = new this.$store.state.web3.eth.Contract([method], this.address)
-      if (method.constant === true) {
-        contract.methods[method.name]().call({from: this.$store.state.wallet.getAddressString()}).then(res => {
-          this.result = res
-        }).catch(err => {
-          console.log(err)
-        })
+      const contract = new this.$store.state.web3.eth.Contract(
+        [method],
+        this.address
+      )
+      if (method.constant === true && method.inputs.length === 0) {
+        contract.methods[method.name]()
+          .call({ from: this.$store.state.wallet.getAddressString() })
+          .then(res => {
+            this.result = res
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } else {
+        this.result = ''
       }
-
       this.selectedMethod = method
     },
     copyToClipboard (ref) {
@@ -165,26 +220,120 @@ export default {
     switchView (direction) {
       switch (direction) {
         case 'forward':
-          this.methods = JSON.parse(this.abi).map(func => func).filter(func => func.type !== 'constructor').filter(func => func.constant !== undefined)
+          this.methods = JSON.parse(this.abi)
+            .map(func => func)
+            .filter(func => func.type !== 'constructor')
+            .filter(func => func.constant !== undefined)
           this.interact = true
           break
         default:
           this.interact = false
       }
     },
-    write () {
-      console.log('Write!')
+    async write () {
+      const contract = new this.$store.state.web3.eth.Contract([this.selectedMethod], this.address)
+      const params = Object.keys(this.writeInputs).map(input => this.writeInputs[input])
+      this.loading = true
+      if (this.selectedMethod.constant === true) {
+        contract.methods[this.selectedMethod.name](...params)
+          .call({ from: this.$store.state.wallet.getAddressString() })
+          .then(res => {
+            this.result = res
+            this.loading = false
+          })
+          .catch(err => {
+            console.log(err)
+            this.loading = false
+          })
+      } else {
+        this.nonce = await this.$store.state.web3.eth.getTransactionCount(this.$store.state.wallet.getAddressString())
+        this.gasLimit = await contract.methods[this.selectedMethod.name](
+          ...params
+        )
+          .estimateGas({ from: this.$store.state.wallet.getAddressString() })
+          .then(res => {
+            this.transactionFee = unit.fromWei(unit.toWei(this.$store.state.gasPrice, 'gwei') * res, 'ether')
+            return res
+          })
+          .catch(err => console.log(err))
+        this.data = contract.methods[this.selectedMethod.name](...params).encodeABI()
+        this.raw = {
+          from: this.$store.state.wallet.getAddressString(),
+          gas: this.gasLimit,
+          nonce: this.nonce,
+          gasPrice: Number(unit.toWei(this.$store.state.gasPrice, 'gwei')),
+          value: this.value,
+          to: this.address,
+          data: this.data
+        }
+
+        const tx = new EthTx(this.raw)
+        tx.sign(this.$store.state.wallet.getPrivateKey())
+        const serializedTx = tx.serialize()
+        this.signedTx = `0x${serializedTx.toString('hex')}`
+        this.confirmationModalOpen()
+      }
+    },
+    confirmationModalOpen () {
+      this.loading = false
+      window.scrollTo(0, 0)
+      const confirmation = this.$children.filter(child => child.$refs.hasOwnProperty('confirmation'))
+      confirmation[0].$refs.confirmation.show()
+    },
+    showSuccessModal () {
+      const success = this.$children.filter(child => child.$refs.hasOwnProperty('success'))
+      success[0].$refs.success.show()
+    },
+    checkInputsFilled () {
+      const inputs = Object.keys(this.writeInputs)
+      for (var i = 0; i < inputs.length; i++) {
+        if (this.writeInputs[inputs[i]] === '') {
+          this.inputsFilled = false
+          return
+        } else {
+          this.inputsFilled = true
+        }
+      }
     }
   },
   watch: {
     abi (newVal) {
       this.validAbi = Misc.isJson(newVal)
     },
-    selectedMethod () {
+    selectedMethod (newVal) {
       this.writeInputs = {}
+      for (let inputs in newVal) {
+        if (newVal.hasOwnProperty(inputs)) {
+          newVal.inputs.forEach(input => {
+            this.$set(this.writeInputs, input.name, '')
+          })
+        }
+      }
     },
     address (newVal) {
       this.validAddress = this.$store.state.web3.utils.isAddress(newVal)
+    },
+    writeInputs: {
+      // Watches nested values instead
+      handler: function (val, oldVal) {
+        this.checkInputsFilled()
+      },
+      deep: true
+    },
+    result (newVal) {
+      this.resType = typeof newVal
+    },
+    network () {
+      this.abi = ''
+      this.address = ''
+      this.interact = false
+      this.validAbi = false
+      this.validAddress = false
+      this.methods = []
+      this.selectedMethod = {}
+      this.result = ''
+      this.writeInputs = {}
+      this.inputsFilled = false
     }
   },
   computed: {
@@ -196,5 +345,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import "InteractWithContractContainer.scss";
+@import "InteractWithContractContainer.scss";
 </style>
