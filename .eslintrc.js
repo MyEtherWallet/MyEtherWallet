328a2b6eca605c6a78fd60fd35638fb27dc88eeb
gamalielhere <gamalielgad1234@gmail.com>
2018-08-15 11:15:01 -0700

    Include recommended on eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,13 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
   plugins: [
+    'eslint:recommended',
     'vue'
   ],
   // add your custom rules here
