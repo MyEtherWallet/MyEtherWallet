daa14a24d2ef69f11f3c7b5f2aabf04fb33df853
David Hong <hsurf22@gmail.com>
2018-08-16 15:58:51 -0700

    Search block done

--- src/layouts/HelpCenterLayout/components/KeywordSearch/KeywordSearch.vue
+++ src/layouts/HelpCenterLayout/components/KeywordSearch/KeywordSearch.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="keyword-search">
     <div class="wrap">
-      Search
+      <div class="page-container">
+
+        <div class="block-title">
+          <h1>Got lost? Let me help you.</h1>
+        </div>
+
+        <div class="search-form mew-custom-form__search">
+          <div class="search-input-block">
+            <input type="text" name="" placeholder="Search the keyword here...">
+          </div>
+        </div>
+
+        <div class="searchable-contents">
+          <p>Top Searches: MEWconnect / Swap / Send ETH / Security/ Blockchain / Create a Wallet</p>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
@@ -16,5 +32,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import "KeywordSearch.scss";
+@import "KeywordSearch.scss";
 </style>
