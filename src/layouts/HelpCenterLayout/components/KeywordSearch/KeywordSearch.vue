9e6b819834053225d3b192f12dfc2ed0bae5a8d0
David Hong <hsurf22@gmail.com>
2018-08-16 12:55:44 -0700

    keyword search component added

---
+++ src/layouts/HelpCenterLayout/components/KeywordSearch/KeywordSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="keyword-search">
+    <div class="wrap">
+      Search
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "KeywordSearch.scss";
+</style>
