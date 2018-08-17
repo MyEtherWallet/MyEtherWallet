4280439e6a75cbe0f914c10ed37c5ca3df27dc12
David Hong <hsurf22@gmail.com>
2018-08-16 23:03:24 -0700

    FAQs Categories added

---
+++ src/layouts/HelpCenterLayout/components/FAQs/FAQs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="faqs">
+    <div class="wrap">
+     
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "FAQs.scss";
+</style>
