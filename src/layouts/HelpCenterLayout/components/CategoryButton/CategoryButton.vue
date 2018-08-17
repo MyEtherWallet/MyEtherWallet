5274c404816d72c4330534c60c8cac6a9896c86c
David Hong <hsurf22@gmail.com>
2018-08-17 15:20:53 -0700

    Support page done

---
+++ src/layouts/HelpCenterLayout/components/CategoryButton/CategoryButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="category-button">
+    <div class="wrap">
+      <div class="button-icon">
+        <img :src="content.icon">
+      </div>
+      <div class="button-title">
+        {{content.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['content'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "CategoryButton.scss";
+</style>
