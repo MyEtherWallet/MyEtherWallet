874621c7c054979992d2c423672092ba6ed4fe5b
David Hong <hsurf22@gmail.com>
2018-08-20 18:09:07 -0700

    Ether Unit Reference Guide added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HomeLayout from '@/layouts/HomeLayout';
 import CreateWalletLayout from '@/layouts/CreateWalletLayout';
+import ConvertUnits from '@/layouts/ConvertUnits';
 import TeamLayout from '@/layouts/TeamLayout';
 import PrivacyPolicyLayout from '@/layouts/PrivacyPolicyLayout';
 import TermsAndConditionsLayout from '@/layouts/TermsAndConditionsLayout';
@@ -56,6 +57,11 @@ export default new Router({
       path: '/help-center',
       name: 'HelpCenterLayout',
       component: HelpCenterLayout
+    },
+    {
+      path: '/convert-units',
+      name: 'ConvertUnits',
+      component: ConvertUnits
     },
     {
       path: '/getting-started',
