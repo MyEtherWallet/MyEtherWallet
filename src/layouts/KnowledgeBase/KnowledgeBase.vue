14d422ee950d97cdcd784be650d3d1b960c6ffe4
David Hong <hsurf22@gmail.com>
2018-08-23 14:24:36 -0700

    KnowledgeBase added

---
+++ src/layouts/KnowledgeBase/KnowledgeBase.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="knowledge-base">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "KnowledgeBase.scss";
+</style>
