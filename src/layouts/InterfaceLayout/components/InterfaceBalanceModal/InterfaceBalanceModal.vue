c01d31c471e1ec60251a7136017205dc7a092ca7
David Hong <hsurf22@gmail.com>
2018-08-28 12:48:47 -0700

    Network modal update

--- src/layouts/InterfaceLayout/components/InterfaceBalanceModal/InterfaceBalanceModal.vue
+++ src/layouts/InterfaceLayout/components/InterfaceBalanceModal/InterfaceBalanceModal.vue
@@ -9,22 +9,29 @@
           </div>
         </div>
       </div>
+
       <div class="content-block">
-        <h4>Equivalent Values</h4>
-        <div class="grid-3">
-          <p>BTC: 122.00</p>
-          <p>CHF: 5234.24</p>
-          <p>REP: 42122.00</p>
-          <p>USD: $234212.32</p>
-          <p>EUR: €133422.32</p>
-          <p>GBP: £634212.32</p>
-        </div>
+        <h4 class="equivalent-values-title">Equivalent Values</h4>
+        <ul class="equivalent-values">
+          <li v-for="ev in equivalentValues" v-bind:key="ev.key">
+            <img :src="ev.image">
+            <p>{{ev.name}}</p>
+            <p class="ev-value">{{ev.value}}</p>
+          </li>
+        </ul>
       </div>
     </b-modal>
   </div>
 </template>
 
 <script>
+import iconBtc from '@/assets/images/currency/btc.svg'
+import iconRep from '@/assets/images/currency/rep.svg'
+import iconEur from '@/assets/images/currency/eur.svg'
+import iconChf from '@/assets/images/currency/chf.svg'
+import iconGbp from '@/assets/images/currency/gbp.svg'
+import iconUsd from '@/assets/images/currency/usd.svg'
+
 export default {
   props: {
     balance: {
@@ -32,8 +39,41 @@ export default {
       default: '0'
     }
   },
-  data() {
-    return {};
+  data () {
+    return {
+      equivalentValues: [
+        {
+          image: iconBtc,
+          name: 'BTC',
+          value: '102.22453'
+        },
+        {
+          image: iconRep,
+          name: 'REP',
+          value: '5656.22'
+        },
+        {
+          image: iconChf,
+          name: 'CHF',
+          value: '12410004.22453'
+        },
+        {
+          image: iconUsd,
+          name: 'USD',
+          value: '312004.53'
+        },
+        {
+          image: iconEur,
+          name: 'EUR',
+          value: '12410.22'
+        },
+        {
+          image: iconGbp,
+          name: 'GBP',
+          value: '687867.53'
+        }
+      ]
+    }
   }
 };
 </script>
