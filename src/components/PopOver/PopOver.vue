0d87d6cd5b7e9a8d6fcdea20d1736f219372c00e
David Hong <hsurf22@gmail.com>
2018-08-30 13:57:50 -0700

    Swap page design update

--- src/components/PopOver/PopOver.vue
+++ src/components/PopOver/PopOver.vue
@@ -11,8 +11,8 @@
           src="@/assets/images/icons/explanation2.svg">
       </b-btn>
       <b-btn v-if="popovertype === 'A'" :id="popOverId" class="popover-button">
-        <img class="nohover-icon" src="@/assets/images/icons/question.png">
-        <img class="hover-icon" src="@/assets/images/icons/question.png">
+        <img class="nohover-icon" src="@/assets/images/icons/question-mark.svg">
+        <img class="hover-icon" src="@/assets/images/icons/question-mark.svg">
       </b-btn>
       <b-popover
         :target="popOverId"
