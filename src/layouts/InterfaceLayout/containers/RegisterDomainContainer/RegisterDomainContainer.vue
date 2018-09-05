a4307deb816c26a72b88daeac211bb6815c9c552
gamalielhere <gamalielgad1234@gmail.com>
2018-09-05 16:29:12 -0700

    Initial setup for UI switching

--- src/layouts/InterfaceLayout/containers/RegisterDomainContainer/RegisterDomainContainer.vue
+++ src/layouts/InterfaceLayout/containers/RegisterDomainContainer/RegisterDomainContainer.vue
@@ -1,153 +1,161 @@
 <template>
   <div class="register-domain-container">
-    <ens-name-modal :ens-name="domainName"/>
     <back-button :reset-view="resetView"/>
-
-    <div class="send-form">
-      <div class="title-container">
-        <div class="title">
-          <h4>{{ $t('interface.registerDns') }}</h4>
-          <p>{{ $t('interface.registerDnsDesc') }}</p>
-        </div>
-      </div>
-      <div class="the-form domain-name">
-        <input
-          v-model="domainName"
-          :class="[domainName.length <= 7 && domainName !== '' ? 'errored' : '']"
-          type="text"
-          name=""
-          placeholder="Please Enter at Least 7 Characters" >
-        <span>.eth</span>
-      </div>
-      <p
-        v-show="domainName.length <= 7 && domainName !== ''"
-        class="erroredMsg"> Domain name is less than 7 characters. </p>
-    </div>
-
-    <div class="submit-button-container">
-      <div
-        :class="[domainName.length <= 7 ? 'disabled' : '','submit-button large-round-button-green-filled clickable']"
-        @click="checkDomain">
-        <span v-show="!loading"> {{ $t('interface.checkDomain') }} </span>
-        <i
-          v-show="loading"
-          class="fa fa-spinner fa-spin"/>
-      </div>
-
-    </div>
-    <div class="flex-container">
-      <div class="title-container">
-        <h4 class="modal-title">{{ $t('interface.subDomain') }}</h4>
-        <div class="margin-left-auto add-custom-network">
-          <div class="sliding-switch-white">
-            <label class="switch">
-              <input
-                type="checkbox"
-                @click="expendDomainCheckForm" >
-              <span class="slider round"/>
-            </label>
+    <ens-bid
+      v-show="uiState === 'nameAvailableAuctionNotStarted'" 
+      :cancel="cancel"
+      :bid-amount="bidAmount"
+      :bid-mask="bidMask"
+      :secret-phrase="secretPhrase"
+      :create-bid="createBid"
+      :ens-name="domainName"/>
+    <div v-show="uiState === 'initial'">
+      <div class="send-form">
+        <div class="title-container">
+          <div class="title">
+            <h4>{{ $t('interface.registerDns') }}</h4>
+            <p>{{ $t('interface.registerDnsDesc') }}</p>
           </div>
         </div>
-      </div>
-      <div
-        ref="checkForm"
-        class="domain-check-form hidden">
-        <div class="domain-checker">
+        <div class="the-form domain-name">
           <input
-            type="number"
+            v-model="domainName"
+            :class="[domainName.length <= 7 && domainName !== '' ? 'errored' : '']"
+            type="text"
             name=""
-            value=""
-            placeholder="Enter Domain Name" >
-          <div
-            class="check-button"
-            @click="domainAvailabilityCheck">
-            {{ $t('common.check') }}
+            placeholder="Please Enter at Least 7 Characters" >
+          <span>.eth</span>
+        </div>
+        <p
+          v-show="domainName.length <= 7 && domainName !== ''"
+          class="erroredMsg"> Domain name is less than 7 characters. </p>
+      </div>
+
+      <div class="submit-button-container">
+        <div
+          :class="[domainName.length <= 7 ? 'disabled' : '','submit-button large-round-button-green-filled clickable']"
+          @click="checkDomain">
+          <span v-show="!loading"> {{ $t('interface.checkDomain') }} </span>
+          <i
+            v-show="loading"
+            class="fa fa-spinner fa-spin"/>
+        </div>
+
+      </div>
+      <div class="flex-container">
+        <div class="title-container">
+          <h4 class="modal-title">{{ $t('interface.subDomain') }}</h4>
+          <div class="margin-left-auto add-custom-network">
+            <div class="sliding-switch-white">
+              <label class="switch">
+                <input
+                  type="checkbox"
+                  @click="expendDomainCheckForm" >
+                <span class="slider round"/>
+              </label>
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        ref="domainList"
-        class="sub-domain-list hidden">
-        <h4 class="title">{{ $t('interface.allSubDomains') }}</h4>
-        <ul>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
-              </div>
+        <div
+          ref="checkForm"
+          class="domain-check-form hidden">
+          <div class="domain-checker">
+            <input
+              type="number"
+              name=""
+              value=""
+              placeholder="Enter Domain Name" >
+            <div
+              class="check-button"
+              @click="domainAvailabilityCheck">
+              {{ $t('common.check') }}
             </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border very-small-circle-button-green-filled"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+          </div>
+        </div>
+        <div
+          ref="domainList"
+          class="sub-domain-list hidden">
+          <h4 class="title">{{ $t('interface.allSubDomains') }}</h4>
+          <ul>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border very-small-circle-button-green-filled"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <p>myetherwallet2018.etherbase.eth</p>
-            <div class="buy-button-container">
-              <p>0 ETH</p>
-              <div
-                class="buy-button very-small-circle-button-green-border"
-                @click="domainBuyButtonClick($event)">
-                {{ $t('common.buy') }}
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+            <li>
+              <p>myetherwallet2018.etherbase.eth</p>
+              <div class="buy-button-container">
+                <p>0 ETH</p>
+                <div
+                  class="buy-button very-small-circle-button-green-border"
+                  @click="domainBuyButtonClick($event)">
+                  {{ $t('common.buy') }}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
     <interface-bottom-text
@@ -161,7 +169,7 @@
 <script>
 import InterfaceBottomText from '@/components/InterfaceBottomText';
 import BackButton from '../../components/BackButton';
-import EnsNameModal from '../../components/EnsNameModal';
+import EnsBid from '../../components/EnsBid';
 import EnsAbi from '@/helpers/ensAbi';
 import RegistrarAbi from '@/helpers/registrarAbi';
 import Misc from '@/helpers/misc';
@@ -170,7 +178,7 @@ export default {
   components: {
     'interface-bottom-text': InterfaceBottomText,
     'back-button': BackButton,
-    'ens-name-modal': EnsNameModal
+    'ens-bid': EnsBid
   },
   props: {
     resetView: {
@@ -181,7 +189,11 @@ export default {
   data() {
     return {
       domainName: '',
-      loading: false
+      loading: false,
+      uiState: 'initial',
+      bidAmount: 0,
+      bidMask: 0,
+      secretPhrase: ''
     };
   },
   methods: {
@@ -223,7 +235,7 @@ export default {
     processResult(res) {
       switch (res) {
         case '0':
-          this.$children[0].$refs.registerEns.show();
+          this.uiState = 'nameAvailableAuctionNotStarted';
           break;
         case '1':
           console.log('Name is available and the auction has been started');
@@ -243,6 +255,18 @@ export default {
           );
           break;
       }
+    },
+    cancel() {
+      this.uiState = 'initial';
+      this.clearInputs();
+    },
+    createBid() {
+      console.log('Lmao');
+    },
+    clearInputs() {
+      this.bidAmount = 0;
+      this.bidMask = 0;
+      this.secretPhrase = '';
     },
     expendDomainCheckForm() {
       this.$refs['checkForm'].classList.toggle('hidden');
