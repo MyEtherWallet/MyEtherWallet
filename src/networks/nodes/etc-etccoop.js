4564ddab1f39090b59012b036fc9cfa3b14d9f2f
gamalielhere <gamalielgad1234@gmail.com>
2018-09-11 11:20:10 -0700

    Add more nodes

---
+++ src/networks/nodes/etc-etccoop.js
@@ -0,0 +1,10 @@
+import { ETC } from '../types';
+export default {
+  type: ETC,
+  service: 'ETC Cooperative',
+  url: 'https://ethereumclassic.network',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
