a304aae52e248efaceedfa8cee71f8244b02236b
gamalielhere <gamalielgad1234@gmail.com>
2018-09-11 11:20:10 -0700

    Add more nodes

---
+++ src/networks/nodes/etc-ethereumcommonwealth-geth.js
@@ -0,0 +1,10 @@
+import { ETC } from '../types';
+export default {
+  type: ETC,
+  service: 'Ethereum Commonwealth Geth',
+  url: 'https://etc-geth.0xinfra.com/',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
