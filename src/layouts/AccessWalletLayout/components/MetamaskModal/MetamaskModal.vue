a51f0135598cc0b39aef57163655b4e983a82059
gamalielhere <gamalielgad1234@gmail.com>
2018-09-11 15:56:50 -0700

    Initial setup

--- src/layouts/AccessWalletLayout/components/MetamaskModal/MetamaskModal.vue
+++ src/layouts/AccessWalletLayout/components/MetamaskModal/MetamaskModal.vue
@@ -5,43 +5,77 @@
     hide-footer
     class="bootstrap-modal modal-metamask"
     centered>
-    <div class="modal-multi-icons">
-      <img
-        class="icon"
-        src="~@/assets/images/icons/button-metamask-fox.svg">
-      <img
-        class="icon"
-        src="~@/assets/images/icons/clip.svg">
-      <img
-        class="icon logo-small"
-        src="~@/assets/images/logo-small.png">
-    </div>
-    <div class="d-block content-container text-center">
-      <h4>
-        {{ $t("accessWallet.metaMaskModalDesc") }}
-      </h4>
-    </div>
-    <div class="accept-terms">
-      <label class="checkbox-container">{{ $t("accessWallet.acceptTerms") }} <a href="/">{{ $t("common.terms") }}</a>.
-        <input
-          type="checkbox"
-          @click="accessMyWalletBtnDisabled = !accessMyWalletBtnDisabled" >
-        <span class="checkmark"/>
-      </label>
-    </div>
-    <div class="button-container">
-      <router-link to="interface">
+    <div v-if="metamaskInstalled">
+      <div class="modal-multi-icons">
+        <img
+          class="icon"
+          src="~@/assets/images/icons/button-metamask-fox.svg">
+        <img
+          class="icon"
+          src="~@/assets/images/icons/clip.svg">
+        <img
+          class="icon logo-small"
+          src="~@/assets/images/logo-small.png">
+      </div>
+      <div class="d-block content-container text-center">
+        <h4>
+          {{ $t("accessWallet.metaMaskModalDesc") }}
+        </h4>
+      </div>
+      <div class="accept-terms">
+        <label class="checkbox-container">{{ $t("accessWallet.acceptTerms") }} <a href="/">{{ $t("common.terms") }}</a>.
+          <input
+            type="checkbox"
+            @click="accessMyWalletBtnDisabled = !accessMyWalletBtnDisabled" >
+          <span class="checkmark"/>
+        </label>
+      </div>
+      <div class="button-container">
         <b-btn
           :disabled="accessMyWalletBtnDisabled"
-          class="mid-round-button-green-filled close-button">
+          class="mid-round-button-green-filled close-button"
+          @click="getMetamaskWallet">
           {{ $t("accessWallet.accessMyWallet") }}
         </b-btn>
-      </router-link>
-      <b-btn
-        :disabled="accessMyWalletBtnDisabled"
-        class="mid-round-button-green-filled close-button">
-        {{ $t("accessWallet.accessMyWallet") }}
-      </b-btn>
+      </div>
+    </div>
+    <div v-else>
+      <div class="modal-multi-icons">
+        <img
+          class="icon"
+          src="~@/assets/images/icons/button-metamask-fox.svg">
+      </div>
+      <div class="d-block content-container text-center">
+        <h4 v-if="unlockMetamask">
+          {{ $t("accessWallet.installMetaMaskModalDesc") }}
+        </h4>
+        <h4 v-else>
+          {{ $t("accessWallet.unlockMetamaskWallet") }}
+        </h4>
+      </div>
+      <div class="accept-terms hidden">
+        <label class="checkbox-container">{{ $t("accessWallet.acceptTerms") }} <a href="/">{{ $t("common.terms") }}</a>.
+          <input type="checkbox" >
+          <span class="checkmark"/>
+        </label>
+      </div>
+      <div class="button-container">
+        <a
+          v-if="unlockMetamask"
+          href="https://metamask.io/"
+          target="_blank"
+          class="mid-round-button-green-filled close-button">
+          {{ $t("accessWallet.installMetamask") }}
+        </a>
+        <b-btn
+          v-else
+          class="mid-round-button-green-filled close-button"
+          @click="getMetamaskWallet"
+        >
+
+          {{ $t("accessWallet.tryAgain") }}
+        </b-btn>
+      </div>
     </div>
     <customer-support/>
   </b-modal>
@@ -49,6 +83,7 @@
 
 <script>
 import CustomerSupport from '@/components/CustomerSupport';
+import EthereumWallet from 'ethereumjs-wallet';
 
 export default {
   components: {
@@ -62,8 +97,40 @@ export default {
   },
   data() {
     return {
-      accessMyWalletBtnDisabled: true
+      accessMyWalletBtnDisabled: true,
+      unlockMetamask: true
     };
+  },
+  computed: {
+    metamaskInstalled() {
+      if (window.web3 !== undefined) return true;
+      return false;
+    }
+  },
+  methods: {
+    getMetamaskWallet() {
+      // if (window.web3 === undefined) {
+      //   window.addEventListener('message', ({ data }) => {
+      //     if (data && data.type && data.type === 'ETHEREUM_PROVIDER_SUCCESS') {
+      //       window.web3 = new Web3(ethereum);
+      //     }
+      //   });
+      //   window.postMessage(
+      //     { type: 'ETHEREUM_PROVIDER_REQUEST', web3: true },
+      //     '*'
+      //   );
+      // }
+
+      window.web3.eth.getAccounts((err, accounts) => {
+        if (err) this.metamaskInstalled = false;
+        if (!accounts.length) this.unlockMetamask = true;
+        const address = accounts[0];
+        // const addrBuffer = Buffer.from(address.slice(2), 'hex');
+        const wallet = new EthereumWallet(address);
+        // eslint-disable-next-line no-console
+        console.log(wallet);
+      });
+    }
   }
 };
 </script>
