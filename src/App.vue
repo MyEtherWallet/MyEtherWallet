4069a0d198c9a1e004f504c0d1a72f96a2ddd93e
David Hong <hsurf22@gmail.com>
2018-09-17 14:06:39 -0700

    Create wallet update

--- src/App.vue
+++ src/App.vue
@@ -80,8 +80,12 @@ export default {
 
 <style lang="scss">
 @import 'App.scss';
+
 @import '~@/scss/Global-desktop';
 @import '~@/scss/Global-tablet';
 @import '~@/scss/Global-mobile';
-@import '~@/scss/CustomForms';
+
+@import '~@/scss/CustomForms-desktop';
+@import '~@/scss/CustomForms-tablet';
+@import '~@/scss/CustomForms-mobile';
 </style>
