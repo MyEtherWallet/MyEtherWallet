d4b30b4d272477b5a78275da73021c3653ede9a4
David Hong <hsurf22@gmail.com>
2018-09-19 05:15:24 +0000

    Access My Wallet mobile update

--- src/layouts/AccessWalletLayout/containers/AccessMyWalletContainer/AccessMyWalletContainer.vue
+++ src/layouts/AccessWalletLayout/containers/AccessMyWalletContainer/AccessMyWalletContainer.vue
@@ -228,5 +228,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import 'AccessMyWalletContainer.scss';
+@import 'AccessMyWalletContainer-desktop.scss';
+@import 'AccessMyWalletContainer-tablet.scss';
+@import 'AccessMyWalletContainer-mobile.scss';
 </style>
