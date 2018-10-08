6d0a939dd4e94f430cb88b9814fa7f95ac8c7fd1
SteveM <SteveM@myEtherWallet.com>
2018-10-08 02:42:14 -0700

    fix conflicts and account for changes to src files

--- tests/unit/helpers/setupTooling.js
+++ tests/unit/helpers/setupTooling.js
@@ -1,13 +1,16 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 import VueI18n from 'vue-i18n';
-import BootstrapVue from "bootstrap-vue";
+import BootstrapVue from 'bootstrap-vue';
 import languages from '@/translations';
-import VueX from 'vuex'
-function createLocalVueInstance(){
+import VueX from 'vuex';
+import VueRouter from 'vue-router';
+
+function createLocalVueInstance() {
   const localVue = createLocalVue();
   localVue.use(VueI18n);
   localVue.use(BootstrapVue);
-  localVue.use(VueX)
+  localVue.use(VueX);
+  localVue.use(VueRouter);
   const i18n = new VueI18n({
     locale: 'en_US',
     fallbackLocale: 'en_US',
@@ -15,12 +18,14 @@ function createLocalVueInstance(){
     silentTranslationWarn: true
   });
 
-  const store = new VueX.Store()
+  const router = new VueRouter();
+  const store = new VueX.Store();
   return {
     localVue,
     i18n,
-    store
-  }
+    store,
+    router
+  };
 }
 
 // likely will remove this function
@@ -34,4 +39,4 @@ function createLocalVueInstance(){
 
 export default {
   createLocalVueInstance
-}
+};
