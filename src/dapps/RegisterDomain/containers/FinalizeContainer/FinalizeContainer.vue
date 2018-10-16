761e86bde59aad4858ee2dcf15d369ea63f9c161
gamalielhere <gamalielgad1234@gmail.com>
2018-10-15 17:21:45 -0700

    Add finalize auction

---
+++ src/dapps/RegisterDomain/containers/FinalizeContainer/FinalizeContainer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="finalize-container">
+      <h3>
+        Finalize auction for: {{ domainName }}.eth
+      </h3>
+      <button class="finalize-button" @click="finalize"> Finalize </button>
+    </div>
+    <interface-bottom-text
+      :link-text="$t('interface.learnMore')"
+      :question="$t('interface.haveIssues')"
+      link="/"/>
+  </div>
+</template>
+<script>
+import InterfaceBottomText from '@/components/InterfaceBottomText';
+export default {
+  components: {
+    'interface-bottom-text': InterfaceBottomText
+  },
+  props: {
+    domainName: {
+      type: String,
+      default: ''
+    },
+    finalize: {
+      type: Function,
+      default: function() {}
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import 'FinalizeContainer.scss';
+</style>
