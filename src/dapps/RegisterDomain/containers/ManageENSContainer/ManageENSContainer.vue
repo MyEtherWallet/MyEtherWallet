60a1f692546397133d83f545635fd0a6723f154a
gamalielhere <gamalielgad1234@gmail.com>
2018-10-22 14:41:58 -0700

    Add styles on form

--- src/dapps/RegisterDomain/containers/ManageENSContainer/ManageENSContainer.vue
+++ src/dapps/RegisterDomain/containers/ManageENSContainer/ManageENSContainer.vue
@@ -1,46 +1,60 @@
 <template>
   <div class="manage-ens-container">
     <h3>Manage {{ domainName }}.eth</h3>
-    <div class="form-container">
-      <form class="manage-form">
-        <label for="updateResolver">
-          Update Resolver Address:
-        </label>
-        <input 
-          v-model="resolverAddress" 
-          type="text" 
-          name="updateResolver" 
-          placeholder="0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D">
-        <div class="submit-container">
-          <button 
-            :class="!$store.state.web3.utils.isAddress(resolverAddress) ? 'disabled' : ''" 
-            type="submit" 
-            @click.prevent="updateResolver(resolverAddress)">Update</button>
-        </div>
-      </form>
+    <div class="inputs-container">
+      <div class="form-container">
+        <form class="manage-form">
+          <div class="input-container">
+            <label for="updateResolver">
+              Update Resolver Address:
+            </label>
+            <input
+              v-model="resolverAddress"
+              type="text"
+              name="updateResolver"
+              placeholder="0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D">
+          </div>
+          <div class="submit-container">
+            <button
+              :class="!$store.state.web3.utils.isAddress(resolverAddress) ? 'disabled' : ''"
+              type="submit"
+              @click.prevent="updateResolver(resolverAddress)">Update</button>
+          </div>
+        </form>
+      </div>
+      <div class="form-container">
+        <form class="manage-form">
+          <div class="input-container">
+            <label for="transferEns">
+              Transfer ENS domain to:
+            </label>
+            <input
+              v-model="transferTo"
+              type="text"
+              name="transferEns"
+              placeholder="0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D">
+          </div>
+          <div class="submit-container">
+            <button
+              :class="!$store.state.web3.utils.isAddress(transferTo) ? 'disabled': ''"
+              type="submit"
+              @click.prevent="">Transfer</button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div class="form-container">
-      <form class="manage-form">
-        <label for="transferEns">
-          Transfer ENS to:
-        </label>
-        <input 
-          v-model="transferTo" 
-          type="text" 
-          name="transferEns" 
-          placeholder="0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D">
-        <div class="submit-container">
-          <button 
-            :class="!$store.state.web3.utils.isAddress(transferTo) ? 'disabled': ''" 
-            type="submit" 
-            @click.prevent="">Transfer</button>
-        </div>
-      </form>
-    </div>
+    <interface-bottom-text
+      :link-text="$t('interface.learnMore')"
+      :question="$t('interface.haveIssues')"
+      link="/"/>
   </div>
 </template>
 <script>
+import InterfaceBottomText from '@/components/InterfaceBottomText';
 export default {
+  components: {
+    'interface-bottom-text': InterfaceBottomText
+  },
   props: {
     domainName: {
       type: String,
