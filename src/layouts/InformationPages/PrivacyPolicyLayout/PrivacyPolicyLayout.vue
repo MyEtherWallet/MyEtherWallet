f38a625b999b134fe1d2f1313680e795aee997a1
David Hong <hsurf22@gmail.com>
2018-10-23 18:04:24 -0700

    Information pages added

---
+++ src/layouts/InformationPages/PrivacyPolicyLayout/PrivacyPolicyLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="privacy-policy">
+    <div class="wrap">
+      <title-text-contents :options="titleAndTextContents" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleTextContentsLayout from '../Components/TitleTextContentsLayout';
+
+export default {
+  components: {
+    'title-text-contents': TitleTextContentsLayout
+  },
+  props: {
+    options: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      titleAndTextContents: {
+        title: 'Privacy Policy',
+        boldSubTitle: 'Last updated May 10, 2018',
+        textContent: [
+           'The measured steps are meant to avoid an escalation of operations by Russia to more serious computer-based attacks on US information systems and infrastructure.',
+           'The US Cyber Command (USCYBERCOM) is engaging in a campaign to deter further disinformation operations by Russian operatives—individuals like those employed through Russian companies as part of the "Project Lakhta" program described in last week\'s Justice Department indictment of Elena Alekseevna Khusyaynova—by letting them know that they are being watched.',
+           'The Cyber Command operation, described by unnamed senior military officials, is limited in scope and does not involve directly threatening Russian operatives.',
+           'According to a report from the New York Times\' Julian E. Barnes, USCYBERCOM has directed operations to identify, track, and directly message individuals involved in disinformation campaigns associated with the upcoming midterm elections.'
+        ]
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'PrivacyPolicyLayout.scss';
+</style>
