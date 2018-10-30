27c67cbfb46f8bd68547da8a4d5757ab13e687e9
gamalielhere <gamalielgad1234@gmail.com>
2018-10-30 12:53:01 -0700

    Remove setweb3wallet action and mutation, update decrypt wallet

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -44,9 +44,6 @@ const SET_GAS_PRICE = function(state, val) {
   store.set('gasPrice', val);
 };
 
-const SET_WEB3_PROVIDER_WALLET = function(state, wallet) {
-  state.wallet = wallet;
-};
 const SET_WEB3_INSTANCE = function(state, web3) {
   state.web3 = web3;
 };
@@ -71,7 +68,6 @@ export default {
   SET_ACCOUNT_BALANCE,
   SET_GAS_PRICE,
   SET_ENS,
-  SET_WEB3_PROVIDER_WALLET,
   SET_WEB3_INSTANCE,
   SWITCH_NETWORK,
   UPDATE_NOTIFICATION
