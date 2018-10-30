f1d344e515cf4de1504cad066c18f5456cb9d0ed
David Hong <hsurf22@gmail.com>
2018-10-30 14:20:35 -0700

    Social update

---
+++ src/components/FeedbackModal/FeedbackModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="modal-container">
+    <b-modal
+      ref="feedback"
+      hide-footer
+      centered
+      class="bootstrap-modal"
+      title="Feedback / Custommer Support">
+      <div class="modal-content">
+        If you want to send us your feedback, please use
+        <a href="mailto:support@myetherwallet.com">support@myetherwallet.com</a>.
+        Thank you.
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'FeedbackModal.scss';
+</style>
