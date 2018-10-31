39250c6453cafc619b993de08823168aae2c7a45
David Hong <hsurf22@gmail.com>
2018-10-30 17:35:33 -0700

    Setting modal added

---
+++ src/components/SettingModal/SettingModal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal-container">
+    <b-modal
+      ref="settings"
+      title="Settings"
+      hide-footer
+      centered
+      class="bootstrap-modal">
+      
+
+    </b-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'SettingModal.scss';
+</style>
