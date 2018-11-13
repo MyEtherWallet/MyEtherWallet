796732d9a68ea568c9329ec86908bd9656d5f4dd
David Hong <hsurf22@gmail.com>
2018-11-13 12:30:27 -0800

    Logout modal added

---
+++ src/components/LogoutModal/LogoutModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="modal-container">
+
+    <b-modal
+      ref="logout"
+      title="Settings"
+      hide-footer
+      centered
+      class="bootstrap-modal nopadding">
+      <div class="modal-contents">
+        Logout
+      </div>
+    </b-modal>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lougout',
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'LogoutModal.scss';
+</style>
