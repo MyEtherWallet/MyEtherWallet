539d0c5e57e3f7f4484fc2a68442f54337fbcdee
Kosala Hemachandra <kvhnuke@aol.com>
2018-11-27 19:01:23 -0800

    fix history mode craziness :p

--- vue.config.js
+++ vue.config.js
@@ -155,7 +155,7 @@ if (process.env.NODE_ENV === 'production') {
   );
 }
 module.exports = {
-  baseUrl: '/',
+  baseUrl: process.env.ROUTER_MODE === 'history' ? '/' : './',
   configureWebpack: webpackConfig,
   chainWebpack: config => {}
 };
