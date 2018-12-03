4daf4d38a1c1ec92010abb4fcfea07e76209ae4e
David Hong <hsurf22@gmail.com>
2018-12-03 11:16:48 -0800

    Mobile interface side menu button add

--- src/layouts/InterfaceLayout/components/InterfaceContainerTitle/InterfaceContainerTitle.vue
+++ src/layouts/InterfaceLayout/components/InterfaceContainerTitle/InterfaceContainerTitle.vue
@@ -1,11 +1,17 @@
 <template lang="html">
   <div class="content-title">
     <h2>{{ title }}</h2>
+    <div class="side-menu-button"><round-button title="Change" /></div>
   </div>
 </template>
 
 <script>
+import RoundButton from '@/components/Buttons/RoundButton';
+
 export default {
+  components: {
+    'round-button': RoundButton
+  },
   props: {
     title: {
       type: String,
