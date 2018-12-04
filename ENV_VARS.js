baf3225589dc6f51b479df06c68febeb0d8137e5
Kosala Hemachandra <kvhnuke@aol.com>
2018-12-03 18:34:37 -0800

    fix router issue

--- ENV_VARS.js
+++ ENV_VARS.js
@@ -1,4 +1,7 @@
+const ROUTER_MODE = process.env.ROUTER_MODE || 'hash';
+const ROOT_URL = process.env.ROUTER_MODE == 'hash' ? '#' : '';
 module.exports = {
-  ROUTER_MODE: JSON.stringify(process.env.ROUTER_MODE || 'hash'),
+  ROUTER_MODE: JSON.stringify(ROUTER_MODE),
+  ROOT_URL: JSON.stringify(ROOT_URL),
   BUILD_TYPE: JSON.stringify(process.env.BUILD_TYPE || 'web')
 };
