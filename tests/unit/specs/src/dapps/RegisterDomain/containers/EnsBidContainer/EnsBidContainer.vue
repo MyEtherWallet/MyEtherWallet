1ad151d32a0c491df4e93503c95fa43659a5714e
gamalielhere <gamalielgad1234@gmail.com>
2018-12-11 12:37:06 -0800

    Linter changes

--- tests/unit/specs/src/dapps/RegisterDomain/containers/EnsBidContainer/EnsBidContainer.vue
+++ tests/unit/specs/src/dapps/RegisterDomain/containers/EnsBidContainer/EnsBidContainer.vue
@@ -1,29 +1,21 @@
 <template lang="html">
   <div>
-    <json-string-modal 
-      ref="jsonStringModal" 
-      :update-json-string="updateJson"/>
+    <json-string-modal ref="jsonStringModal" :update-json-string="updateJson" />
     <div class="name-available-container">
-      <div
-        v-if="$route.fullPath.includes('auction')"
-        class="content-header">
+      <div v-if="$route.fullPath.includes('auction')" class="content-header">
         <div>
-          <h3> {{ domainName }}.eth </h3>
+          <h3>{{ domainName }}.eth</h3>
           <p>Cheers! This Domain is available.</p>
         </div>
       </div>
-      <div
-        v-if="$route.fullPath.includes('bid')"
-        class="auction-started">
+      <div v-if="$route.fullPath.includes('bid')" class="auction-started">
         <div>
-          <h3> An auction has been started for {{ domainName }}.eth </h3>
+          <h3>An auction has been started for {{ domainName }}.eth</h3>
         </div>
       </div>
-      <div
-        v-if="$route.fullPath.includes('reveal')"
-        class="auction-started">
+      <div v-if="$route.fullPath.includes('reveal')" class="auction-started">
         <h3>
-          Reveal your bid for {{ domainName }}.eth now. <br>
+          Reveal your bid for {{ domainName }}.eth now. <br />
           {{ highestBidder }} ETH (Current highest bid)
         </h3>
       </div>
@@ -31,90 +23,87 @@
         <timer
           v-if="$route.fullPath.includes('bid')"
           :date-number="auctionDateEnd"
-          date-type="reveal" />
+          date-type="reveal"
+        />
         <timer
-          v-if="$route.fullPath.includes('bid') || $route.fullPath.includes('reveal')"
+          v-if="
+            $route.fullPath.includes('bid') ||
+              $route.fullPath.includes('reveal')
+          "
           :date-number="auctionDateEnd"
-          :style="{width: $route.fullPath.includes('reveal') ? '100%': ''}"
-          date-type="auction" />
+          :style="{ width: $route.fullPath.includes('reveal') ? '100%' : '' }"
+          date-type="auction"
+        />
       </div>
       <div role="tablist">
         <b-card-header
-          :class="[showDetail ? 'done': '', 'accordion-header']"
+          :class="[showDetail ? 'done' : '', 'accordion-header']"
           header-tag="header"
         >
-          <div>
-            <span> 1 </span>
-            &nbsp; Bid Information
-          </div>
-          <div
-            v-show="showDetail"
-            class="edit"
-            @click="editInputs"
-          >
-            Edit
-          </div>
+          <div><span> 1 </span> &nbsp; Bid Information</div>
+          <div v-show="showDetail" class="edit" @click="editInputs">Edit</div>
         </b-card-header>
         <b-collapse
           id="bidAccordion"
           v-model="showInfo"
           accordion="bidAccordionCollection"
-          role="tabpanel">
+          role="tabpanel"
+        >
           <div class="inputs-container">
             <div class="input-container">
               <label for="localBidAmount">Actual Bid Amount</label>
               <input
                 v-model="localBidAmount"
                 type="number"
-                name="localBidAmount">
+                name="localBidAmount"
+              />
             </div>
             <div class="input-container">
               <label for="localBidMask">Bid Mask</label>
               <input
                 v-model="localBidMask"
-                :class="[localBidAmount >= localBidMask ? 'errored': '']"
+                :class="[localBidAmount >= localBidMask ? 'errored' : '']"
                 type="number"
-                name="localBidMask">
-              <p
-                v-show="localBidAmount >= localBidMask"
-                class="erroredMsg">We recommend having your Bid mask higher than your Bid amount.</p>
+                name="localBidMask"
+              />
+              <p v-show="localBidAmount >= localBidMask" class="erroredMsg">
+                We recommend having your Bid mask higher than your Bid amount.
+              </p>
             </div>
             <div class="input-container">
-              <label
-                for="localSecretPhrase"
-                class="secret-phrase-label">
+              <label for="localSecretPhrase" class="secret-phrase-label">
                 <span> Secret Phrase </span>
-                <span
-                  class="random"
-                  @click.prevent="generateKeyPhrase"> <i class="fa fa-lg fa-refresh"/> Random  </span>
+                <span class="random" @click.prevent="generateKeyPhrase">
+                  <i class="fa fa-lg fa-refresh" /> Random
+                </span>
               </label>
               <input
                 v-model="localSecretPhrase"
                 type="text"
-                name="localSecretPhrase">
+                name="localSecretPhrase"
+              />
             </div>
           </div>
         </b-collapse>
         <b-card-header
-          :class="[showInfo ? 'disable': '', 'accordion-header-details']"
+          :class="[showInfo ? 'disable' : '', 'accordion-header-details']"
           header-tag="header"
         >
-          <div>
-            <span> 2 </span> &nbsp; Details
-          </div>
+          <div><span> 2 </span> &nbsp; Details</div>
         </b-card-header>
         <b-collapse
           id="detailAccordion"
           v-model="showDetail"
           accordion="bidAccordionCollection"
-          role="tabpanel">
+          role="tabpanel"
+        >
           <div class="inputs-container">
             <div class="confirmation-warning">
-              You CAN NOT claim your name unless you have this information during the reveal process. We suggest that you have to save those information.
+              You CAN NOT claim your name unless you have this information
+              during the reveal process. We suggest that you have to save those
+              information.
             </div>
-            <div
-              ref="printableData"
-              class="detail-info">
+            <div ref="printableData" class="detail-info">
               <div class="detail-info-item">
                 <span class="detail-title">Actual Bid Amount</span>
                 <span class="detail-value">{{ raw.bidAmount }} ETH</span>
@@ -125,7 +114,9 @@
               </div>
               <div class="detail-info-item">
                 <span class="detail-title">Reveal Date</span>
-                <span class="detail-value">{{ formatDate(raw.revealDate) }}</span>
+                <span class="detail-value">{{
+                  formatDate(raw.revealDate)
+                }}</span>
               </div>
               <div class="detail-info-item">
                 <span class="detail-title">Bid Mask</span>
@@ -133,20 +124,19 @@
               </div>
               <div class="detail-info-item">
                 <span class="detail-title">Auction Ends</span>
-                <span class="detail-value">{{ formatDate(raw.auctionDateEnd) }}</span>
+                <span class="detail-value">{{
+                  formatDate(raw.auctionDateEnd)
+                }}</span>
               </div>
 
               <div class="json-container">
                 <div class="json-label-container">
                   <span class="json-title">JSON String</span>
-                  <span
-                    class="json-copy"
-                    @click="copyString">{{ $t('common.copy') }}</span>
+                  <span class="json-copy" @click="copyString">{{
+                    $t('common.copy')
+                  }}</span>
                 </div>
-                <textarea
-                  ref="json"
-                  v-model="jsonText"
-                  class="json-content" />
+                <textarea ref="json" v-model="jsonText" class="json-content" />
               </div>
             </div>
           </div>
@@ -155,7 +145,8 @@
           <button
             v-if="$route.fullPath.includes('reveal')"
             class="json-string"
-            @click.prevent="openJsonModal">
+            @click.prevent="openJsonModal"
+          >
             JSON string
           </button>
           <button
@@ -163,17 +154,23 @@
             name="submit"
             class="submit"
             role="tab"
-            @click.prevent="$route.fullPath.includes('auction') ? startAuctionAndBid(): $route.fullPath.includes('bid') ? sendBid() : revealBid()">
+            @click.prevent="
+              $route.fullPath.includes('auction')
+                ? startAuctionAndBid()
+                : $route.fullPath.includes('bid')
+                ? sendBid()
+                : revealBid()
+            "
+          >
             <span v-if="loading === false"> Next </span>
-            <i
-              v-else
-              class="fa fa-spinner fa-spin"/>
+            <i v-else class="fa fa-spinner fa-spin" />
           </button>
           <button
             v-show="showDetail"
             class="submit"
             role="tab"
-            @click.prevent="downloadAndSend">
+            @click.prevent="downloadAndSend"
+          >
             Save & Next
           </button>
         </div>
