199736ff92fc0bccf46324cd24988020ef2faf34
David Hong <hsurf22@gmail.com>
2018-12-20 14:43:28 -0800

    Add Swap send form

---
+++ src/layouts/InterfaceLayout/containers/SwapContainer/components/SwapSendForm/SwapSendForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="swap-send-form">
+    <div class="wrap">
+      <interface-container-title isbackbutton="true" />
+      <div class="content-container">
+        iuehfowh osijfo gv
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InterfaceContainerTitle from '@/layouts/InterfaceLayout/components/InterfaceContainerTitle';
+export default {
+  components: {
+    'interface-container-title': InterfaceContainerTitle
+  },
+  props: {
+    providerData: {
+      type: Array,
+      default: function() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'SwapSendForm.scss';
+</style>
