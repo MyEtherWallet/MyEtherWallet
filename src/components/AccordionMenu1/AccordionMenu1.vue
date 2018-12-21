84ef22a3101a93405b97304509aa003afd6d441d
David Hong <hsurf22@gmail.com>
2018-12-20 16:05:50 -0800

    Add acordion menu 1

---
+++ src/components/AccordionMenu1/AccordionMenu1.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="accordion-menu-1">
+    <div class="wrap">
+      <div class="menu-title">
+        <div class="title-number"><span>{{ number }}</span></div>
+        <div>{{ title }}</div>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    number: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    isopen: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'AccordionMenu1.scss';
+</style>
--- src/layouts/InterfaceLayout/containers/SwapContainer/components/SwapSendForm/SwapSendForm.vue
+++ src/layouts/InterfaceLayout/containers/SwapContainer/components/SwapSendForm/SwapSendForm.vue
@@ -2,16 +2,25 @@
   <div class="swap-send-form">
     <div class="wrap">
       <interface-container-title isbackbutton="true" />
-      <div class="content-container">iuehfowh osijfo gv</div>
+      <div class="content-container">
+        <accordion-menu title="Verification" number="1">
+          <p>
+            You will receive a 6 digits PIN number through your phone number. To
+            continue, enter the PIN number below.
+          </p>
+        </accordion-menu>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import InterfaceContainerTitle from '@/layouts/InterfaceLayout/components/InterfaceContainerTitle';
+import AccordionMenu1 from '@/components/AccordionMenu1';
 export default {
   components: {
-    'interface-container-title': InterfaceContainerTitle
+    'interface-container-title': InterfaceContainerTitle,
+    'accordion-menu': AccordionMenu1
   },
   props: {
     providerData: {
