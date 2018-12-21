7b5005829d0f942d7f9b615729b61b59835a7d0a
David Hong <hsurf22@gmail.com>
2018-12-21 14:48:01 -0800

    Add standard input to Swap send form

---
+++ src/components/StandardDropdown/StandardDropdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="standard-dropdown">
+    <div>{{ chosenValue }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {
+      chosenValue: 'CA'
+    };
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'StandardDropdown.scss';
+</style>
