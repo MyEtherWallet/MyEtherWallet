af5e8424fd335afbda8ba8a7c1500c2ead676c28
Meri Herrera <herrerameri@gmail.com>
2018-12-21 13:31:48 -0300

    Adding RSK paths

--- src/wallets/bip44/ledgerPaths.js
+++ src/wallets/bip44/ledgerPaths.js
@@ -18,5 +18,13 @@ export default [
   {
     path: "m/44'/1'/0'/0",
     label: 'Ethereum Testnet (Ropsten)'
+  },
+  {
+    path: "m/44'/137'/0'/0",
+    label: 'RSK Mainnet'
+  },
+  {
+    path: "m/44'/37310'/0'/0",
+    label: 'RSK Testnet'
   }
 ];
--- src/wallets/bip44/trezorPaths.js
+++ src/wallets/bip44/trezorPaths.js
@@ -10,5 +10,13 @@ export default [
   {
     path: "m/44'/1'/0'/0",
     label: 'Ethereum Testnet (Ropsten)'
+  },
+  {
+    path: "m/44'/137'/0'/0",
+    label: 'RSK Mainnet'
+  },
+  {
+    path: "m/44'/37310'/0'/0",
+    label: 'RSK Testnet'
   }
 ];
