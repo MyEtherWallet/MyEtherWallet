cea3a6e5fadb0decebb5b62ae78a065752c2a78c
gamalielhere <gamalielgad1234@gmail.com>
2019-01-02 17:02:50 -0800

    Change pricebar animation

--- src/layouts/AccessWalletLayout/components/PriceBar/PriceBar.vue
+++ src/layouts/AccessWalletLayout/components/PriceBar/PriceBar.vue
@@ -2,40 +2,58 @@
   <div class="price-bar">
     <div class="wrap" style="overflow: hidden;">
       <div class="page-container">
-        <infinite-slider duration="20s" delay="0s">
-          <div class="slider-container">
-            <div
-              v-for="token in tokens"
-              :key="token.symbol"
-              class="token-container"
-            >
-              <p class="token-text">{{ token.symbol }}</p>
-              <p class="token-text">${{ token.quotes.USD.price }}</p>
-              <p class="token-text percent-container">
-                <span>{{ token.quotes.USD.percent_change_24h }}%</span>
-                <i
-                  :class="
-                    token.quotes.USD.percent_change_24h > 0
-                      ? 'fa fa-arrow-up'
-                      : 'fa fa-arrow-down'
-                  "
-                  aria-hidden="true"
-                />
-              </p>
+        <div class="slider-container">
+          <transition :duration="{ enter: 800, leave: 800 }" name="first-set">
+            <div v-if="!hidden">
+              <div
+                v-for="token in token1"
+                :key="token.symbol"
+                class="token-container"
+              >
+                <p class="token-text">{{ token.symbol }}</p>
+                <p class="token-text">${{ token.quotes.USD.price }}</p>
+                <p class="token-text percent-container">
+                  <span>{{ token.quotes.USD.percent_change_24h }}%</span>
+                  <i
+                    :class="
+                      token.quotes.USD.percent_change_24h > 0
+                        ? 'fa fa-arrow-up'
+                        : 'fa fa-arrow-down'
+                    "
+                    aria-hidden="true"
+                  />
+                </p>
+              </div>
             </div>
-          </div>
-        </infinite-slider>
+            <div v-else>
+              <div
+                v-for="token in token2"
+                :key="token.symbol"
+                class="token-container"
+              >
+                <p class="token-text">{{ token.symbol }}</p>
+                <p class="token-text">${{ token.quotes.USD.price }}</p>
+                <p class="token-text percent-container">
+                  <span>{{ token.quotes.USD.percent_change_24h }}%</span>
+                  <i
+                    :class="
+                      token.quotes.USD.percent_change_24h > 0
+                        ? 'fa fa-arrow-up'
+                        : 'fa fa-arrow-down'
+                    "
+                    aria-hidden="true"
+                  />
+                </p>
+              </div>
+            </div>
+          </transition>
+        </div>
       </div>
     </div>
   </div>
 </template>
-2
 <script>
-import InfiniteSlider from '@/components/InfiniteSlider/InfiniteSlider.vue';
 export default {
-  components: {
-    'infinite-slider': InfiniteSlider
-  },
   props: {
     tokens: {
       type: Array,
@@ -43,6 +61,26 @@ export default {
         return [];
       }
     }
+  },
+  data() {
+    return {
+      token1: [],
+      token2: [],
+      hidden: true
+    };
+  },
+  watch: {
+    tokens(newVal) {
+      if (newVal.length > 0) {
+        this.token1 = newVal.slice(0, 8);
+        this.token2 = newVal.slice(9, 17);
+      }
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.hidden = !this.hidden;
+    }, 3000);
   }
 };
 </script>
