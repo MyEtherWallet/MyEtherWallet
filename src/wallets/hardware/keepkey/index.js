2de9fa0c9c4b7cfe904f7bdcced7241d964142ec
Kosala Hemachandra <kvhnuke@aol.com>
2019-01-03 23:33:21 -0800

    coding done, waiting for UI

--- src/wallets/hardware/keepkey/index.js
+++ src/wallets/hardware/keepkey/index.js
@@ -1,9 +1,17 @@
-import { WebUSBDevice, Messages, KeepKey } from 'keepkey';
+import { WebUSBDevice, Messages, KeepKey } from '@keepkey/keepkey.js';
 import { KEEPKEY as keepkeyType } from '../../bip44/walletTypes';
 import bip44Paths from '../../bip44';
 import HDWalletInterface from '@/wallets/HDWalletInterface';
-import { nodeVector } from './utils';
+import { nodeVector, getUint8Tx } from './utils';
+import {
+  getBufferFromHex,
+  sanitizeHex,
+  getSignTransactionObject,
+  calculateChainIdFromV
+} from '../../utils';
 import * as HDKey from 'hdkey';
+import ethUtil from 'ethereumjs-util';
+import ethTx from 'ethereumjs-tx';
 
 const { MessageType } = Messages;
 const {
@@ -14,7 +22,7 @@ const {
 
 const NEED_PASSWORD = false;
 
-class keepkeyWallet {
+class KeepkeyWallet {
   constructor() {
     this.identifier = keepkeyType;
     this.isHardware = true;
@@ -26,24 +34,31 @@ class keepkeyWallet {
     const usbDevice = await WebUSBDevice.requestPair();
     const device = new WebUSBDevice({ usbDevice });
     this.keepkey = KeepKey.withWebUSB(device);
-    this.keepkey.device.promptEvents.on(
+    this.keepkey.device.events.on(
       String(MESSAGETYPE_BUTTONREQUEST),
       requestMsg => {
         console.log('button request', requestMsg);
       }
     );
-    this.keepkey.device.promptEvents.on(
+    this.keepkey.device.events.on(
       String(MESSAGETYPE_PINMATRIXREQUEST),
       requestMsg => {
         console.log('pin request', requestMsg);
+        window.setPin = pin => {
+          this.keepkey.acknowledgeWithPin(pin).then(console.log);
+        };
       }
     );
-    this.keepkey.device.promptEvents.on(
+    this.keepkey.device.events.on(
       String(MESSAGETYPE_PASSPHRASEREQUEST),
       requestMsg => {
         console.log('passphrase request', requestMsg);
+        window.setPass = pin => {
+          this.keepkey.acknowledgeWithPassphrase(pin).then(console.log);
+        };
       }
     );
+    await this.keepkey.initialize();
     const rootPub = await getRootPubKey(this.keepkey, this.basePath);
     this.hdKey = new HDKey();
     this.hdKey.publicKey = Buffer.from(rootPub.publicKey, 'hex');
@@ -51,8 +66,42 @@ class keepkeyWallet {
   }
   async getAccount(idx) {
     const derivedKey = this.hdKey.derive('m/' + idx);
-    const txSigner = () => {};
-    const msgSigner = () => {};
+    const txSigner = async tx => {
+      tx = new ethTx(tx);
+      const hexTx = getUint8Tx(tx);
+      const networkId = tx._chainId;
+      hexTx.addressNList = nodeVector(this.basePath + '/' + idx);
+      const result = await this.keepkey.ethereumSignTx(
+        hexTx,
+        null,
+        null,
+        hexTx.data,
+        networkId
+      );
+      tx.v = getBufferFromHex(sanitizeHex(result.v));
+      tx.r = getBufferFromHex(sanitizeHex(result.r));
+      tx.s = getBufferFromHex(sanitizeHex(result.s));
+      const signedChainId = calculateChainIdFromV(tx.v);
+      if (signedChainId !== networkId)
+        throw new Error(
+          'Invalid networkId signature returned. Expected: ' +
+            networkId +
+            ', Got: ' +
+            signedChainId,
+          'InvalidNetworkId'
+        );
+      return getSignTransactionObject(tx);
+    };
+    const msgSigner = async msg => {
+      const signMessage = new Messages.EthereumSignMessage();
+      signMessage.setAddressNList(nodeVector(this.basePath + '/' + idx));
+      signMessage.setMessage(new Uint8Array(ethUtil.toBuffer(msg)));
+      const [, response] = await this.keepkey.device.exchange(
+        Messages.MessageType.MESSAGETYPE_ETHEREUMSIGNMESSAGE,
+        signMessage
+      );
+      return Buffer.from(response.toObject().signature, 'base64');
+    };
     return new HDWalletInterface(
       this.basePath + '/' + idx,
       derivedKey.publicKey,
@@ -70,18 +119,19 @@ class keepkeyWallet {
   }
 }
 const createWallet = async basePath => {
-  const _keepkeyWallet = new keepkeyWallet();
+  const _keepkeyWallet = new KeepkeyWallet();
   await _keepkeyWallet.init(basePath);
   return _keepkeyWallet;
 };
 const getRootPubKey = async (_keepkey, _path) => {
   const getPublicKey = new Messages.GetPublicKey();
   getPublicKey.setAddressNList(nodeVector(_path));
-  getPublicKey.setShowDisplay(true);
+  getPublicKey.setShowDisplay(false);
   const pubObj = await _keepkey.getPublicKey(getPublicKey.toObject());
+  const hdkey = HDKey.fromExtendedKey(pubObj[1]);
   return {
-    publicKey: pubObj.getPublicKey_asU8().toString('hex'),
-    chainCode: pubObj.getChainCode_asU8().toString('hex')
+    publicKey: hdkey.publicKey.toString('hex'),
+    chainCode: hdkey.chainCode.toString('hex')
   };
 };
 
--- src/wallets/hardware/keepkey/utils.js
+++ src/wallets/hardware/keepkey/utils.js
@@ -13,4 +13,15 @@ const nodeVector = path => {
   });
   return result;
 };
-export { nodeVector };
+const getUint8Tx = tx => {
+  return {
+    to: new Uint8Array(tx.to),
+    value: new Uint8Array(tx.value),
+    data: new Uint8Array(tx.data),
+    chainId: tx._chainId,
+    nonce: new Uint8Array(tx.nonce),
+    gasLimit: new Uint8Array(tx.gasLimit),
+    gasPrice: new Uint8Array(tx.gasPrice)
+  };
+};
+export { nodeVector, getUint8Tx };
