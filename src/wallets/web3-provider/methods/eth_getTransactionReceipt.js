2ebb5c3298723977c4a49d465672cd0c035d1236
SteveM <SteveM@myEtherWallet.com>
2019-01-03 20:22:24 -0800

    clean

--- src/wallets/web3-provider/methods/eth_getTransactionReceipt.js
+++ src/wallets/web3-provider/methods/eth_getTransactionReceipt.js
@@ -16,7 +16,6 @@ export default async ({ payload, requestManager }, res, next) => {
       timestamp: new Date().getTime(),
       receipt
     };
-    console.log(payload); // todo remove dev item
     res(null, toPayload(payload.id, receipt));
   } else {
     res(null, toPayload(payload.id, memcache[txHash].receipt || null));
