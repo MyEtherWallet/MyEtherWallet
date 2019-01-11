1da5cf1a5d865859374a4005e7cd798c0d22af3f
Steve Mieskoski <Stevemieskoski@gmail.com>
2019-01-10 16:31:43 -0800

    Bug/bity fix (#423)
    * fix bity swap not working and related notifications for bity swap

--- src/layouts/InterfaceLayout/containers/SendCurrencyContainer/SendCurrencyContainer.vue
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer/SendCurrencyContainer.vue
@@ -322,7 +322,6 @@ export default {
       window.scrollTo(0, 0);
     },
     setBalanceToAmt() {
-      console.log(this.selectedCurrency.symbol, this.network.type.name);
       if (this.selectedCurrency.symbol === this.network.type.name) {
         const txFee = new BigNumber(this.gasLimit)
           .times(unit.toWei(this.gasPrice, 'gwei'))
