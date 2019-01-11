90c1cbc708a5a7de20ca20d64fa831eaaa534b08
SteveM <SteveM@myEtherWallet.com>
2019-01-10 19:53:56 -0800

    add beta to header logo

--- src/containers/HeaderContainer/HeaderContainer.vue
+++ src/containers/HeaderContainer/HeaderContainer.vue
@@ -124,6 +124,11 @@
                   class="logo-large"
                   src="~@/assets/images/short-hand-logo.png"
                 />
+                <img
+                  :class="!isPageOnTop && !isMobileMenuOpen ? 'logo-small' : ''"
+                  class="beta-tag"
+                  src="~@/assets/images/beta.png"
+                />
               </div>
             </router-link>
             <div class="top-menu">
