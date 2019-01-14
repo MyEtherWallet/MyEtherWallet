61db4ddfa9f1e40fe3a4805a518f7c4a48703a23
gamalielhere <gamalielgad1234@gmail.com>
2019-01-14 10:05:38 -0800

    Setup print modal for ens

---
+++ src/dapps/RegisterDomain/components/PrintModal/PrintModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>PRINT</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'PrintModal.scss';
+</style>
