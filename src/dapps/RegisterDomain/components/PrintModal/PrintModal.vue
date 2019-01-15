fb2ecda0fe35a6754462ed0489e3f456cc197431
gamalielhere <gamalielgad1234@gmail.com>
2019-01-15 08:34:37 -0800

    Finish print feature for ens

--- src/dapps/RegisterDomain/components/PrintModal/PrintModal.vue
+++ src/dapps/RegisterDomain/components/PrintModal/PrintModal.vue
@@ -7,7 +7,7 @@
     class="nopadding print-mod"
     size="lg"
   >
-    <div id="printContainer" class="print-modal">
+    <div ref="printContainer" class="print-modal">
       <div class="print-modal-header">
         <div class="logo-container">
           <img src="@/assets/images/logo.png" height="35px" />
@@ -42,13 +42,16 @@
       </div>
     </div>
     <div class="button-container">
-      <div class="print-button" @click="print">Print</div>
+      <div class="print-button" @click="print">
+        <i v-show="loading" class="fa fa-spinner fa-spin" />
+        <span v-show="!loading">Print</span>
+      </div>
     </div>
   </b-modal>
 </template>
 <script>
 import html2canvas from 'html2canvas';
-// import printJS from 'print-js';
+import printJS from 'print-js';
 import { mapGetters } from 'vuex';
 
 export default {
@@ -66,7 +69,8 @@ export default {
         .toDateString()
         .split(' ')
         .splice(1, 3)
-        .join(' ')
+        .join(' '),
+      loading: false
     };
   },
   computed: {
@@ -104,24 +108,21 @@ export default {
   },
   methods: {
     async print() {
-      const element = document.getElementById('printContainer');
+      this.loading = !this.loading;
+      const element = this.$refs.printContainer;
       const screen = await html2canvas(element, {
         async: true,
-        logging: false,
-        width: element.clientWidth,
-        height: element.clientHeight,
-        windowWidth: element.clientWidth,
-        windowHeight: element.clientHeight
-      }).then(canvas => {
-        return canvas;
+        logging: false
       });
+      this.loading = !this.loading;
 
-      console.log(screen);
-
-      // printJS({
-      //   printable: screen.toDataURL('image/png'),
-      //   type: 'image'
-      // });
+      printJS({
+        printable: screen.toDataURL('image/png'),
+        type: 'image',
+        onPrintDialogClose: () => {
+          this.$refs.print.hide();
+        }
+      });
     }
   }
 };
