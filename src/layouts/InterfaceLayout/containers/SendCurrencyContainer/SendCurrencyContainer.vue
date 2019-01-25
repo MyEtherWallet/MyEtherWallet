60b6de9550167baf051dedf6e1e0ddcfcf037515
gamalielhere <gamalielgad1234@gmail.com>
2019-01-25 14:11:28 -0800

    Add txFee check

--- src/layouts/InterfaceLayout/containers/SendCurrencyContainer/SendCurrencyContainer.vue
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer/SendCurrencyContainer.vue
@@ -226,9 +226,13 @@ export default {
       );
       const txFeeEth = ethUnit.fromWei(txFee, 'ether');
 
-      if (this.isToken)
+      if (this.isToken) {
         return new BigNumber(this.value).lte(this.selectedCurrency.balance);
-      return new BigNumber(this.value + txFeeEth).lte(this.balanceDefault);
+      }
+      return (
+        new BigNumber(txFeeEth).lt(this.balanceDefault) &&
+        new BigNumber(this.value + txFeeEth).lte(this.balanceDefault)
+      );
     },
     balanceDefault() {
       return new BigNumber(ethUnit.fromWei(this.account.balance, 'ether'));
