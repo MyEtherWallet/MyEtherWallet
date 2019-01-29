f4e0d2749d02333bda2d90398e4b342c795cd12a
David Hong <hsurf22@gmail.com>
2019-01-29 03:43:32 +0000

    Add Send Offline Helper page Add link to footer

---
+++ src/layouts/SendOfflineHelper/SendOfflineHelper.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="send-offline-helper">
+    <div class="wrap">
+      <div class="page-container">
+        <p>SendOfflineHelper SendOfflineHelper</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'SendOfflineHelper.scss';
+</style>
