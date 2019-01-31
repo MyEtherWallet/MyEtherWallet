071ef5591158e067bd32e9e159b349bcb55c5e10
Bagaric <joseph@josephit.com>
2019-01-31 10:00:05 +0100

    Fix TimeBounty validation check wrongly named

--- src/dapps/ScheduleTransaction/ScheduleTransaction.vue
+++ src/dapps/ScheduleTransaction/ScheduleTransaction.vue
@@ -361,7 +361,7 @@ export default {
         this.isValidExecutionWindow &&
         this.isValidFutureGasPrice &&
         this.isValidGasLimit &&
-        this.isValidBounty
+        this.isValidTimeBounty
       );
     },
     isValidAddress() {
