6f92710cd19f9681c7d1f8008cbdff828762caa0
David Hong <hsurf22@gmail.com>
2019-01-31 19:46:14 -0800

    package lock update

--- package-test.js
+++ package-test.js
@@ -1,7 +1,7 @@
 const package = require('./package.json');
 const packageJson = require('package-json');
 const SAFE_TIME = 1000 * 1 * 60 * 60 * 24 * 7; //7days
-const EXCEPTIONS = ['canvas', 'ethereum-ens'];
+const EXCEPTIONS = ['canvas', 'ethereum-ens', 'webpack'];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
 };
