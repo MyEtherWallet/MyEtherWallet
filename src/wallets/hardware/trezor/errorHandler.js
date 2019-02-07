b874385f03fd5f938edfc3dacd5c035c3aae50d0
Kosala Hemachandra <kvhnuke@aol.com>
2019-02-07 15:12:20 -0800

    Trezor popup closed

--- src/wallets/hardware/trezor/errorHandler.js
+++ src/wallets/hardware/trezor/errorHandler.js
@@ -1,6 +1,9 @@
 import * as Sentry from '@sentry/browser';
-const ERRORS = {};
+const ERRORS = {
+  POPUP_CLOSED: 'Popup closed'
+};
 const ErrorHandler = err => {
+  console.log(err.message);
   const errorValues = Object.values(ERRORS);
   if (errorValues.includes(err.message)) {
     console.error(err.message, err); // eslint-disable-line
