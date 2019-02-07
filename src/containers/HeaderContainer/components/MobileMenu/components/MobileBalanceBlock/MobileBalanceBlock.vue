4d679b7139b2bcf12a13ec18753cc892ed2c139c
David Hong <hsurf22@gmail.com>
2019-02-07 13:52:00 -0800

    Update Mobile menu

---
+++ src/containers/HeaderContainer/components/MobileMenu/components/MobileBalanceBlock/MobileBalanceBlock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="mobile-address-block">
+    <interface-balance-modal ref="balanceModal" />
+    <div class="wrap">
+      <div class="top-block">
+        <div class="block-title">
+          Balance
+        </div>
+        <button class="show-balance-button" @click="showBalanceModal">
+          <img src="~@/assets/images/icons/more.svg" />
+        </button>
+      </div>
+      <div class="bottom-block">
+        <p class="the-address">4000.340000 ETH</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import InterfaceBalanceModal from '@/layouts/InterfaceLayout/components/InterfaceBalanceModal';
+
+export default {
+  name: 'MobileBalanceBlock',
+  components: {
+    'interface-balance-modal': InterfaceBalanceModal
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {
+    showBalanceModal() {
+      this.$refs.balanceModal.$refs.balance.show();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'MobileBalanceBlock.scss';
+</style>
---
+++ src/containers/HeaderContainer/components/MobileMenu/components/MobileNetworkBlock/MobileNetworkBlock.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mobile-network-block">
+    <interface-balance-modal ref="balanceModal" />
+    <div class="wrap">
+      <div class="top-block">
+        <div class="block-title">
+          Network
+        </div>
+        <button class="change-button">
+          Change
+        </button>
+      </div>
+      <div class="bottom-block">
+        <p class="network">Myetherapi.com (ETH)</p>
+        <p class="last-block">Last Block#: 5699679</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MobileBalanceBlock',
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  created() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'MobileNetworkBlock.scss';
+</style>
