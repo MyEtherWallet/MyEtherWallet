6e4a2ebcf1dd9b0396bf324390e0eec189e8e3d9
gamalielhere <gamalielgad1234@gmail.com>
2019-02-10 01:21:24 -0800

    Remove beta on mobile, disable side menus

--- src/layouts/InterfaceLayout/components/InterfaceSideMenu/InterfaceSideMenu.vue
+++ src/layouts/InterfaceLayout/components/InterfaceSideMenu/InterfaceSideMenu.vue
@@ -10,6 +10,7 @@
           <div
             :class="[
               isTabActive(tab.routes) ? 'active' : '',
+              disableTab(tab.name) ? 'disable' : '',
               'menu-group-title'
             ]"
             @click.prevent="tabAction(tab)"
@@ -53,147 +54,39 @@
 </template>
 
 <script>
-// import tabsConfig from './InterfaceSideMenu.config';
+import tabsConfig from './InterfaceSideMenu.config';
+import mapGetters from 'vuex';
+const env = NODE_ENV;
 export default {
   data() {
     return {
-      tabData: [
-        {
-          name: 'send-transaction',
-          routes: [
-            '/interface/send-transaction',
-            '/interface',
-            '/interface/send-offline',
-            '/interface/send-offline/generate-info',
-            '/interface/send-offline/generate-tx',
-            '/interface/send-offline/send-tx'
-          ],
-          icons: {
-            active: require('@/assets/images/sidemenu/send-active.svg'),
-            inactive: require('@/assets/images/sidemenu/send.svg')
-          },
-          titleKey: 'interface.txSideMenuTitle',
-          children: [
-            {
-              name: 'send-transaction',
-              routes: ['/interface/send-transaction', '/interface'],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.sendTx'
-            },
-            {
-              name: 'send-offline',
-              routes: [
-                '/interface/send-offline',
-                '/interface/send-offline/generate-info',
-                '/interface/send-offline/generate-tx',
-                '/interface/send-offline/send-tx'
-              ],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.offline'
-            }
-          ]
-        },
-        {
-          name: 'swap',
-          routes: ['/interface/swap'],
-          icons: {
-            active: require('@/assets/images/sidemenu/swap-active.svg'),
-            inactive: require('@/assets/images/sidemenu/swap.svg')
-          },
-          titleKey: 'common.swap',
-          children: []
-        },
-        {
-          name: 'dapps',
-          routes: [
-            '/interface/dapps',
-            '/interface/dapps/register-domain',
-            '/interface/dapps/register-domain/bid',
-            '/interface/dapps/register-domain/reveal',
-            '/interface/dapps/register-domain/owned',
-            '/interface/dapps/register-domain/forbidden',
-            '/interface/dapps/register-domain/auction',
-            '/interface/dapps/register-domain/confirm',
-            '/interface/dapps/domain-sale'
-          ],
-          icons: {
-            active: require('@/assets/images/sidemenu/dapps-active.svg'),
-            inactive: require('@/assets/images/sidemenu/dapps.svg')
-          },
-          titleKey: 'common.dapps',
-          children: []
-        },
-        {
-          name: 'contracts',
-          routes: [
-            '/interface/interact-with-contract',
-            '/interface/deploy-contract'
-          ],
-          icons: {
-            active: require('@/assets/images/sidemenu/contract-active.svg'),
-            inactive: require('@/assets/images/sidemenu/contract.svg')
-          },
-          titleKey: 'interface.txSideMenuContract',
-          children: [
-            {
-              name: 'interact-with-contract',
-              routes: ['/interface/interact-with-contract'],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.interactWcontract'
-            },
-            {
-              name: 'deploy-contract',
-              routes: ['/interface/deploy-contract'],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.depContract'
-            }
-          ]
-        },
-        {
-          name: 'messages',
-          routes: ['/interface/sign-message', '/interface/verify-message'],
-          icons: {
-            active: require('@/assets/images/sidemenu/message-active.svg'),
-            inactive: require('@/assets/images/sidemenu/message.svg')
-          },
-          titleKey: 'interface.txSideMenuMessage',
-          children: [
-            {
-              name: 'sign-message',
-              routes: ['/interface/sign-message'],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.signMessage'
-            },
-            {
-              name: 'verify-message',
-              routes: ['/interface/verify-message'],
-              icons: {
-                active: '',
-                inactive: ''
-              },
-              titleKey: 'common.verifyMessage'
-            }
-          ]
-        }
-      ]
+      env: env,
+      tabData: tabsConfig.tab
     };
   },
+  computed: {
+    ...mapGetters({
+      online: 'online',
+      network: 'network'
+    })
+  },
   methods: {
+    disableTab(tabName) {
+      if (!this.online) {
+        if (tabName === 'message') {
+          return false;
+        }
+      } else {
+        if (this.env === 'production') {
+          if (tabName === 'message' || tabName === 'swap') {
+            return false;
+          }
+        }
+
+        if (this.env === 'production') return false;
+      }
+      return true;
+    },
     toggleSideMenu() {
       this.$store.commit('TOGGLE_SIDEMENU');
     },
