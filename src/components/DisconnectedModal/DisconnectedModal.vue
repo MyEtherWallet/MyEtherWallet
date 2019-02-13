74ab9bcb603a2d05ed06becfec4263161057a625
David Hong <hsurf22@gmail.com>
2019-02-12 16:19:41 -0800

    Add disconnected modal

---
+++ src/components/DisconnectedModal/DisconnectedModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="address-qrcode-modal">
+    <b-modal
+      ref="disconnected"
+      hide-footer
+      hide-header
+      centered
+      class="bootstrap-modal nopadding"
+    >
+      <div class="modal-contents">Disconnected content</div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisconnectedModal',
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'DisconnectedModal.scss';
+</style>
