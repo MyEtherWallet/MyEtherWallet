58c902c653b7af5dd39a172ba9ab7651ff97f6d6
gamalielhere <gamalielgad1234@gmail.com>
2019-02-13 18:43:09 -0800

    Include warning objects in hardware wallet errorHandlers

--- src/wallets/hardware/trezor/errorHandler.js
+++ src/wallets/hardware/trezor/errorHandler.js
@@ -4,12 +4,19 @@ const ERRORS = {
   DEVICE_DISCONNECTED: 'Device disconnected',
   DEVICE_DISCONNECTED_ACTION: 'device disconnected during action',
   ACTION_CANCELLED: 'Action cancelled by user',
-  NO_PERMISSION: 'Permissions not granted'
+  NO_PERMISSION: 'Permissions not granted',
+  TRANSPORT_MISSING: 'Device call in progress'
 };
+
+const WARNING = {};
+
 export default err => {
   const errorValues = Object.values(ERRORS);
+  const warningValues = Object.values(WARNING);
   if (errorValues.includes(err.message)) {
-    Toast.responseHandler(err, true);
+    Toast.responseHandler(err, Toast.ERROR);
+  } else if (warningValues.includes(err.message)) {
+    Toast.responseHandler(err, Toast.WARN);
   } else {
     Toast.responseHandler(err, false);
   }
--- src/wallets/hybrid/MEWconnect/errorHandler.js
+++ src/wallets/hybrid/MEWconnect/errorHandler.js
@@ -1,9 +1,14 @@
 import { Toast } from '@/helpers';
 const ERRORS = {};
+const WARNING = {};
+
 export default err => {
   const errorValues = Object.values(ERRORS);
+  const warningValues = Object.values(WARNING);
   if (errorValues.includes(err.message)) {
-    Toast.responseHandler(err, true);
+    Toast.responseHandler(err, Toast.ERROR);
+  } else if (warningValues.includes(err.message)) {
+    Toast.responseHandler(err, Toast.WARN);
   } else {
     Toast.responseHandler(err, false);
   }
