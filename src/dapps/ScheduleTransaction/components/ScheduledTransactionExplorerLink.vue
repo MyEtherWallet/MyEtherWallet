91e5637752485a14a6db498e539036c6fd84824c
Bagaric <joseph@josephit.com>
2019-02-15 14:01:19 +0100

    UI improvements

---
+++ src/dapps/ScheduleTransaction/components/ScheduledTransactionExplorerLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <a
+    :href="'localhost:8081/awaiting/scheduler/' + txHash"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {{ txHash }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ScheduledTransactionExplorerLink',
+  props: {
+    txHash: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
