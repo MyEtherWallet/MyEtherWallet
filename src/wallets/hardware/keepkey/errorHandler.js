d7219be2cf5b7cbbe69faca2aab616b89d33a2f2
Kosala Hemachandra <kvhnuke@aol.com>
2019-02-18 19:57:11 -0800

    couple more fixes

--- src/wallets/hardware/keepkey/errorHandler.js
+++ src/wallets/hardware/keepkey/errorHandler.js
@@ -1,10 +1,13 @@
 import { Toast } from '@/helpers';
 const ERRORS = {
-  NO_DEVICE_SELECTED: 'No device selected.'
+  NO_DEVICE_SELECTED: 'No device selected.',
+  INVALID_PIN: 'Invalid PIN',
+  ALREADY_IN_USE: 'Unable to claim interface.'
 };
 const WARNING = {};
 
 export default err => {
+  console.log(err, err.message);
   const errorValues = Object.values(ERRORS);
   const warningValues = Object.values(WARNING);
   if (errorValues.includes(err.message)) {
