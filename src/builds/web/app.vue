503915d0387e71fbaad59ffea825ed66b5d21b75
David Hong <hsurf22@gmail.com>
2019-02-18 19:43:44 -0800

    Fix logout warning modal

--- src/builds/web/app.vue
+++ src/builds/web/app.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <logout-warning-modal ref="logoutWarningModal" />
     <header-container v-show="$route.fullPath !== '/getting-started'" />
     <welcome-modal ref="welcome" />
     <router-view />
@@ -16,6 +17,7 @@ import WelcomeModal from '@/components/WelcomeModal';
 import store from 'store';
 import { mapGetters } from 'vuex';
 import { Toast } from '@/helpers';
+import LogoutWarningModal from '@/components/LogoutWarningModal';
 
 export default {
   name: 'App',
@@ -23,6 +25,7 @@ export default {
     'header-container': HeaderContainer,
     'footer-container': FooterContainer,
     'confirmation-container': ConfirmationContainer,
+    'logout-warning-modal': LogoutWarningModal,
     'welcome-modal': WelcomeModal
   },
   computed: {
@@ -36,11 +39,9 @@ export default {
         from.matched[0].path === '/interface' &&
         to.matched[0].path !== '/interface'
       ) {
-        console.log('Getting out of interface page!!!!');
+        //console.log('Getting out of interface page!!!!');
+        this.$refs.logoutWarningModal.$refs.logoutWarningModal.show();
       }
-
-      //console.log(to.matched[0].path);
-      //console.log(from.matched[0].path);
     }
   },
   created() {
