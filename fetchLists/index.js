fe7445e693289d8d8d75e48a04ab8c71badf0954
Kosala Hemachandra <kvhnuke@aol.com>
2019-02-20 17:28:59 -0800

    redo list fetching

--- fetchLists/index.js
+++ fetchLists/index.js
@@ -1,34 +1,28 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
-const tokenFolder = './src/tokens';
-const contractFolder = './src/contracts';
-const darklistFolder = './src/darklist';
+const configs = require('./configs');
+const tokenList = require('./lists/tokens.json');
+const contractList = require('./lists/contracts.json');
 
-async function fetchTokens() {
+const fetchTokens = async () => {
   try {
-    if (!fs.existsSync(tokenFolder)) {
-      fs.mkdirSync(tokenFolder);
+    if (!fs.existsSync(configs.TOKENS_PATH)) {
+      fs.mkdirSync(configs.TOKENS_PATH);
     }
-
-    const tokenList = await fetch(
-      'https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/dist/tokens'
-    )
-      .then(res => res.json())
-      .catch(err => console.log(err));
     const tokenFileURL =
       'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/tokens/';
     if (tokenList !== undefined && tokenList.length > 0) {
       for (let i = 0; i < tokenList.length; i++) {
-        const tokenFile = tokenList[i]
-        let tokensCollection = await fetch(
+        const tokenFile = tokenList[i];
+        const tokensCollection = await fetch(
           `${tokenFileURL + tokenFile.name}/tokens-${tokenFile.name}.json`
         )
           .then(res => res.json())
           .catch(err => console.log(err));
         if (tokensCollection !== undefined) {
-          console.log(`${tokenFolder}/tokens-${tokenFile.name}.json`)
+          console.log(`${configs.TOKENS_PATH}/tokens-${tokenFile.name}.json`);
           fs.writeFileSync(
-            `${tokenFolder}/tokens-${tokenFile.name}.json`,
+            `${configs.TOKENS_PATH}/tokens-${tokenFile.name}.json`,
             JSON.stringify(tokensCollection)
           );
         }
@@ -37,12 +31,12 @@ async function fetchTokens() {
   } catch (e) {
     console.error(e); // Not captured by sentry
   }
-}
+};
 
-async function fetchDarkList() {
+const fetchDarkList = async () => {
   try {
-    if (!fs.existsSync(darklistFolder)) {
-      fs.mkdirSync(darklistFolder)
+    if (!fs.existsSync(configs.DARKLIST_PATH)) {
+      fs.mkdirSync(configs.DARKLIST_PATH);
     }
 
     const darkList = await fetch(
@@ -51,63 +45,64 @@ async function fetchDarkList() {
       .then(res => res.json())
       .catch(console.log);
     const jsonToStore = {
-      'data': darkList,
-      'timestamp': Date.now()
-    }
-    console.log(`${darklistFolder}/address-darklist.json`)
-    fs.writeFileSync(`${darklistFolder}/address-darklist.json`, JSON.stringify(jsonToStore));
+      data: darkList,
+      timestamp: Date.now()
+    };
+    console.log(`${configs.DARKLIST_PATH}/address-darklist.json`);
+    fs.writeFileSync(
+      `${configs.DARKLIST_PATH}/address-darklist.json`,
+      JSON.stringify(jsonToStore)
+    );
   } catch (e) {
     console.log(e); // Not captured by sentry
   }
-}
+};
 
-async function fetchContracts() {
+const fetchContracts = async () => {
   try {
-    if (!fs.existsSync(contractFolder)) {
-      fs.mkdirSync(contractFolder);
+    if (!fs.existsSync(configs.CONTRACTS_PATH)) {
+      fs.mkdirSync(configs.CONTRACTS_PATH);
     }
 
-    const contractList = await fetch(
-      'https://api.github.com/repos/MyEtherWallet/ethereum-lists/contents/dist/contracts'
-    )
-      .then(res => res.json())
-      .catch(err => console.log(err));
     const contractFileURL =
       'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/contracts/';
     if (contractList !== undefined && contractList.length > 0) {
       for (let i = 0; i < contractList.length; i++) {
-        const contractFile = contractList[i]
-        let contractsCollection = await fetch(
+        const contractFile = contractList[i];
+        const contractsCollection = await fetch(
           `${contractFileURL + contractFile.name}/contract-abi-${
-          contractFile.name
+            contractFile.name
           }.json`
         )
           .then(res => res.json())
           .catch(err => console.log(err));
         if (contractsCollection !== undefined) {
-          console.log(`${contractFolder}/contract-abi-${contractFile.name}.json`)
+          console.log(
+            `${configs.CONTRACTS_PATH}/contract-abi-${contractFile.name}.json`
+          );
           fs.writeFileSync(
-            `${contractFolder}/contract-abi-${contractFile.name}.json`,
+            `${configs.CONTRACTS_PATH}/contract-abi-${contractFile.name}.json`,
             JSON.stringify(contractsCollection)
           );
         }
-
       }
     }
   } catch (e) {
     console.error(e); // todo replace with proper error
   }
-}
+};
 
 const run = async () => {
-  await fetchTokens().then(fetchContracts).then(fetchDarkList);
-}
+  await fetchTokens()
+    .then(fetchContracts)
+    .then(fetchDarkList);
+};
 
 (async () => {
   try {
     await run();
-    console.log("Done");
+    console.log('Done');
   } catch (e) {
-    console.error(e)
+    console.error(e);
   }
 })();
