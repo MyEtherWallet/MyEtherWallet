3549c293b0b664c41cbf20acda3690134968eda1
AnilChinchawale <Anil24593@gmail.com>
2019-02-22 17:19:17 +0530

    XinFin Network Added in Routes

--- src/networks/nodes/index.js
+++ src/networks/nodes/index.js
@@ -1,66 +1,71 @@
-import akaremote from './aka-remote';
-import ath from './ath';
-import clo from './clo';
-import egem from './egem';
-import ella from './ella';
-import eosc from './eosc';
-import esn from './esn';
-import etcethereumcommonwealthgeth from './etc-ethereumcommonwealth-geth';
-import etcethereumcommonwealthparity from './etc-ethereumcommonwealth-parity';
+import xdc from './xdc-testnet';
 import ethethscan from './eth-ethscan';
 import ethinfura from './eth-infura';
 import ethinfuraws from './eth-infura-ws';
 import ethmew from './eth-mew';
 import ethmewws from './eth-mew-ws';
-import etho from './etho';
-import go from './go';
-import kovethscan from './kov-ethscan';
-import kovinfura from './kov-infura';
-import music from './music';
-import pirl from './pirl';
-import poa from './poa';
 import rinethscan from './rin-ethscan';
 import rininfura from './rin-infura';
 import ropinfura from './rop-infura';
 import rininfuraws from './rin-infura-ws';
 import ropinfuraws from './rop-infura-ws';
 import ropmew from './rop-mew';
+import poa from './poa';
+
+// import akaremote from './aka-remote';
+// import ath from './ath';
+// import clo from './clo';
+// import egem from './egem';
+// import ella from './ella';
+// import eosc from './eosc';
+// import esn from './esn';
+// import etcethereumcommonwealthgeth from './etc-ethereumcommonwealth-geth';
+// import etcethereumcommonwealthparity from './etc-ethereumcommonwealth-parity';
+// import etho from './etho';
+// import go from './go';
+// import kovethscan from './kov-ethscan';
+// import kovinfura from './kov-infura';
+// import music from './music';
+// import pirl from './pirl';
+
 import rskpublic from './rsk-public';
 import rsktestpublic from './rsk-test-public';
-import tomo from './tomo';
-import ubq from './ubq';
-import goerli from './goerli-mew-ws';
+// import tomo from './tomo';
+// import ubq from './ubq';
+// import goerli from './goerli-mew-ws';
 export {
-  akaremote,
-  ath,
-  clo,
-  egem,
-  ella,
-  eosc,
-  esn,
-  etcethereumcommonwealthgeth,
-  etcethereumcommonwealthparity,
+  xdc,
   ethethscan,
   ethinfura,
   ethinfuraws,
   ethmew,
   ethmewws,
-  etho,
-  go,
-  kovethscan,
-  kovinfura,
-  music,
-  pirl,
   poa,
   rinethscan,
   rininfura,
   ropinfura,
   rininfuraws,
   ropinfuraws,
-  ropmew,
   rskpublic,
   rsktestpublic,
-  tomo,
-  ubq,
-  goerli
+  ropmew
+  // akaremote,
+  // ath,
+  // clo,
+  // egem,
+  // ella,
+  // eosc,
+  // esn,
+  // etcethereumcommonwealthgeth,
+  // etcethereumcommonwealthparity,
+  // etho,
+  // go,
+  // kovethscan,
+  // kovinfura,
+  // music,
+  // pirl,
+
+  // tomo,
+  // ubq,
+  // goerli
 };
--- src/networks/types/index.js
+++ src/networks/types/index.js
@@ -1,46 +1,51 @@
-import AKA from './AKA.js';
-import ATH from './ATH.js';
-import CLO from './CLO.js';
-import EGEM from './EGEM.js';
-import ELLA from './ELLA.js';
-import EOSC from './EOSC.js';
-import ESN from './ESN.js';
-import ETC from './ETC.js';
-import ETH from './ETH.js';
-import ETHO from './ETHO.js';
-import GO from './GO.js';
-import KOV from './KOV.js';
-import MUSIC from './MUSIC.js';
-import PIRL from './PIRL.js';
-import POA from './POA.js';
+import XDC from './XDC.js';
 import RIN from './RIN.js';
 import ROP from './ROP.js';
+import POA from './POA.js';
+import ETH from './ETH.js';
+// import AKA from './AKA.js';
+// import ATH from './ATH.js';
+// import CLO from './CLO.js';
+// import EGEM from './EGEM.js';
+// import ELLA from './ELLA.js';
+// import EOSC from './EOSC.js';
+// import ESN from './ESN.js';
+// import ETC from './ETC.js';
+// import ETHO from './ETHO.js';
+// import GO from './GO.js';
+// import KOV from './KOV.js';
+// import MUSIC from './MUSIC.js';
+// import PIRL from './PIRL.js';
 import RSK from './RSK.js';
 import RSKTEST from './RSKTEST.js';
-import TOMO from './TOMO.js';
-import UBQ from './UBQ.js';
-import GOERLI from './GOERLI';
+// import TOMO from './TOMO.js';
+// import UBQ from './UBQ.js';
+// import GOERLI from './GOERLI';
 export {
-  AKA,
-  ATH,
-  CLO,
-  EGEM,
-  ELLA,
-  EOSC,
-  ESN,
-  ETC,
-  ETH,
-  ETHO,
-  GO,
-  KOV,
-  MUSIC,
-  POA,
-  PIRL,
+  XDC,
   RIN,
   ROP,
+  ETH,
   RSK,
   RSKTEST,
-  TOMO,
-  UBQ,
-  GOERLI
+  POA
+  // AKA,
+  // ATH,
+  // CLO,
+  // EGEM,
+  // ELLA,
+  // EOSC,
+  // ESN,
+  // ETC,
+  // ETHO,
+  // GO,
+  // KOV,
+  // MUSIC,
+  // PIRL,
+  // RIN,
+  // ROP,
+
+  // TOMO,
+  // UBQ,
+  // GOERLI
 };
