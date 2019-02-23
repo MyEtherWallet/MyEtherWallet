8f1db17185d47ac3764980969fa11ba732db30a5
AnilChinchawale <Anil24593@gmail.com>
2019-02-23 11:56:44 +0530

    Dependencies removed

--- src/helpers/addressUtils.js
+++ src/helpers/addressUtils.js
@@ -1,9 +1,9 @@
-import store from '@/store';
-import {
-  isValidChecksumAddress as isValidRSKChecksumAddress,
-  toChecksumAddress as toRSKChecksumAddress
-} from 'rskjs-util';
-import { RSK, RSKTEST } from '@/networks/types';
+// import store from '@/store';
+// import {
+//   isValidChecksumAddress as isValidRSKChecksumAddress,
+//   toChecksumAddress as toRSKChecksumAddress
+// } from 'rskjs-util';
+// import { RSK, RSKTEST } from '@/networks/types';
 import web3 from 'web3';
 const isAddress = address => {
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
@@ -14,15 +14,15 @@ const isAddress = address => {
   ) {
     return true;
   }
-  const chainID = store.getters.network.type.chainID;
-  if (chainID === RSK.chainID || chainID === RSKTEST.chainID)
-    return isValidRSKChecksumAddress(address, chainID);
+  // const chainID = store.getters.network.type.chainID;
+  // if (chainID === RSK.chainID || chainID === RSKTEST.chainID)
+  //   return isValidRSKChecksumAddress(address, chainID);
   return web3.utils.checkAddressChecksum(address);
 };
 const toChecksumAddress = address => {
-  const chainID = store.getters.network.type.chainID;
-  if (chainID === RSK.chainID || chainID === RSKTEST.chainID)
-    return toRSKChecksumAddress(address, chainID);
+  // const chainID = store.getters.network.type.chainID;
+  // if (chainID === RSK.chainID || chainID === RSKTEST.chainID)
+  //   return toRSKChecksumAddress(address, chainID);
   return web3.utils.toChecksumAddress(address);
 };
 export { isAddress, toChecksumAddress };
