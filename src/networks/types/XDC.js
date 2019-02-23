20397c833f5a44a75200946f929b821d6ea66116
AnilChinchawale <Anil24593@gmail.com>
2019-02-23 11:58:37 +0530

    Explorer Dead URL removed | Active URL added

--- src/networks/types/XDC.js
+++ src/networks/types/XDC.js
@@ -6,10 +6,15 @@ export default {
   name: 'XDC',
   name_long: 'XDC',
   homePage: 'https://xinfin.org/',
-  blockExplorerTX: 'https://explorer.xinfin.network/#/tx/[[txHash]]',
-  blockExplorerAddr: 'https://explorer.xinfin.network/#/address/[[address]]',
+  blockExplorerAddr:
+    'https://explorer_testnet.xinfin.network/account/[[address]]',
+  blockExplorerTX: 'https://explorer_testnet.xinfin.network/tx/[[txHash]]',
   chainID: 853,
   tokens: tokens,
   contracts: contracts,
+  ens: {
+    registry: '0x41ab1b6fcbb2fa9dced81acbdec13ea6315f2bf2',
+    registrarTLD: 'xdc'
+  },
   icon: xdc
 };
