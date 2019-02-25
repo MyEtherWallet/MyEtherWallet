ee7c389f6322fd36241e10a05dceb874542e059f
gamalielhere <gamalielgad1234@gmail.com>
2019-02-25 15:28:42 -0800

    Sort by network support, add row gap

--- src/layouts/InterfaceLayout/containers/DappsContainer/DappsContainer.vue
+++ src/layouts/InterfaceLayout/containers/DappsContainer/DappsContainer.vue
@@ -4,14 +4,14 @@
       <interface-container-title :title="$t('common.dapps')" />
       <div class="buttons-container">
         <dapp-buttons
-          v-for="dapp in Object.keys(localDapps)"
-          :key="localDapps[dapp].title"
-          :title="$t(localDapps[dapp].title)"
-          :icon="localDapps[dapp].icon"
-          :icon-disabled="localDapps[dapp].iconDisabled"
-          :desc="$t(localDapps[dapp].desc)"
-          :param="localDapps[dapp].route"
-          :supported-networks="localDapps[dapp].supportedNetworks"
+          v-for="dapp in sortedObject"
+          :key="dapp.title"
+          :title="$t(dapp.title)"
+          :icon="dapp.icon"
+          :icon-disabled="dapp.iconDisabled"
+          :desc="$t(dapp.desc)"
+          :param="dapp.route"
+          :supported-networks="dapp.supportedNetworks"
         />
       </div>
     </div>
@@ -22,6 +22,7 @@
 import InterfaceContainerTitle from '../../components/InterfaceContainerTitle';
 import DappButtons from '../../components/DappButtons';
 import dapps from '@/dapps';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'DappsContainer',
@@ -33,6 +34,29 @@ export default {
     return {
       localDapps: dapps
     };
+  },
+  computed: {
+    ...mapGetters({
+      network: 'network'
+    }),
+    sortedObject() {
+      const arrayedDapp = [];
+      Object.keys(dapps).forEach(dapp => {
+        arrayedDapp.push(dapps[dapp]);
+      });
+
+      return arrayedDapp.sort((a, b) => {
+        if (
+          a.supportedNetworks.includes(this.network.type.name) ||
+          b.supportedNetworks.includes(this.network.type.name)
+        )
+          return 1;
+        return 0;
+      });
+    }
+  },
+  mounted() {
+    console.log(this.sortedObject);
   }
 };
 </script>
