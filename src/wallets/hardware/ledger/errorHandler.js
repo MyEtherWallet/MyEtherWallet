c5c30ada4812e9f51c8b0e73ddb1f02651eeee34
gamalielhere <gamalielgad1234@gmail.com>
2019-03-07 17:19:11 -0800

    Move errorHandler into wallet obbject, theoretical fix for #398 on sentry, use errorhandler from hw

--- src/wallets/hardware/ledger/errorHandler.js
+++ src/wallets/hardware/ledger/errorHandler.js
@@ -13,7 +13,9 @@ const ERRORS = {
   'Ledger device: UNKNOWN_ERROR (0x6801)': 'ledgerError.unknown0x6801',
   'Ledger device: Invalid data received (0x6a80)':
     'ledgerError.invalidData0x6a80',
-  'Ledger device: CLA_NOT_SUPPORTED (0x6e00)': 'ledgerError.claNotSupport0x6e00'
+  'Ledger device: CLA_NOT_SUPPORTED (0x6e00)':
+    'ledgerError.claNotSupport0x6e00',
+  'Invalid networkId signature returned.': 'ledgerError.invalidNetworkID'
 };
 const WARNING = {
   'Ledger device: Condition of use not satisfied (denied by the user?) (0x6985)':
@@ -23,20 +25,32 @@ const WARNING = {
 };
 
 export default err => {
+  console.log(err, err.message);
+  const expected = err.message
+    ? err.message.substr(err.message.indexOf('Expected'), err.message.length)
+    : err.substr(err.indexOf('Expected'), err.message);
   const errorValues = Object.keys(ERRORS);
   const warningValues = Object.keys(WARNING);
   const foundError = errorValues.find(item => {
+    if (err && err.message.includes(item)) return item;
     return item.includes(err.message) || item.includes(err);
   });
 
   const foundWarning = warningValues.find(item => {
+    if (err && err.message.includes(item)) return item;
     return item.includes(err.message) || item.includes(err);
   });
 
   if (foundError) {
-    Toast.responseHandler(Vue.$i18n.t(ERRORS[foundError]), Toast.ERROR);
+    Toast.responseHandler(
+      `${Vue.$i18n.t(ERRORS[foundError])}${expected}`,
+      Toast.ERROR
+    );
   } else if (foundWarning) {
-    Toast.responseHandler(Vue.$i18n.t(WARNING[foundWarning]), Toast.WARN);
+    Toast.responseHandler(
+      `${Vue.$i18n.t(WARNING[foundWarning])}${expected}`,
+      Toast.WARN
+    );
   } else {
     Toast.responseHandler(err, false);
   }
