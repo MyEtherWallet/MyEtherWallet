09b96ad71c6a2784f6aeb33aee0ef24960adbb2a
gamalielhere <gamalielgad1234@gmail.com>
2019-03-26 13:12:31 -0700

    Raise listener length to 10 secs

--- src/wallets/hardware/ledger/index.js
+++ src/wallets/hardware/ledger/index.js
@@ -115,7 +115,7 @@ const createWallet = async basePath => {
 };
 createWallet.errorHandler = errorHandler;
 const getLedgerTransport = async () => {
-  const transport = await u2fTransport.create(3000, 3000);
+  const transport = await u2fTransport.create(3000, 10000);
   return transport;
 };
 const getLedgerAppConfig = async _ledger => {
