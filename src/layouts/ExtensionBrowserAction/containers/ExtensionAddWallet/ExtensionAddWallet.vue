bcbd2e7658a6fe4dc328fc89c965c388b81a35ad
gamalielhere <gamalielgad1234@gmail.com>
2019-04-17 18:04:28 -0700

    Setup add wallet layout

---
+++ src/layouts/ExtensionBrowserAction/containers/ExtensionAddWallet/ExtensionAddWallet.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="add-wallet-container">
+    <h3> Add My Wallet </h3>
+    <p> How would you like to add your wallet? </p>
+    <div class="add-wallet-options">
+      <router-link class="wallet-option" v-for="option in options" :to="option.link">
+        <img :src="option.icon" />
+        <p>{{ option.text }}</p>
+        <p>{{ option.warning }}</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          icon: '',
+          text: 'Generate a New Wallet',
+          link: '/generate',
+          warning: ''
+        },
+        {
+          icon: '',
+          text: 'Mnemonic Phrase',
+          link: '/mnemonic',
+          warning: 'Not Recommended'
+        },
+        {
+          icon: '',
+          text: 'Private Key',
+          link: '/private-key',
+          warning: 'Not Recommended'
+        },
+        {
+          icon: '',
+          text: 'Keystore File (UTC/JSON)',
+          link: '/keystore',
+          warning: 'Not Recommended'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'ExtensionAddWallet.scss';
+</style>
