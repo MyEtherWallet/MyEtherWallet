3e4ede3d02fee420b171620a7fa5bdbe9ded8724
David Hong <hsurf22@gmail.com>
2019-04-17 18:35:31 -0700

    Add CDP confirmation modal

---
+++ src/dapps/MakerDai/components/DaiConfirmationModal/DaiConfirmationModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal-container">
+    <b-modal
+      ref="modal"
+      title="DAI Confirmation"
+      centered
+      class="bootstrap-modal bootstrap-modal-wide padding-40-20"
+      hide-footer
+    >
+      <div class="inputs-container"></div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    activeCdp: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'DaiConfirmationModal';
+</style>
