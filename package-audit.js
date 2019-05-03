993d9883f2a9b0cc89ac3640dc835f4737f3fe38
Kosala Hemachandra <kvhnuke@aol.com>
2019-05-03 15:03:17 -0700

    change npm to 6.5

--- package-audit.js
+++ package-audit.js
@@ -1,19 +1,24 @@
 const exec = require('child_process').exec;
 
 //tar package is not applicable to web and waiting for other packages to update
-const AUDIT_EXCEPTIONS = ['tar', 'js-yaml']
+const AUDIT_EXCEPTIONS = ['tar', 'js-yaml'];
 
 const execute = (command, callback) => {
-    exec(command, (error, stdout, stderr) => { callback(stdout); });
+  exec(command, (error, stdout, stderr) => {
+    callback(stdout);
+  });
 };
-execute('npm audit --json', (json) => {
-    const advisories = JSON.parse(json).advisories
-    let auditPass = true;
-    for (const id in advisories) {
-        if (advisories[id].severity === 'high' && !AUDIT_EXCEPTIONS.includes(advisories[id].module_name)) {
-            console.error("AUDIT Failed", advisories[id])
-            auditPass = false;
-        }
+execute('npm audit --json', json => {
+  const advisories = JSON.parse(json).advisories;
+  let auditPass = true;
+  for (const id in advisories) {
+    if (
+      advisories[id].severity === 'high' &&
+      !AUDIT_EXCEPTIONS.includes(advisories[id].module_name)
+    ) {
+      console.error('AUDIT Failed', advisories[id]);
+      auditPass = false;
     }
-    if (!auditPass) process.exit(1)
-})
+  }
+  if (!auditPass) process.exit(1);
+});
