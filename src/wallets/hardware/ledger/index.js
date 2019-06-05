a3711d72dcb892f24b686630c8de7eebc5b45e55
gamalielhere <gamalielgad1234@gmail.com>
2019-06-05 12:42:35 -0700

    Increase ledger listener timeout

--- src/wallets/hardware/ledger/index.js
+++ src/wallets/hardware/ledger/index.js
@@ -20,7 +20,7 @@ import errorHandler from './errorHandler';
 
 const NEED_PASSWORD = false;
 const OPEN_TIMEOUT = 10000;
-const LISTENER_TIMEOUT = 15000;
+const LISTENER_TIMEOUT = 30000;
 
 class ledgerWallet {
   constructor() {
