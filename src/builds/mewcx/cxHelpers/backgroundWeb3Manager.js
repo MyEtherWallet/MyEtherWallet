64b578cd2839331805802298251ff03bc688a184
gamalielhere <gamalielgad1234@gmail.com>
2019-06-12 14:45:59 -0700

    Inject web3 properly

--- src/builds/mewcx/cxHelpers/backgroundWeb3Manager.js
+++ src/builds/mewcx/cxHelpers/backgroundWeb3Manager.js
@@ -1,52 +1,43 @@
-import Web3 from 'web3';
-import { getMode } from '../../configs';
+// import Web3 from 'web3';
+// import { getMode } from '../../configs';
 const chrome = window.chrome;
-const web3 = new Web3('https://api.myetherwallet.com/eth');
-const injectWeb3 =
-  '(' +
-  function() {
-    window.web3 = web3;
-    window.web3.currentProvider.isMew = true;
-  } +
-  ')' +
-  '(' +
-  web3 +
-  ');';
+// const web3 = new Web3('https://api.myetherwallet.com/eth');
+// const injectWeb3 =
+//   '(' +
+//   function() {
+//     window.web3 = web3;
+//     window.web3.currentProvider.isMew = true;
+//   } +
+//   ')' +
+//   '(' +
+//   web3 +
+//   ');';
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  const useHash = getMode() === 'hash' ? '#' : '';
   if (request.msg === 'injectWeb3') {
-    if (
-      window.hasOwnProperty('web3') &&
-      !window.web3.currentPRovider.hasOwnProperty('isMew')
-    ) {
-      sendResponse({ response: 'Found a Web3 instance!!!!!!!' });
-      chrome.windows.create({
-        url: chrome.runtime.getURL(
-          `index.html${useHash}/extension-popups/web3-detected`
-        ),
-        type: 'popup',
-        height: 400,
-        width: 300,
-        focused: true
-      });
-    } else {
-      injector();
-      sendResponse({
-        response: 'REEEEEEEEEEEEE! SUCCESSFULLY ADDED WEB3 MY DUDES'
-      });
-    }
+    console.log('injector called');
+    const script = document.createElement('script');
+    script.src = chrome.extension.getURL('cxWeb3.js');
+    (document.head || document.documentElement).appendChild(script);
+    script.onload = function() {
+      script.parentNode.removeChild(script);
+    };
+    sendResponse({
+      response: 'REEEEEEEEEEEEE! SUCCESSFULLY ADDED WEB3 MY DUDES'
+    });
     return true;
   }
 });
 
-function injector() {
-  console.log('injector called');
-  const script = document.createElement('script');
-  script.textContent = injectWeb3;
-  (document.head || document.documentElement).appendChild(script);
-  script.remove();
-}
+// function injector() {
+//   console.log('injector called');
+//   const script = document.createElement('script');
+//   script.src = chrome.extension.getUrl('script.js');
+//   (document.head || document.documentElement).appendChild(script);
+//   script.onload = function() {
+//     script.parentNode.removeChild(script);
+//   };
+// }
 
 // import Web3 from 'web3';
 // import { getMode } from '../../configs';
---
+++ src/builds/mewcx/cxHelpers/cxWeb3.js
@@ -0,0 +1,22 @@
+import Web3 from 'web3';
+import { getMode } from '../../configs';
+const chrome = window.chrome;
+const useHash = getMode() === 'hash' ? '#' : '';
+if (
+  window.hasOwnProperty('web3') &&
+  !window.web3.currentProvider.hasOwnProperty('isMew')
+) {
+  chrome.windows.create({
+    url: chrome.runtime.getURL(
+      `index.html${useHash}/extension-popups/web3-detected`
+    ),
+    type: 'popup',
+    height: 400,
+    width: 300,
+    focused: true
+  });
+} else if (!window.hasOwnProperty('web3')) {
+  const web3 = new Web3('https://api.myetherwallet.com/eth');
+  window.web3 = web3;
+  web3.currentProvider.isMew = true;
+}
