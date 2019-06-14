39682093f2fd318a2e8e60b8ac5df37798580433
gamalielhere <gamalielgad1234@gmail.com>
2019-06-14 13:15:36 -0700

    Update package from greenkeeper, run lint

--- src/dapps/ManageENS/containers/CreateCommitmentContainer/CreateCommitmentContainer.vue
+++ src/dapps/ManageENS/containers/CreateCommitmentContainer/CreateCommitmentContainer.vue
@@ -44,7 +44,7 @@
 
 <script>
 import InterfaceBottomText from '@/components/InterfaceBottomText';
-import {mapState} from 'vuex';
+import { mapState } from 'vuex';
 
 export default {
   components: {
