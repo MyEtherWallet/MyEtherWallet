57c734c990199b798fc1f5874a71da6d09d4c572
SteveM <SteveM@myEtherWallet.com>
2019-06-18 15:50:09 -0700

    clean up one comment

--- src/store/actions.js
+++ src/store/actions.js
@@ -89,16 +89,16 @@ const createAndSignTx = function({ commit }, val) {
 };
 
 const decryptWallet = function({ commit, dispatch }, params) {
-  // if (params[0]) {
-  if (params[0].identifier === MEW_CONNECT) {
-    params[0].mewConnect().on('RtcClosedEvent', () => {
-      if (params[0].mewConnect().getConnectonState()) {
-        this._vm.$eventHub.$emit('mewConnectDisconnected');
-        dispatch('clearWallet');
-      }
-    });
+  if (params[0]) {
+    if (params[0].identifier === MEW_CONNECT) {
+      params[0].mewConnect().on('RtcClosedEvent', () => {
+        if (params[0].mewConnect().getConnectonState()) {
+          this._vm.$eventHub.$emit('mewConnectDisconnected');
+          dispatch('clearWallet');
+        }
+      });
+    }
   }
-  // }
 
   commit('DECRYPT_WALLET', params[0]);
   dispatch('setWeb3Instance', params[1]);
