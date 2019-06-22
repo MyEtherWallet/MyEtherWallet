MyEtherWallet/MyEtherWallet

commit d5d4764ae931b6a4062ddcffc86071a0233ac725

Generate common when building networks

src/wallets/hardware/keepkey/index.js
@@ -89,7 +89,9 @@ class KeepkeyWallet {
       accountPath = this.basePath + '/' + idx;
     }
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, {
+        common: store.commostore.state.network.confign
+      });
       const hexTx = getUint8Tx(tx);
       const networkId = tx.getChainId();
       hexTx.addressNList = bip32ToAddressNList(accountPath);

src/networks/index.js
@@ -1,12 +1,15 @@
 const platform = require('platform');
 import * as types from './types';
 import * as nodes from './nodes';
+import commonGenerator from '@/helpers/commonGenerator';
+
 const nodeList = {};
 Object.keys(types).forEach(key => {
   nodeList[types[key].name] = [];
 });
 
 Object.keys(nodes).forEach(key => {
+  nodes[key].config = commonGenerator(nodes[key]);
   if (nodes[key].service === nodes['ethmew'].service) {
     nodeList[nodes[key].type.name].splice(0, 0, nodes[key]);
   } else if (

src/wallets/hardware/bitbox/index.js
@@ -38,7 +38,7 @@ class BitBoxWallet {
   getAccount(idx) {
     const derivedKey = this.hdKey.derive('m/' + idx);
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, { common: store.state.network.config });
       const networkId = tx.getChainId();
       const result = await this.bitbox.signTransaction(
         this.basePath + '/' + idx,

src/wallets/hardware/ledger/index.js
@@ -60,40 +60,45 @@ class ledgerWallet {
       accountPath = this.basePath + '/' + idx;
     }
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, { common: store.state.network.config });
       const networkId = tx.getChainId();
       tx.raw[6] = networkId;
       tx.raw[7] = Buffer.from([]);
       tx.raw[8] = Buffer.from([]);
       const tokenInfo = byContractAddress('0x' + tx.to.toString('hex'));
-      if (tokenInfo) await this.ledger.provideERC20TokenInformation(tokenInfo);
-      const result = await this.ledger.signTransaction(
-        accountPath,
-        tx.serialize().toString('hex')
-      );
-
-      // EIP155 support. check/recalc signature v value.
-      let v = result.v;
-      const rv = parseInt(v, 16);
-      let cv = networkId * 2 + 35;
-      if (rv !== cv && (rv & cv) !== rv) {
-        cv += 1; // add signature v bit.
-      }
-      v = cv.toString(16);
-
-      tx.v = getBufferFromHex(v);
-      tx.r = getBufferFromHex(result.r);
-      tx.s = getBufferFromHex(result.s);
-      const signedChainId = calculateChainIdFromV(tx.v);
-      if (signedChainId !== networkId)
-        throw new Error(
-          'Invalid networkId signature returned. Expected: ' +
-            networkId +
-            ', Got: ' +
-            signedChainId,
-          'InvalidNetworkId'
+      try {
+        if (tokenInfo)
+          await this.ledger.provideERC20TokenInformation(tokenInfo);
+        const result = await this.ledger.signTransaction(
+          accountPath,
+          tx.serialize().toString('hex')
         );
-      return getSignTransactionObject(tx);
+
+        // EIP155 support. check/recalc signature v value.
+        let v = result.v;
+        const rv = parseInt(v, 16);
+        let cv = networkId * 2 + 35;
+        if (rv !== cv && (rv & cv) !== rv) {
+          cv += 1; // add signature v bit.
+        }
+        v = cv.toString(16);
+
+        tx.v = getBufferFromHex(v);
+        tx.r = getBufferFromHex(result.r);
+        tx.s = getBufferFromHex(result.s);
+        const signedChainId = calculateChainIdFromV(tx.v);
+        if (signedChainId !== networkId)
+          throw new Error(
+            'Invalid networkId signature returned. Expected: ' +
+              networkId +
+              ', Got: ' +
+              signedChainId,
+            'InvalidNetworkId'
+          );
+        return getSignTransactionObject(tx);
+      } catch (e) {
+        console.log(e);
+      }
     };
     const msgSigner = async msg => {
       const result = await this.ledger.signPersonalMessage(

src/wallets/hardware/secalot/index.js
@@ -36,7 +36,7 @@ class SecalotWallet {
   getAccount(idx) {
     const derivedKey = this.hdKey.derive('m/' + idx);
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, { common: store.state.network.config });
       const networkId = tx.getChainId();
       const result = await this.secalot.signTransactionAsync(
         this.basePath + '/' + idx,

src/wallets/hardware/trezor/index.js
@@ -38,7 +38,7 @@ class TrezorWallet {
   getAccount(idx) {
     const derivedKey = this.hdKey.derive('m/' + idx);
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, { common: store.state.network.config });
       const networkId = tx.getChainId();
       const options = {
         path: this.basePath + '/' + idx,

src/wallets/hybrid/MEWconnect/index.js
@@ -62,7 +62,9 @@ class MEWconnectWallet {
       return new Promise(resolve => {
         this.mewConnect.sendRtcMessage('signTx', JSON.stringify(tx));
         this.mewConnect.once('signTx', result => {
-          tx = new Transaction(sanitizeHex(result), { common: store.common });
+          tx = new Transaction(sanitizeHex(result), {
+            common: store.state.network.config
+          });
           const signedChainId = calculateChainIdFromV(tx.v);
           if (signedChainId !== networkId)
             throw new Error(

src/wallets/software/mnemonic/index.js
@@ -31,7 +31,7 @@ class MnemonicWallet {
   getAccount(idx) {
     const derivedKey = this.hdKey.derive(this.basePath + '/' + idx);
     const txSigner = async tx => {
-      tx = new Transaction(tx, { common: store.common });
+      tx = new Transaction(tx, { common: store.state.network.config });
       const networkId = tx.getChainId();
       tx.sign(derivedKey.privateKey);
       const signedChainId = calculateChainIdFromV(tx.v);

commit 079e693616b33d6c84712d6909e8e41aeb8368e3

keepkey: fix typo introduced in d5d4764ae
https://github.com/MyEtherWallet/MyEtherWallet/commit/d5d4764ae931b6a4062ddcffc86071a0233ac725#r34041005

src/wallets/hardware/keepkey/index.js
@@ -90,7 +90,7 @@ class KeepkeyWallet {
     }
     const txSigner = async tx => {
       tx = new Transaction(tx, {
-        common: store.commostore.state.network.confign
+        common: store.state.network.config
       });
       const hexTx = getUint8Tx(tx);
       const networkId = tx.getChainId();