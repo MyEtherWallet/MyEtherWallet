ae1a41b9dcceb3ca6e7083c13076f1bb380db098
gamalielhere <gamalielgad1234@gmail.com>
2019-06-24 19:02:49 -0700

    remove comment

--- src/builds/mewcx/cxHelpers/cxWeb3.js
+++ src/builds/mewcx/cxHelpers/cxWeb3.js
@@ -6,15 +6,6 @@ if (
 ) {
   const event = new CustomEvent(`web3${window.extensionID}web3Detected`);
   window.dispatchEvent(event);
-  // chrome.windows.create({
-  //   url: chrome.runtime.getURL(
-  //     `index.html${cxHelpers.buildMode()}/extension-popups/web3-detected`
-  //   ),
-  //   type: 'popup',
-  //   height: 400,
-  //   width: 300,
-  //   focused: true
-  // });
 } else if (!window.hasOwnProperty('web3')) {
   window.web3 = new web3(new MEWProvider('https://mainnet.infura.io/mew'));
   window.web3.currentProvider.isMew = true;
