2b37fda8c172092305eb1ad06f0be464cd589f1e
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-07-08 14:45:08 -0400

    added pocket provider for supported nets

---
+++ src/wallets/web3-provider/providers/pocket-provider.js
@@ -0,0 +1,45 @@
+import PocketRequestManger from './pocket-request-manager';
+import MiddleWare from '../middleware';
+import {
+  ethSendTransaction,
+  ethSignTransaction,
+  ethSign,
+  ethAccounts,
+  ethCoinbase,
+  ethGetTransactionCount,
+  ethGetTransactionReceipt,
+  ethGetBlockByNumber,
+  ethGetBlockNumber,
+  netVersion
+} from '../methods';
+class PoktProvider {
+  constructor(host, options, store, eventHub) {
+    const requestManager = new PocketRequestManger(host);
+    this.poktProvider = {
+      send: (payload, callback) => {
+        const req = {
+          payload,
+          store,
+          requestManager,
+          eventHub
+        };
+        const middleware = new MiddleWare();
+        middleware.use(ethSendTransaction);
+        middleware.use(ethSignTransaction);
+        middleware.use(ethGetTransactionCount);
+        middleware.use(ethGetTransactionReceipt);
+        middleware.use(ethSign);
+        middleware.use(ethAccounts);
+        middleware.use(ethCoinbase);
+        middleware.use(ethGetBlockByNumber);
+        middleware.use(ethGetBlockNumber);
+        middleware.use(netVersion);
+        middleware.run(req, callback).then(() => {
+          requestManager.provider.send(payload, callback);
+        });
+      }
+    };
+    return this.poktProvider;
+  }
+}
+export default PoktProvider;
