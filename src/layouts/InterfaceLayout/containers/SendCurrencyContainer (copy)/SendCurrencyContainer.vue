7632ffc253bf63c5aab5d80effc7b18651ef0f8b
David Hong <hsurf22@gmail.com>
2019-07-09 15:20:39 -0700

    Add dashboard container files(copy of SendCurrencyContainer) into interface layout

--- src/layouts/InterfaceLayout/containers/SendCurrencyContainer (copy)/SendCurrencyContainer.vue
+++ src/layouts/InterfaceLayout/containers/SendCurrencyContainer (copy)/SendCurrencyContainer.vue
@@ -1,16 +1,196 @@
 <template>
-  <div class="dashboard-container grid-container">
-    <b-row>
-      <b-col>
-        <div class="card">1111111111</div>
-      </b-col>
-      <b-col><div class="card">111ergerg1111111</div></b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <div class="card">1111111111</div>
-      </b-col>
-    </b-row>
+  <div class="send-currency-container">
+    <interface-container-title :title="$t('common.sendTx')" />
+    <div class="send-form">
+      <div class="form-block amount-to-address">
+        <div class="amount">
+          <div class="single-input-block">
+            <div class="title">
+              <h4>{{ $t('interface.sendTxType') }}</h4>
+            </div>
+            <currency-picker
+              :currency="tokensWithBalance"
+              :page="'sendEthAndTokens'"
+              :token="true"
+              :default="selectedCurrency !== '' ? selectedCurrency : {}"
+              @selectedCurrency="selectedCurrency = $event"
+            />
+          </div>
+          <div class="single-input-block">
+            <div class="title">
+              <h4>{{ $t('interface.sendTxAmount') }}</h4>
+              <p
+                class="title-button prevent-user-select"
+                @click="sendEntireBalance"
+              >
+                Entire Balance
+              </p>
+            </div>
+            <div class="the-form amount-number">
+              <input
+                v-validate="'min_value:0'"
+                v-model="value"
+                type="number"
+                placeholder="Amount"
+                min="0"
+                name="value"
+                step="any"
+              />
+              <i
+                :class="[
+                  !isValidAmount.valid || errors.has('value') ? 'not-good' : '',
+                  'fa fa-check-circle good-button'
+                ]"
+                aria-hidden="true"
+              />
+            </div>
+            <div
+              v-if="!isValidAmount.valid || errors.has('value')"
+              class="error-message-container"
+            >
+              <p>{{ isValidAmount.msg }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="to-address">
+          <div class="title">
+            <h4>
+              {{ $t('interface.sendTxToAddr') }}
+              <blockie
+                v-show="isValidAddress"
+                :address="hexAddress"
+                :size="8"
+                :scale="16"
+                width="32px"
+                height="32px"
+                class="blockie-image"
+              />
+            </h4>
+
+            <p
+              class="copy-button prevent-user-select"
+              @click="copyToClipboard('address')"
+            >
+              {{ $t('common.copy') }}
+            </p>
+          </div>
+          <div class="the-form address-block">
+            <input
+              v-ens-resolver="'address'"
+              ref="address"
+              v-model="address"
+              type="text"
+              name="name"
+              autocomplete="off"
+            />
+            <i
+              :class="[
+                isValidAddress && hexAddress.length !== 0 ? '' : 'not-good',
+                'fa fa-check-circle good-button'
+              ]"
+              aria-hidden="true"
+            />
+          </div>
+        </div>
+        <div class="tx-fee">
+          <div class="title">
+            <h4>
+              {{ $t('common.txFee') }}
+            </h4>
+            <p class="copy-button prevent-user-select" @click="openSettings">
+              {{ $t('common.edit') }}
+            </p>
+          </div>
+          <div class="fee-value">
+            <div class="gwei">
+              {{ gasPrice }} Gwei
+              <!--(Economic)-->
+            </div>
+            <div v-show="network.type.name === 'ETH'" class="usd">
+              Cost {{ txFeeEth }} ETH = ${{ convert }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="send-form advanced">
+      <div class="advanced-content">
+        <div class="toggle-button-container">
+          <h4>{{ $t('common.advanced') }}</h4>
+          <div class="toggle-button">
+            <span>{{ $t('interface.dataGas') }}</span>
+            <!-- Rounded switch -->
+            <div class="sliding-switch-white">
+              <label class="switch">
+                <input
+                  type="checkbox"
+                  @click="advancedExpand = !advancedExpand"
+                />
+                <span class="slider round" />
+              </label>
+            </div>
+          </div>
+        </div>
+        <div
+          :class="advancedExpand && 'input-container-open'"
+          class="input-container"
+        >
+          <div class="margin-container">
+            <div v-show="!isToken" class="the-form user-input">
+              <p>Add Data</p>
+              <input
+                v-model="data"
+                type="text"
+                placeholder="Add Data (e.g. 0x7834f874g298hf298h234f)"
+                autocomplete="off"
+              />
+              <i
+                :class="[
+                  isValidData ? '' : 'not-good',
+                  'fa fa-check-circle good-button'
+                ]"
+                aria-hidden="true"
+              />
+            </div>
+            <div class="the-form user-input">
+              <p>{{ $t('common.gasLimit') | capitalize }}</p>
+              <input
+                v-model="gasLimit"
+                :placeholder="$t('common.gasLimit')"
+                type="number"
+                min="0"
+                name
+              />
+              <i
+                :class="[
+                  isValidGasLimit ? '' : 'not-good',
+                  'fa fa-check-circle good-button'
+                ]"
+                aria-hidden="true"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="submit-button-container">
+      <div
+        :class="[
+          validInputs ? '' : 'disabled',
+          'submit-button large-round-button-green-filled'
+        ]"
+        @click="submitTransaction"
+      >
+        {{ $t('interface.sendTx') }}
+      </div>
+      <interface-bottom-text
+        :link-text="$t('interface.helpCenter')"
+        :question="$t('interface.haveIssues')"
+        link="https://kb.myetherwallet.com"
+      />
+    </div>
   </div>
 </template>
 
