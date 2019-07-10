b50cdee64443cdd11c60ed009a1d79b536ccc51e
David Hong <hsurf22@gmail.com>
2019-07-10 15:01:42 -0700

    Add HorizontalButtonGroup component

---
+++ src/components/HorizontalButtonGroup/HorizontalButtonGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="horizontal-button-group">buttons</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    greytitle: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'HorizontalButtonGroup.scss';
+</style>
