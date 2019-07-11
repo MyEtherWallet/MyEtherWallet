23b3a4b2e389d9a6b6c8cab56830513c3efa0a44
David Hong <hsurf22@gmail.com>
2019-07-10 17:35:38 -0700

    Add icon assets

--- src/components/HorizontalButtonGroup/HorizontalButtonGroup.vue
+++ src/components/HorizontalButtonGroup/HorizontalButtonGroup.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="horizontal-button-group">buttons</div>
+  <div class="horizontal-button-group">
+    <ul class="flex--row--align-center button-container">
+      <li v-for="button in buttons" :key="button.key">
+        <img :src="button.icon" />
+        <h4>{{ button.title }}</h4>
+        <p>{{ button.description }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import IconEns from '@/assets/images/icons/dashboard/domain.svg';
+import IconMaker from '@/assets/images/icons/dashboard/maker.svg';
+import IconDomain from '@/assets/images/icons/dashboard/domain.svg';
+
 export default {
   components: {},
   props: {
@@ -12,7 +24,25 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      buttons: [
+        {
+          title: 'Register ENS',
+          description: 'Register or buy your domain here',
+          icon: IconEns
+        },
+        {
+          title: 'Maker',
+          description: 'Collateralize & Generate DAI',
+          icon: IconMaker
+        },
+        {
+          title: 'Domain Sale',
+          description: ' Sale your domain here',
+          icon: IconDomain
+        }
+      ]
+    };
   },
   methods: {}
 };
