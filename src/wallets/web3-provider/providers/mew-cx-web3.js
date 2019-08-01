7905b75f50a4ed76814845b9e7d5291e22b6bd48
gamalielhere <gamalielgad1234@gmail.com>
2019-08-01 10:53:49 -0700

    Finish 1193 provider

--- src/wallets/web3-provider/providers/mew-cx-web3.js
+++ src/wallets/web3-provider/providers/mew-cx-web3.js
@@ -8,6 +8,11 @@ import {
   ethGetBlockNumber
 } from '../methods';
 import {
+  WEB3_ACCOUNT_CHANGE,
+  WEB3_NETWORK_CHANGE,
+  WEB3_CHAIN_CHANGE
+} from '@/builds/mewcx/cxHelpers/cxEvents.js';
+import {
   ethAccounts,
   ethCoinbase,
   ethSendTransaction,
@@ -60,43 +65,73 @@ class MewCxEthereum extends EventEmitter {
     this._id = 0;
     this._promises = {};
     this._connect();
-  }
 
-  send(method, params = []) {
-    if (!method || typeof method !== 'string') {
-      return new Error('Method is not a valid string.');
-    }
+    this.httpProvider = {
+      send: (method, params = []) => {
+        if (!method || typeof method !== 'string') {
+          return new Error('Method is not a valid string.');
+        }
 
-    if (!(params instanceof Array)) {
-      return new Error('Params is not a valid array.');
-    }
-    const id = this._id++;
-    const jsonrpc = '2.0';
-    const payload = { jsonrpc, id, method, params };
-    const requestManager = this._requestManager;
-    const req = {
-      payload,
-      requestManager
+        if (!(params instanceof Array)) {
+          return new Error('Params is not a valid array.');
+        }
+        const id = this._id++;
+        const jsonrpc = '2.0';
+        const payload = { jsonrpc, id, method, params };
+        const requestManager = this._requestManager;
+        const req = {
+          payload,
+          requestManager
+        };
+
+        const promise = new Promise((resolve, reject) => {
+          this._promises[payload.id] = { resolve, reject };
+        });
+        const cb = (e, res) => {
+          if (e !== null) this._promises[res.id].reject(e);
+          this._promises[res.id].resolve(res.result);
+        };
+
+        this.middleware.run(req, cb).then(() => {
+          this._requestManager.provider.send(req, cb);
+        });
+
+        return promise;
+      },
+      sendAsync: payload => {
+        return this.send(payload.method, payload.params);
+      },
+      on: this.on,
+      emit: this.emit,
+      removeListener: this.removeListener
     };
 
-    const promise = new Promise((resolve, reject) => {
-      this._promises[payload.id] = { resolve, reject };
-    });
-    const cb = (e, res) => {
-      if (e !== null) this._promises[res.id].reject(e);
-      this._promises[res.id].resolve(res.result);
-    };
-
-    this.middleware.run(req, cb).then(() => {
-      this._requestManager.provider.send(req, cb);
-    });
-
-    return promise;
+    return this.httpProvider;
   }
 
-  sendAsync(payload) {
-    return this.send(payload.method, payload.params);
+  setListeners() {
+    const id = window.extensionID;
+    window.addEventListener(
+      WEB3_ACCOUNT_CHANGE.replace('{{id}}', id),
+      eventRes => {
+        this.emit('accountsChanged', [eventRes.detail.account]);
+      }
+    );
+    window.addEventListener(
+      WEB3_NETWORK_CHANGE.replace('{{id}}', id),
+      eventRes => {
+        this.emit('networkChanged', eventRes.detail.network);
+      }
+    );
+    window.addEventListener(
+      WEB3_CHAIN_CHANGE.replace('{{id}}', id),
+      eventRes => {
+        this.emit('chainChanged', eventRes.detail.chain);
+      }
+    );
   }
+
+  // clearListeners() {}
 
   async _connect() {
     fetch(this.host, {
@@ -123,6 +158,7 @@ class MewCxEthereum extends EventEmitter {
 
   _emitClose(code, reason) {
     this.emit('close', code, reason);
+    this.removeAllListeners(); // not too sure about removing all listeners of this instance yet
   }
 }
 
