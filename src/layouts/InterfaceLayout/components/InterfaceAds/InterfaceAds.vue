0da1182709326788761ae50b6163986e462cf4af
David Hong <hsurf22@gmail.com>
2019-08-05 13:51:44 -0700

    Add sliding banner buttons

--- src/layouts/InterfaceLayout/components/InterfaceAds/InterfaceAds.vue
+++ src/layouts/InterfaceLayout/components/InterfaceAds/InterfaceAds.vue
@@ -1,20 +1,65 @@
 <template>
   <div>
-    <div class="twitter-container">
-      <div class="twitter-content">
-        <a
-          href="https://twitter.com/intent/follow?screen_name=myetherwallet"
-          onclick="window.open(this.href, 'twitter-share', 'width=580,height=296');return false;"
-          title="Share on Twitter"
+    <div class="">
+      <a
+        :title="$t('common.twitterFollow')"
+        href="https://twitter.com/intent/follow?screen_name=myetherwallet"
+        onclick="window.open(this.href, 'twitter-share', 'width=580,height=296');return false;"
+      >
+        <div
+          class="global__interface-block flex--row--align-center twitter-ad-block"
         >
-          <div class="twitter-container">
+          <p class="block-title">
+            {{ $t('common.twitterFollow') }}
+          </p>
+          <div class="margin--left--auto block-twitter">
             <i class="fa fa-twitter"></i>
-            <span>{{ $t('common.twitterFollow') }}</span>
           </div>
-        </a>
-      </div>
+        </div>
+      </a>
     </div>
-    <div class="bottom-image-container">
+    <b-carousel v-model="slide" :interval="4000" controls indicators>
+      <!-- Text slides with image -->
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=58"
+      ></b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img
+          slot="img"
+          class="d-block img-fluid w-100"
+          width="1024"
+          height="480"
+          src="https://picsum.photos/1024/480/?image=55"
+          alt="image slot"
+        />
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
+
+    <div class="global__interface-block--margin-top bottom-image-container">
       <div class="ad-container">
         <keep-alive :max="adDetails.length">
           <component
