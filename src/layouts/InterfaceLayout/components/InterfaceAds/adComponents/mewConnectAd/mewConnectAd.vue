590605ca0b040b4173d83434ad90a9715a73a69b
David Hong <hsurf22@gmail.com>
2019-08-06 14:36:15 -0700

    Add MEWconnect ad component

---
+++ src/layouts/InterfaceLayout/components/InterfaceAds/adComponents/mewConnectAd/mewConnectAd.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mew-connect-ad">
+    <a href="https://mewconnect.myetherwallet.com/#/" target="_blank">
+      <div class="content-block">
+        <img src="@/assets/images/ads/mewconnect.jpg" />
+        <div class="text-content">
+          <h3>MEWconnect</h3>
+          <p>Available for iOS<br />and Android</p>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'mewConnectAd.scss';
+</style>
