14673ed00d014a045697fade9eda731358868ebf
SteveM <SteveM@myEtherWallet.com>
2019-08-12 15:22:25 -0700

    make changes

--- src/layouts/InterfaceLayout/components/InterfaceAds/adComponents/mewConnectAd/mewConnectAd.vue
+++ src/layouts/InterfaceLayout/components/InterfaceAds/adComponents/mewConnectAd/mewConnectAd.vue
@@ -12,20 +12,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  components: {},
-  props: {},
-  data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  mounted() {},
-  methods: {}
-};
-</script>
-
 <style lang="scss" scoped>
 @import 'mewConnectAd.scss';
 </style>
