48b9297394b5e1275e97a4f87d53a647b33cbe20
Jessica Peng <jessicapeng@jessicas-mbp.lan>
2019-08-13 11:48:21 -0700

    linter changes

--- src/layouts/DappSubmissionLayout/containers/AboutYourDappContainer/AboutYourDappContainer.vue
+++ src/layouts/DappSubmissionLayout/containers/AboutYourDappContainer/AboutYourDappContainer.vue
@@ -1,33 +1,41 @@
 <template>
   <div class="about-dapp-container">
     <b-form>
-      <b-form-group class="form-label mt-2" label="DApp Name" label-for="dappName">
+      <b-form-group label-align="left" label="DApp Name" label-for="dappName">
         <b-form-input id="dappName" type="text" required></b-form-input>
       </b-form-group>
-      <b-form-group class="form-label" label="Category" label-for="dappCategory">
+      <b-form-group
+        label-align="left"
+        label="Category"
+        label-for="dappCategory"
+      >
         <b-form-select
           id="dappCategory"
           :options="categories"
           required
         ></b-form-select>
       </b-form-group>
-      <b-form-group class="form-label" label="Tags" label-for="dappTags">
+      <b-form-group label-align="left" label="Tags" label-for="dappTags">
         <b-form-input id="dappTags" type="text" required></b-form-input>
       </b-form-group>
-      <b-form-group class="form-label" label="Description">
+      <b-form-group label-align="left" label="Description">
         <b-form-textarea
           size="lg"
           placeholder="800 characters"
         ></b-form-textarea>
       </b-form-group>
-      <b-form-group class="form-label" label="DApp status" label-for="dappStatus">
+      <b-form-group
+        label-align="left"
+        label="DApp status"
+        label-for="dappStatus"
+      >
         <b-form-select
           id="dappStatus"
           :options="categories"
           required
         ></b-form-select>
       </b-form-group>
-      <b-form-group class="form-label" label="Mock user flow">
+      <b-form-group label-align="left" label="Mock user flow">
         <b-form-file
           v-model="file"
           :state="false"
@@ -36,14 +44,18 @@
         ></b-form-file>
       </b-form-group>
 
-      <b-form-group class="form-label" label="DApp Name" label-for="dappName">
+      <b-form-group label-align="left" label="DApp Name" label-for="dappName">
         <b-form-input id="dappName" type="text" required></b-form-input>
       </b-form-group>
-      <b-form-group class="form-label" label="DApp Name" label-for="dappName">
+      <b-form-group label-align="left" label="DApp Name" label-for="dappName">
         <b-form-input id="dappName" type="text" required></b-form-input>
       </b-form-group>
 
-      <b-form-group class="form-label" label="Category" label-for="dappCategory">
+      <b-form-group
+        label-align="left"
+        label="Category"
+        label-for="dappCategory"
+      >
         <b-form-select
           id="dappCategory"
           :options="categories"
