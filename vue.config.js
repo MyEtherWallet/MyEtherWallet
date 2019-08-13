0fbc1061e7be0b5b0fa1060b7fc58259540a329e
gamalielhere <gamalielgad1234@gmail.com>
2019-08-13 12:14:11 -0700

    Sync

--- vue.config.js
+++ vue.config.js
@@ -180,11 +180,11 @@ if (process.env.NODE_ENV === 'production') {
           'src/builds/web/public/manifest.json',
           'src/builds/web/public/robots.txt',
           'src/layouts/DappSubmissionLayout/components/InputComponent/index.js',
-'src/layouts/DappSubmissionLayout/components/InputComponent/InputComponent.scss',
-'src/layouts/DappSubmissionLayout/components/InputComponent/InputComponent.vue',
-'src/layouts/DappSubmissionLayout/containers/SummaryContainer/index.js',
-'src/layouts/DappSubmissionLayout/containers/SummaryContainer/SummaryContainer.scss',
-'src/layouts/DappSubmissionLayout/containers/SummaryContainer/SummaryContainer.vue',
+          'src/layouts/DappSubmissionLayout/components/InputComponent/InputComponent.scss',
+          'src/layouts/DappSubmissionLayout/components/InputComponent/InputComponent.vue',
+          'src/layouts/DappSubmissionLayout/containers/SummaryContainer/index.js',
+          'src/layouts/DappSubmissionLayout/containers/SummaryContainer/SummaryContainer.scss',
+          'src/layouts/DappSubmissionLayout/containers/SummaryContainer/SummaryContainer.vue'
         ]
       }
     })
