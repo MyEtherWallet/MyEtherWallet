a051e314668d341cec33224b10079a576bb57d22
Kosala Hemachandra <kvhnuke@aol.com>
2019-08-14 19:46:24 -0700

    set maxworkers

--- package-test.js
+++ package-test.js
@@ -1,7 +1,6 @@
 const package = require('./package.json');
 const packageJson = require('package-json');
 const SAFE_TIME = 1000 * 1 * 60 * 60 * 24 * 7; //7days
-// webpack 4.29.0 is conflicting with vue-cli and breaking hot reload [1-31-19]
 // babel-jest 24.0.0 is breaking all the tests [2-1-19]
 // 'web3', 'web3-core-helpers', 'web3-core-method', 'web3-utils' 1.0.0-beta.41 introduced breaking changes [2-4-19]
 //@xkeshi/vue-qrcode no longer maintained, forked out to mew
@@ -14,7 +13,6 @@ const EXCEPTIONS = [
   '@myetherwallet/mewconnect-web-client',
   'canvas',
   'ethereum-ens',
-  'webpack',
   'babel-jest',
   '@xkeshi/vue-qrcode',
   'multicoin-address-validator',
