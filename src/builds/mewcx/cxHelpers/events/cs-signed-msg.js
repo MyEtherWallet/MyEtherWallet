c7fbc429f34a49f9c3437626e308cdcff6c46266
gamalielhere <gamalielgad1234@gmail.com>
2019-08-19 16:07:58 -0700

    Make sure it works properly

--- src/builds/mewcx/cxHelpers/events/cs-signed-msg.js
+++ src/builds/mewcx/cxHelpers/events/cs-signed-msg.js
@@ -1,16 +1,13 @@
 /* eslint-disable no-undef */
-import { MEW_SIGNED_MSG } from '../cxEvents';
+import { MEW_SIGNED_MSG, WEB3_RECEIVE_SIGNED_MSG } from '../cxEvents';
 
-export default async ({ event, payload }, _, next) => {
+export default async ({ event, payload, id }, _, next) => {
   if (event !== MEW_SIGNED_MSG) return next();
   window.dispatchEvent(
-    new CustomEvent(
-      WEB3_RECEIVE_SIGNED_MSG.replace('{{id}}', window.extensionID),
-      {
-        detail: {
-          payload: payload
-        }
+    new CustomEvent(WEB3_RECEIVE_SIGNED_MSG.replace('{{id}}', id), {
+      detail: {
+        payload: payload
       }
-    )
+    })
   );
 };
--- src/builds/mewcx/cxHelpers/events/cs-tx-hash.js
+++ src/builds/mewcx/cxHelpers/events/cs-tx-hash.js
@@ -1,16 +1,13 @@
 /* eslint-disable no-undef */
-import { MEW_TX_HASH } from '../cxEvents';
+import { MEW_TX_HASH, WEB3_RECEIVE_TX_HASH } from '../cxEvents';
 
-export default async ({ event, payload }, _, next) => {
+export default async ({ event, payload, id }, _, next) => {
   if (event !== MEW_TX_HASH) return next();
   window.dispatchEvent(
-    new CustomEvent(
-      WEB3_RECEIVE_TX_HASH.replace('{{id}}', window.extensionID),
-      {
-        detail: {
-          payload: payload
-        }
+    new CustomEvent(WEB3_RECEIVE_TX_HASH.replace('{{id}}', id), {
+      detail: {
+        payload: payload
       }
-    )
+    })
   );
 };
