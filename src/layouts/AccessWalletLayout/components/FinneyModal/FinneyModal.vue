9f97809ba920442fa981aa749238f0dcc5f88911
David Hong <hsurf22@gmail.com>
2019-08-26 16:25:38 -0700

    Add new Finney bg images

--- src/layouts/AccessWalletLayout/components/FinneyModal/FinneyModal.vue
+++ src/layouts/AccessWalletLayout/components/FinneyModal/FinneyModal.vue
@@ -6,45 +6,16 @@
     class="bootstrap-modal nopadding"
     centered
   >
-    <div class="finney-modal-content">
-      <div class="content-container">
-        <div class="finney-img">
-          <img :src="finneyImg" />
-        </div>
-        <div class="finney-text-container">
-          <div class="scan-container">
-            <p>
-              To connect please scan the QR code from FinneyWallet app on your
-              FINNEY device
-            </p>
-          </div>
-          <div class="qr-container">
-            <div class="large">
-              <qrcode :value="qrcode" :options="{ size: 186 }" />
-            </div>
-            <div class="small">
-              <qrcode :value="qrcode" :options="{ size: 125 }" />
-            </div>
-          </div>
-          <div class="text-container">
-            <a
-              href="http://shop.sirinlabs.com?rfsn=2397639.54fdf&utm_source=refersion&utm_medium=affiliate&utm_campaign=2397639.54fdf"
-              target="blank"
-              rel="noopener noreferrer"
-            >
-              <span>
-                Get your
-              </span>
-              <h3>
-                FINNEY™
-              </h3>
-              <span>
-                now
-              </span>
-            </a>
-          </div>
-        </div>
+    <div class="finney-desktop">
+      <div class="modal-content-right">
+        <p>
+          To connect please scan the QR code from FinneyWallet app on your
+          FINNEY device
+        </p>
       </div>
+    </div>
+    <div v-if="false" class="finney-mobile">
+      mobile
     </div>
   </b-modal>
 </template>
