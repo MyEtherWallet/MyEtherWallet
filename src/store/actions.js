a3d8820c39c3dc20d42dc944e2e6c59acf7041f0
SteveM <SteveM@myEtherWallet.com>
2019-08-27 13:11:10 -0700

    handle param[0] as null in decryptWallet

--- src/store/actions.js
+++ src/store/actions.js
@@ -90,7 +90,7 @@ const createAndSignTx = function({ commit }, val) {
 };
 
 const decryptWallet = function({ commit, dispatch }, params) {
-  if (params[0]) {
+  if (params[0] !== undefined && params[0] !== null) {
     if (params[0].identifier === MEW_CONNECT) {
       params[0].mewConnect().on('RtcClosedEvent', () => {
         if (params[0].mewConnect().getConnectonState()) {
