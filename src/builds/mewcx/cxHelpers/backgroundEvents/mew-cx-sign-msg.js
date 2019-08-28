42cd3a7f50a00637c788bdcaae8eeaccd0980c92
gamalielhere <gamalielgad1234@gmail.com>
2019-08-28 16:50:26 -0700

    Fix sign

--- src/builds/mewcx/cxHelpers/backgroundEvents/mew-cx-sign-msg.js
+++ src/builds/mewcx/cxHelpers/backgroundEvents/mew-cx-sign-msg.js
@@ -3,6 +3,7 @@ import { CX_SIGN_MSG } from '../cxEvents';
 
 export default async ({ event, payload }, _, next) => {
   if (event !== CX_SIGN_MSG) return next();
+  console.log(payload);
   chrome.windows.create({
     url: chrome.runtime.getURL(
       `index.html#/extension-popups/sign-msg?url=${payload.url}&msgToSign=${payload.msgToSign}&address=${payload.address}`
