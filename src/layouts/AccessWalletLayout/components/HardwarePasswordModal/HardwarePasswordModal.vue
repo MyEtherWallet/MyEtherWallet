c7682968008b5e3ab509fda64136f0fc8f4493af
Jessica Peng <jessicap@myetherwallet.com>
2019-08-28 17:17:12 -0700

    Reverting veevalidate 3.0

--- src/layouts/AccessWalletLayout/components/HardwarePasswordModal/HardwarePasswordModal.vue
+++ src/layouts/AccessWalletLayout/components/HardwarePasswordModal/HardwarePasswordModal.vue
@@ -10,41 +10,40 @@
     @shown="focusInput"
   >
     <form class="password-form">
-      <validation-provider v-slot="{ errors }" rules="required">
-        <div class="input-container">
-          <input
-            ref="passwordInput"
-            :type="show ? 'text' : 'password'"
-            v-model="password"
-            name="Password"
-            autocomplete="off"
-          />
-          <img
-            v-if="show"
-            src="@/assets/images/icons/show-password.svg"
-            @click.prevent="switchViewPassword"
-          />
-          <img
-            v-if="!show"
-            src="@/assets/images/icons/hide-password.svg"
-            @click.prevent="switchViewPassword"
-          />
-        </div>
-        <p v-show="error !== ''" class="error">{{ error }}</p>
-        <p v-show="errors[0]" class="error">
-          {{ errors[0] }}
-        </p>
-        <button
-          :class="[
-            errors[0] || password.length === 0 ? 'disabled' : '',
-            'submit-button large-round-button-green-filled'
-          ]"
-          type="submit"
-          @click.prevent="unlockWallet"
-        >
-          {{ $t('accessWallet.unlock') }} {{ hardwareBrand }}
-        </button>
-      </validation-provider>
+      <div class="input-container">
+        <input
+          v-validate="'required'"
+          ref="passwordInput"
+          :type="show ? 'text' : 'password'"
+          v-model="password"
+          name="Password"
+          autocomplete="off"
+        />
+        <img
+          v-if="show"
+          src="@/assets/images/icons/show-password.svg"
+          @click.prevent="switchViewPassword"
+        />
+        <img
+          v-if="!show"
+          src="@/assets/images/icons/hide-password.svg"
+          @click.prevent="switchViewPassword"
+        />
+      </div>
+      <p v-show="error !== ''" class="error">{{ error }}</p>
+      <p v-show="errors.has('password')" class="error">
+        {{ errors.first('password') }}
+      </p>
+      <button
+        :class="[
+          errors.has('password') || password.length === 0 ? 'disabled' : '',
+          'submit-button large-round-button-green-filled'
+        ]"
+        type="submit"
+        @click.prevent="unlockWallet"
+      >
+        {{ $t('accessWallet.unlock') }} {{ hardwareBrand }}
+      </button>
     </form>
   </b-modal>
 </template>
--- src/layouts/CreateWalletLayout/components/CreateWalletInput/CreateWalletInput.vue
+++ src/layouts/CreateWalletLayout/components/CreateWalletInput/CreateWalletInput.vue
@@ -1,67 +1,63 @@
 <template>
-  <ValidationProvider
-    v-slot="{ errors }"
-    :rules="param === 'Json' ? 'required|min:9' : ''"
-    name="password"
-  >
-    <form class="user-input">
-      <!-- === MEW custom form ======================================== -->
-      <div
-        :class="fullWidth ? 'full-width' : ''"
-        class="mew-custom-form mew-custom-form__password "
-      >
-        <div class="user-input-field">
-          <input
-            :class="strengthClass"
-            :type="password.showPassword ? 'text' : 'password'"
-            :value="value"
-            v-model="password"
-            name="password"
-            placeholder="Please Enter At Least 9 Characters"
-            autocomplete="off"
-            @input="updateValue($event.target.value)"
-          />
-        </div>
-        <div
-          class="password-icons"
-          @click="password.showPassword = !password.showPassword"
-        >
-          <img
-            v-if="!password.showPassword"
-            class="hide-password"
-            src="~@/assets/images/icons/hide-password.svg"
-          />
-          <img
-            v-if="password.showPassword"
-            class="show-password"
-            src="~@/assets/images/icons/show-password.svg"
-          />
-        </div>
-
-        <p v-show="value.length > 0" class="passwd-strength">
-          Password strength: <span :class="strengthClass">{{ strength }}</span>
-        </p>
-        <p v-if="value.length > 0" class="passwd-strength">
-          {{ errors[0] }}
-        </p>
+  <form class="user-input">
+    <!-- === MEW custom form ======================================== -->
+    <div
+      :class="fullWidth ? 'full-width' : ''"
+      class="mew-custom-form mew-custom-form__password "
+    >
+      <div class="user-input-field">
+        <input
+          v-validate="param === 'Json' ? 'required|min:9' : ''"
+          :class="strengthClass"
+          :type="password.showPassword ? 'text' : 'password'"
+          :value="value"
+          name="password"
+          placeholder="Please Enter At Least 9 Characters"
+          autocomplete="off"
+          @input="updateValue($event.target.value)"
+        />
       </div>
-      <!-- === MEW custom form ======================================== -->
-      <button
-        v-if="showButton"
-        :class="[
-          errors[0] || value.length === 0 || strengthClass !== 'strong'
-            ? 'disabled'
-            : '',
-          'large-round-button-green-filled next-button'
-        ]"
-        type="submit"
-        @click.prevent="switcher(param)"
+      <div
+        class="password-icons"
+        @click="password.showPassword = !password.showPassword"
       >
-        {{ $t('common.next') }}
-        <img src="~@/assets/images/icons/right-arrow.png" />
-      </button>
-    </form>
-  </ValidationProvider>
+        <img
+          v-if="!password.showPassword"
+          class="hide-password"
+          src="~@/assets/images/icons/hide-password.svg"
+        />
+        <img
+          v-if="password.showPassword"
+          class="show-password"
+          src="~@/assets/images/icons/show-password.svg"
+        />
+      </div>
+
+      <p v-show="value.length > 0" class="passwd-strength">
+        Password strength: <span :class="strengthClass">{{ strength }}</span>
+      </p>
+      <p v-if="value.length > 0" class="passwd-strength">
+        {{ errors.first('password') }}
+      </p>
+    </div>
+    <!-- === MEW custom form ======================================== -->
+    <button
+      v-if="showButton"
+      :class="[
+        errors.has('password') ||
+        value.length === 0 ||
+        strengthClass !== 'strong'
+          ? 'disabled'
+          : '',
+        'large-round-button-green-filled next-button'
+      ]"
+      type="submit"
+      @click.prevent="switcher(param)"
+    >
+      {{ $t('common.next') }}
+      <img src="~@/assets/images/icons/right-arrow.png" />
+    </button>
+  </form>
 </template>
 
 <script>
@@ -101,7 +97,6 @@ export default {
   methods: {
     async updateValue(value) {
       const score = await zxcvbn(value).score;
-
       this.$emit('input', value);
       switch (score) {
         case 1:
