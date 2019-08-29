54288729af340d3bfe405bf6ae22b6f628a78446
gamalielhere <gamalielgad1234@gmail.com>
2019-08-28 17:36:32 -0700

    Get send tx to work like before

--- src/builds/mewcx/cxHelpers/contentScriptEvents/cs-errors.js
+++ src/builds/mewcx/cxHelpers/contentScriptEvents/cs-errors.js
@@ -6,12 +6,19 @@ import {
   WEB3_REJECT
 } from '../cxEvents';
 
-export default async ({ event, id }, _, next) => {
+export default async ({ event, payload, id }, _, next) => {
   if (
     event !== REJECT_MEW_CX_ACC &&
     event !== REJECT_MEW_TX_SIGN &&
     event !== REJECT_MEW_SIGN_MSG
   )
     return next();
-  window.dispatchEvent(new CustomEvent(WEB3_REJECT.replace('{{id}}', id), {}));
+  const obj = {
+    detail: payload
+  };
+
+  if (!payload) {
+    delete obj['detail'];
+  }
+  window.dispatchEvent(new CustomEvent(WEB3_REJECT.replace('{{id}}', id), obj));
 };
