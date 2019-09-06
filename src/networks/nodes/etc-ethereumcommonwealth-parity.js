cc43a7637a6f6072064a9bd71aa9b30a10f4ec99
Yaz Khoury <yaz.khoury@gmail.com>
2019-09-06 08:54:30 -0400

    fix: Return commonwealth nodes back

---
+++ src/networks/nodes/etc-ethereumcommonwealth-parity.js
@@ -0,0 +1,10 @@
+import { ETC } from '../types';
+export default {
+  type: ETC,
+  service: 'CW Parity',
+  url: 'https://etc-parity.0xinfra.com/',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
