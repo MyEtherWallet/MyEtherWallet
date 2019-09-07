be62ab89260eb3e66e59f4fa1319b9aebc6b1213
David Hong <hsurf22@gmail.com>
2019-09-06 22:34:09 -0700

    Add Mdcontainer component

---
+++ src/components/DecisionTree/components/MdContainer/MdContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    md
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MdContainer',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'MdContainer.scss';
+</style>
