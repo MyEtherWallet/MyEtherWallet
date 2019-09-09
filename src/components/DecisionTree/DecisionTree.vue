3c65ae6914e4f069629babf87813386846c1cc37
David Hong <hsurf22@gmail.com>
2019-09-09 16:13:14 -0700

    remove md

--- src/components/DecisionTree/DecisionTree.vue
+++ src/components/DecisionTree/DecisionTree.vue
@@ -11,9 +11,42 @@
       class="bootstrap-modal nopadding decision-tree-modal"
     >
       <div class="modal-contents">
-        <div>header</div>
-        <md-container />
-        <div>footer</div>
+        <div class="header">
+          <button @click="back()">
+            <i class="fa fa-angle-left" aria-hidden="true"></i>
+          </button>
+          <p>header</p>
+        </div>
+        <div class="search">search</div>
+
+        <div class="md-content">
+          <ul v-if="currentIndex.sub">
+            <li
+              v-for="(qa, key) in currentIndex.sub"
+              :key="key"
+              class="p-4 flex--row--align-center"
+            >
+              <p class="qa-title" @click="getSub(index[qa])">
+                {{ index[qa].title }}
+              </p>
+              <p class="ml-auto right-arrow">></p>
+            </li>
+          </ul>
+          <div v-else>
+            <md-container :md="mdToHtml(currentIndex.md)" />
+          </div>
+        </div>
+
+        <div class="footer">
+          <div class="help flex--row--align-center">
+            <p>Contact support</p>
+            <p class="ml-2 mr-2">|</p>
+            <p>Help center</p>
+          </div>
+          <button class="ml-auto" @click="top()">
+            <i class="fa fa-angle-left" aria-hidden="true"></i>
+          </button>
+        </div>
       </div>
     </b-modal>
   </div>
@@ -21,6 +54,8 @@
 
 <script>
 import MdContainer from './components/MdContainer';
+//import marked from 'marked';
+import qaIndex from './data/index.js';
 
 export default {
   name: 'DecisionTree',
@@ -29,7 +64,11 @@ export default {
   },
   props: {},
   data() {
-    return {};
+    return {
+      index: qaIndex,
+      currentIndex: qaIndex.ROOT,
+      historyStack: []
+    };
   },
   mounted() {
     this.toggle();
@@ -37,6 +76,23 @@ export default {
   methods: {
     toggle() {
       this.$refs.DecisionTree.toggle();
+    },
+    getSub(qa) {
+      this.historyStack.push(this.currentIndex);
+      this.currentIndex = qa;
+    },
+    back() {
+      if (this.historyStack.length > 0) {
+        this.currentIndex = this.historyStack.pop();
+      }
+    },
+    top() {
+      this.currentIndex = qaIndex.ROOT;
+      this.historyStack = [];
+    },
+    mdToHtml(md) {
+      return md;
+      //return marked(md);
     }
   }
 };
@@ -53,6 +109,14 @@ export default {
     width: 350px;
     position: fixed;
     right: 100px;
+  }
+
+  .modal-content {
+    border-radius: 10px;
+  }
+
+  .modal-body {
+    background-color: transparent;
   }
 }
 </style>
---
+++ src/components/DecisionTree/data/index.js
@@ -0,0 +1,63 @@
+import Find_and_install_the_MetaMask from './doc/Find_and_install_the_MetaMask';
+import MyEtherWallet_An_Introduction from './doc/MyEtherWallet_An_Introduction';
+
+export default {
+  /*
+  =====================================================================================
+  ROOT
+  =====================================================================================
+  */
+  ROOT: {
+    title: 'Have a question?',
+    sub: [
+      'Back_to_the_Basics_10_Tips_for_Beginners',
+      'Does_MEW_Support_BTC_or_other_Coins',
+      'How_to_Access_Your_Wallet',
+      'How_to_Create_a_Wallet'
+    ]
+  },
+
+  /*
+  =====================================================================================
+  How to create new wallet
+  =====================================================================================
+  */
+  Good_to_be_home: {
+    title: 'Good to be home',
+    sub: ['Do_you_have_a_dog', 'My_name_is_Great_man']
+  },
+  Back_to_the_Basics_10_Tips_for_Beginners: {
+    title: 'Back to the Basics: 10 Tips for Beginners',
+    sub: ['Good_to_be_home']
+  },
+  Does_MEW_Support_BTC_or_other_Coins: {
+    title: 'Does MEW Support BTC or other Coins?',
+    md: MyEtherWallet_An_Introduction
+  },
+  Are_we_going_to_be_okay: {
+    title: 'Are you going to be okay?',
+    md: MyEtherWallet_An_Introduction
+  },
+  Do_you_have_a_dog: {
+    title: 'Do you have a dog?',
+    md: MyEtherWallet_An_Introduction
+  },
+  My_name_is_Great_man: {
+    title: 'My name is Great man?',
+    md: Find_and_install_the_MetaMask
+  },
+
+  /*
+  =====================================================================================
+  How to access wallet
+  =====================================================================================
+  */
+  How_to_Access_Your_Wallet: {
+    title: 'How to Access Your Wallet',
+    sub: ['How_to_Create_a_Wallet', 'Does_MEW_Support_BTC_or_other_Coins']
+  },
+  How_to_Create_a_Wallet: {
+    title: 'How to Create a Wallet',
+    md: MyEtherWallet_An_Introduction
+  }
+};
