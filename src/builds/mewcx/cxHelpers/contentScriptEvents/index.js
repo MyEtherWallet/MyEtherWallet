752bd9cf87fe4b65c66c7550d223f82590138dbf
gamalielhere <gamalielgad1234@gmail.com>
2019-09-17 18:57:35 -0700

    Redoing eth_sub

--- src/builds/mewcx/cxHelpers/contentScriptEvents/index.js
+++ src/builds/mewcx/cxHelpers/contentScriptEvents/index.js
@@ -4,11 +4,15 @@ import csSelecctedAcc from './cs-selected-acc.js';
 import csSignedMsg from './cs-signed-msg.js';
 import csTxHash from './cs-tx-hash.js';
 import csWebInjectionSuccessful from './cs-web3-injection-successful.js';
+import csWeb3SubscriptionError from './cs-web3-subscription-error.js';
+import csWeb3SubscriptionSuccess from './cs-web3-subscription-success.js';
 export {
   csErrors,
   csInjectedWeb3,
   csSelecctedAcc,
   csSignedMsg,
   csTxHash,
-  csWebInjectionSuccessful
+  csWebInjectionSuccessful,
+  csWeb3SubscriptionError,
+  csWeb3SubscriptionSuccess
 };
--- src/wallets/web3-provider/cx-web3-methods/eth_subscribe.js
+++ src/wallets/web3-provider/cx-web3-methods/eth_subscribe.js
@@ -7,28 +7,36 @@ import {
 export default async ({ payload }, res, next) => {
   if (payload.method !== 'eth_subscribe') return next();
   const id = window.extensionID;
-  new Promise((resolve, reject) => {
-    const actualEvent = new CustomEvent(WEB3_SUBSCRIBE.replace('{{id}}', id), {
-      detail: payload
-    });
-    window.addEventListener(WEB3_SUBSCRIPTION.replace('{{id}}', id), res => {
-      console.log('what', res);
-      resolve(res.detail);
-    });
-    window.addEventListener(WEB3_REJECT, res => {
-      console.log('what', res);
-      if (res.detail) {
-        reject(new Error(res.detail));
-      } else {
-        reject(new Error('User cancelled request!'));
-      }
-    });
-    window.dispatchEvent(actualEvent);
-  })
-    .then(res => {
-      res(null, toPayload(payload.id, res.payload));
-    })
-    .catch(e => {
-      res(e);
-    });
+  const actualEvent = new CustomEvent(WEB3_SUBSCRIBE.replace('{{id}}', id), {
+    detail: payload
+  });
+  window.addEventListener(WEB3_SUBSCRIPTION.replace('{{id}}', id), response => {
+    // window.ethereum.emit('notification', response.detail);
+    console.log(response, 'just id');
+    res(null, toPayload(payload, response.detail));
+  });
+  window.addEventListener(
+    WEB3_SUBSCRIPTION.replace('{{id}}', id) + 'notification',
+    response => {
+      window.ethereum.emit('notification', response.detail);
+      console.log(response);
+    }
+  );
+  window.addEventListener(WEB3_REJECT, response => {
+    if (response.detail) {
+      res(new Error(response.detail));
+    } else {
+      res(new Error('User cancelled request!'));
+    }
+  });
+  window.dispatchEvent(actualEvent);
+  // new Promise((resolve, reject) => {
+  // })
+  //   .then(res => {
+  //     console.log(res, 'here???');
+  //     res(null, toPayload(payload.id, res.payload));
+  //   })
+  //   .catch(e => {
+  //     res(e);
+  //   });
 };
