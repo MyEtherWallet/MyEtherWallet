605a2010f7b5e4a1349f4ee0c10d23e4a9ec602b
Jessica Peng <jessicap@myetherwallet.com>
2019-09-20 13:25:48 -0700

    Code update

--- src/dapps/Ambrpay/Ambrpay.vue
+++ src/dapps/Ambrpay/Ambrpay.vue
@@ -6,24 +6,37 @@
         <h3 class="page-title">Ambrpay</h3>
 
         <div class="btns-container">
-          <b-button class="withdraw-btn">Withdraw Funds</b-button>
-          <b-button class="add-btn">Add Funds</b-button>
+          <b-button
+            class="withdraw-btn"
+            @click="openManageFundsModal('Withdraw')"
+            >Withdraw Funds</b-button
+          >
+          <b-button class="add-btn" @click="openManageFundsModal('Add')"
+            >Add Funds</b-button
+          >
         </div>
       </div>
       <div class="funds-container">
         <span class="fund-text">Subscriptions Balance</span>
         <div>
-          <span class="fund-text">{{ fundsBalance }} ETH </span>
+          <span class="fund-text">{{ availableBalanceEth }} ETH </span>
           <span class="usd-text">USD</span>
         </div>
       </div>
       <subscription-form></subscription-form>
     </b-container>
+    <manage-funds-modal
+      ref="manageFunds"
+      :manage-funds-text="manageFundsText"
+      :availabe-balance-eth="availableBalanceEth"
+      :availabe-balance-usd="availableBalanceUSD"
+    />
   </div>
 </template>
 
 <script>
 import SubscriptionForm from './containers/SubscriptionForm';
+import ManageFundsModal from './components/ManageFundsModal';
 import Ambrpay from './AmbrpayModified';
 import BackButton from '@/layouts/InterfaceLayout/components/BackButton';
 import { mapState } from 'vuex';
@@ -31,11 +44,14 @@ import { mapState } from 'vuex';
 export default {
   components: {
     'subscription-form': SubscriptionForm,
-    'back-button': BackButton
+    'back-button': BackButton,
+    'manage-funds-modal': ManageFundsModal
   },
   data() {
     return {
-      fundsBalance: 0
+      availableBalanceEth: 0,
+      availableBalanceUSD: 0,
+      manageFundsText: ''
     };
   },
   computed: {
@@ -56,7 +72,7 @@ export default {
       ambrpay
         .getSubscriptionFunds()
         .then(res => {
-          this.fundsBalance = res;
+          this.availableBalanceEth = res;
           // eslint-disable-next-line
           console.log('res', res);
         })
@@ -64,6 +80,11 @@ export default {
           // eslint-disable-next-line
           console.log('err', err);
         });
+    },
+    openManageFundsModal(str) {
+      this.manageFundsText = str;
+      console.error('in here');
+      this.$refs.manageFunds.$refs.manageFundsModal.show();
     }
   }
 };
---
+++ src/dapps/Ambrpay/components/ManageFundsModal/ManageFundsModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal-container">
+    <b-modal
+      ref="manageFundsModal"
+      :title="`${manageFundsText} Funds`"
+      class="bootstrap-modal"
+      centered
+      hide-footer
+      static="true"
+      lazy="true"
+    >
+      <div class="modal-contents">
+        <div class="balance-container">
+          <span class="balance-txt">Available Balance</span>
+          <div>
+            <span>{{ availableBalanceEth }}</span>
+            <span>{{ availableBalanceUSD }}</span>
+          </div>
+        </div>
+        <hr />
+        <div class="btn-container">
+          <button class="large-round-button-green-filled">
+            {{ manageFundsText }}
+          </button>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    manageFundsText: {
+      type: String,
+      default: ''
+    },
+    availableBalanceEth: {
+      type: Number,
+      default: 0
+    },
+    availableBalanceUSD: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'ManageFundsModal.scss';
+</style>
