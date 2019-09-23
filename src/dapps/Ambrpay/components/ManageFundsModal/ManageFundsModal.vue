612ba39d3ea3933c4028544350d847f4ccd13d2f
Jessica Peng <jessicap@myetherwallet.com>
2019-09-23 14:46:47 -0700

    Code update

--- src/dapps/Ambrpay/components/ManageFundsModal/ManageFundsModal.vue
+++ src/dapps/Ambrpay/components/ManageFundsModal/ManageFundsModal.vue
@@ -6,21 +6,39 @@
       class="bootstrap-modal"
       centered
       hide-footer
-      static="true"
-      lazy="true"
+      static
+      lazy
     >
       <div class="modal-contents">
         <div class="balance-container">
-          <span class="balance-txt">Available Balance</span>
+          <span class="funds-txt">Available Balance</span>
           <div>
-            <span>{{ availableBalanceEth }}</span>
-            <span>{{ availableBalanceUSD }}</span>
+            <p class="funds-txt">{{ availableBalanceEth }} ETH</p>
+            <p>{{ availableBalanceUSD }}</p>
           </div>
         </div>
         <hr />
+        <div v-if="actionStep" class="action-container">
+          <p class="funds-txt">
+            How many ETH you want to
+            <span class="action-txt"> {{ manageFundsText }}? </span>
+          </p>
+          <input id="" class="mt-3" type="text" name="" />
+        </div>
+        <div v-if="!actionStep" class="confirmation-container">
+          <i class="check-icon fa fa-check" aria-hidden="true" />
+          <p v-if="manageFundsText === 'Add'" class="mr-5 ml-5 mt-3 mb-1">
+            Your subscription fund <span class="mew-txt">(1 ETH)</span> should
+            be updated within the next miunte.
+          </p>
+          <p v-if="manageFundsText === 'Withdraw'" class="mr-5 ml-5 mt-2">
+            Your withdraw fund <span class="mew-txt"> (0.0031 ETH)</span> should
+            be in your wallet within the next miunte.
+          </p>
+        </div>
         <div class="btn-container">
-          <button class="large-round-button-green-filled">
-            {{ manageFundsText }}
+          <button class="mew-btn mt-4 mb-4">
+            {{ actionStep ? manageFundsText : 'OK' }}
           </button>
         </div>
       </div>
@@ -44,10 +62,22 @@ export default {
       default: 0
     }
   },
+  data() {
+    return {
+      actionStep: false
+    };
+  },
   methods: {}
 };
 </script>
 
 <style lang="scss" scoped>
 @import 'ManageFundsModal.scss';
+@import '../../AmbrpayGlobal.scss';
+</style>
+
+<style lang="scss">
+.modal-body {
+  padding: 0;
+}
 </style>
--- src/dapps/Ambrpay/containers/SubscriptionForm/SubscriptionForm.vue
+++ src/dapps/Ambrpay/containers/SubscriptionForm/SubscriptionForm.vue
@@ -1,28 +1,52 @@
 <template>
-  <div class="ambrpay-container">
-    <div class="ambrpay-content">
-      <div class="ambrpay-form-container">
+  <div class="subscription-container">
+    <div class="subscription-content">
+      <div class="subscription-form-container">
         <b-container>
           <b-row>
             <span class="label-text">To Address</span>
-            <p class="action-text prevent-user-select" @click="sdfasdf()">
+            <p class="action-text prevent-user-select" @click="copyToClipboard">
               {{ $t('common.copy') }}
             </p>
           </b-row>
           <b-row>
-            <input ref="address" type="text" />
+            <blockie
+              v-show="isValidAddress"
+              :address="address"
+              :size="6"
+              :scale="16"
+              width="32px"
+              height="32px"
+              class="blockie-image"
+            />
+            <input
+              v-ens-resolver="'address'"
+              ref="address"
+              v-model="address"
+              name="name"
+              autocomplete="off"
+              type="text"
+            />
           </b-row>
           <b-row>
             <b-col cols="12" md="5">
               <span class="label-text">Type</span>
-              <input id="" type="text" name="" />
+              <div class="fake-input">
+                <p>
+                  <img
+                    class="currency-icon"
+                    src="@/assets/images/currency/eth.svg"
+                  />
+                  <span class="token-txt">ETH</span>-Ethereum
+                </p>
+              </div>
             </b-col>
             <b-col class="amount-container" cols="12" md="7">
               <div class="amount-text">
                 <span class="label-text">Amount</span>
-                <span class="action-text">Copy Balance</span>
+                <span class="action-text">Entire Balance</span>
               </div>
-              <input type="text" />
+              <input type="text" placeholder="0.0000" />
               <p class="fee-text">1% automation-fee will be added on top</p>
             </b-col>
           </b-row>
@@ -30,13 +54,8 @@
             <span class="label-text">Interval in Every</span>
             <input type="text" placeholder="Day(s)" />
           </b-row>
-          <b-row class="mt-4">
+          <b-row class="mt-5">
             <b-button class="mx-auto mew-btn">Start Recurring</b-button>
-          </b-row>
-          <b-row>
-            <b-button class="mx-auto active-sub-btn"
-              >My Active Subscriptions</b-button
-            >
           </b-row>
         </b-container>
       </div>
@@ -49,7 +68,8 @@ import Blockie from '@/components/Blockie';
 import StandardInput from '@/components/StandardInput';
 import { mapState } from 'vuex';
 import { canBeConvertedToWei } from '../../AmbrpayHelpers';
-// import BigNumber from 'bignumber.js';
+import { Toast } from '@/helpers';
+import { isAddress } from '@/helpers/addressUtils';
 
 export default {
   components: {
@@ -58,6 +78,7 @@ export default {
   },
   data() {
     return {
+      address: '',
       amount: 0,
       amountInputOptions() {
         return {
@@ -69,7 +90,10 @@ export default {
     };
   },
   computed: {
-    ...mapState(['web3'])
+    ...mapState(['web3']),
+    isValidAddress() {
+      return isAddress(this.address);
+    }
   },
   isValidAmount() {
     if (!canBeConvertedToWei(this.web3, this.amount.toString())) return false;
@@ -77,9 +101,10 @@ export default {
     // const enteredAmount = new BigNumber(this.amount);
   },
   methods: {
-    copyToClipboard(ref) {
-      this.$refs[ref].select();
+    copyToClipboard() {
+      this.$refs.address.select();
       document.execCommand('copy');
+      Toast.responseHandler('Copied', Toast.INFO);
     }
   }
 };
