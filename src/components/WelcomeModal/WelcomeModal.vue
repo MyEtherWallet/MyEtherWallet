b986f5bbc110d940dd7089d53fd005a039bf6a89
olgakup <olga.kupchevskaya.812@my.csun.edu>
2019-10-04 13:09:27 -0700

    refactor: welcome module strings

--- src/components/WelcomeModal/WelcomeModal.vue
+++ src/components/WelcomeModal/WelcomeModal.vue
@@ -14,27 +14,27 @@
       <img src="@/assets/images/modal/garlands.png" alt />
       <div class="welcome-modal-container">
         <div class="welcome-modal-text">
-          <h3>{{ $t('home.welcomeTov5') }}</h3>
-          <i18n path="home.welcomeToV5Desc1" tag="p" for="home.welcomeToLink1">
+          <h3>{{ $t('home.welcome.title') }}</h3>
+          <i18n path="home.welcome.desc1" tag="p" for="home.welcome.link1">
             <a
               href="https://medium.com/@myetherwallet/lets-make-it-official-mew5-myetherwallet-s-all-new-interface-is-here-2063117180a4"
               target="_blank"
               rel="noopener noreferrer"
-              >{{ $t('home.welcomeToLink1') }}</a
+              >{{ $t('home.welcome.link1') }}</a
             >
           </i18n>
-          <i18n path="home.welcomeToV5Desc2" tag="p" for="home.welcomeToLink2">
+          <i18n path="home.welcome.desc2" tag="p" for="home.welcome.link2">
             <a
               aria-label="Vintage MyEtherWallet"
               href="https://vintage.myetherwallet.com"
               target="_blank"
               rel="noopener noreferrer"
-              >{{ $t('home.welcomeToLink2') }}</a
+              >{{ $t('home.welcome.link2') }}</a
             >
           </i18n>
         </div>
         <div class="welcome-modal-button">
-          <button @click="closeModal">{{ $t('home.onwards') }}!</button>
+          <button @click="closeModal">{{ $t('home.welcome.button-onwards') }}!</button>
         </div>
       </div>
     </div>
