150343dc3817bef671940372d8b1dca35153b058
Kosala Hemachandra <kvhnuke@aol.com>
2019-10-10 16:04:03 -0700

    add values in interact contract

--- src/layouts/InterfaceLayout/containers/InteractWithContractContainer/InteractWithContractContainer.vue
+++ src/layouts/InterfaceLayout/containers/InteractWithContractContainer/InteractWithContractContainer.vue
@@ -44,9 +44,9 @@
             <h4>{{ $t('interface.abiJsonInt') }}</h4>
             <div class="copy-buttons">
               <span @click="deleteInput('abi')">{{ $t('common.clear') }}</span>
-              <span @click="copyToClipboard('abi')">{{
-                $t('common.copy')
-              }}</span>
+              <span @click="copyToClipboard('abi')">
+                {{ $t('common.copy') }}
+              </span>
             </div>
           </div>
         </div>
@@ -211,9 +211,9 @@
                 :key="item.name + idx"
                 class="result-container"
               >
-                <label :for="item.name !== '' ? item.name : item.type + idx">{{
-                  item.name !== '' ? item.name : item.type | capitalize
-                }}</label>
+                <label :for="item.name !== '' ? item.name : item.type + idx">
+                  {{ item.name !== '' ? item.name : item.type | capitalize }}
+                </label>
                 <input
                   :name="item.name !== '' ? item.name : item.type + idx"
                   :value="result[idx]"
@@ -249,12 +249,12 @@
             ]"
             @click="write"
           >
-            <span v-show="!loading && !selectedMethod.constant">
-              {{ $t('interface.write') }}
-            </span>
-            <span v-show="!loading && selectedMethod.constant">{{
-              $t('interface.read')
+            <span v-show="!loading && !selectedMethod.constant">{{
+              $t('interface.write')
             }}</span>
+            <span v-show="!loading && selectedMethod.constant">
+              {{ $t('interface.read') }}
+            </span>
             <i v-show="loading" class="fa fa-spinner fa-spin fa-lg" />
           </div>
         </div>
@@ -347,6 +347,9 @@ export default {
         });
       }
       return _contractArgs;
+    },
+    txValue() {
+      return Misc.sanitizeHex(unit.toWei(this.value, 'ether').toString(16));
     }
   },
   watch: {
@@ -467,7 +470,10 @@ export default {
         const gasLimit = await contract.methods[this.selectedMethod.name](
           ...this.contractArgs
         )
-          .estimateGas({ from: this.account.address.toLowerCase() })
+          .estimateGas({
+            from: this.account.address.toLowerCase(),
+            value: this.txValue
+          })
           .then(res => {
             return res;
           })
@@ -486,7 +492,7 @@ export default {
             gas: gasLimit,
             nonce: nonce,
             gasPrice: Number(unit.toWei(this.gasPrice, 'gwei')),
-            value: 0,
+            value: this.txValue,
             to: this.address.toLowerCase(),
             data: data
           };
