1e71f1a4a6a816bb78cbda821672c10e53184bc5
David Hong <hsurf22@gmail.com>
2019-10-30 15:58:13 -0700

    Add header icons

--- src/containers/HeaderContainer/components/HeaderDesktop/HeaderDesktop.vue
+++ src/containers/HeaderContainer/components/HeaderDesktop/HeaderDesktop.vue
@@ -7,30 +7,39 @@
         <v-spacer></v-spacer>
 
         <v-toolbar-items>
-          <v-menu open-on-hover bottom offset-y>
+          <v-menu open-on-hover bottom offset-y transition="fade-transition">
             <template v-slot:activator="{ on }">
               <v-btn text dark class="mx-3" v-on="on">
                 How it works
               </v-btn>
             </template>
           </v-menu>
-          <v-menu open-on-hover bottom offset-y>
+          <v-menu open-on-hover bottom offset-y transition="fade-transition">
             <template v-slot:activator="{ on }">
               <v-btn class="mx-3" text dark v-on="on">
                 About<MenuOpenArrow
               /></v-btn>
             </template>
-            <div class="white">
+            <div class="white pa-5">
               <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title>Single-line item</v-list-item-title>
+                  <v-list-item-title>Company</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
 
-              <v-list-item two-line>
+              <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title>Two-line item</v-list-item-title>
-                  <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+                  <v-list-item-title>Team</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>Why MEW</v-list-item-title>
+                  <v-list-item-subtitle
+                    >Let us show you why you should use
+                    MEW.</v-list-item-subtitle
+                  >
                 </v-list-item-content>
               </v-list-item>
 
@@ -44,20 +53,42 @@
               </v-list-item>
             </div>
           </v-menu>
-          <v-menu open-on-hover bottom offset-y>
+          <v-menu open-on-hover bottom offset-y transition="fade-transition">
             <template v-slot:activator="{ on }">
               <v-btn class="mx-3" text dark v-on="on">
                 Tools<MenuOpenArrow
               /></v-btn>
             </template>
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>aaaa</v-list-item-title>
-                <v-list-item-title>bbbb</v-list-item-title>
-              </v-list-item>
-            </v-list>
+            <div class="white pa-5">
+              <v-row>
+                <v-col>
+                  <div class="font-weight-bold text-uppercase">Popular</div>
+                  <v-divider></v-divider>
+                  <div>
+                    <v-list>
+                      <v-list-item-group v-model="item" color="primary">
+                        <v-list-item>
+                          <v-list-item-icon>
+                            <v-img :src="icons.wallet" />
+                          </v-list-item-icon>
+                          <v-list-item-content>
+                            <v-list-item-title
+                              >View wallet info</v-list-item-title
+                            >
+                          </v-list-item-content>
+                        </v-list-item>
+                      </v-list-item-group>
+                    </v-list>
+                  </div>
+                </v-col>
+                <v-col>
+                  <div class="font-weight-bold text-uppercase">Advanced</div>
+                  <v-divider></v-divider>
+                </v-col>
+              </v-row>
+            </div>
           </v-menu>
-          <v-menu open-on-hover bottom offset-y>
+          <v-menu open-on-hover bottom offset-y transition="fade-transition">
             <template v-slot:activator="{ on }">
               <v-btn class="mx-3" text dark v-on="on">
                 Support
@@ -83,12 +114,19 @@
 <script>
 import MenuOpenArrow from '@/components/Common/MenuOpenArrow';
 
+import wallet from '@/assets/images/common/icons/iconset1/wallet.svg';
+import eth from '@/assets/images/common/icons/iconset1/eth.svg';
+
 export default {
   name: 'HeaderDesktop',
   components: {
     MenuOpenArrow
   },
   data: () => ({
+    icons: {
+      wallet: wallet,
+      eth: eth
+    },
     menuItems: [
       {
         title: 'How it works'
@@ -107,4 +145,8 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.theme--dark.v-btn:hover::before {
+  opacity: 0;
+}
+</style>
