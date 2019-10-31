e23c1ddf2146453c9e91a102ba7d3b0226ebdd55
David Hong <hsurf22@gmail.com>
2019-10-31 16:37:51 -0700

    feature title update

--- src/layouts/HomeLayout/components/Common/BlockTitle/BlockTitle.vue
+++ src/layouts/HomeLayout/components/Common/BlockTitle/BlockTitle.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="block-title">title</div>
+  <div class="block-title" :class="center ? 'text-center' : ''">
+    <div
+      class="title text-uppercase font-weight-bold grey--text text--lighten-1"
+    >
+      {{ title }}
+    </div>
+    <div class="display-1 font-weight-bold">{{ description }}</div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BlockTitle'
+  name: 'BlockTitle',
+  props: {
+    title: { default: '', type: String },
+    description: { default: '', type: String },
+    center: { default: false, type: Boolean }
+  },
+  data: () => ({})
 };
 </script>
 
 <style lang="scss" scoped>
 @import '@/assets/styles/GlobalVariables';
+
+.subtitle-1 {
+  letter-spacing: -0.3px;
+}
 </style>
--- src/layouts/HomeLayout/components/Features/Features.vue
+++ src/layouts/HomeLayout/components/Features/Features.vue
@@ -1,18 +1,23 @@
 <template>
   <v-container>
-    <div class="home-features">features</div>
+    <BlockTitle
+      title="Features"
+      description="All-in-one gateway to your Ethereum Blockchain service"
+    />
   </v-container>
 </template>
 
 <script>
 //import Flexbox from '@/components/Common/Flexbox';
 //import StdButton from '@/components/StdButton';
+import BlockTitle from '../Common/BlockTitle';
 
 export default {
   name: 'HomeFeatures',
   components: {
     //Flexbox,
-    //StdButton
+    //StdButton,
+    BlockTitle
   }
 };
 </script>
