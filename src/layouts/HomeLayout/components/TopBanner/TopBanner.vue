e9ab781c55b9be6a789c235ac62f93104181c002
David Hong <hsurf22@gmail.com>
2019-10-30 18:09:39 -0700

    Update front page top banner

---
+++ src/layouts/HomeLayout/components/TopBanner/TopBanner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="light-blue darken-4 top-banner">
+    <div class="banner-container pointer-event--none user-select--none">
+      <img
+        class="banner-img"
+        src="@/assets/images/banners/homepage-top-banner.svg"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeContainer'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/GlobalVariables';
+
+.banner-container {
+  overflow: hidden;
+}
+
+.banner-img {
+  margin-top: -160px;
+  width: 100%;
+}
+</style>
