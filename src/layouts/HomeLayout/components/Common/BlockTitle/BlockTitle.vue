d85f529d3f9878fe4f0446bb427d03aaf147eb8c
David Hong <hsurf22@gmail.com>
2019-10-31 15:53:16 -0700

    Add home features block

---
+++ src/layouts/HomeLayout/components/Common/BlockTitle/BlockTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="block-title">title</div>
+</template>
+
+<script>
+export default {
+  name: 'BlockTitle'
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/GlobalVariables';
+</style>
