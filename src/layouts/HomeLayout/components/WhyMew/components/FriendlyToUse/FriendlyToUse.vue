94e594b2549db4ce27e8a5ff406dc2f85f85258d
David Hong <hsurf22@gmail.com>
2019-11-04 13:39:32 -0800

    Add why mew contents

---
+++ src/layouts/HomeLayout/components/WhyMew/components/FriendlyToUse/FriendlyToUse.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <LinkBox>
+      <div class="">
+        <v-img
+          class="white--text align-end"
+          height="150px"
+          contain
+          src="@/assets/images/Etc/LandingPage/friendly.svg"
+        >
+        </v-img>
+      </div>
+      <div>Friendly to use</div>
+      <div>
+        Access the Ethereum blockchain's original and most-trusted wallet
+        client, now with a host of new features all contained in an elegant,
+        easy and friendly interface.
+      </div>
+      <div>
+        <v-btn color="orange" text>
+          Explore
+        </v-btn>
+      </div>
+    </LinkBox>
+  </div>
+</template>
+
+<script>
+import LinkBox from '../Common/LinkBox';
+
+export default {
+  name: 'FriendlyToUse',
+  components: { LinkBox }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/GlobalVariables';
+</style>
