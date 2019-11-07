82d9941ca0507fe161662652e02292dbea60b84d
David Hong <hsurf22@gmail.com>
2019-11-07 14:58:17 -0800

    uUpdate partners

--- src/layouts/HomeLayout/components/Partners/Partners.vue
+++ src/layouts/HomeLayout/components/Partners/Partners.vue
@@ -7,16 +7,36 @@
         description="Connect with the powerful crypto communities"
       />
       <v-row>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
-        <v-col cols="2">1</v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Ledger.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Trezor.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Bitbox.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Secalot.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Bity.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Sirin.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Keepkey.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Kyber.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Changelly.png" />
+        </v-col>
+        <v-col cols="3">
+          <img src="@/assets/images/Etc/LandingPage/Partners/Simplex.png" />
+        </v-col>
       </v-row>
     </v-container>
   </div>
