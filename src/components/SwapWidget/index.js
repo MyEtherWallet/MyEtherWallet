9fb3433894f829c52e87888d0a90de7450b9c2fe
Jessica <jessicap@myetherwallet.com>
2019-11-12 16:03:22 -0800

    lint

--- src/components/SwapWidget/index.js
+++ src/components/SwapWidget/index.js
@@ -1,1 +1,3 @@
-export { default } from '@/layouts/InterfaceLayout/containers/SwapContainer/components/SwapWidget';
+export {
+  default
+} from '@/layouts/InterfaceLayout/containers/SwapContainer/components/SwapWidget';
