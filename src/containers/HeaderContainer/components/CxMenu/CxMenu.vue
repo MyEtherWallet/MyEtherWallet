fcc646fe1df50cf75d34fc5873aef5d650196dbd
David Hong <hsurf22@gmail.com>
2019-11-13 22:32:26 -0800

    Update menu position

--- src/containers/HeaderContainer/components/CxMenu/CxMenu.vue
+++ src/containers/HeaderContainer/components/CxMenu/CxMenu.vue
@@ -1,43 +1,41 @@
 <template>
   <div class="cx-menu-container">
-    <b-container>
-      <div class="cx-menu d-flex">
-        <div class="d-flex logo-block">
-          <img
-            :src="require(`@/assets/images/short-hand-logo-mewcx.png`)"
-            alt
-            height="50"
+    <div class="cx-menu d-flex">
+      <div class="d-flex logo-block">
+        <img
+          :src="require(`@/assets/images/short-hand-logo-mewcx.png`)"
+          alt
+          height="50"
+        />
+        <p>Chrome Extension</p>
+      </div>
+      <div class="spacer"></div>
+      <div class="d-flex">
+        <div class="d-flex menu-items">
+          <a href="https://ccswap.myetherwallet.com/#/" target="_blank">
+            <div class="d-flex buy-eth">
+              <img
+                alt
+                class="buy-eth-icon"
+                src="@/assets/images/icons/buy-eth.svg"
+                height="24"
+              />
+              <p>Buy ETH</p>
+            </div>
+          </a>
+          <a data-v-2fc56d39="" href="#/" class="nav-link" target="_self">
+            <p>Home</p>
+          </a>
+          <extension-notification
+            v-if="
+              isMewCx &&
+                !$route.fullPath.includes('view-wallet-info') &&
+                !$route.fullPath.includes('interface')
+            "
           />
-          <p>Chrome Extension</p>
-        </div>
-        <div class="spacer"></div>
-        <div class="d-flex">
-          <div class="d-flex menu-items">
-            <a href="https://ccswap.myetherwallet.com/#/" target="_blank">
-              <div class="d-flex buy-eth">
-                <img
-                  alt
-                  class="buy-eth-icon"
-                  src="@/assets/images/icons/buy-eth.svg"
-                  height="24"
-                />
-                <p>Buy ETH</p>
-              </div>
-            </a>
-            <a data-v-2fc56d39="" href="#/" class="nav-link" target="_self">
-              <p>Home</p>
-            </a>
-            <extension-notification
-              v-if="
-                isMewCx &&
-                  !$route.fullPath.includes('view-wallet-info') &&
-                  !$route.fullPath.includes('interface')
-              "
-            />
-          </div>
         </div>
       </div>
-    </b-container>
+    </div>
   </div>
 </template>
 
