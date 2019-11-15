161f37cbfb83faa75f3d1d464679df1c081dedc3
David Hong <hsurf22@gmail.com>
2019-11-14 16:44:38 -0800

    Update partner grid layout

--- src/layouts/HomeLayout/components/Partners/Partners.vue
+++ src/layouts/HomeLayout/components/Partners/Partners.vue
@@ -7,52 +7,52 @@
         description="Connect with the powerful crypto communities"
       />
       <v-row class="mt-8 px-4">
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://www.ledger.com/?r=fa4b" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Ledger.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://trezor.io/?offer_id=12&aff_id=2029" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Trezor.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://shiftcrypto.ch/?ref=mew" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Bitbox.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://www.secalot.com/" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Secalot.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://bity.com/af/jshkb37v" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Bity.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://sirinlabs.com/" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Sirin.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="http://lddy.no/a4im" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Keepkey.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://kyber.network/" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Kyber.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://changelly.com/" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Changelly.png" />
           </a>
         </v-col>
-        <v-col cols="2">
+        <v-col cols="3" md="3" lg="2">
           <a href="https://www.simplex.com/" target="_blank">
             <img src="@/assets/images/Etc/LandingPage/Partners/Simplex.png" />
           </a>
