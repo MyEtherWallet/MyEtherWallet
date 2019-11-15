ffe5c1b4fad9613c497a46a485339915925adb5d
David Hong <hsurf22@gmail.com>
2019-11-14 18:11:23 -0800

    Add Global element container

---
+++ src/containers/GlobalElementsContainer/GlobalElementsContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <PrivacyWarning />
+  </div>
+</template>
+
+<script>
+import PrivacyWarning from './components/PrivacyWarning';
+
+export default {
+  name: 'GlobalElementsContainer',
+  components: {
+    PrivacyWarning
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
