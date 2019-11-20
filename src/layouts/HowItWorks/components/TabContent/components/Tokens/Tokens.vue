502ee20086000600c0121a10918d17b8c8ef904f
David Hong <hsurf22@gmail.com>
2019-11-19 19:39:07 -0800

    Update how it works contents

--- src/layouts/HowItWorks/components/TabContent/components/Tokens/Tokens.vue
+++ src/layouts/HowItWorks/components/TabContent/components/Tokens/Tokens.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>Tokens</div>
+  <div>
+    <div class="py-7" />
+    <BlockTitle :title="titleText" centered />
+  </div>
 </template>
 
 <script>
+import BlockTitle from '@/layouts/components/BlockTitle';
+
 export default {
-  components: {}
+  components: { BlockTitle },
+  data: () => ({
+    titleText: 'All-in-one gateway to your Ethereum Blockchain service'
+  })
 };
 </script>
 
