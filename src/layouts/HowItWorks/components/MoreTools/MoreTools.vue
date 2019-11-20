f45fefb36f612e09ab0b1ef685e85ab573d0e129
David Hong <hsurf22@gmail.com>
2019-11-19 23:14:48 -0800

    Add rest of components to How it works

---
+++ src/layouts/HowItWorks/components/MoreTools/MoreTools.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="component-container">
+    <v-container>
+      MoreTools
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
