dd35aa4a857ad611b6ced90c27fd109f914832a9
David Hong <hsurf22@gmail.com>
2019-11-20 13:32:27 -0800

    Update about us file structure

--- src/builds/web/index.js
+++ src/builds/web/index.js
@@ -1,6 +1,8 @@
 import HomeLayout from '@/layouts/HomeLayout';
 import HowItWorks from '@/layouts/HowItWorks';
 import AboutUsCompany from '@/layouts/AboutUs/Company';
+import AboutUsTeam from '@/layouts/AboutUs/Team';
+import AboutUsWhyMEW from '@/layouts/AboutUs/WhyMEW';
 
 import app from './app';
 const webRoutes = [
@@ -24,6 +26,22 @@ const webRoutes = [
     path: '/about-us/company',
     name: 'AboutUsCompany',
     component: AboutUsCompany,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/about-us/team',
+    name: 'AboutUsTeam',
+    component: AboutUsTeam,
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/about-us/why-mew',
+    name: 'AboutUsWhyMEW',
+    component: AboutUsWhyMEW,
     meta: {
       requiresAuth: false
     }
--- src/layouts/AboutUs/Team/Team.vue
+++ src/layouts/AboutUs/Team/Team.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
+    <PageTitle title="About us" />
     <div class="py-7" />
-    <BlockTitle :title="titleText" centered />
+    <BlockTitle />
   </div>
 </template>
 
 <script>
+import PageTitle from '@/layouts/components/PageTitle';
 import BlockTitle from '@/layouts/components/BlockTitle';
 
 export default {
-  components: { BlockTitle },
-  data: () => ({
-    titleText: 'All-in-one gateway to your Ethereum Blockchain service'
-  })
+  components: { PageTitle, BlockTitle },
+  data: () => ({})
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables';
+
+a {
+  text-decoration: underline;
+}
+</style>
