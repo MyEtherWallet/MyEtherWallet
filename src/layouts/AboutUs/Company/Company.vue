1c0911881c30b91f5d41597a5c933ad997d1c308
David Hong <hsurf22@gmail.com>
2019-11-20 16:31:36 -0800

    Update /#/about-us/company content

--- src/layouts/AboutUs/Company/Company.vue
+++ src/layouts/AboutUs/Company/Company.vue
@@ -5,10 +5,13 @@
     <WhatIsMew />
     <div class="py-7" />
     <OurStory />
-    <div class="py-7" />
+    <div class="py-10" />
+    <TwoBlockContent />
+    <div class="py-10" />
     <Partners />
-    <div class="py-7" />
+    <div class="py-9" />
     <Ad2 />
+    <div class="py-1" />
   </div>
 </template>
 
@@ -18,9 +21,17 @@ import Partners from '@/layouts/HomeLayout/components/Partners';
 import Ad2 from '@/layouts/HomeLayout/components/Ad2';
 import WhatIsMew from './components/WhatIsMew';
 import OurStory from './components/OurStory';
+import TwoBlockContent from './components/TwoBlockContent';
 
 export default {
-  components: { PageTitle, Partners, Ad2, WhatIsMew, OurStory },
+  components: {
+    PageTitle,
+    Partners,
+    Ad2,
+    WhatIsMew,
+    OurStory,
+    TwoBlockContent
+  },
   data: () => ({})
 };
 </script>
---
+++ src/layouts/AboutUs/Company/components/TwoBlockContent/components/CustomerSupport/CustomerSupport.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-sheet max-width="400px">
+    <v-img
+      src="@/assets/images/Common/Icons/CustomerSupport.svg"
+      max-width="60px"
+      max-height="60px"
+      aspect-ratio="1"
+      class="mb-5"
+    />
+    <BlockTitle title="Customer support" :description="descriptionText" />
+    <StdButton buttonclass="button--green-border" class="mt-5">
+      Contact Support
+    </StdButton>
+  </v-sheet>
+</template>
+
+<script>
+import BlockTitle from '@/layouts/components/BlockTitle';
+import StdButton from '@/components/StdButton';
+
+export default {
+  components: { BlockTitle, StdButton },
+  data: () => ({
+    descriptionText:
+      'Et voluptua takimata sit clita amet, takimata dolor stet sit accusam tempor at est labore.'
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables';
+</style>
