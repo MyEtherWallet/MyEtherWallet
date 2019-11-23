7489200294f4fa48874336e19acd259830391947
David Hong <hsurf22@gmail.com>
2019-11-23 00:51:26 -0800

    Update contents for new block title

--- src/layouts/HowItWorks/components/Swap/Swap.vue
+++ src/layouts/HowItWorks/components/Swap/Swap.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="component-container">
     <v-container>
-      <div class="py-6" />
-      <v-row>
+      <v-row class="align-center">
         <v-col cols="6">
           <v-img
+            class="mt-5"
             src="@/assets/images/Etc/AboutUs/InterfaceSwap.svg"
-            class="mt-n6"
           />
         </v-col>
-        <v-col cols="6" class="text-content">
+        <v-col cols="6" class="text-content pl-12">
           <BlockTitle :data="titleData" class="mb-8 pl-12" />
-          <div class="mb-6 d-flex">
+          <div class="mb-9 d-flex">
             <div class="mr-3">
-              <v-img src="@/assets/images/Etc/AboutUs/Icons/Send.svg" />
+              <v-img src="@/assets/images/Etc/AboutUs/Icons/WhiteBTC.svg" />
             </div>
             <div>
-              <div class="mb-1 font-weight-black subtitle-1">
-                Send Transaction
+              <div class="mb-1 font-weight-black subtitle-1 white--text">
+                Bitcoin
               </div>
-              <div>
+              <div class="white--text">
                 Dolor tempor ipsum lorem sadipscing, erat sadipscing et dolor
                 sed stet takimata ipsum invidunt, est ea no dolor.
               </div>
             </div>
           </div>
 
-          <div class="mb-6 d-flex">
+          <div class="mb-9 d-flex">
             <div class="mr-3">
-              <v-img src="@/assets/images/Etc/AboutUs/Icons/Offline.svg" />
+              <v-img src="@/assets/images/Etc/AboutUs/Icons/WhiteETH.svg" />
             </div>
             <div>
-              <div class="mb-1 font-weight-black subtitle-1">
-                Send Offline Helper
+              <div class="mb-1 font-weight-black subtitle-1 white--text">
+                Tokens
               </div>
-              <div>
+              <div class="white--text">
                 Dolor tempor ipsum lorem sadipscing, erat sadipscing et dolor
                 sed stet takimata ipsum invidunt, est ea no dolor.
               </div>
@@ -43,27 +42,21 @@
 
           <div class="d-flex">
             <div class="mr-3">
-              <v-img src="@/assets/images/Etc/AboutUs/Icons/NFT.svg" />
+              <v-img src="@/assets/images/Etc/AboutUs/Icons/WhiteFIAT.svg" />
             </div>
             <div>
-              <div class="mb-1 font-weight-black subtitle-1">
-                NFT Manager
+              <div class="mb-1 font-weight-black subtitle-1 white--text">
+                Fiats
               </div>
-              <div>
+              <div class="white--text">
                 Dolor tempor ipsum lorem sadipscing, erat sadipscing et dolor
                 sed stet takimata ipsum invidunt, est ea no dolor.
               </div>
-              <div class="d-flex align-center mt-4">
-                <a
-                  href="#"
-                  class="mr-5 teal--text text--lighten-2 font-weight-bold"
-                >
+              <div class="d-flex align-center mt-7">
+                <a href="#" class="mr-5 white--text font-weight-bold">
                   Try free demo
                 </a>
-                <a
-                  href="#"
-                  class="mr-5 teal--text text--lighten-2 font-weight-bold"
-                >
+                <a href="#" class="white--text font-weight-bold">
                   Watch tutorial
                 </a>
               </div>
@@ -85,7 +78,8 @@ export default {
       textProps: 'white--text',
       toptitle: '',
       title: 'Swap',
-      description: 'MEW has partnered with Bity',
+      description:
+        'MEW has partnered with Bity, Kyber Network, and Simplex to allow users to swap fiat to crypto, ETH and BTC.',
       centered: false
     }
   })
