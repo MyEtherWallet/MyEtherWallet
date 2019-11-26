607850a84ec21e08ec350d83acfa83f00d46ab86
David Hong <hsurf22@gmail.com>
2019-11-25 22:41:12 -0800

    Update MEWconnect

--- src/layouts/CreateWallet/MewConnect/MewConnect.vue
+++ src/layouts/CreateWallet/MewConnect/MewConnect.vue
@@ -3,19 +3,70 @@
     <div class="py-8" />
     <v-container>
       <BlockTitle :data="titleData" class="mb-12" />
-      <v-sheet max-width="800px" class="mx-auto">
-        <v-stepper alt-labels>
-          <v-stepper-header>
-            <v-stepper-step step="1">Ad unit details</v-stepper-step>
+      <v-sheet max-width="800px" class="mx-auto" color="white">
+        <v-stepper v-model="e1" :vertical="vertical" :alt-labels="altLabels">
+          <template v-if="vertical">
+            <template v-for="n in steps">
+              <v-stepper-step
+                :key="`${n}-step`"
+                :complete="e1 > n"
+                :step="n"
+                :editable="editable"
+              >
+                Step {{ n }}
+              </v-stepper-step>
 
-            <v-divider></v-divider>
+              <v-stepper-content :key="`${n}-content`" :step="n">
+                <v-card
+                  class="mb-12"
+                  color="grey lighten-1"
+                  height="200px"
+                ></v-card>
 
-            <v-stepper-step editable step="2">Ad sizes</v-stepper-step>
+                <v-btn color="primary" @click="nextStep(n)">
+                  Continue
+                </v-btn>
 
-            <v-divider></v-divider>
+                <v-btn text>Cancel</v-btn>
+              </v-stepper-content>
+            </template>
+          </template>
+          <template v-else>
+            <v-stepper-header>
+              <template v-for="n in steps">
+                <v-stepper-step
+                  :key="`${n}-step`"
+                  :complete="e1 > n"
+                  :step="n"
+                  :editable="editable"
+                >
+                  Step {{ n }}
+                </v-stepper-step>
 
-            <v-stepper-step step="3">Ad templates</v-stepper-step>
-          </v-stepper-header>
+                <v-divider v-if="n !== steps" :key="n"></v-divider>
+              </template>
+            </v-stepper-header>
+
+            <v-stepper-items>
+              <v-stepper-content
+                v-for="n in steps"
+                :key="`${n}-content`"
+                :step="n"
+              >
+                <v-card
+                  class="mb-12"
+                  color="grey lighten-1"
+                  height="200px"
+                ></v-card>
+
+                <v-btn color="primary" @click="nextStep(n)">
+                  Continue
+                </v-btn>
+
+                <v-btn text>Cancel</v-btn>
+              </v-stepper-content>
+            </v-stepper-items>
+          </template>
         </v-stepper>
       </v-sheet>
     </v-container>
