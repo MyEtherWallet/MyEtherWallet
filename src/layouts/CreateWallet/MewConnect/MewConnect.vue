70ed7af98e84d4f7cd444aac50acc11209208393
David Hong <hsurf22@gmail.com>
2019-11-26 17:01:22 -0800

    Update MewConnect content

--- src/layouts/CreateWallet/MewConnect/MewConnect.vue
+++ src/layouts/CreateWallet/MewConnect/MewConnect.vue
@@ -16,9 +16,86 @@
 
           <div>
             <v-tabs-items v-model="tab">
-              <v-tab-item :value="'tab-1'">1</v-tab-item>
-              <v-tab-item :value="'tab-2'">2</v-tab-item>
-              <v-tab-item :value="'tab-3'">3</v-tab-item>
+              <v-tab-item :value="'tab-1'">
+                <v-sheet class="d-flex align-end" min-height="350px">
+                  <v-col cols="6">
+                    <div class="subtitle-1 font-weight-bold grey--text">
+                      STEP 1.
+                    </div>
+                    <div class="headline font-weight-bold mb-3">
+                      Download &amp; Install
+                    </div>
+                    <div class="mb-6">
+                      Please scan the QR Code, or search "MEWconnect" in Apple
+                      App Store or Google Play Store. Continue if you have
+                      already installed the App.
+                    </div>
+                    <StdButton buttonclass="button--green">Continue</StdButton>
+                    <div class="mt-4">Already knew how to do it? Skip</div>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-img
+                      class="mb-n9 mx-auto"
+                      max-height="330px"
+                      max-width="290px"
+                      src="@/assets/images/Etc/CreateWallet/DownloadAndInstall.png"
+                    />
+                  </v-col>
+                </v-sheet>
+              </v-tab-item>
+              <v-tab-item :value="'tab-2'">
+                <v-sheet class="d-flex align-end" min-height="350px">
+                  <v-col cols="6">
+                    <div class="subtitle-1 font-weight-bold grey--text">
+                      STEP 2.
+                    </div>
+                    <div class="headline font-weight-bold mb-3">
+                      Download &amp; Install
+                    </div>
+                    <div class="mb-6">
+                      Please follow the prompts in MEWconnect App and create a
+                      new wallet. Continue if you have already created a new
+                      wallet in the App.
+                    </div>
+                    <StdButton buttonclass="button--green">Continue</StdButton>
+                    <div class="mt-4">Already knew how to do it? Skip</div>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-img
+                      class="mb-n9 mx-auto"
+                      max-height="330px"
+                      max-width="290px"
+                      src="@/assets/images/Etc/CreateWallet/DownloadAndInstall.png"
+                    />
+                  </v-col>
+                </v-sheet>
+              </v-tab-item>
+              <v-tab-item :value="'tab-3'">
+                <v-sheet class="d-flex align-end" min-height="350px">
+                  <v-col cols="6">
+                    <div class="subtitle-1 font-weight-bold grey--text">
+                      STEP 3.
+                    </div>
+                    <div class="headline font-weight-bold mb-3">
+                      Download &amp; Install
+                    </div>
+                    <div class="mb-6">
+                      Congratulations! Please use the MEWconnect App to scan
+                      this QR code in order to access your new wallet.
+                    </div>
+                    <StdButton buttonclass="button--green">Continue</StdButton>
+                    <div class="mt-4">Already knew how to do it? Skip</div>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-img
+                      class="mb-n9 mx-auto"
+                      max-height="330px"
+                      max-width="290px"
+                      src="@/assets/images/Etc/CreateWallet/DownloadAndInstall.png"
+                    />
+                  </v-col>
+                </v-sheet>
+              </v-tab-item>
             </v-tabs-items>
           </div>
         </div>
@@ -30,10 +107,11 @@
 
 <script>
 import BlockTitle from '@/layouts/components/BlockTitle';
+import StdButton from '@/components/StdButton';
 
 export default {
   name: 'MewConnect',
-  components: { BlockTitle },
+  components: { BlockTitle, StdButton },
   data: () => ({
     stepItemClass: 'light-blue darken-4 white--text pa-2 caption',
     titleData: {
