17369936685de2df7704890c6d9cbb90f99b4438
Jessica <jessicap@myetherwallet.com>
2019-11-27 12:07:24 -0800

    Committing what  I have

--- src/dapps/Aave/Aave.vue
+++ src/dapps/Aave/Aave.vue
@@ -4,7 +4,7 @@
       <back-button :title="$t('dappsAmbrpay.exit-dapp')" :hide-border="true" />
       <div class="tab-container">
         <div
-          :class="['action-btn', depositActiveTab ? 'active-tab' : '']"
+          :class="['action-btn', activeDepositTab ? 'active-tab' : '']"
           @click="toggleTabs('deposit')"
         >
           {{ $tc('dappsAave.deposit', 2) }}
@@ -13,7 +13,7 @@
           :class="[
             'action-btn',
             'borrow-btn',
-            borrowActiveTab ? 'active-tab' : ''
+            activeBorrowTab ? 'active-tab' : ''
           ]"
           @click="toggleTabs('borrow')"
         >
@@ -33,9 +33,9 @@
     <div class="about-aave-container">
       <div class="row">
         <img src="@/assets/images/icons/dapps/aave_logo.svg" alt="Aave Logo" />
-        <button>
+        <button @click="openActionModal">
           {{
-            depositActiveTab
+            activeDepositTab
               ? $tc('dappsAave.deposit', 1)
               : $t('dappsAave.borrow')
           }}
@@ -46,48 +46,61 @@
     <div class="balance-row">
       <balance-display
         :title="
-          depositActiveTab
+          activeDepositTab
             ? $t('dappsAave.aggregated-balance')
             : $t('dappsAave.you-borrowed')
         "
       />
       <balance-display
         :title="
-          depositActiveTab
+          activeDepositTab
             ? $t('dappsAave.earnings')
             : $t('dappsAave.your-collateral')
         "
         class="ml-3"
       />
     </div>
+    <div v-if="activeBorrowTab" class="loan-container">
+      <span class="loan-value">{{ $t('dappsAave.loan-value') }}</span>
+      <!-- placeholder -->
+      <span class="loan-percent">65.04%</span>
+    </div>
+    <action-modal ref="actionModal" :deposit-modal="activeDepositTab" />
   </div>
 </template>
 
 <script>
 import BackButton from '@/layouts/InterfaceLayout/components/BackButton';
 import BalanceDisplay from '@/dapps/Aave/components/BalanceDisplay';
+import ActionModal from '@/dapps/Aave/components/ActionModal';
+import LendingPoolAbi from '@/dapps/Aave/abi/LendingPoolAbi.js';
 
 export default {
   components: {
     'back-button': BackButton,
-    'balance-display': BalanceDisplay
+    'balance-display': BalanceDisplay,
+    'lending-pool-abi': LendingPoolAbi,
+    'action-modal': ActionModal
   },
   data() {
     return {
-      depositActiveTab: true,
-      borrowActiveTab: false
+      activeDepositTab: true,
+      activeBorrowTab: false
     };
   },
   methods: {
     toggleTabs(action) {
       if (
-        (action === 'borrow' && this.borrowActiveTab === true) ||
-        (action === 'deposit' && this.depositActiveTab === true)
+        (action === 'borrow' && this.activeBorrowTab === true) ||
+        (action === 'deposit' && this.activeDepositTab === true)
       ) {
         return;
       }
-      this.depositActiveTab = !this.depositActiveTab;
-      this.borrowActiveTab = !this.borrowActiveTab;
+      this.activeDepositTab = !this.activeDepositTab;
+      this.activeBorrowTab = !this.activeBorrowTab;
+    },
+    openActionModal() {
+      this.$refs.actionModal.$refs.actionModal.show();
     }
   }
 };
