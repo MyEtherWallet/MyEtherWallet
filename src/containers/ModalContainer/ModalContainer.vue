690a9244976339ea0368b3de6e9add56cd84b018
David Hong <hsurf22@gmail.com>
2019-11-27 21:59:44 -0800

    Modal container added

---
+++ src/containers/ModalContainer/ModalContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <QRcode />
+  </div>
+</template>
+
+<script>
+import QRcode from '@/containers/ModalContainer/components/CreateWallet/MewConnect/QRcode';
+
+export default {
+  name: 'ModalContainer',
+  components: { QRcode }
+};
+</script>
