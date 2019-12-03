9de224fdebcfb1297dfffb4bce9adc6738fc1acf
David Hong <hsurf22@gmail.com>
2019-12-02 16:37:13 -0800

    QRCode modal updated

--- src/builds/web/app.vue
+++ src/builds/web/app.vue
@@ -1,25 +1,25 @@
 <template>
   <v-app>
-    <ModalContainer />
     <HeaderContainer />
     <v-content>
       <router-view />
     </v-content>
     <FooterContainer />
     <GlobalElementsContainer />
+    <GlobalModalContainer />
   </v-app>
 </template>
 
 <script>
-import ModalContainer from '@/containers/ModalContainer';
 import HeaderContainer from '@/containers/HeaderContainer';
 import FooterContainer from '@/containers/FooterContainer';
 import GlobalElementsContainer from '@/containers/GlobalElementsContainer';
+import GlobalModalContainer from '@/containers/GlobalModalContainer';
 
 export default {
   name: 'App',
   components: {
-    ModalContainer,
+    GlobalModalContainer,
     HeaderContainer,
     FooterContainer,
     GlobalElementsContainer
