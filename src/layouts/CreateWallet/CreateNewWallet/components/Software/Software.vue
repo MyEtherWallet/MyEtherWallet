76bb2f3dc0ccec8a885ed26cf1013a9813c2f757
David Hong <hsurf22@gmail.com>
2019-12-04 03:07:22 -0800

    Update /create-wallet contents

--- src/layouts/CreateWallet/CreateNewWallet/components/Software/Software.vue
+++ src/layouts/CreateWallet/CreateNewWallet/components/Software/Software.vue
@@ -1,11 +1,22 @@
 <template>
   <v-sheet color="transparent" tile>
-    <div class="border--white border-radius--10px pa-8">
-      <div class="white--text title font-weight-bold mb-1">Software</div>
-      <p class="white--text mb-0">
+    <div class="border--white border-radius--10px py-8 px-10">
+      <div class="d-flex align-center">
+        <div class="white--text headline font-weight-bold mb-2 mr-2">
+          Software
+        </div>
+        <p class="mb-1 orange--text font-italic">
+          (Keystore files / Mnemonic phrase)
+        </p>
+      </div>
+      <p class="white--text mb-4">
         Ipsum consetetur sit dolores ipsum consetetur diam et. Elitr magna ipsum
         aliquyam magna at. Consetetur sit dolor eos eirmod amet.
       </p>
+      <div class="d-flex align-center">
+        <v-icon color="orange" class="mr-1">mdi-alert</v-icon>
+        <p class="ma-0 orange--text font-weight-bold">Not recommanded</p>
+      </div>
     </div>
   </v-sheet>
 </template>
