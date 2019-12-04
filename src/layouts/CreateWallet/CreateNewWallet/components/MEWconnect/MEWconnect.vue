17b6bc71d95135005237c1de8353a7a35ae90bf5
David Hong <hsurf22@gmail.com>
2019-12-04 03:07:22 -0800

    Update /create-wallet contents

--- src/layouts/CreateWallet/CreateNewWallet/components/MEWconnect/MEWconnect.vue
+++ src/layouts/CreateWallet/CreateNewWallet/components/MEWconnect/MEWconnect.vue
@@ -1,10 +1,16 @@
 <template>
-  <v-sheet class="pa-8 border-radius--10px">
-    <div class="title font-weight-bold mb-1">MEWconnect</div>
-    <p class="mb-0">
+  <v-sheet class="border-radius--10px py-8 px-10">
+    <div class="headline font-weight-bold mb-2">MEWconnect</div>
+    <p class="mb-4">
       Ipsum consetetur sit dolores ipsum consetetur diam et. Elitr magna ipsum
       aliquyam magna at. Consetetur sit dolor eos eirmod amet.
     </p>
+    <div class="d-flex align-center">
+      <v-icon color="teal lighten-1" class="mr-1">mdi-shield</v-icon>
+      <p class="ma-0 teal--text text--lighten-1 font-weight-bold">
+        Recommanded
+      </p>
+    </div>
   </v-sheet>
 </template>
 
