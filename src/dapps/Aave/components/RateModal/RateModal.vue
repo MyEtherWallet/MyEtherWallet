091cca4f8b7ff91c1698ec2c58c38ad4aa0d01e7
Jessica <jessicap@myetherwallet.com>
2019-12-09 16:27:17 -0800

    Committing

--- src/dapps/Aave/components/RateModal/RateModal.vue
+++ src/dapps/Aave/components/RateModal/RateModal.vue
@@ -11,12 +11,12 @@
       <div class="modal-contents">
         <p>{{ $t('dappsAave.select-rate-info') }}</p>
         <div class="button-container">
-          <div class="mr-3">
+          <div @click="toggleBtns('stable')" :class="['mr-3', selectStable ? 'selected-btn' : '']">
             <img src="@/assets/images/icons/stable.svg" alt="" />
             <p>{{ $t('dappsAave.stable') }}</p>
             <p class="stable">{{ convertFromRay(stableRate) }}%</p>
           </div>
-          <div>
+          <div @click="toggleBtns('variable')" :class="selectVariable ? 'selected-btn' : ''">
             <img src="@/assets/images/icons/variable.svg" alt="" />
             <p>{{ $t('dappsAave.variable') }}</p>
             <p class="variable">{{ convertFromRay(variableRate) }}%</p>
@@ -34,6 +34,12 @@
 import BigNumber from 'bignumber.js';
 
 export default {
+  data() {
+    return {
+      selectStable: false,
+      selectVariable: false
+    }
+  },
   props: {
     stableRate: {
       type: String,
@@ -49,6 +55,15 @@ export default {
       const rayUnit = new BigNumber(10).pow(27);
       const convertedInt = new BigNumber(int).div(rayUnit);
       return new BigNumber(convertedInt).times(100).toFixed(2);
+    },
+    toggleBtns(type) {
+      if (type === 'stable') {
+        this.selectStable = true;
+        this.selectVariable = false;
+      } else {
+        this.selectStable = false;
+        this.selectVariable = true;
+      }
     }
   }
 };
