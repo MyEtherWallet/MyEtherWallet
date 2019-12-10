47c52a284c09141f5be192c9a653b9bea0d2ac61
gamalielhere <gamalielgad1234@gmail.com>
2019-12-09 16:29:59 -0800

    Update jest with the new config

--- jest.config.js
+++ jest.config.js
@@ -42,5 +42,5 @@ module.exports = {
   ],
   testEnvironmentOptions: {},
   testURL: 'http://localhost/',
-  setupTestFrameworkScriptFile: '<rootDir>/tests/unit/__mocks__/mocks.js'
+  setupFilesAfterEnv: ['<rootDir>/tests/unit/__mocks__/mocks.js']
 };
