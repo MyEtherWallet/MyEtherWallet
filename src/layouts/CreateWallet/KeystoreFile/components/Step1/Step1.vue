637955bd173cf8796fb89f868ca2f9c971033631
David Hong <hsurf22@gmail.com>
2019-12-13 15:13:16 -0800

    Update Keysore components

--- src/layouts/CreateWallet/KeystoreFile/components/Step1/Step1.vue
+++ src/layouts/CreateWallet/KeystoreFile/components/Step1/Step1.vue
@@ -1,22 +1,19 @@
 <template>
-  <v-sheet min-height="500px" class="d-flex flex-column">
-    <v-sheet class="pt-8 px-3" width="100%">
+  <v-sheet class="d-flex flex-column">
+    <v-sheet class="pt-8 px-3 mb-10" width="100%">
       <div class="subtitle-1 font-weight-bold grey--text">STEP 1.</div>
       <div class="headline font-weight-bold mb-3">Create password</div>
       <div class="my-5" />
       <PasswdFieldButton btnlabel="Create" fldlabel="Password" />
     </v-sheet>
-    <v-spacer />
-    <NotRecommended class="mx-n6 border-radius-bottom" />
   </v-sheet>
 </template>
 
 <script>
 import PasswdFieldButton from '@/components/PasswdFieldButton';
-import NotRecommended from '@/components/NotRecommended';
 
 export default {
-  components: { PasswdFieldButton, NotRecommended },
+  components: { PasswdFieldButton },
   data: () => ({}),
   methods: {
     linkToStep(step) {
--- src/layouts/CreateWallet/KeystoreFile/components/Step2/Step2.vue
+++ src/layouts/CreateWallet/KeystoreFile/components/Step2/Step2.vue
@@ -1,13 +1,28 @@
 <template>
-  <v-sheet min-height="500px" class="d-flex flex-column">
+  <v-sheet class="d-flex flex-column">
     <div>
       <v-sheet class="pt-8 px-3" width="100%">
         <div class="subtitle-1 font-weight-bold grey--text">STEP 2.</div>
         <div class="headline font-weight-bold mb-3">Download keystore file</div>
-        <div class="my-5" />
-        <div class="d-flex"></div>
+        <div class="my-2" />
+        <v-row class="align-stretch">
+          <v-col v-for="d in warningData" :key="d.key">
+            <div class="warning-box pa-6">
+              <v-img
+                :src="d.img"
+                max-width="63px"
+                max-height="76px"
+                class="mx-auto mb-4"
+              />
+              <div class="subtitle-1 font-weight-medium mb-1">
+                {{ d.title }}
+              </div>
+              <div>{{ d.description }}</div>
+            </div>
+          </v-col>
+        </v-row>
       </v-sheet>
-
+      <div class="my-5" />
       <StdButton
         buttonclass="button--green"
         class="mx-auto display--block"
@@ -15,6 +30,7 @@
         >Acknowledge &amp; Download</StdButton
       >
     </div>
+    <div class="my-4" />
     <v-spacer />
     <NotRecommended class="mx-n6 border-radius-bottom" />
   </v-sheet>
@@ -23,10 +39,33 @@
 <script>
 import StdButton from '@/components/StdButton';
 import NotRecommended from '@/components/NotRecommended';
+import PaperPlane from '@/assets/images/Etc/CreateWallet/PaperPlane.svg';
+import Stolen from '@/assets/images/Etc/CreateWallet/Stolen.svg';
+import Copy from '@/assets/images/Etc/CreateWallet/Copy.svg';
 
 export default {
   components: { StdButton, NotRecommended },
-  data: () => ({}),
+  data: () => ({
+    warningData: [
+      {
+        img: PaperPlane,
+        title: "Don't lose it",
+        description: 'Be careful, it can not be recovered if you lose it.'
+      },
+      {
+        img: Stolen,
+        title: "Don't share it",
+        description:
+          'Your funds will be stolen if you use this file on a malicious phishing site.'
+      },
+      {
+        img: Copy,
+        title: 'Make a backup',
+        description:
+          'Secure it like the millions of dollars it may one day be worth.'
+      }
+    ]
+  }),
   methods: {
     linkToStep(step) {
       this.$router.push({
@@ -39,6 +78,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.warning-box {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.05);
+  height: 100%;
+}
+
 .border-radius-bottom {
   border-radius: 0 0 10px 10px;
 }
