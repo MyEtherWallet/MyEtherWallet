a07edb505efed02f5fb78c7bfc7d2d743ba146c6
gamalielhere <gamalielgad1234@gmail.com>
2019-12-17 18:05:39 -0800

    Add titles to sotd dapps

--- src/dapps/SOTDapps/SOTDapps.vue
+++ src/dapps/SOTDapps/SOTDapps.vue
@@ -65,6 +65,7 @@ export default {
           // eslint-disable-next-line
         console.error(e);
         });
+      console.log(dapp.item);
       this.dapp = Object.assign({}, dapp.item);
       this.siteKeys = Object.keys(dapp.item.sites);
       this.socials = dapp.item.socials.map(item => item);
