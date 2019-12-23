26a363f44bd2f551426af7aa529c632229876224
Jessica <jessicaspeng@gmail.com>
2019-12-23 11:18:06 -0800

    Commiting

--- src/components/SettingsModal/SettingsModal.vue
+++ src/components/SettingsModal/SettingsModal.vue
@@ -190,8 +190,8 @@
                       {{ contact.nickname }}
                     </td>
                     <td>
-                      <span class="remove-txt" @click="removeContact(index)">
-                        {{ $t('interface.address-book.remove') }}
+                      <span class="edit-txt" @click="openAddrBookModal('edit', index)">
+                        {{ $t('interface.address-book.edit') }}
                       </span>
                     </td>
                   </tr>
@@ -199,9 +199,9 @@
               </table>
             </div>
 
-            <span v-if="addrBookErrMsg" class="err">{{ addrBookErrMsg }}</span>
+            <!-- <span v-if="addrBookErrMsg" class="err">{{ addrBookErrMsg }}</span> -->
 
-            <div class="address-inputs">
+            <!-- <div class="address-inputs">
               <blockie
                 v-show="isValidAddress"
                 :address="contactAddress"
@@ -216,9 +216,10 @@
                 :placeholder="$t('common.addr')"
                 type="text"
               />
-            </div>
+            </div> -->
             <div class="addr-btn-container">
-              <input
+              <button @click="openAddrBookModal('add')">+{{ $t('interface.address-book.add') }}</button>
+              <!-- <input
                 v-model="contactNickname"
                 :placeholder="$t('interface.address-book.nickname')"
                 class="nickname-input"
@@ -229,17 +230,19 @@
                 :button-disabled="
                   !contactAddress || !isValidAddress || addrBookErrMsg !== null
                 "
-                :click-function="addContact"
-              />
+                :click-function="openAddrBookModal"
+              /> -->
             </div>
           </full-width-dropdown>
         </div>
       </b-modal>
     </div>
+    <address-book-modal ref="addressBook" :title="addrBookModalTitle"/>
   </div>
 </template>
 
 <script>
+import AddressBookModal from '@/components/AddressBookModal';
 import FullWidthDropdownMenu from '@/components/FullWidthDropdownMenu';
 import BigNumber from 'bignumber.js';
 import utils from 'web3-utils';
@@ -252,6 +255,7 @@ export default {
   name: 'Settings',
   components: {
     'full-width-dropdown': FullWidthDropdownMenu,
+    'address-book-modal': AddressBookModal,
     blockie: Blockie
   },
   props: {
@@ -316,7 +320,7 @@ export default {
       isValidAddress: false,
       contactAddress: '',
       contactNickname: '',
-      addrBookErrMsg: null
+      addrBookModalTitle: null
     };
   },
   computed: {
@@ -547,43 +551,12 @@ export default {
 
       this.ethPrice = price.data.ETH.quotes.USD.price;
     },
-    removeContact(idx) {
-      this.addressBook.splice(idx, 1);
-      this.$store.dispatch('setAddressBook', this.addressBook);
-      this.addrBookErrMsg = null;
-    },
-    addContact() {
-      const alreadyExists = Object.keys(this.addressBook).some(key => {
-        return this.addressBook[key].address === this.contactAddress;
-      });
-
-      if (this.addressBook.length > 9) {
-        this.addrBookErrMsg = this.$t('interface.address-book.add-up-to');
-        this.contactAddress = '';
-        this.contactNickname = '';
-        return;
-      } else if (alreadyExists) {
-        Toast.responseHandler(
-          new Error(this.$t('interface.address-book.already-exists')),
-          Toast.ERROR
-        );
-        this.contactAddress = '';
-        this.contactNickname = '';
-        return;
-      }
-
-      this.addrBookErrMsg = null;
-
-      this.addressBook.push({
-        address: this.contactAddress,
-        currency: 'ETH',
-        nickname: this.contactNickname || this.addressBook.length + 1
-      });
-
-      this.$store.dispatch('setAddressBook', this.addressBook);
-
-      this.contactAddress = '';
-      this.contactNickname = '';
+    openAddrBookModal(action, idx) {
+      this.addrBookModalTitle =
+        action === 'add'
+          ? this.$t('interface.address-book.add-new')
+          : this.$t('interface.address-book.save-addr');
+      this.$refs.addressBook.$refs.addressBookModal.show();
     }
   }
 };
