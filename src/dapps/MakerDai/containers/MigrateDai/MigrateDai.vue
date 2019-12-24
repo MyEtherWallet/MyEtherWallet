0229abb24268e093f6957ab1410be27ea1d8df91
David Hong <hsurf22@gmail.com>
2019-12-23 21:13:37 -0800

    Update UI /interface/dapps/maker-dai/migrateCDP

--- src/dapps/MakerDai/containers/MigrateDai/MigrateDai.vue
+++ src/dapps/MakerDai/containers/MigrateDai/MigrateDai.vue
@@ -1,40 +1,38 @@
 <template>
-  <div class="p-4">
+  <div style="max-width: 500px;">
+    <div class="d-flex align-items-center mb-3">
+      <h3 class="mr-2 ">{{ $t('dappsMaker.upgrade-sai-to-dai') }}</h3>
+      <img :src="DaiIcon" class="icon-size" />
+    </div>
     <div style="max-width: 500px;" class="mb-5">
       {{ $t('dappsMaker.upgrade-informational') }}
     </div>
-    <div style="max-width: 500px;">
-      <div class="interface__block-title d-flex align-items-center">
-        <h3 class="mr-2 ">{{ $t('dappsMaker.upgrade-sai-to-dai') }}</h3>
-        <img :src="DaiIcon" class="icon-size" />
-      </div>
-      <div class="top-buttons">
-        <p @click="setMax">{{ $t('dappsMaker.entire-sai-balance') }}</p>
-      </div>
-      <div class="dropdown-text-container dropdown-container no-point">
-        <p>
-          <span class="cc DAI cc-icon cc-icon-dai currency-symbol" />
-          {{ $t('dappsMaker.sai') }}
-        </p>
-      </div>
-      <input
-        v-model="daiQty"
+    <div class="top-buttons">
+      <p @click="setMax">{{ $t('dappsMaker.entire-sai-balance') }}</p>
+    </div>
+    <div class="dropdown-text-container dropdown-container no-point">
+      <p>
+        <span class="cc DAI cc-icon cc-icon-dai currency-symbol" />
+        {{ $t('dappsMaker.sai') }}
+      </p>
+    </div>
+    <input
+      v-model="daiQty"
+      :class="[
+        'currency-picker-container',
+        'dropdown-text-container',
+        'dropdown-container'
+      ]"
+    />
+    <div class="buttons-container">
+      <div
         :class="[
-          'currency-picker-container',
-          'dropdown-text-container',
-          'dropdown-container'
+          validInputs ? '' : 'disabled',
+          'submit-button large-round-button-green-filled'
         ]"
-      />
-      <div class="buttons-container">
-        <div
-          :class="[
-            validInputs ? '' : 'disabled',
-            'submit-button large-round-button-green-filled'
-          ]"
-          @click="submitTransaction"
-        >
-          {{ $t('dappsMaker.upgrade') }}
-        </div>
+        @click="submitTransaction"
+      >
+        {{ $t('dappsMaker.upgrade') }}
       </div>
     </div>
   </div>
