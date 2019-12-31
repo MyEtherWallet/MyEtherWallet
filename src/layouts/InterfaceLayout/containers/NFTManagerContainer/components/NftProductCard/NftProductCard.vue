587a264884ed2dcb3ef41c5cdcbe666b871dee8a
David Hong <hsurf22@gmail.com>
2019-12-30 18:02:48 -0800

    Add NftProductCard

---
+++ src/layouts/InterfaceLayout/containers/NFTManagerContainer/components/NftProductCard/NftProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>1111111111</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    selectedTitle: {
+      type: String,
+      default: 'Error'
+    },
+    nft: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
