f29ad57ec616cfaef74b92d88c7a053d2cc798cd
David Hong <hsurf22@gmail.com>
2019-12-31 16:31:12 -0800

    Add NFT product images

--- src/layouts/InterfaceLayout/containers/NFTManagerContainer/components/NftProductCard/NftProductCard.vue
+++ src/layouts/InterfaceLayout/containers/NFTManagerContainer/components/NftProductCard/NftProductCard.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div>{{ data.title }}</div>
-    <div>{{ data.itemName }}</div>
+  <div class="nft-card p-3">
+    <div class="card-image">
+      <img v-if="data.itemName" :src="getImgUrl(data.itemName)" />
+      <img v-else :src="getImgUrl(data.title)" />
+    </div>
+    <p>{{ data.itemName }}</p>
+    <p>{{ data.title }}</p>
   </div>
 </template>
 
@@ -18,8 +22,36 @@ export default {
   },
   data() {
     return {};
+  },
+  methods: {
+    getImgUrl(name) {
+      if (name == 'Kitties')
+        return require('@/assets/images/nft/products/Kitties.png');
+      if (name == 'Stamp')
+        return require('@/assets/images/nft/products/Stamp.png');
+      if (name == 'Cards')
+        return require('@/assets/images/nft/products/Cards.png');
+      if (name == 'Flowers')
+        return require('@/assets/images/nft/products/Flowers.png');
+      if (name == 'Etheremon')
+        return require('@/assets/images/nft/products/Ethermon.png');
+      if (name == 'Crabs')
+        return require('@/assets/images/nft/products/Cryptantcrab.png');
+      if (name == 'parcels')
+        return require('@/assets/images/nft/products/Decentraland.png');
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.nft-card {
+  border: 1px solid black;
+  border-radius: 10px;
+}
+
+.card-image img {
+  width: 100px;
+  height: 100px;
+}
+</style>
