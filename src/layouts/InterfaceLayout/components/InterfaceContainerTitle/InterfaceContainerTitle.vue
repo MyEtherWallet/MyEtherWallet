cbca44010ec80343001f869729c6a6734d4ffecb
David Hong <hsurf22@gmail.com>
2020-01-15 12:30:06 -0800

    Update interface title

--- src/layouts/InterfaceLayout/components/InterfaceContainerTitle/InterfaceContainerTitle.vue
+++ src/layouts/InterfaceLayout/components/InterfaceContainerTitle/InterfaceContainerTitle.vue
@@ -1,30 +1,12 @@
 <template lang="html">
   <div class="interface-container-title">
-    <div class="desktop-title">
-      <h2>{{ title }} <slot></slot></h2>
-      <slot name="right"></slot>
-      <div class="side-menu-button">
-        <round-button
-          :title="$t('interface.menu.string')"
-          @click.native="toggleSideMenu"
-        />
-      </div>
-    </div>
-    <div class="mobile-title">
-      <mobile-menu :title="title" />
-    </div>
+    <div class="title-block">{{ title }}</div>
   </div>
 </template>
 
 <script>
-import RoundButton from '@/components/Buttons/RoundButton';
-import InterfaceMobileMenu from '@/layouts/InterfaceLayout/components/InterfaceMobileMenu';
-
 export default {
-  components: {
-    'round-button': RoundButton,
-    'mobile-menu': InterfaceMobileMenu
-  },
+  components: {},
   props: {
     title: {
       type: String,
@@ -33,11 +15,6 @@ export default {
   },
   data() {
     return {};
-  },
-  methods: {
-    toggleSideMenu() {
-      this.$store.commit('TOGGLE_SIDEMENU');
-    }
   }
 };
 </script>
