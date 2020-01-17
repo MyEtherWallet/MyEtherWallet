9e4eeedfa1480b962bc3580da5d1a05a6ba290dc
gamalielhere <gamalielgad1234@gmail.com>
2020-01-17 13:18:51 -0800

    Remove some logic

--- src/layouts/AccessWalletLayout/AccessWalletLayout.vue
+++ src/layouts/AccessWalletLayout/AccessWalletLayout.vue
@@ -8,8 +8,6 @@
 <script>
 import AccessMyWalletContainer from './containers/AccessMyWalletContainer';
 import FaqsContainer from '@/containers/FaqsContainer';
-import { mapState } from 'vuex';
-import { Toast } from '@/helpers';
 
 export default {
   name: 'AccessWalletLayout',
@@ -18,70 +16,7 @@ export default {
     faqs: FaqsContainer
   },
   data() {
-    return {
-      tokens: []
-    };
-  },
-  computed: {
-    ...mapState(['online'])
-  },
-  async mounted() {
-    if (this.online) {
-      this.tokens = await this.getRates();
-    }
-  },
-  methods: {
-    async getRates() {
-      this.tokens = [];
-      const tokenNames = [
-        'BTC',
-        'ETH',
-        'REP',
-        'KNC',
-        'OMG',
-        'EOS',
-        'XRP',
-        'BCH',
-        'LTC',
-        'TRX',
-        'NEO',
-        'ETC',
-        'QTUM',
-        'ADA',
-        'XMR',
-        'QTUM',
-        'SNT',
-        'ELF'
-      ];
-      return await fetch(
-        'https://cryptorates.mewapi.io/ticker?filter=' + tokenNames.join(',')
-      )
-        .then(res => {
-          return res.json();
-        })
-        .then(rates => {
-          return Object.keys(rates.data)
-            .map(item => Object.assign(rates.data[item]))
-            .sort((a, b) => {
-              if (a.market_cap_rank < b.market_cap_rank) return -1;
-              if (a.market_cap_rank > b.market_cap_rank) return 1;
-              return 0;
-            })
-            .filter(item => {
-              if (
-                tokenNames.find(el => {
-                  return el === item.symbol;
-                }) !== undefined
-              ) {
-                return item;
-              }
-            });
-        })
-        .catch(err => {
-          Toast.responseHandler(err, Toast.ERROR);
-          return [];
-        });
-    }
+    return {};
   }
 };
 </script>
