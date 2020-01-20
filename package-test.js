b0d05ee5a89ca94bacc4b043aa759cf5a9315743
gamalielhere <gamalielgad1234@gmail.com>
2020-01-20 14:49:05 -0800

    Bring back removed package, fix build

--- package-test.js
+++ package-test.js
@@ -24,7 +24,9 @@ const EXCEPTIONS = [
   'web3-core-helpers',
   'web3-core-method',
   'web3-utils',
-  '@vue/test-utils'
+  '@vue/test-utils',
+  'sass-loader',
+  'stylelint'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
