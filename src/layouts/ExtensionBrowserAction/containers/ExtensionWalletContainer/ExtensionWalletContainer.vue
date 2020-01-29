dfab109625582587ef453d5d5dc86ddbdb7fa37c
gamalielhere <gamalielgad1234@gmail.com>
2020-01-28 18:16:26 -0800

    Some cleanup

--- src/layouts/ExtensionBrowserAction/containers/ExtensionWalletContainer/ExtensionWalletContainer.vue
+++ src/layouts/ExtensionBrowserAction/containers/ExtensionWalletContainer/ExtensionWalletContainer.vue
@@ -1,167 +1,163 @@
 <template>
-  <div class="extension-wallets-container">
-    <div class="wallets-container">
-      <interface-network-modal ref="network" />
-      <watch-only-modal
-        ref="watchOnlyModal"
-        :add-watch-only="addWatchOnlyWallet"
-        :loading="loading"
+  <extension-browser-action-wrapper>
+    <template v-if="hasAccounts" name="header">
+      <wallet-title-and-search-component
+        title="All Wallets"
+        @search="e => (search = e)"
       />
-      <password-only-modal
-        ref="passwordOnlyModal"
-        :path="path"
-        :submit="accessWallet"
-        :disabled="validInput"
-        :loading="loading"
-        @password="updatePassword"
-      />
+    </template>
+    <watch-only-modal
+      ref="watchOnlyModal"
+      :add-watch-only="addWatchOnlyWallet"
+      :loading="loading"
+    />
+    <password-only-modal
+      ref="passwordOnlyModal"
+      :path="path"
+      :submit="accessWallet"
+      :disabled="validInput"
+      :loading="loading"
+      @password="updatePassword"
+    />
 
-      <div v-if="!hasAccounts" class="no-wallet-found">
-        <div class="text-and-img-container">
-          <img src="@/assets/images/icons/alien.png" />
-          <p>No wallet found, please...</p>
-        </div>
-        <div class="wallet-options">
-          <button class="large-round-button-green-filled">
-            Add My Wallet
-          </button>
-          <div class="button-border-container">
-            <div class="button-border"></div>
-            <span> OR </span>
-            <div class="button-border"></div>
-          </div>
-          <button
-            class="large-round-button-green-filled"
-            @click="openWatchOnlyModal"
-          >
-            Add Watch Only Address
-          </button>
-        </div>
+    <div v-if="!hasAccounts" class="no-wallet-found">
+      <div class="text-and-img-container">
+        <img src="@/assets/images/icons/alien.png" />
+        <p>No wallet found, please...</p>
       </div>
-      <div v-else>
-        <wallet-title-and-search-component
-          title="All Wallets"
-          @search="e => (search = e)"
-        />
-        <div class="wallet-containers">
-          <div class="wallet-container-header">
-            <div class="add-and-wallet-count">
-              <p>
-                Total of
-                {{
-                  showMyWallets === 0
-                    ? myWallets.length
-                    : watchOnlyAddresses.length
-                }}
-                wallets
-              </p>
-              <div
-                class="add-wallet-button"
-                @click="showMyWallets === 0 ? () => {} : openWatchOnlyModal()"
-              >
-                <i class="fa fa-plus" />
-                Add
-              </div>
-            </div>
-            <b-tabs
-              v-model="showMyWallets"
-              nav-class="wallet-nav"
-              active-nav-item-class="wallet-nav-active"
-              nav-wrapper-class="wallet-nav-wrapper"
+      <div class="wallet-options">
+        <button class="large-round-button-green-filled">
+          Add My Wallet
+        </button>
+        <div class="button-border-container">
+          <div class="button-border"></div>
+          <span> OR </span>
+          <div class="button-border"></div>
+        </div>
+        <button
+          class="large-round-button-green-filled"
+          @click="openWatchOnlyModal"
+        >
+          Add Watch Only Address
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="wallet-containers">
+        <div class="wallet-container-header">
+          <div class="add-and-wallet-count">
+            <p>
+              Total of
+              {{
+                showMyWallets === 0
+                  ? myWallets.length
+                  : watchOnlyAddresses.length
+              }}
+              wallets
+            </p>
+            <div
+              class="add-wallet-button"
+              @click="showMyWallets === 0 ? () => {} : openWatchOnlyModal()"
             >
-              <b-tab title="My Wallets" title-link-class="tab-default-style">
-                <div
-                  v-if="myWallets.length > 0"
-                  class="wallet-display-container"
-                >
-                  <div class="total-balance-container">
-                    <div>
-                      <p class="portfolio-text">My Portfolio Balance</p>
-                    </div>
-                    <div>
-                      <p class="total-amt">{{ totalDollarAmount }}</p>
-                      <p class="eth-amt">
-                        {{ totalBalance }} {{ network.type.currencyName }}
-                      </p>
-                    </div>
-                  </div>
-                  <keep-alive>
-                    <wallet-info-component
-                      v-for="wallet in searchResult"
-                      :key="wallet.address"
-                      :usd="ethPrice"
-                      :address="wallet.address"
-                      :balance="wallet.balance"
-                      :wallet="wallet.wallet"
-                      :nickname="wallet.nickname"
-                      :wallet-type="wallet.type"
-                      :access="togglePasswordModal"
-                      :detail="togglePasswordModal"
-                      :prices="prices"
-                    />
-                  </keep-alive>
-                </div>
-                <div v-else class="wallet-display-container">
-                  <div class="no-wallet-found empty-wallet">
-                    <div class="text-and-img-container">
-                      <img src="@/assets/images/icons/alien.png" />
-                      <p>No wallet found, please...</p>
-                    </div>
-                    <div class="wallet-options">
-                      <button class="large-round-button-green-filled">
-                        Add My Wallet
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </b-tab>
-              <b-tab
-                title="Watch Only Address"
-                title-link-class="tab-default-style"
-              >
-                <div
-                  v-if="watchOnlyAddresses.length > 0"
-                  class="wallet-display-container"
-                >
-                  <keep-alive>
-                    <wallet-info-component
-                      v-for="wallet in searchResult"
-                      :key="wallet.address"
-                      :usd="ethPrice"
-                      :address="wallet.address"
-                      :balance="wallet.balance"
-                      :wallet="wallet.wallet"
-                      :nickname="wallet.nickname"
-                      :wallet-type="wallet.type"
-                      :access="togglePasswordModal"
-                      :detail="togglePasswordModal"
-                      :prices="prices"
-                    />
-                  </keep-alive>
-                </div>
-                <div v-else class="wallet-display-container">
-                  <div class="no-wallet-found empty-wallet">
-                    <div class="text-and-img-container">
-                      <img src="@/assets/images/icons/alien.png" />
-                      <p>No wallet found, please...</p>
-                    </div>
-                    <div class="wallet-options">
-                      <button
-                        class="large-round-button-green-filled"
-                        @click="openWatchOnlyModal"
-                      >
-                        Add Watch Only Address
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </b-tab>
-            </b-tabs>
+              <i class="fa fa-plus" />
+              Add
+            </div>
           </div>
+          <b-tabs
+            v-model="showMyWallets"
+            nav-class="wallet-nav"
+            active-nav-item-class="wallet-nav-active"
+            nav-wrapper-class="wallet-nav-wrapper"
+          >
+            <b-tab title="My Wallets" title-link-class="tab-default-style">
+              <div v-if="myWallets.length > 0" class="wallet-display-container">
+                <div class="total-balance-container">
+                  <div>
+                    <p class="portfolio-text">My Portfolio Balance</p>
+                  </div>
+                  <div>
+                    <p class="total-amt">{{ totalDollarAmount }}</p>
+                    <p class="eth-amt">
+                      {{ totalBalance }} {{ network.type.currencyName }}
+                    </p>
+                  </div>
+                </div>
+                <keep-alive>
+                  <wallet-info-component
+                    v-for="wallet in searchResult"
+                    :key="wallet.address"
+                    :usd="ethPrice"
+                    :address="wallet.address"
+                    :balance="wallet.balance"
+                    :wallet="wallet.wallet"
+                    :nickname="wallet.nickname"
+                    :wallet-type="wallet.type"
+                    :access="togglePasswordModal"
+                    :detail="togglePasswordModal"
+                    :prices="tokenPrices"
+                  />
+                </keep-alive>
+              </div>
+              <div v-else class="wallet-display-container">
+                <div class="no-wallet-found empty-wallet">
+                  <div class="text-and-img-container">
+                    <img src="@/assets/images/icons/alien.png" />
+                    <p>No wallet found, please...</p>
+                  </div>
+                  <div class="wallet-options">
+                    <button class="large-round-button-green-filled">
+                      Add My Wallet
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </b-tab>
+            <b-tab
+              title="Watch Only Address"
+              title-link-class="tab-default-style"
+            >
+              <div
+                v-if="watchOnlyAddresses.length > 0"
+                class="wallet-display-container"
+              >
+                <keep-alive>
+                  <wallet-info-component
+                    v-for="wallet in searchResult"
+                    :key="wallet.address"
+                    :usd="ethPrice"
+                    :address="wallet.address"
+                    :balance="wallet.balance"
+                    :wallet="wallet.wallet"
+                    :nickname="wallet.nickname"
+                    :wallet-type="wallet.type"
+                    :access="togglePasswordModal"
+                    :detail="togglePasswordModal"
+                    :prices="tokenPrices"
+                  />
+                </keep-alive>
+              </div>
+              <div v-else class="wallet-display-container">
+                <div class="no-wallet-found empty-wallet">
+                  <div class="text-and-img-container">
+                    <img src="@/assets/images/icons/alien.png" />
+                    <p>No wallet found, please...</p>
+                  </div>
+                  <div class="wallet-options">
+                    <button
+                      class="large-round-button-green-filled"
+                      @click="openWatchOnlyModal"
+                    >
+                      Add Watch Only Address
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </b-tab>
+          </b-tabs>
         </div>
       </div>
     </div>
-  </div>
+  </extension-browser-action-wrapper>
 </template>
 
 <script>
@@ -176,16 +172,30 @@ import { WalletInterface } from '@/wallets';
 import walletWorker from 'worker-loader!@/workers/wallet.worker.js';
 import { mapState, mapActions } from 'vuex';
 import { isAddress, toChecksumAddress } from '@/helpers/addressUtils';
-import InterfaceNetworkModal from '@/layouts/InterfaceLayout/components/InterfaceNetworkModal';
 import WalletInfoComponent from '../../components/WalletInfoComponent';
 import WalletTitleAndSearchComponent from '../../components/WalletTitleAndSearchComponent';
+import ExtensionBrowserActionWrapper from '../../wrappers/ExtensionBrowserActionWrapper';
 export default {
   components: {
     'watch-only-modal': WatchOnlyModal,
     'password-only-modal': PasswordOnlyModal,
-    'interface-network-modal': InterfaceNetworkModal,
     'wallet-info-component': WalletInfoComponent,
-    'wallet-title-and-search-component': WalletTitleAndSearchComponent
+    'wallet-title-and-search-component': WalletTitleAndSearchComponent,
+    'extension-browser-action-wrapper': ExtensionBrowserActionWrapper
+  },
+  props: {
+    tokenPrices: {
+      type: Object,
+      default: () => {}
+    },
+    ethPrice: {
+      type: Number,
+      default: 0
+    },
+    wallets: {
+      type: Array,
+      default: () => {}
+    }
   },
   data() {
     return {
@@ -197,16 +207,15 @@ export default {
       path: '',
       password: '',
       nickname: '',
-      convertedBalance: '$ 0',
-      ethPrice: 0,
-      hasAccounts: false,
       search: '',
-      showMyWallets: 0,
-      prices: {}
+      showMyWallets: 0
     };
   },
   computed: {
     ...mapState('main', ['web3', 'network']),
+    hasAccounts() {
+      return this.wallets.length > 0;
+    },
     totalDollarAmount() {
       const totalDollarAmt = new BigNumber(this.totalBalance).times(
         this.ethPrice
@@ -241,104 +250,25 @@ export default {
       return this.showMyWallets === 0
         ? this.myWallets
         : this.watchOnlyAddresses;
+    },
+    convertedBalance() {
+      return `$ ${new BigNumber(this.ethPrice)
+        .times(this.totalBalance)
+        .toFixed(2)}`;
     }
   },
-  created() {
-    window.chrome.storage.onChanged.addListener(this.getAccounts);
+  watch: {
+    wallets(newVal) {
+      this.processAccounts(newVal);
+    }
   },
   mounted() {
     this.$refs.watchOnlyModal.$refs.watchOnlyWallet.$on('hidden', () => {
       this.loading = false;
     });
-    this.fetchTokenRates();
-    this.getAccounts();
-  },
-  destroyed() {
-    window.chrome.storage.onChanged.removeListener(this.getAccounts);
   },
   methods: {
-    ...mapActions('main', [
-      'switchNetwork',
-      'setWeb3Instance',
-      'decryptWallet'
-    ]),
-    async fetchTokenRates() {
-      try {
-        const prices = await fetch('https://cryptorates.mewapi.io/ticker')
-          .then(res => {
-            return res.json();
-          })
-          .catch(e => {
-            console.error(e);
-          });
-        this.prices = Object.assign({}, prices.data);
-      } catch (e) {
-        console.error(e);
-      }
-    },
-    clearSearch() {
-      this.search = '';
-    },
-    async fetchEthBalance() {
-      const price = await fetch(
-        'https://cryptorates.mewapi.io/ticker?filter=ETH'
-      )
-        .then(res => {
-          return res.json();
-        })
-        .catch(e => {
-          // eslint-disable-next-line
-          console.error(e);
-        });
-
-      this.convertedBalance = `$ ${new BigNumber(
-        price.data.ETH.quotes.USD.price
-      )
-        .times(this.totalBalance)
-        .toFixed(2)}`;
-
-      this.ethPrice = price.data.ETH.quotes.USD.price;
-    },
-    getAccountsCb(res) {
-      const accounts = Object.keys(res)
-        .filter(item => {
-          if (isAddress(item)) {
-            return item;
-          }
-        })
-        .map(item => {
-          const newObj = Object.assign(
-            {},
-            { address: toChecksumAddress(item), wallet: res[item] }
-          );
-
-          return newObj;
-        });
-      if (accounts.length > 0) {
-        this.hasAccounts = true;
-        this.processAccounts(accounts);
-      } else {
-        this.hasAccounts = false;
-      }
-    },
-    getAccounts(changed) {
-      if (changed && changed.hasOwnProperty('defNetwork')) {
-        const networkProps = JSON.parse(changed['defNetwork'].newValue);
-        const network = this.$store.state.main.Networks[networkProps.key].find(
-          actualNetwork => {
-            return actualNetwork.service === networkProps.service;
-          }
-        );
-        this.switchNetwork(
-          !network ? this.$store.state.Networks[networkProps.key][0] : network
-        ).then(() => {
-          this.setWeb3Instance();
-        });
-      } else {
-        this.setWeb3Instance();
-      }
-      ExtensionHelpers.getAccounts(this.getAccountsCb);
-    },
+    ...mapActions('main', ['decryptWallet']),
     walletRequirePass(ethjson) {
       if (ethjson.encseed != null) return true;
       else if (ethjson.Crypto != null || ethjson.crypto != null) return true;
@@ -434,7 +364,6 @@ export default {
         this.showMyWallets = 1;
       }
       this.loading = false;
-      this.fetchEthBalance();
     },
     async getBalance(addr) {
       const balance = await this.web3.eth.getBalance(addr);
