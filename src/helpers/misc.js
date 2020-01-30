2e9448afde2e705c33ff9e583b16f55814d2b0e5
gamalielhere <gamalielgad1234@gmail.com>
2020-01-30 11:08:16 -0800

    Add date checker

--- src/helpers/misc.js
+++ src/helpers/misc.js
@@ -235,6 +235,13 @@ const isMewCx = () => {
   return BUILD_TYPE === MEW_CX;
 };
 
+const dateChecker = () => {
+  const today = new Date();
+  const startDate = new Date('02/03/2020');
+  const endDate = new Date('02/05/2020');
+  return today >= startDate && today <= endDate;
+};
+
 export default {
   isJson,
   doesExist,
@@ -256,5 +263,6 @@ export default {
   isContractArgValid,
   stripTags,
   isMewCx,
-  toBuffer
+  toBuffer,
+  dateChecker
 };
