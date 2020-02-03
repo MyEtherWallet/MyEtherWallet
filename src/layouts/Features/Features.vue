9f241ad311cd2dafc635fedfac51360e457c3377
David Hong <hsurf22@gmail.com>
2020-02-03 14:44:41 -0800

    Add Features footer

--- src/layouts/Features/Features.vue
+++ src/layouts/Features/Features.vue
@@ -1,12 +1,17 @@
 <template>
-  <v-container>
-    <div>Features</div>
-  </v-container>
+  <div>
+    <v-container>
+      <div>Features</div>
+    </v-container>
+    <Footer />
+  </div>
 </template>
 
 <script>
+import Footer from './components/Footer';
+
 export default {
   name: 'Features',
-  components: {}
+  components: { Footer }
 };
 </script>
---
+++ src/layouts/Features/components/Footer/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Footer
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
