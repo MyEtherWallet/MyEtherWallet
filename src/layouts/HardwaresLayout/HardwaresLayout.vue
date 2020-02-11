36b30085c5cc6fc15ceee41473307eb532d8a9e9
gamalielhere <gamalielgad1234@gmail.com>
2020-02-11 12:34:07 -0800

    fix test

--- src/layouts/HardwaresLayout/HardwaresLayout.vue
+++ src/layouts/HardwaresLayout/HardwaresLayout.vue
@@ -6,34 +6,31 @@
           <h2>{{ $t('buyHardwareWallet.page.title') }}</h2>
         </div>
         <div class="hardware-items-container">
-          <div
-            v-for="item in items"
-            v-show="item.price !== ''"
-            :key="item.text"
-            class="hardware-item"
-          >
-            <div class="hardware-item-text">
-              <p class="starting-from">
-                {{ $t('buyHardwareWallet.caption-starting-from') }}
-              </p>
-              <div class="price-container">
-                <span>{{ item.currency }}</span>
-                <p>{{ item.price }}</p>
+          <div v-for="item in items" :key="item.text">
+            <div v-if="item.price !== ''" class="hardware-item">
+              <div class="hardware-item-text">
+                <p class="starting-from">
+                  {{ $t('buyHardwareWallet.caption-starting-from') }}
+                </p>
+                <div class="price-container">
+                  <span>{{ item.currency }}</span>
+                  <p>{{ item.price }}</p>
+                </div>
+                <p class="item-description">
+                  {{ getItemDesc(item.description) }}
+                </p>
+                <a
+                  :href="item.href"
+                  class="more-info"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {{ $t('buyHardwareWallet.button-more-info') }}
+                </a>
               </div>
-              <p class="item-description">
-                {{ getItemDesc(item.description) }}
-              </p>
-              <a
-                :href="item.href"
-                class="more-info"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {{ $t('buyHardwareWallet.button-more-info') }}
-              </a>
-            </div>
-            <div class="hardware-item-logo">
-              <img :src="getImg(item.logo)" alt />
+              <div class="hardware-item-logo">
+                <img :src="getImg(item.logo)" alt />
+              </div>
             </div>
           </div>
         </div>
