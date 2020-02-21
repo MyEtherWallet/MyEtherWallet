c37003c4d61849c020d43f9b1437f1368a7df9a4
David Hong <hsurf22@gmail.com>
2020-02-20 19:02:37 -0800

    Add Dashboard -> Dapps

--- src/Features/Dashboard/Dashboard.vue
+++ src/Features/Dashboard/Dashboard.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="d-flex">
-    <div class="flex-grow-1">
-      <EthBalance />
+  <div>
+    <div class="d-flex">
+      <div class="flex-grow-1">
+        <EthBalance />
+        <Spacer />
+        <TokensValue />
+      </div>
       <Spacer />
-      <TokensValue />
+      <div>
+        <Network />
+        <Spacer />
+        <swap />
+      </div>
     </div>
     <Spacer />
-    <div>
-      <Network />
-      <Spacer />
-      <swap />
-    </div>
+    <Dapps />
   </div>
 </template>
 
@@ -18,11 +22,13 @@
 import Spacer from '@/components/Common/Spacer';
 import EthBalance from './EthBalance';
 import TokensValue from './TokensValue';
+import Dapps from './Dapps';
+
 import Network from '@/layouts/Features/components/SideInfo/Network';
 import Swap from '@/layouts/Features/components/SideInfo/Swap';
 
 export default {
-  components: { Spacer, EthBalance, TokensValue, Network, Swap }
+  components: { Spacer, EthBalance, TokensValue, Dapps, Network, Swap }
 };
 </script>
 
