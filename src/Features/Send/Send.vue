b64206119ac7e274f5025a984566326bb86606db
David Hong <hsurf22@gmail.com>
2020-02-21 11:38:53 -0800

    Add Contract, Dapps, Send, SignMessage, Swap components

---
+++ src/Features/Send/Send.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <div class="flex-grow-1">
+        <EthBalance />
+        <Spacer />
+        <TokensValue />
+      </div>
+      <Spacer />
+      <div>
+        <Network />
+        <Spacer />
+        <Swap />
+        <Spacer />
+        <MEWconnect />
+      </div>
+    </div>
+    <Spacer />
+    <Dapps />
+  </div>
+</template>
+
+<script>
+import Spacer from '@/components/Common/Spacer';
+import EthBalance from './EthBalance';
+import TokensValue from './TokensValue';
+import Dapps from './Dapps';
+
+import Network from '@/layouts/Features/components/SideInfo/Network';
+import Swap from '@/layouts/Features/components/SideInfo/Swap';
+import MEWconnect from '@/layouts/Features/components/SideInfo/MEWconnect';
+
+export default {
+  components: {
+    Spacer,
+    EthBalance,
+    TokensValue,
+    Dapps,
+    Network,
+    Swap,
+    MEWconnect
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
