c5ebc6575075a69028e4e94109b43b3c2aca088e
Jessica <jessicaspeng@gmail.com>
2020-02-24 10:22:56 -0800

    Commit

--- src/layouts/Features/components/SideInfo/Swap/Swap.vue
+++ src/layouts/Features/components/SideInfo/Swap/Swap.vue
@@ -1,15 +1,30 @@
 <template>
   <WhiteSheet sideinfo class="pa-7">
-    <h4 class="text-color--dark-space bold">{{ $t('common.swap') }}
-    </h4>
+    <div class="header-container">
+      <h4 class="text-color--dark-space bold">{{ $t('common.swap') }}</h4>
+      <StdButton>{{ $t('common.more') }}...</StdButton>
+    </div>
   </WhiteSheet>
 </template>
 
 <script>
 import WhiteSheet from '@/components/Common/WhiteSheet';
+import StdButton from '@/components/StdButton';
 
 export default {
-  components: { WhiteSheet }
+  components: { WhiteSheet, StdButton },
+  data() {
+    return {
+      fakeData: [
+        {
+          amt: '20',
+          from: 'ETH',
+          rate: '0.002',
+          to: 'BTC'
+        }
+      ]
+    }
+  }
 };
 </script>
 
