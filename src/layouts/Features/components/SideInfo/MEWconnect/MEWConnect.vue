9ecfb52151a2f88ffd838cb7712e3d7324468d46
Jessica <jessicaspeng@gmail.com>
2020-02-24 17:58:42 -0800

    Side info

--- src/layouts/Features/components/SideInfo/MEWconnect/MEWConnect.vue
+++ src/layouts/Features/components/SideInfo/MEWconnect/MEWConnect.vue
@@ -3,15 +3,24 @@
     <h4 class="text-color--dark-space bold">
       {{ $t('common.mewconnect.string') }}
     </h4>
+    <StdButton class="mt-2" :button-class="'button--dark-space-border'">
+      {{ $t('interface.download') }}
+    </StdButton>
+    <!-- <img src="@/assets/images/Common/Icons/wallet.svg" alt="ethereum" /> -->
   </WhiteSheet>
 </template>
 
 <script>
 import WhiteSheet from '@/components/Common/WhiteSheet';
+import StdButton from '@/components/StdButton';
 
 export default {
-  components: { WhiteSheet }
+  components: { WhiteSheet, StdButton }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.std-button {
+  opacity: 0.5;
+}
+</style>
