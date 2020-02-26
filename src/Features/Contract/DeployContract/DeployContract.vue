21cac0767f6745762b0b6a7bec58aa0fad040b39
David Hong <hsurf22@gmail.com>
2020-02-25 17:58:56 -0800

    Add sub url components for Send, Contract

---
+++ src/Features/Contract/DeployContract/DeployContract.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <div class="flex-grow-1">
+        <Deploy />
+      </div>
+      <Spacer />
+      <div>
+        <Network />
+        <Spacer />
+        <Swap />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Spacer from '@/components/Common/Spacer';
+import Deploy from './Deploy';
+
+import Network from '@/layouts/Features/components/SideInfo/Network';
+import Swap from '@/layouts/Features/components/SideInfo/Swap';
+
+export default {
+  components: {
+    Spacer,
+    Deploy,
+    Network,
+    Swap
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
---
+++ src/Features/Send/NFTManager/SendTransaction/SendTransaction.vue
@@ -0,0 +1,25 @@
+<template>
+  <WhiteSheet>
+    <InterfaceWrap title="Send Transaction">
+      Contents!!!!
+    </InterfaceWrap>
+  </WhiteSheet>
+</template>
+
+<script>
+//import Var from '@/variables.js';
+import WhiteSheet from '@/components/Common/WhiteSheet';
+import InterfaceWrap from '@/components/InterfaceWraps/InterfaceWrap1';
+//import StdButton from '@/components/StdButton';
+
+export default {
+  components: { WhiteSheet, InterfaceWrap },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
