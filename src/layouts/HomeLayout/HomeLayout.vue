f6c046ea84b18534133e7a438751826e16d263dc
gamalielhere <gamalielgad1234@gmail.com>
2020-02-26 16:38:12 -0800

    CoolWallet starting point

--- src/layouts/HomeLayout/HomeLayout.vue
+++ src/layouts/HomeLayout/HomeLayout.vue
@@ -13,6 +13,21 @@ import FaqsContainer from '@/containers/FaqsContainer';
 import Social from './components/Social';
 import TopBanner from './components/TopBanner';
 import { mapState } from 'vuex';
+import CoolWallet, { generateKeyPair } from '@coolwallets/wallet';
+import WebBleTransport from '@coolwallets/transport-web-ble';
+
+const {
+  publicKey: appPublicKey,
+  privateKey: appPrivateKey
+} = generateKeyPair();
+
+// store it locally on web storage
+localStorage.setItem('appPublicKey', appPublicKey);
+localStorage.setItem('appPrivateKey', appPrivateKey);
+
+const transport = new WebBleTransport();
+const coolWallet = new CoolWallet(transport, appPrivateKey);
+window.coolWallet = coolWallet;
 
 export default {
   name: 'HomeContainer',
