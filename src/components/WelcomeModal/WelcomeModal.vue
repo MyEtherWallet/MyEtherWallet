8af583a9ff29ca6b6a64beeea0c23d16dd59212d
David Hong <hsurf22@gmail.com>
2020-02-27 13:03:27 -0800

    Fix Russian welcome modal contents

--- src/components/WelcomeModal/WelcomeModal.vue
+++ src/components/WelcomeModal/WelcomeModal.vue
@@ -15,46 +15,18 @@
       <div class="welcome-modal-container">
         <div class="welcome-modal-text">
           <h3>{{ $t('welcomeMessage.title') }}</h3>
-          <i18n
-            v-if="!firstTimeRu"
-            path="welcomeMessage.desc1"
-            tag="p"
-            for="welcomeMessage.link1"
-          >
-            <a
-              href="https://medium.com/@myetherwallet/lets-make-it-official-mew5-myetherwallet-s-all-new-interface-is-here-2063117180a4"
-              target="_blank"
-              rel="noopener noreferrer"
-              >{{ $t('welcomeMessage.link1') }}</a
-            >
-          </i18n>
-          <i18n
-            v-if="firstTimeRu"
-            path="welcomeMessage.first-time-ru.desc1"
-            tag="p"
-          >
-            <a
-              slot="link1"
-              href="https://vk.com/public190491855"
-              target="_blank"
-              rel="noopener noreferrer"
-              >{{ $t('welcomeMessage.first-time-ru.link1') }}</a
-            >
-            <a
-              slot="supportEmail"
-              href="mailto:support@myetherwallet.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              >{{ $t('common.support-email') }}</a
-            >
-            <a
-              slot="link2"
-              href="https://twitter.com/myetherwallet"
-              target="_blank"
-              rel="noopener noreferrer"
-              >{{ $t('welcomeMessage.first-time-ru.link2') }}</a
-            >
-          </i18n>
+          <p>
+            Если Вам удобнее общаться с нами и другими пользователями MEW на
+            русском языке, присоединяйтесь к нашему сообществу в
+            <a target="_blank" href=" https://vk.com/public190491855">ВК</a>!
+            Для поддержки, пишите на
+            <a target="_blank" href="mailto:support@myetherwallet.com"
+              >support@myetherwallet.com</a
+            >. Для самых свежих новостей, подпишитесь на наш
+            <a target="_blank" href="https://twitter.com/myetherwallet"
+              >Твиттер</a
+            >."
+          </p>
         </div>
         <div class="welcome-modal-button">
           <button @click="closeModal">
