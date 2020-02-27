68c2204a7345812ff6743f6da969dc27f5877f6f
David Hong <hsurf22@gmail.com>
2020-02-27 11:10:30 -0800

    Add Accordion Menu Component

---
+++ src/layouts/Features/components/SideMenu/components/AccordionMenu/AccordionMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    AccordionMenu
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      menuLinks: [
+        {
+          name: 'Dashboard',
+          icon: '',
+          url: ''
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
