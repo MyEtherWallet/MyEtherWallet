6e59b62cc03ebf160b6da3fab07c34eb564afa4a
Jessica <jessicaspeng@gmail.com>
2020-02-27 14:40:14 -0800

    Add decompress to audit exceptions

--- package-audit.js
+++ package-audit.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-const AUDIT_EXCEPTIONS = ['handlebars'];
+const AUDIT_EXCEPTIONS = ['handlebars', 'decompress'];
 
 const execute = (command, callback) => {
   exec(
