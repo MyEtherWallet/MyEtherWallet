38de119af40b9ef6370169420e3847accc406ce5
Jessica <jessicaspeng@gmail.com>
2020-02-26 17:13:40 -0800

    Removing unnecessary components

--- src/layouts/Features/components/SideInfo/BannerAds/BannerAds.vue
+++ src/layouts/Features/components/SideInfo/BannerAds/BannerAds.vue
@@ -12,10 +12,10 @@
         <Ad1 />
       </v-carousel-item>
       <v-carousel-item>
-        <Ad2 />
+        <Ad1 />
       </v-carousel-item>
       <v-carousel-item>
-        <Ad3 />
+        <Ad1 />
       </v-carousel-item>
     </v-carousel>
   </WhiteSheet>
@@ -24,11 +24,9 @@
 <script>
 import WhiteSheet from '@/components/Common/WhiteSheet';
 import Ad1 from './components/Ad1';
-import Ad2 from './components/Ad2';
-import Ad3 from './components/Ad3';
 
 export default {
-  components: { WhiteSheet, Ad1, Ad2, Ad3 }
+  components: { WhiteSheet, Ad1 }
 };
 </script>
 
