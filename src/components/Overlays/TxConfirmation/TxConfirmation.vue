ef26bf5b2a7bd2649748ad7f5a5df398b5817dba
David Hong <hsurf22@gmail.com>
2020-02-28 14:34:44 -0800

    Add overlay components

---
+++ src/components/Overlays/TxConfirmation/TxConfirmation.vue
@@ -0,0 +1,30 @@
+<template>
+  <BaseOverlay :open="open">
+    <slot />
+  </BaseOverlay>
+</template>
+
+<script>
+import BaseOverlay from '../BaseOverlay';
+
+export default {
+  components: { BaseOverlay },
+  props: {
+    open: { default: false, type: Boolean }
+  },
+  data() {
+    return {
+      openBaseOverlay: false
+    };
+  },
+  watch: {
+    open(newVal) {
+      this.openBaseOverlay = newVal;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/GlobalVariables.scss';
+</style>
