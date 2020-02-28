ab72ada41bd0f9002fa8e6c40fa2d63e3dc93a66
SteveM <SteveM@myEtherWallet.com>
2020-02-27 17:31:03 -0800

    add updated mew connect client library

--- src/partners/kyber/kyber.js
+++ src/partners/kyber/kyber.js
@@ -614,37 +614,21 @@ export default class Kyber {
       fromCurrency !== kyberBaseCurrency && toCurrency !== kyberBaseCurrency
     );
   }
-  getTokenTradeGas() {
-    // if (
-    //   toCurrency === DAI &&
-    //   toBigNumber(fromValueWei).gt(this.convertToTokenWei('ETH', 499))
-    // ) {
-    //   return toBigNumber(1500000);
-    // }
-    // const fromGas = this.getTokenSwapGas(fromCurrency);
-    // const toGas = this.getTokenSwapGas(toCurrency);
-    // return toBigNumber(fromGas)
-    //   .plus(toBigNumber(toGas))
-    //   .toFixed(0)
-    //   .toString();
-    return toBigNumber(1500000);
-  }
 
-  // getTokenTradeGas(fromCurrency, toCurrency, fromValueWei) {
-  //   if (
-  //     toCurrency === DAI &&
-  //     toBigNumber(fromValueWei).gt(this.convertToTokenWei('ETH', 499))
-  //   ) {
-  //     return toBigNumber(1500000);
-  //   }
-  //   const fromGas = this.getTokenSwapGas(fromCurrency);
-  //   const toGas = this.getTokenSwapGas(toCurrency);
-  //   return toBigNumber(fromGas)
-  //     .plus(toBigNumber(toGas))
-  //     .toFixed(0)
-  //     .toString();
-  //   return toBigNumber(1500000);
-  // }
+  getTokenTradeGas(fromCurrency, toCurrency, fromValueWei) {
+    const shouldIncreaseGas = // check if value is likely in wei (should be, but checking anyway)
+      toBigNumber(fromValueWei).gt(this.convertToTokenWei('ETH', 499)) ||
+      toBigNumber(fromValueWei).gt(100000000);
+    if (toCurrency === DAI && shouldIncreaseGas) {
+      return toBigNumber(1500000);
+    }
+    const fromGas = this.getTokenSwapGas(fromCurrency);
+    const toGas = this.getTokenSwapGas(toCurrency);
+    return toBigNumber(fromGas)
+      .plus(toBigNumber(toGas))
+      .toFixed(0)
+      .toString();
+  }
 
   getTokenApprovalGas(token) {
     const gasLimits = this.getGasLimits(token);
