cb8c54063f8c47b730817162072ce64f88ff5566
David Hong <hsurf22@gmail.com>
2020-03-04 18:47:28 -0800

    Add more overlay components

--- src/layouts/Features/components/Header/Header.vue
+++ src/layouts/Features/components/Header/Header.vue
@@ -3,8 +3,12 @@
     <div class="d-flex align-center">
       <img class="mr-3" src="@/assets/images/Common/Icons/Puppy.svg" />
       <div>
-        <div class="font-weight-bold mb-n1">MEWtopia</div>
-        <div>Blah</div>
+        <div class="font-weight-bold">MEWtopia</div>
+        <div>
+          Find out what Crypto Wallets is and learn to use MEW securly by using
+          many different mobile wallets.
+          <a href="#">Read...</a>
+        </div>
       </div>
     </div>
     <div class="ml-auto">
@@ -18,3 +22,13 @@ export default {
   components: {}
 };
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/GlobalVariables.scss';
+
+a {
+  text-decoration: none;
+  color: $emerald !important;
+  font-weight: 500;
+}
+</style>
