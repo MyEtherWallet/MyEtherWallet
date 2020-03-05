f6174ba251a0633303ce58ed70ee8406a6c2ca2a
David Hong <hsurf22@gmail.com>
2020-03-04 19:35:26 -0800

    Add MEWconnect on header

---
+++ src/layouts/Features/components/Header/components/MEWconnect/MEWconnect.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex align-center">
+    <img src="@/assets/images/Icons/mew-connect.svg" />
+    <div class="ml-1">MEWconnect</div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
