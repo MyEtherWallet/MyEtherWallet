9c4862df56fb3d2851460ae71513da5c2476184a
David Hong <hsurf22@gmail.com>
2020-03-09 14:05:48 -0700

    Wallet > Side menu - Add accordion menu

--- src/web/views/WalletView/components/SideMenu/components/AccordionMenu/AccordionMenu.vue
+++ src/web/views/WalletView/components/SideMenu/components/AccordionMenu/AccordionMenu.vue
@@ -1,77 +1,32 @@
 <template>
-  <div>
-    <div>
-      <v-list>
-        <v-list-item>
-          <v-list-item-title><v-icon>mdi-home</v-icon> Home</v-list-item-title>
+  <div class="accordion-menu-container user-select--none">
+    <v-list color="transparent">
+      <div v-for="(i, key1) in menuItems" :key="key1">
+        <v-list-item v-if="i.url">
+          <v-list-item-title dark class="d-flex align-center">
+            <img width="23" height="23" :src="i.iconLight" class="mr-3" />
+            <div>{{ i.name }}</div>
+          </v-list-item-title>
         </v-list-item>
 
-        <v-list-group>
+        <v-list-group v-else>
           <template v-slot:activator>
-            <v-list-item-title
-              ><v-icon>mdi-home</v-icon> Users</v-list-item-title
-            >
+            <v-list-item-title class="d-flex align-center">
+              <img width="23" height="23" :src="i.iconLight" class="mr-3" />
+              <div>{{ i.name }}</div>
+            </v-list-item-title>
           </template>
 
-          <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
-          </v-list-item-content>
-
-          <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
-          </v-list-item-content>
-        </v-list-group>
-
-        <v-list-group prepend-icon="mdi-home">
-          <template v-slot:activator>
-            <v-list-item-title>Users</v-list-item-title>
-          </template>
-
-          <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
-          </v-list-item-content>
-
-          <v-list-item-content>
-            <v-list-item-title>Admin</v-list-item-title>
+          <v-list-item-content
+            v-for="(c, key2) in i.children"
+            :key="key2"
+            class="py-2"
+          >
+            <div class="menu-sub-item">{{ c.name }}</div>
           </v-list-item-content>
         </v-list-group>
-      </v-list>
-    </div>
-
-    <ul style="padding-left: 0;">
-      <li
-        v-for="(i, key1) in menuItems"
-        :key="key1"
-        class="user-select--none cursor--pointer list-style-type--none"
-      >
-        <div v-if="!i.url">
-          <div class="d-flex align-center py-3">
-            <img width="20" height="20" :src="i.iconLight" class="mr-3" />
-            <div :class="currentURL == i.url ? 'active' : ''">{{ i.name }}</div>
-          </div>
-          <div>
-            <ul>
-              <li v-for="(c, key2) in i.children" :key="key2">
-                <div
-                  :class="currentURL == c.url ? 'active' : ''"
-                  class="py-3 pl-3"
-                  @click="routerPush(c.url)"
-                >
-                  {{ c.name }}
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div v-if="i.url">
-          <div class="d-flex align-center py-3" @click="routerPush(i.url)">
-            <img width="20" height="20" :src="i.iconLight" class="mr-3" />
-            <div :class="currentURL == i.url ? 'active' : ''">{{ i.name }}</div>
-          </div>
-        </div>
-      </li>
-    </ul>
+      </div>
+    </v-list>
   </div>
 </template>
 
@@ -159,17 +114,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/styles/GlobalVariables.scss';
-
-.active {
+//@import '@/assets/styles/GlobalVariables.scss';
+* {
   color: white;
 }
 
-ul {
-  li {
-    div {
-      color: $independence;
-    }
-  }
+.menu-sub-item {
+  padding-left: 51px;
+}
+</style>
+
+<style lang="scss">
+.accordion-menu-container .v-icon {
+  color: rgb(77, 104, 126);
+}
+
+.accordion-menu-container .v-list .v-list-item--active .v-icon {
+  color: white;
 }
 </style>
