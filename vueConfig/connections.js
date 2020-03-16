54217f95ef82034c850c7f30c481f8abc01d7a40
SteveM <SteveM@myEtherWallet.com>
2020-03-16 14:09:08 -0700

    add adex endpoint to connections list

--- vueConfig/connections.js
+++ vueConfig/connections.js
@@ -54,5 +54,6 @@ module.exports = [
   'https://nft2.mewapi.io:443',
   'wss://connect.mewapi.io:443',
   'wss://connect2.mewapi.io:443',
-  'https://protocol-api.aave.com:443'
+  'https://protocol-api.aave.com:443',
+  'https://viewm.moonicorn.network:443'
 ];
