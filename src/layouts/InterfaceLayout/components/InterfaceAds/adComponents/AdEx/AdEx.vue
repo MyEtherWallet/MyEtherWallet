822604100c45942f4e1b9f42be57d9627098a5c6
SteveM <SteveM@myEtherWallet.com>
2020-03-18 12:53:08 -0700

    placeholder ad showing up on localhost

--- src/layouts/InterfaceLayout/components/InterfaceAds/adComponents/AdEx/AdEx.vue
+++ src/layouts/InterfaceLayout/components/InterfaceAds/adComponents/AdEx/AdEx.vue
@@ -1,17 +1,14 @@
 <template>
-<!--  <div>-->
-<!--    <div class="ad-image-container">-->
-
-<!--    </div>-->
-<!--  </div>-->
-  <iframe
-    src="https://viewm.moonicorn.network/#%7B%22options%22%3A%7B%22publisherAddr%22%3A%220x6F0B465eC1B0d32fb3D7eCe31723677092Fb2e81%22%2C%22whitelistedTokens%22%3A%5B%220x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359%22%2C%220x6B175474E89094C44Da98b954EedeAC495271d0F%22%5D%2C%22whitelistedType%22%3A%22legacy_300x250%22%2C%22randomize%22%3Atrue%2C%22targeting%22%3A%5B%7B%22tag%22%3A%22cryptocurrency%22%2C%22score%22%3A50%7D%2C%7B%22tag%22%3A%22crypto%20fintech%22%2C%22score%22%3A1%7D%2C%7B%22tag%22%3A%22blockchain%22%2C%22score%22%3A50%7D%2C%7B%22tag%22%3A%22computers%20%26%20electronics%22%2C%22score%22%3A1%7D%5D%2C%22width%22%3A%22300%22%2C%22height%22%3A%22250%22%2C%22minPerImpression%22%3A%220%22%2C%22minTargetingScore%22%3A%220%22%2C%22fallbackUnit%22%3A%22QmUvECaaFuAHYePCSpGL7E1NHCrGTqkn7ZhsEyKvBMZkKG%22%2C%22marketSlot%22%3A%22QmYbwYbEi3symTT6ooG1CBSYx21JzdT7kViK54mdAJP8Yr%22%7D%7D"
-    width="300"
-    height="250"
-    scrolling="no"
-    frameborder="0"
-    style="border: 0;"
-    onload="window.addEventListener('message', function(ev) {
+  <div>
+    <div class="ad-image-container">
+      <iframe
+        src="https://viewm.moonicorn.network/#%7B%22options%22%3A%7B%22publisherAddr%22%3A%220x6F0B465eC1B0d32fb3D7eCe31723677092Fb2e81%22%2C%22whitelistedTokens%22%3A%5B%220x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359%22%2C%220x6B175474E89094C44Da98b954EedeAC495271d0F%22%5D%2C%22whitelistedType%22%3A%22legacy_300x250%22%2C%22randomize%22%3Atrue%2C%22targeting%22%3A%5B%7B%22tag%22%3A%22cryptocurrency%22%2C%22score%22%3A50%7D%2C%7B%22tag%22%3A%22crypto%20fintech%22%2C%22score%22%3A1%7D%2C%7B%22tag%22%3A%22blockchain%22%2C%22score%22%3A50%7D%2C%7B%22tag%22%3A%22computers%20%26%20electronics%22%2C%22score%22%3A1%7D%5D%2C%22width%22%3A%22300%22%2C%22height%22%3A%22250%22%2C%22minPerImpression%22%3A%220%22%2C%22minTargetingScore%22%3A%220%22%2C%22fallbackUnit%22%3A%22QmUvECaaFuAHYePCSpGL7E1NHCrGTqkn7ZhsEyKvBMZkKG%22%2C%22marketSlot%22%3A%22QmYbwYbEi3symTT6ooG1CBSYx21JzdT7kViK54mdAJP8Yr%22%7D%7D"
+        width="300"
+        height="250"
+        scrolling="no"
+        frameborder="0"
+        style="border: 0;"
+        onload="window.addEventListener('message', function(ev) {
           console.log(ev); // todo remove dev item
 		if (ev.data.hasOwnProperty('adexHeight') && ('https://viewm.moonicorn.network' === ev.origin)) {
 			for (let f of document.getElementsByTagName('iframe')) {
@@ -21,7 +18,10 @@
 			}
 		}
 	}, false)"
-  ></iframe>
+      ></iframe>
+    </div>
+  </div>
+
 </template>
 
 <script>
