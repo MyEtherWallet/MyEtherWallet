130bacc29c1ac65e654d709dcda78be93233652c
gamalielhere <gamalielgad1234@gmail.com>
2020-03-20 14:30:35 -0700

    Dont transform coolwallet

--- tests/unit/specs/src/layouts/AccessWalletLayout/AccessWalletLayout.spec.js
+++ tests/unit/specs/src/layouts/AccessWalletLayout/AccessWalletLayout.spec.js
@@ -12,7 +12,7 @@ function roundPercentage(num) {
   return new BigNumber(num).toFixed(2);
 }
 
-xdescribe('AccessWalletLayout.vue', () => {
+describe('AccessWalletLayout.vue', () => {
   let localVue, i18n, wrapper, store;
 
   beforeAll(() => {
@@ -90,6 +90,4 @@ xdescribe('AccessWalletLayout.vue', () => {
       );
     }
   });
-
-  describe('AccessWalletLayout.vue Methods', () => {});
 });
