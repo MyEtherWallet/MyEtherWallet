467d6b70fb5704a6a989bdbe96be96d3ef118ab8
David Hong <hsurf22@gmail.com>
2020-03-20 13:29:31 -0700

    Wallet > Change Address - Add address selection table component

--- src/components/Overlays/ChangeAddress/components/AddressSelection/AddressSelection.vue
+++ src/components/Overlays/ChangeAddress/components/AddressSelection/AddressSelection.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <div class="pa-3">
-      <v-sheet
+      <div
         class="d-flex align-center theme--light-text--white border-radius--5px network-block pl-5 pr-2 py-1 mb-3"
       >
         <div class="font-weight-medium subtitle-1">Network</div>
         <div class="ml-auto d-flex align-center">
           <div>ETH - myetherapi.com</div>
-          <v-btn icon class="ml-1">
+          <v-btn icon class="ml-1" dark>
             <v-icon
               class="theme--light-text--white theme--dark-text--blue"
               size="20px"
@@ -15,27 +15,33 @@
             >
           </v-btn>
         </div>
-      </v-sheet>
-      <v-sheet
+      </div>
+      <div
         class="d-flex align-center theme--light-text--white theme--dark-text--white border-radius--5px address-block pl-5 pr-2 py-1"
       >
         <div class="font-weight-medium subtitle-1">
           Address to interact with
         </div>
         <div class="ml-auto">
-          <v-btn icon>
+          <v-btn icon dark>
             <v-icon class="theme--light-text--white" size="20px"
               >mdi-pencil-outline</v-icon
             >
           </v-btn>
         </div>
-      </v-sheet>
+      </div>
+      <Table />
     </div>
   </div>
 </template>
 
 <script>
+import Table from './components/Table';
+
 export default {
+  components: {
+    Table
+  },
   props: {
     text: { default: '', type: String }
   },
---
+++ src/components/Overlays/ChangeAddress/components/AddressSelection/components/Table/Table.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-tabs v-model="activeTab">
+      <v-tab :key="1" />
+      <v-tab :key="2" />
+
+      <v-tab-item>
+        <div class="width--100-percent">
+          1
+        </div>
+      </v-tab-item>
+      <v-tab-item>
+        <div class="width--100-percent">
+          2
+        </div>
+      </v-tab-item>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: { default: '', type: String }
+  },
+  data() {
+    return {
+      activeTab: 1
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
