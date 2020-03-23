fe5eb71e933da72740b28551972ecb7909bcc4c8
David Hong <hsurf22@gmail.com>
2020-03-23 12:44:02 -0700

    Waller > Dashboard - Add No Balance component - Add No Tokens component

---
+++ src/web/views/DashboardView/containers/NoTokens/NoTokens.vue
@@ -0,0 +1,29 @@
+<template>
+  <WhiteSheet>
+    <div>No Tokens</div>
+    <StdButton
+      buttonclass="button--green-border"
+      size="x-large"
+      :minwidth="false"
+      fontclass="font-weight-regular"
+    >
+      Buy ETH with a credit card
+    </StdButton>
+  </WhiteSheet>
+</template>
+
+<script>
+import WhiteSheet from '@/web/components/Common/WhiteSheet';
+import StdButton from '@/web/components/StdButton';
+
+export default {
+  components: { WhiteSheet, StdButton },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
