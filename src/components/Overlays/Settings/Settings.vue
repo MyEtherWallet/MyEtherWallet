112325adcae73d7f39624814f6d08e7318300e5f
David Hong <hsurf22@gmail.com>
2020-03-25 17:33:09 -0700

    Wallet > Settings - Update expending pannels

--- src/components/Overlays/Settings/Settings.vue
+++ src/components/Overlays/Settings/Settings.vue
@@ -1,79 +1,85 @@
 <template>
   <BaseOverlay :open="open" :close="close" :back="false" close-text="Cancel">
     <OverlayTitle title="Settings" />
-    <WhiteSheet class="mb-4">
-      <div class="overlay-content pa-3">
-        <v-expansion-panels flat light>
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              <div class="d-flex align-center">
-                <div class="title font-weight-bold mr-3">Gas price</div>
-                <div>1 Gwei (Economic)</div>
-              </div>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-              <div class="text-center">
-                <StdButton>Confirm & send</StdButton>
-              </div>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </div>
-    </WhiteSheet>
-    <WhiteSheet class="mb-4">
-      <div class="overlay-content pa-3">
-        <v-expansion-panels flat light>
-          <v-expansion-panel>
-            <v-expansion-panel-header>Item</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-              <div class="text-center">
-                <StdButton>Confirm & send</StdButton>
-              </div>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </div>
-    </WhiteSheet>
-    <WhiteSheet>
-      <div class="overlay-content pa-3">
-        <v-expansion-panels flat light>
-          <v-expansion-panel>
-            <v-expansion-panel-header>Item</v-expansion-panel-header>
-            <v-expansion-panel-content>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-              <div class="text-center">
-                <StdButton>Confirm & send</StdButton>
-              </div>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </div>
-    </WhiteSheet>
+
+    <div class="overlay-content">
+      <v-expansion-panels flat light>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="d-flex align-center">
+              <PanelHeader title="Gas price" subtitle="1 Gwei (Economic)" />
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <div class="text-center">
+              <StdButton>Confirm & send</StdButton>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <div class="d-flex align-center">
+              <PanelHeader title="Import configurations" />
+            </div>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <div class="text-center">
+              <StdButton>Confirm & send</StdButton>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <PanelHeader title="Export configurations" />
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <div class="text-center">
+              <StdButton>Confirm & send</StdButton>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            <PanelHeader title="Contact address" />
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+            <div class="text-center">
+              <StdButton>Confirm & send</StdButton>
+            </div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </BaseOverlay>
 </template>
 
 <script>
 import BaseOverlay from '../BaseOverlay';
 import OverlayTitle from '@/components/OverlayTitle';
-import WhiteSheet from '@/web/components/Common/WhiteSheet';
+import PanelHeader from './components/PanelHeader';
 import StdButton from '@/web/components/StdButton';
 
 export default {
   components: {
     BaseOverlay,
     OverlayTitle,
-    WhiteSheet,
+    PanelHeader,
     StdButton
   },
   props: {
@@ -95,6 +101,17 @@ export default {
 @import '@/assets/styles/GlobalVariables.scss';
 
 .overlay-content {
-  width: 550px;
+  width: 650px;
+}
+
+.v-expansion-panel {
+  margin-top: 20px !important;
+  border-radius: 12px !important;
+  box-shadow: $gray-3 0px 0px 15px;
+  padding: 12px 15px;
+
+  &:first-child {
+    margin-top: 0px !important;
+  }
 }
 </style>
