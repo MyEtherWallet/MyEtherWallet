83e813d10ed3100ffaf8e39291e694887eb1d5d7
Jessica <jessicaspeng@gmail.com>
2020-03-27 14:36:40 -0700

    Change links

--- src/layouts/CreateWalletLayout/components/ScanToRedirectQR/ScanToRedirectQR.vue
+++ src/layouts/CreateWalletLayout/components/ScanToRedirectQR/ScanToRedirectQR.vue
@@ -9,42 +9,30 @@ export default {
   },
   methods: {
     getMobileOperatingSystem() {
-      if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      if (/windows phone/i.test(userAgent)) {
+        return;
+      }
+
+      if (/android/i.test(userAgent)) {
         window.location.href =
           'http://play.google.com/store/apps/details?id=PACKAGEURL';
       }
-      if (navigator.userAgent.toLowerCase().indexOf('iphone') > -1) {
+
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
         window.location.href = 'https://itunes.apple.com/app/id1464614025';
       }
+
+      if (
+        userAgent.match(
+          /SAMSUNG|Samsung|SGH-[I|N|T]|GT-[I|N]|SM-[A|N|P|T|Z]|SHV-E|SCH-[I|J|R|S]|SPH-L/i
+        )
+      ) {
+        window.location.href =
+          'https://galaxystore.samsung.com/detail/com.myetherwallet.mewwallet?session_id=W_29942474fffbd49127ee223d697f1518';
+      }
     }
-    // getMobileOperatingSystem() {
-    //   console.error(('in here'))
-    //   const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    //   // Windows Phone must come first because its UA also contains "Android"
-    //   if (/windows phone/i.test(userAgent)) {
-    //     return 'Windows Phone';
-    //   }
-
-    //   if (/android/i.test(userAgent)) {
-    //     return 'Android';
-    //   }
-
-    //   // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    //   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    //     console.error("hihihihi")
-    //   }
-
-    //   if (
-    //     userAgent.match(
-    //       /SAMSUNG|Samsung|SGH-[I|N|T]|GT-[I|N]|SM-[A|N|P|T|Z]|SHV-E|SCH-[I|J|R|S]|SPH-L/i
-    //     )
-    //   ) {
-    //     console.log("it's Samsung");
-    //   }
-
-    //   return 'unknown';
-    // }
   }
 };
 </script>
