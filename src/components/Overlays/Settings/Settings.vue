f9fd804ab3ae273c71a7a1c0e372e869d09f8cb0
David Hong <hsurf22@gmail.com>
2020-03-26 17:03:50 -0700

    Wallet > Settings - Add/update all Expansion Panel contents

--- src/components/Overlays/Settings/Settings.vue
+++ src/components/Overlays/Settings/Settings.vue
@@ -5,13 +5,34 @@
     <div class="overlay-content">
       <v-expansion-panels flat light>
         <ExpansionPanelContent title="Gas price" subtitle="1 Gwei (Economic)">
-          <template v-slot:headerButtons> </template>
-          <template v-slot:content> </template>
+          <template v-slot:content>
+            <v-item-group>
+              <v-row>
+                <v-col v-for="n in 4" :key="n" cols="12" md="3">
+                  <v-item v-slot:default="{ active, toggle }">
+                    <v-card
+                      flat
+                      :color="active ? 'primary' : 'grey'"
+                      class="d-flex align-center"
+                      height="200"
+                      @click="toggle"
+                    >
+                      <v-scroll-y-transition>
+                        <div v-if="active" class="flex-grow-1 text-center">
+                          a
+                        </div>
+                      </v-scroll-y-transition>
+                    </v-card>
+                  </v-item>
+                </v-col>
+              </v-row>
+            </v-item-group>
+          </template>
         </ExpansionPanelContent>
 
         <ExpansionPanelContent title="Import configurations">
           <template v-slot:content>
-            <div class="mb-7">
+            <div class="descriptions mb-7">
               Please upload the file, and click the button on the left top to
               open and import you configuration file from your local computer.
             </div>
@@ -33,11 +54,33 @@
         </ExpansionPanelContent>
 
         <ExpansionPanelContent title="Export configurations">
-          <StdButton />
+          <template v-slot:content>
+            <div class="descriptions mb-7">
+              Please click the button on the left top to download your
+              configuration file into your local computer.
+            </div>
+            <StdButton buttonclass="button--green-border">
+              Export
+            </StdButton>
+          </template>
         </ExpansionPanelContent>
 
         <ExpansionPanelContent title="Contact address">
-          aaa
+          <template v-slot:content>
+            <div class="descriptions mb-7">
+              You can add up to 10 contact addresses.
+            </div>
+            <div class="mb-5">
+              <v-data-table
+                :headers="adresses.headers"
+                :items="adresses.desserts"
+                :items-per-page="5"
+              ></v-data-table>
+            </div>
+            <StdButton buttonclass="button--green-border">
+              + Add
+            </StdButton>
+          </template>
         </ExpansionPanelContent>
       </v-expansion-panels>
     </div>
@@ -67,7 +110,34 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      adresses: {
+        headers: [
+          {
+            text: 'ADDRESS',
+            align: 'start',
+            sortable: true,
+            value: 'address'
+          },
+          { text: 'NICKNAME', value: 'nickname' },
+          { text: '', value: 'edit' }
+        ],
+        desserts: [
+          {
+            address: '8945792398723908729358',
+            iron: 'Moms'
+          },
+          {
+            address: '8945792398723908729358',
+            iron: 'Moms'
+          },
+          {
+            address: '8945792398723908729358',
+            iron: 'Moms'
+          }
+        ]
+      }
+    };
   }
 };
 </script>
@@ -76,7 +146,7 @@ export default {
 @import '@/assets/styles/GlobalVariables.scss';
 
 .overlay-content {
-  width: 650px;
+  width: 750px;
 }
 
 .v-expansion-panel {
@@ -87,5 +157,9 @@ export default {
   &:first-child {
     margin-top: 0px !important;
   }
+}
+
+.descriptions {
+  max-width: 450px;
 }
 </style>
