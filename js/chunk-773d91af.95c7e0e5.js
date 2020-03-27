b2664b3781342acad5ae1ad90e799242f971abcc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-27 00:30:44 +0000

    Deploy MyEtherWallet/MyEtherWallet to github.com/MyEtherWallet/MyEtherWallet.git:gh-pages-history

---
+++ js/chunk-773d91af.95c7e0e5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-773d91af"],{ec51:function(n,w,c){}}]);
+//# sourceMappingURL=chunk-773d91af.95c7e0e5.js.map
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+
+importScripts(
+  "/precache-manifest.77e1b558ba9ad4c32b9b2d4714760f53.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "myetherwallet"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
