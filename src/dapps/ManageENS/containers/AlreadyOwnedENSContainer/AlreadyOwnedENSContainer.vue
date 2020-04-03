72edfeb08c9da082aabf1e788424706cdffed428
gamalielhere <gamalielgad1234@gmail.com>
2020-04-03 16:28:12 -0700

    Get avatar for ens lookup

--- src/dapps/ManageENS/containers/AlreadyOwnedENSContainer/AlreadyOwnedENSContainer.vue
+++ src/dapps/ManageENS/containers/AlreadyOwnedENSContainer/AlreadyOwnedENSContainer.vue
@@ -1,6 +1,59 @@
 <template lang="html">
   <div class="already-owned-container">
     <h3>{{ $t('ens.already-owned', { domain: fullDomainName }) }}</h3>
+    <div v-if="hasAvatar" class="container-with-avatar content-container">
+      <div class="img-info-container">
+        <div class="profile-img">
+          <img :src="txtRecordsWithValue.avatar" />
+        </div>
+        <div class="profile-info">
+          <p v-if="txtRecordsWithValue['url']">
+            Website:
+            <a :href="'mailto:' + txtRecordsWithValue['url']" target="_blank">
+              {{ txtRecordsWithValue['url'] }}
+            </a>
+          </p>
+          <p v-if="txtRecordsWithValue['email']">
+            Email:
+            <a :href="'mailto:' + txtRecordsWithValue['email']" target="_blank">
+              {{ txtRecordsWithValue['email'] }}
+            </a>
+          </p>
+          <p v-if="txtRecordsWithValue['vnd.twitter']">
+            Twitter:
+            <a
+              :href="
+                'https://twitter.com/' + txtRecordsWithValue['vnd.twitter']
+              "
+              target="_blank"
+            >
+              {{ txtRecordsWithValue['vnd.twitter'] }}
+            </a>
+          </p>
+          <p v-if="txtRecordsWithValue['vnd.github']">
+            Github:
+            <a
+              :href="'https://github.com/' + txtRecordsWithValue['vnd.github']"
+              target="_blank"
+            >
+              {{ txtRecordsWithValue['vnd.github'] }}
+            </a>
+          </p>
+          <p v-if="txtRecordsWithValue['vnd.peepeth']">
+            Peepeth:
+            <a
+              :href="
+                'https://peepeth.com/' + txtRecordsWithValue['vnd.peepeth']
+              "
+              target="_blank"
+            >
+              {{ txtRecordsWithValue['vnd.peepeth'] }}
+            </a>
+          </p>
+        </div>
+      </div>
+      <div class="ens-status-and-description"></div>
+    </div>
     <div class="content-container">
       <p class="label">{{ $t('ens.label-hash') }}({{ hostName }}):</p>
       <p class="content">{{ labelHash }}</p>
@@ -98,6 +151,9 @@ export default {
       for (const k in this.supportedCoins)
         if (this.supportedCoins[k].value) coins[k] = this.supportedCoins[k];
       return coins;
+    },
+    hasAvatar() {
+      return this.txtRecordsWithValue.hasOwnProperty('avatar');
     },
     txtRecordsWithValue() {
       const txtRecords = {};
