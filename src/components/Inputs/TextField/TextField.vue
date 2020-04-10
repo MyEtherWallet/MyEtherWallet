13e4ca34f7cb6908366587720b5998d7a03bb0a6
David Hong <hsurf22@gmail.com>
2020-04-09 18:06:07 -0700

    Global - Add TextField, DeopdownSelect component
    Wallet > Change Network
    - Update user input contents

---
+++ src/components/Inputs/TextField/TextField.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-text-field
+    v-model="textData"
+    :label="label"
+    :placeholder="placeholder"
+    outlined
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  props: {
+    label: { default: '', type: String },
+    placeholder: { default: '', type: String }
+  },
+  data: () => ({
+    textData: ''
+  }),
+  watch: {
+    textData(val) {
+      this.$emit('changed', val);
+    }
+  }
+};
+</script>
