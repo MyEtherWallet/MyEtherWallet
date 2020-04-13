0966a1d4a11a05696a747d2c6f9bafb6564cc496
gamalielhere <gamalielgad1234@gmail.com>
2020-04-13 10:46:41 -0700

    Remove console log

--- src/builds/mewcx/cxHelpers/contentScript.js
+++ src/builds/mewcx/cxHelpers/contentScript.js
@@ -266,7 +266,6 @@ events[WEB3_RPC_REQUEST] = function (e) {
     },
     {},
     data => {
-      console.log(data);
       if (data.error) {
         window.dispatchEvent(
           new CustomEvent(`${WEB3_RPC_REQUEST}-${e.detail.id}-err`, {
