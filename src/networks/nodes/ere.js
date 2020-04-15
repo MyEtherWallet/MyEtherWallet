da74168131c4c530b2fe7697e83bcf08527890ee
cryptocoderkyle <cryptocoderkyle@gmail.com>
2020-04-15 17:14:13 +0900

    Update EtherCore RPC Host

--- src/networks/nodes/ere.js
+++ src/networks/nodes/ere.js
@@ -1,8 +1,8 @@
 import { ERE } from '../types';
 export default {
   type: ERE,
-  service: 'ethercore.org',
-  url: 'https://rpc.ethercore.org',
+  service: 'ethercore.io',
+  url: 'https://rpc.ethercore.io',
   port: 443,
   auth: false,
   username: '',
