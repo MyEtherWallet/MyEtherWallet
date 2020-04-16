60e9f7ffdd0c1d30166fdc9f7b571bb5e757b89d
David Hong <hsurf22@gmail.com>
2020-04-16 15:33:59 -0700

    Global - Add bsed dialog component

---
+++ src/components/Dialogs/BaseDialog/BaseDialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on }">
+        <slot name="opener" />
+        <v-btn color="red lighten-2" dark v-on="on">
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <slot name="content" />
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+//@import '@/assets/styles/GlobalVariables.scss';
+</style>
