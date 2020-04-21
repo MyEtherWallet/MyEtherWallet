8a50690edd14ffbb63fcad6e780aa5b40f414acd
David Hong <hsurf22@gmail.com>
2020-04-21 16:44:39 -0700

    Policy - Add new layout

---
+++ src/layouts/PolicyLayout/PolicyLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-container">PolicyLayout</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
