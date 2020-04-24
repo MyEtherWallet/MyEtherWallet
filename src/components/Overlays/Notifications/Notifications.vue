2fb1c5fedbd6e26b4a339c54061b9859ba5889a5
David Hong <hsurf22@gmail.com>
2020-04-23 20:00:58 -0700

    Wallet > Notification - Update notification content

--- src/components/Overlays/Notifications/Notifications.vue
+++ src/components/Overlays/Notifications/Notifications.vue
@@ -3,14 +3,22 @@
     <div class="overlay-content">
       <OverlayTitle title="Notifications" />
       <WhiteSheet>
-        <div class="pa-8">
+        <div class="pa-4">
           <div>
             <div class="text-center font-weight-bold">1/8/2020</div>
             <v-expansion-panels light hover flat>
               <v-expansion-panel>
                 <v-expansion-panel-header color="red">
                   <div class="d-flex align-center">
-                    <div class="mr-6">icon</div>
+                    <div class="mr-6">
+                      <blockie
+                        address="0xc2a933600c3fe776777b4000665409c61493d417"
+                        :size="8"
+                        :scale="16"
+                        width="40px"
+                        height="40px"
+                      />
+                    </div>
                     <div>
                       <div>From: 0x92349857934875</div>
                       <div>Abount: 0.0000123 ETH</div>
@@ -24,6 +32,7 @@
                   laboris nisi ut aliquip ex ea commodo consequat.
                 </v-expansion-panel-content>
               </v-expansion-panel>
+
               <v-expansion-panel>
                 <v-expansion-panel-header color="blue"
                   >Item</v-expansion-panel-header
@@ -58,9 +67,11 @@
 import BaseOverlay from '../BaseOverlay';
 import OverlayTitle from '@/components/OverlayTitle';
 import WhiteSheet from '@/web/components/Common/WhiteSheet';
+import Blockie from '@/web/components/Blockie';
 
 export default {
   components: {
+    Blockie,
     BaseOverlay,
     OverlayTitle,
     WhiteSheet
@@ -75,7 +86,36 @@ export default {
     }
   },
   data() {
-    return {};
+    return {
+      notifications: [
+        {
+          txType: 'in',
+          address: '0xabc23787066c571a61274929746895c24fa9dbfc',
+          amount: '0.000243',
+          time: '1 min',
+          currency: 'eth',
+          data: [
+            {
+              name: 'Transaction hash',
+              value:
+                '0xc06cdcc936536224054723679ba8ff616b1a5f1cac8cfa606ee03488bfb82ef0'
+            },
+            {
+              name: 'Gas price',
+              value: '41 Gwei'
+            },
+            {
+              name: 'Gas limit',
+              value: '40000'
+            },
+            {
+              name: 'Max transaction fee',
+              value: '0.00074444'
+            }
+          ]
+        }
+      ]
+    };
   }
 };
 </script>
