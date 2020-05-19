a7bdce51a473b4901a6809fdb680d5f5989b904b
Kosala Hemachandra <kvhnuke@aol.com>
2020-05-18 18:32:55 -0700

    fix 1673869148

--- src/layouts/InterfaceLayout/InterfaceLayout.vue
+++ src/layouts/InterfaceLayout/InterfaceLayout.vue
@@ -457,9 +457,10 @@ export default {
       });
     },
     async setExpiry(param) {
-      const names = param.hasOwnProperty(ENS_TOKEN_ADDRESS)
-        ? param[ENS_TOKEN_ADDRESS].tokens
-        : [];
+      const names =
+        param && param.hasOwnProperty(ENS_TOKEN_ADDRESS)
+          ? param[ENS_TOKEN_ADDRESS].tokens
+          : [];
       if (names.length > 0) {
         const hashes = names.map(item => {
           return item.id;
