4e13ed9c1b515825cad2f226410ad8da5dcded63
Kosala Hemachandra <kvhnuke@aol.com>
2020-05-20 19:31:38 -0700

    set token balances to load on error

--- src/layouts/InterfaceLayout/InterfaceLayout.vue
+++ src/layouts/InterfaceLayout/InterfaceLayout.vue
@@ -538,7 +538,7 @@ export default {
           });
         } catch (e) {
           tokens = this.network.type.tokens.map(token => {
-            token.balance = 0;
+            token.balance = 'Load';
             return token;
           });
         }
