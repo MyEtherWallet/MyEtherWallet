d1c63712ff9d0db33e406657668e9d62c75e90c2
gamalielhere <gamalielgad1234@gmail.com>
2020-05-26 16:39:36 -0700

    Fix comments

--- src/dapps/routes.js
+++ src/dapps/routes.js
@@ -1,6 +1,5 @@
 import manageENSRoutes from './ManageENS/routes';
 import unstoppableRoutes from './Unstoppable/routes';
-// import manageUnstoppableRoutes from './ManageUnstoppable/routes';
 import buySubDomainRoutes from './BuySubdomain/routes';
 import scheduleTransactionRoutes from './ScheduleTransaction/routes';
 import makerDaiRoutes from './MakerDai/routes';
@@ -11,7 +10,6 @@ import aaveRoutes from './Aave/routes';
 const routes = {
   manageENS: manageENSRoutes,
   unstoppable: unstoppableRoutes,
-  // manageUnstoppable: manageUnstoppableRoutes,
   buySubDomain: buySubDomainRoutes,
   scheduleTransaction: scheduleTransactionRoutes,
   makerDaiRoutes: makerDaiRoutes,
