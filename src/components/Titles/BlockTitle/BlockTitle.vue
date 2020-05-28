62812723b9d73aa0c6ee13579353ac5b4df780cb
David Hong <hsurf22@gmail.com>
2020-05-27 23:17:10 -0700

    Update Unstoppable domain

---
+++ src/components/Titles/BlockTitle/BlockTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="px-3 mb-10">
+    <div class="font-weight-bold">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { default: '', type: String }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
