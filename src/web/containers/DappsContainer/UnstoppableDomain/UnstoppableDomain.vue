4fea1a5751511c194ab86ca77dca8b3d5f0852ec
David Hong <hsurf22@gmail.com>
2020-05-28 15:49:19 -0700

    Update (dapps/unstoppable-domain) buy domain

--- src/web/containers/DappsContainer/UnstoppableDomain/UnstoppableDomain.vue
+++ src/web/containers/DappsContainer/UnstoppableDomain/UnstoppableDomain.vue
@@ -1,38 +1,63 @@
 <template>
   <WhiteSheet>
     <div>
-      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-        <v-tab>
+      <v-tabs v-model="tab" background-color="transparent" class="px-10 py-6">
+        <v-tab class="text-transform--initial mew-heading-2">
           Buy domain
         </v-tab>
-        <v-tab>
+        <v-tab class="text-transform--initial mew-heading-2">
           Manage domain
         </v-tab>
       </v-tabs>
 
       <v-tabs-items v-model="tab">
         <v-tab-item>
-          <div class="py-12 tab-width">
-            <div class="mb-5">
-              <BlockTitle title="Find your blockchain domain" />
-              <div class="d-flex align-start">
-                <mew-input
-                  :has-clear-btn="true"
-                  right-label=".ctypto"
-                  placeholder="Domain name"
-                  class="mr-2"
-                />
-                <mew-button
-                  :has-full-width="false"
-                  button-size="xlarge"
-                  title="Check availability"
-                />
+          <v-sheet color="bg_white_sheet" class="py-12">
+            <div class="tab-width">
+              <div class="mb-5">
+                <BlockTitle title="Find your blockchain domain" />
+                <div class="d-flex align-start">
+                  <mew-input
+                    :has-clear-btn="true"
+                    right-label=".ctypto"
+                    placeholder="Domain name"
+                    class="mr-2"
+                  />
+                  <mew-button
+                    :has-full-width="false"
+                    button-size="xlarge"
+                    title="Check availability"
+                  />
+                </div>
+              </div>
+              <div>
+                <BlockTitle title="Results" />
+                <v-card flat color="bg_datablock" class="pa-10">
+                  <div class="d-flex align-center justify-space-between mb-8">
+                    <div class="mew-heading-1">myetherwallet.crypto</div>
+                    <div class="mew-heading-3 orange--text">Unavailable</div>
+                  </div>
+                  <v-list-item
+                    v-for="(r, key) in results"
+                    :key="key"
+                    two-line
+                    class="px-0"
+                  >
+                    <v-list-item-content>
+                      <div
+                        class="mew-body font-weight-medium titlePrimary--text mb-2"
+                      >
+                        {{ r.label }}:
+                      </div>
+                      <div class="mew-body titlePrimary--text">
+                        {{ r.value }}
+                      </div>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
               </div>
             </div>
-            <div>
-              <BlockTitle title="Results" />
-            </div>
-          </div>
+          </v-sheet>
         </v-tab-item>
         <v-tab-item>
           <div class="py-12 tab-width">
@@ -51,7 +76,31 @@ import BlockTitle from '@/components/Titles/BlockTitle';
 export default {
   components: { WhiteSheet, BlockTitle },
   data() {
-    return { tab: null };
+    return {
+      tab: null,
+      results: [
+        {
+          label: 'Labelhash (mewwallet)',
+          value: '0x527b9715d99aCfB7E1b01C6C864DC8402fC6C864DC8402f2a3C3b'
+        },
+        {
+          label: 'Namehash (mewwallet.eth)',
+          value: '0x527b9715d99aCfB7E1b01C6C864DC8402fC6C864DC8402f2a3C3b'
+        },
+        {
+          label: 'Owner',
+          value: '0x527b9715d99aCfB7E1b01C6C864DC8402f2a3C3b'
+        },
+        {
+          label: 'Multi-coin',
+          value: 'None'
+        },
+        {
+          label: 'ETH address',
+          value: '0x502e119bbf7b476a449a67fdabfa35f486194c9a'
+        }
+      ]
+    };
   }
 };
 </script>
