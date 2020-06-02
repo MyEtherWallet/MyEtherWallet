5798b3f058019a22dd4eb070e4ee4eb8d28337c0
David Hong <hsurf22@gmail.com>
2020-06-01 19:05:34 -0700

    WooCommerce dialog update

---
+++ src/components/Dialogs/WooCommerceDialog/WooCommerceDialog.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="400px">
+      <v-sheet color="gray">
+        aaa
+      </v-sheet>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: { default: false, type: Boolean }
+  },
+  data() {
+    return { dialog: false };
+  },
+  watch: {
+    show(val) {
+      this.dialog = val;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
