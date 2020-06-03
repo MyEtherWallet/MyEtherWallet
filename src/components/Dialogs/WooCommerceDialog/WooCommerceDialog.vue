daa5fb801c096787137a31b987c326e04c1e63d5
David Hong <hsurf22@gmail.com>
2020-06-03 15:26:45 -0700

    Update woocommerce

--- src/components/Dialogs/WooCommerceDialog/WooCommerceDialog.vue
+++ src/components/Dialogs/WooCommerceDialog/WooCommerceDialog.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="text-center">
     <v-dialog v-model="dialog" width="350px">
-      <v-sheet color="bg_wallet" class="mew-logo">
-        <div class="text-center pt-10 pb-10">
-          <div class="mew-heading-3">WooCommerce Videos</div>
-          <div>videos@woocommerce.com</div>
+      <v-sheet color="bg_wallet" class="">
+        <div class="text-center title-background">
+          <div class="mew-heading-2 pt-12 pb-5 top-title">
+            WooCommerce Videos
+          </div>
+          <div class="py-2 top-email">videos@woocommerce.com</div>
+        </div>
+        <div>
+          <v-btn-toggle v-model="toggle_exclusive" mandatory>
+            <v-btn>
+              <v-icon>mdi-format-align-left</v-icon>
+            </v-btn>
+            <v-btn>
+              <v-icon>mdi-format-align-center</v-icon>
+            </v-btn>
+            <v-btn>
+              <v-icon>mdi-format-align-right</v-icon>
+            </v-btn>
+            <v-btn>
+              <v-icon>mdi-format-align-justify</v-icon>
+            </v-btn>
+          </v-btn-toggle>
         </div>
       </v-sheet>
     </v-dialog>
@@ -28,9 +46,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.mew-logo {
+.title-background {
+  background-image: linear-gradient(#ffffff, #d1d1d1);
+}
+
+.top-title {
   background-image: url('~@/assets/images/icons/logo-mew-small.png');
-  background-position: 10px 10px;
-  background-size: 70px;
+  background-position: right -30px top -30px;
+  background-size: 90px;
+  border-bottom: 1px solid #c9c9c9;
+}
+
+.top-email {
+  border-top: 1px solid white;
 }
 </style>
