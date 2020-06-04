f94e08569326f3166be9928463a6de7f736ad8ab
zosodemon <36483920+zosodemon@users.noreply.github.com>
2020-06-04 08:59:13 -0400

    Create EWT.js

---
+++ src/networks/types/EWT.js
@@ -0,0 +1,22 @@
+import tokens from '@/_generated/tokens/tokens-ewt.json';
+import contracts from '@/_generated/contracts/contract-abi-ewt.json';
+import ewt from '@/assets/images/networks/ewt.svg';
+
+export default {
+  name: ‘ewt’,
+  name_long: ‘Energy Web Chain’,
+  homePage: ‘
+https://www.energyweb.org/'
+,
+  blockExplorerTX: ‘
+https://explorer.energyweb.org/txs/[[txHash]]'
+,
+  blockExplorerAddr: ‘
+https://explorer.energyweb.org//accounts/[[address]]'
+,
+  chainID: 246,
+  tokens: tokens,
+  contracts: contracts,
+  icon: ewt,
+  currencyName: ‘ewt’
+};
