0d37d5fdfa46047b8c85bfd6a0def8bfa830ed95
David Hong <hsurf22@gmail.com>
2020-06-03 18:15:51 -0700

    Update ens

--- src/web/containers/DappsContainer/ENSManager/ENSManager.vue
+++ src/web/containers/DappsContainer/ENSManager/ENSManager.vue
@@ -1,11 +1,16 @@
 <template>
   <WhiteSheet>
+    <mew-banner
+      :text-obj="topBanner"
+      :banner-img="BG"
+      @closeBanner="closeBanner"
+    />
     <div>
-      <v-tabs v-model="tab" background-color="transparent" color="basil" grow>
-        <v-tab>
-          Buy domain
+      <v-tabs v-model="tab" background-color="transparent" class="px-10 py-6">
+        <v-tab class="text-transform--initial mew-heading-2">
+          Register domain
         </v-tab>
-        <v-tab>
+        <v-tab class="text-transform--initial mew-heading-2">
           Manage domain
         </v-tab>
       </v-tabs>
@@ -30,8 +35,37 @@
           </div>
         </v-tab-item>
         <v-tab-item>
-          <div class="py-12 tab-width">
-            aaa
+          <div class="py-12 px-12">
+            <div class="d-flex align-center justify-space-between">
+              <div class="mew-heading-1">My domains (1)</div>
+              <mew-button
+                button-size="xlarge"
+                title="+Add your dowmain"
+                :has-full-width="false"
+              ></mew-button>
+            </div>
+            <div>
+              <mew-expand-panel
+                :is-toggle="true"
+                :panel-items="[
+                  { name: 'mewdev01.eth', tooltip: '', subtext: 'Expired' }
+                ]"
+              >
+                <template v-slot:panelBody0>
+                  <span>Panel slot example</span>
+                </template>
+              </mew-expand-panel>
+              <mew-expand-panel
+                :is-toggle="true"
+                :panel-items="[
+                  { name: 'mewdev01.eth', tooltip: '', subtext: '' }
+                ]"
+              >
+                <template v-slot:panelBody0>
+                  <span>Panel slot example</span>
+                </template>
+              </mew-expand-panel>
+            </div>
           </div>
         </v-tab-item>
       </v-tabs-items>
@@ -40,20 +74,22 @@
 </template>
 
 <script>
+import BG from '@/assets/images/backgrounds/bg-ens.png';
 import WhiteSheet from '@/web/components/Common/WhiteSheet';
 import BlockTitle from '@/components/Titles/BlockTitle';
 
 export default {
   components: { WhiteSheet, BlockTitle },
   data() {
-    return { tab: null };
+    return {
+      BG: BG,
+      tab: null,
+      topBanner: {
+        title: 'ENS manager',
+        subtext: 'Manage the public address',
+        exit: 'Exit Dapp'
+      }
+    };
   }
 };
 </script>
-
-<style>
-.tab-width {
-  max-width: 800px;
-  margin: 0 auto;
-}
-</style>
