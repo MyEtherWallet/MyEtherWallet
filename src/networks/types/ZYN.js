70865b391388ff27bf1895ec223d2e23991e4427
Himanshu Kumar <himanshu.kumar@oodlestechnologies.com>
2020-06-08 20:29:41 +0530

    Create ZYN.js
    Adding type information for ZYN coin

---
+++ src/networks/types/ZYN.js
@@ -0,0 +1,12 @@
+import zyn from '@/assets/images/networks/zyn.svg';
+
+export default {
+  name: 'ZYN',
+  name_long: 'zyncoin',
+  homePage: 'https://mainnet.wethio.io/',
+  blockExplorerTX: 'https://explorer.wethio.io/txs/[[txHash]]',
+  blockExplorerAddr: 'https://explorer.wethio.io/address/[[address]]',
+  chainID: 78,
+  icon: zyn,
+  currencyName: 'ZYN'
+};
