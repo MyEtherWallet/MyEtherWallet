e8313058e3b47278d24c37f8e3a7fcae999226d4
Zynecoin <47103225+zynecoin@users.noreply.github.com>
2020-06-09 17:14:47 +0530

    Create zyn.js
    added node info for ZYN coin.

---
+++ src/networks/nodes/zyn.js
@@ -0,0 +1,10 @@
+import { ZYN } from '../types';
+export default {
+  Type: 'ZYN',
+  service: Wethio,
+  url: 'https://rpc.wethio.io' ,
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
