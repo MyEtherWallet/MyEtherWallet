888df77fd828db1dcfaa2b11d876f61a5e2d44f0
David Hong <hsurf22@gmail.com>
2020-06-08 22:42:47 -0700

    Update how it work conten

--- src/web/views/LandingPageView/HowItWorks/components/MoreTools/MoreTools.vue
+++ src/web/views/LandingPageView/HowItWorks/components/MoreTools/MoreTools.vue
@@ -13,7 +13,7 @@
           class="pb-12"
         >
           <img :src="content.img" height="60" />
-          <div class="mt-3">{{ content.title }}</div>
+          <h5 class="font-weight-bold mt-3">{{ content.title }}</h5>
           <v-sheet max-width="300px" class="mt-3">
             <p class="ma-0">{{ content.description }}</p>
           </v-sheet>
@@ -47,39 +47,39 @@ export default {
     blockContent: [
       {
         img: Contract,
-        title: 'Contract',
+        title: 'Smart Contract',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'Interact with any smart contract on the Ethereum Blockchain.'
       },
       {
         img: Message,
         title: 'Message',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'MEWconnect, an official, free companion app for MEW that helps you secure your funds as never before.'
       },
       {
         img: ViewOnlyWallet,
         title: 'View Only Wallet',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'MEWconnect, an official, free companion app for MEW that helps you secure your funds as never before.'
       },
       {
         img: ChromeEx,
-        title: 'MEW Chrome Extension',
+        title: 'MEW CX',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'MUse our Web3-enabled Chrome Extension to interact with Dapps right in your browser.'
       },
       {
         img: BuyEth,
-        title: 'Buy Ethereum',
+        title: 'Buy Eth',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'Purchase Ether using only a credit card through our partner Simplex.'
       },
       {
         img: AdvancedTools,
-        title: 'More Advanced Tools',
+        title: 'Advanced Tools',
         description:
-          'Ipsum dolor diam justo ea gubergren tempor takimata takimata lorem lorem, voluptua ipsum voluptua consetetur sea nonumy.'
+          'MEWconnect, an official, free companion app for MEW that helps you secure your funds as never before.'
       }
     ]
   })
