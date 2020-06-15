04baf40f050ed45f032edb70c7def86ec0f31480
David Hong <hsurf22@gmail.com>
2020-06-15 15:47:09 -0700

    Add MnemonicPhrase pages

---
+++ src/web/views/CreateWalletView/MnemonicPhrase/components/Step2/Step2.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <v-sheet max-width="800px" class="mx-auto border-radius--10px pa-12">
+      <div class="subtitle-1 font-weight-bold grey--text">STEP 2.</div>
+      <div class="headline font-weight-bold mb-5">Download keystore file</div>
+      <v-row class="align-stretch">
+        <v-col v-for="d in warningData" :key="d.key">
+          <div class="warning-box border-radius--10px pa-6 text-center">
+            <v-img
+              :src="d.img"
+              max-width="63px"
+              max-height="76px"
+              class="mx-auto mb-4"
+            />
+            <div class="subtitle-1 font-weight-medium mb-1">
+              {{ d.title }}
+            </div>
+            <div>{{ d.description }}</div>
+          </div>
+        </v-col>
+      </v-row>
+      <div class="d-flex justify-center mt-6">
+        <mew6-std-btn
+          size="x-large"
+          buttonclass="button--green"
+          @click.native="linkToStep(3)"
+        >
+          Acknowledge &amp; Download
+        </mew6-std-btn>
+      </div>
+    </v-sheet>
+    <NotRecommended class="mt-6 border-radius--10px" />
+  </div>
+</template>
+
+<script>
+import NotRecommended from '@/web/components/NotRecommended';
+
+export default {
+  components: { NotRecommended },
+  data: () => ({
+    warningData: [
+      {
+        img: require('@/assets/images/icons/icon-paper-plane-mew.svg'),
+        title: "Don't lose it",
+        description: 'Be careful, it can not be recovered if you lose it.'
+      },
+      {
+        img: require('@/assets/images/icons/icon-money-bag-mew.svg'),
+        title: "Don't share it",
+        description:
+          'Your funds will be stolen if you use this file on a malicious phishing site.'
+      },
+      {
+        img: require('@/assets/images/icons/icon-copy-mew.svg'),
+        title: 'Make a backup',
+        description:
+          'Secure it like the millions of dollars it may one day be worth.'
+      }
+    ]
+  }),
+  methods: {
+    linkToStep(step) {
+      this.$router.push({
+        path: '/create-wallet/keystore',
+        query: { step: step }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.warning-box {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.05);
+  height: 100%;
+}
+</style>
