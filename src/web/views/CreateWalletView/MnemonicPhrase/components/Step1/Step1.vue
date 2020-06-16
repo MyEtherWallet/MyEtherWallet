476436ee12c20c456c1ee6981552405c03277efe
David Hong <hsurf22@gmail.com>
2020-06-15 21:17:37 -0700

    Update Mnemonic phrase

--- src/web/views/CreateWalletView/MnemonicPhrase/components/Step1/Step1.vue
+++ src/web/views/CreateWalletView/MnemonicPhrase/components/Step1/Step1.vue
@@ -4,8 +4,7 @@
       <div class="subtitle-1 font-weight-bold grey--text">STEP 1.</div>
       <div class="headline font-weight-bold mb-5">Write down the words</div>
       <PhraseBlock>
-        <div>aaaa</div>
-        <div></div>
+        <MnemonicPhraseTable :data="phrases" />
       </PhraseBlock>
       <mew6-std-btn size="x-small" min-width="0" @click.native="linkToStep(2)">
         Next page -->
@@ -18,10 +17,17 @@
 <script>
 import Caution from '@/components/WarningBlocks/MnemonicCaution';
 import PhraseBlock from '../PhraseBlock';
+import MnemonicPhraseTable from '@/components/MnemonicPhrase/MnemonicPhraseTable';
+import MnemonicTools from '@/common/helpers/mnemonicTools';
 
 export default {
-  components: { Caution, PhraseBlock },
-  data: () => ({}),
+  components: { Caution, PhraseBlock, MnemonicPhraseTable },
+  data: () => ({
+    phrases: ['duty', 'aaa', 'bbbb']
+  }),
+  mounted() {
+    console.log(MnemonicTools);
+  },
   methods: {
     linkToStep(step) {
       this.$router.push({
