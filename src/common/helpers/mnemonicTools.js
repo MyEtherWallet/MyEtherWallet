a38c457c4aa71be1d6f784ae03466f4e3a5654f6
David Hong <hsurf22@gmail.com>
2020-06-15 21:17:37 -0700

    Update Mnemonic phrase

---
+++ src/common/helpers/mnemonicTools.js
@@ -0,0 +1,12 @@
+function phrase128() {
+  return ['11111112222222'];
+}
+
+function phrase256() {
+  return ['11111112222222'];
+}
+
+export default {
+  phrase128,
+  phrase256
+};
---
+++ src/components/MnemonicPhrase/MnemonicPhraseTable/MnemonicPhraseTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col v-for="(p, key) in data" :key="key"> {{ key + 1 }}. {{ p }} </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    data: {
+      default: () => [],
+      type: Array
+    }
+  },
+  data: () => ({})
+};
+</script>
