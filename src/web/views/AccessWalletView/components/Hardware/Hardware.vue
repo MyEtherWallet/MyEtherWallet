715ba63b13088a032784a413fc35e9cceeb67c80
David Hong <hsurf22@gmail.com>
2020-06-19 17:22:47 -0700

    Add access hardware wallets

--- src/web/views/AccessWalletView/components/Hardware/Hardware.vue
+++ src/web/views/AccessWalletView/components/Hardware/Hardware.vue
@@ -1,31 +1,28 @@
 <template>
-  <a
-    href="https://chrome.google.com/webstore/detail/mew-cx/nlbmnnijcnlegkjjpcfjclmcfggfefdm?hl=en"
-    target="_blank"
-    class="display--block text-decoration--none"
+  <mew6-flexible-btn
+    :to="{ name: 'AccessWalletHardwareWallets', query: {} }"
+    small
   >
-    <mew6-flexible-btn small>
-      <div class="d-flex align-center justify-space-between">
-        <div class="pr-9">
-          <div class="d-flex align-center mb-2">
-            <h4 class="mr-2 font-weight-bold">Hardware wallets</h4>
-            <mew6-icon-secure />
-          </div>
-          <div>
-            Ledge, Trezor, Keep key, FINNEY, BitBox, Secalot
-          </div>
+    <div class="d-flex align-center justify-space-between">
+      <div class="pr-9">
+        <div class="d-flex align-center mb-2">
+          <h4 class="mr-2 font-weight-bold">Hardware wallets</h4>
+          <mew6-icon-secure />
         </div>
         <div>
-          <img
-            height="80"
-            src="@/assets/images/icons/icon-hardware-wallet.png"
-            alt="MEWcx"
-            class="mr-n4"
-          />
+          Ledge, Trezor, Keep key, FINNEY, BitBox, Secalot
         </div>
       </div>
-    </mew6-flexible-btn>
-  </a>
+      <div>
+        <img
+          height="80"
+          src="@/assets/images/icons/icon-hardware-wallet.png"
+          alt="MEWcx"
+          class="mr-n4"
+        />
+      </div>
+    </div>
+  </mew6-flexible-btn>
 </template>
 
 <script>
