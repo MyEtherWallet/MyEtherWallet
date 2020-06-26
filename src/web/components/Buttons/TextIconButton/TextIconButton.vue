223903c0e02cdf5f96a015fa33cf2577713b5c06
David Hong <hsurf22@gmail.com>
2020-06-26 16:14:24 -0700

    Update access wallet > mobile apps

--- src/web/components/Buttons/TextIconButton/TextIconButton.vue
+++ src/web/components/Buttons/TextIconButton/TextIconButton.vue
@@ -1,7 +1,10 @@
 <template>
   <mew6-flexible-btn small :to="to">
     <div class="d-flex align-center justify-space-between py-2">
-      <h4 class="mr-2 font-weight-bold">{{ label }}</h4>
+      <div>
+        <h4 class="mr-2 font-weight-bold">{{ label }}</h4>
+        <div v-if="description">{{ description }}</div>
+      </div>
       <img :src="icon" :alt="label" height="50" />
     </div>
   </mew6-flexible-btn>
@@ -12,6 +15,7 @@ export default {
   components: {},
   props: {
     label: { default: '', type: String },
+    description: { default: '', type: String },
     icon: { default: '', type: String },
     to: { default: () => {}, type: Object }
   },
