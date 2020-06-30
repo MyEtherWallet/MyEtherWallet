1275b21f9723cbb092bfe503506d87be238e7d44
gamalielhere <gamalielgad1234@gmail.com>
2020-06-30 16:21:59 -0700

    Add vuex actions

--- src/store/mewcx/actions.js
+++ src/store/mewcx/actions.js
@@ -1,1 +1,52 @@
-export default {};
+const setState = function ({ commit }, obj) {
+  commit('SET_STATE', obj);
+};
+
+const addAccount = function ({ commit }, obj) {
+  commit('ADD_ACCOUNT', obj);
+};
+
+const deleteAccount = function ({ commit }, obj) {
+  commit('DELETE_ACCOUNT', obj);
+};
+
+const updateAccount = function ({ commit }, obj) {
+  commit('UPDATE_ACCOUNT', obj);
+};
+
+const addFavoriteWallet = function ({ commit }, obj) {
+  commit('ADD_FAVORITE_WALLET', obj);
+};
+
+const deleteFavoriteWallet = function ({ commit }, obj) {
+  commit('DELETE_FAVORITE_WALLET', obj);
+};
+
+const setDefaultNetwork = function ({ commit }, obj) {
+  commit('SET_DEF_NETWORK', obj);
+};
+
+const setDefaultChainId = function ({ commit }, obj) {
+  commit('SET_DEF_CHAIN_ID', obj);
+};
+
+const addSite = function ({ commit }, obj) {
+  commit('ADD_SITE', obj);
+};
+
+const deleteSite = function ({ commit }, obj) {
+  commit('DELETE_SITE', obj);
+};
+
+export default {
+  setState,
+  addAccount,
+  deleteAccount,
+  updateAccount,
+  addFavoriteWallet,
+  setDefaultNetwork,
+  deleteFavoriteWallet,
+  setDefaultChainId,
+  addSite,
+  deleteSite
+};
--- src/store/mewcx/mutations.js
+++ src/store/mewcx/mutations.js
@@ -1,1 +1,6 @@
-export default {};
+const SET_STATE = function (state, obj) {
+  Object.keys(obj).forEach(item => {
+    state[item] = obj[item];
+  });
+};
+export default { SET_STATE };
