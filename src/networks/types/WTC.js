1ded8dfd0326b371da83455bb6049ee45d372aaa
WaltonChain <33472780+WaltonChain@users.noreply.github.com>
2020-06-30 11:51:10 +0800

    Create WTC.js

---
+++ src/networks/types/WTC.js
@@ -0,0 +1,16 @@
+import tokens from '@/_generated/tokens/wtc/0x668df218d073f413Ed2FCEa0D48CfbFd59C030Ae.json';
+import contracts from '@/_generated/contracts/wtc/0x668df218d073f413Ed2FCEa0D48CfbFd59C030Ae.json';
+import wtc from '@/assets/images/networks/wtc.svg';
+
+export default {
+  name: 'WTC',
+  name_long: 'Waltonchain',
+  homePage: 'https://www.waltonchain.org/en/',
+  blockExplorerTX: 'https://waltonchain.pro/pc/src/searchTran.html?[[txHash]]&ver=1.3',
+  blockExplorerAddr: 'https://waltonchain.pro/pc/src/searchMiner.html?[[address]]&ver=1.3',
+  chainID: 15,
+  tokens: tokens,
+  contracts: contracts,
+  icon: wtc,
+  currencyName: 'WTC'
+};
