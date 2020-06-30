0c1918ff6e3db85802621c8dd227efd52f313258
WaltonChain <33472780+WaltonChain@users.noreply.github.com>
2020-06-30 11:52:19 +0800

    Create wtc.js

---
+++ src/networks/nodes/wtc.js
@@ -0,0 +1,10 @@
+import { WTC } from '../types';
+export default {
+  type: WTC,
+  service: 'waltonchain.pro',
+  url: 'https://node.waltonchain.pro',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
