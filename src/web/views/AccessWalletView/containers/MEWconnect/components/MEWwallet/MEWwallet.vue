877bfc6208ed5a0a5edf4a880c761ecd257f4f06
David Hong <hsurf22@gmail.com>
2020-07-01 01:06:03 -0700

    Add mew components to create wallet

--- src/web/views/AccessWalletView/containers/MEWconnect/components/MEWwallet/MEWwallet.vue
+++ src/web/views/AccessWalletView/containers/MEWconnect/components/MEWwallet/MEWwallet.vue
@@ -1,50 +1,62 @@
 <template>
-  <a
-    href="https://www.mewwallet.com/"
-    target="_blank"
-    class="display--block text-decoration--none"
-  >
-    <mew6-flexible-btn nopadding>
-      <div class="d-flex bg pa-10 border-radius--10px">
-        <div>
-          <div class="d-flex align-center mb-2">
-            <h4 class="mr-2 white--text font-weight-bold">Get MEWwallet</h4>
-            <mew6-icon-secure color="white" />
+  <div>
+    <mew-super-button
+      class="mb-1"
+      color-theme="basic"
+      title="Get MEWwallet"
+      subtitle="Still stick on MEWconnect? Fully fledged new MEWwallet App is now
+              available."
+      :right-icon="require('@/assets/images/snippets/bg-mew-wallet.png')"
+      :title-icon="require('@/assets/images/icons/icon-mew-cx.png')"
+    />
+
+    <a
+      href="https://www.mewwallet.com/"
+      target="_blank"
+      class="display--block text-decoration--none"
+    >
+      <mew6-flexible-btn nopadding>
+        <div class="d-flex bg pa-10 border-radius--10px">
+          <div>
+            <div class="d-flex align-center mb-2">
+              <h4 class="mr-2 white--text font-weight-bold">Get MEWwallet</h4>
+              <mew6-icon-secure color="white" />
+            </div>
+            <div class="white--text">
+              Still stick on MEWconnect? Fully fledged new MEWwallet App is now
+              available.
+            </div>
+            <div class="mt-6">
+              <img
+                height="30"
+                src="@/assets/images/icons/button-app-store.png"
+                alt="Apple store"
+                class="mr-1"
+              />
+              <img
+                height="30"
+                src="@/assets/images/icons/button-play-store.png"
+                alt="Apple store"
+                class="mr-1"
+              />
+              <img
+                height="30"
+                src="@/assets/images/icons/button-samsung.png"
+                alt="Apple store"
+              />
+            </div>
           </div>
-          <div class="white--text">
-            Still stick on MEWconnect? Fully fledged new MEWwallet App is now
-            available.
-          </div>
-          <div class="mt-6">
+          <div class="side-image position--relative">
             <img
-              height="30"
-              src="@/assets/images/icons/button-app-store.png"
-              alt="Apple store"
-              class="mr-1"
-            />
-            <img
-              height="30"
-              src="@/assets/images/icons/button-play-store.png"
-              alt="Apple store"
-              class="mr-1"
-            />
-            <img
-              height="30"
-              src="@/assets/images/icons/button-samsung.png"
-              alt="Apple store"
+              height="160"
+              src="@/assets/images/snippets/bg-mew-wallet.png"
+              alt="MEWwallet"
             />
           </div>
         </div>
-        <div class="side-image position--relative">
-          <img
-            height="160"
-            src="@/assets/images/snippets/bg-mew-wallet.png"
-            alt="MEWwallet"
-          />
-        </div>
-      </div>
-    </mew6-flexible-btn>
-  </a>
+      </mew6-flexible-btn>
+    </a>
+  </div>
 </template>
 
 <script>
--- src/web/views/AccessWalletView/containers/MEWconnect/components/ScanToConnect/ScanToConnect.vue
+++ src/web/views/AccessWalletView/containers/MEWconnect/components/ScanToConnect/ScanToConnect.vue
@@ -1,19 +1,29 @@
 <template>
-  <mew6-flexible-btn noclick>
-    <div class="d-flex align-center justify-space-between">
-      <div class="pr-9">
-        <div class="d-flex align-center mb-2">
-          <h4 class="mr-2 font-weight-bold">Scan to connect</h4>
+  <div>
+    <mew-super-button
+      class="mb-1"
+      color-theme="basic"
+      title="Scan to connect"
+      subtitle="Please use the MEWconnect App to scan the QR code."
+      :right-icon="require('@/assets/images/snippets/bg-mew-wallet.png')"
+    />
+
+    <mew6-flexible-btn noclick>
+      <div class="d-flex align-center justify-space-between">
+        <div class="pr-9">
+          <div class="d-flex align-center mb-2">
+            <h4 class="mr-2 font-weight-bold">Scan to connect</h4>
+          </div>
+          <div>Please use the MEWconnect App to scan the QR code.</div>
         </div>
-        <div>Please use the MEWconnect App to scan the QR code.</div>
+        <VueQrcode
+          class="my-n2"
+          value="C4078c76aa30fb32"
+          :options="{ size: 130 }"
+        />
       </div>
-      <VueQrcode
-        class="my-n2"
-        value="C4078c76aa30fb32"
-        :options="{ size: 130 }"
-      />
-    </div>
-  </mew6-flexible-btn>
+    </mew6-flexible-btn>
+  </div>
 </template>
 
 <script>
