9680054870ccefee396ce179196d85081ea60a8e
David Hong <hsurf22@gmail.com>
2020-07-09 15:33:01 -0700

    Update NFT manager

--- src/web/containers/SendContainer/NFTManager/NFTManagerContent/NFTManagerContent.vue
+++ src/web/containers/SendContainer/NFTManager/NFTManagerContent/NFTManagerContent.vue
@@ -12,124 +12,7 @@
 
     <mew6-white-sheet>
       <InterfaceWrap title="NFT Manager">
-        <v-tabs vertical>
-          <v-tab>
-            <div class="text-transform--initial">Cryptokitties (3)</div>
-          </v-tab>
-          <v-tab>
-            <div class="text-transform--initial">Gods Unchained (2)</div>
-          </v-tab>
-          <v-tab>
-            <div class="text-transform--initial">CryptoFlowers (0)</div>
-          </v-tab>
-          <v-tab>
-            <div class="text-transform--initial">MyCryptoHeroes (1)</div>
-          </v-tab>
-
-          <v-tab-item>
-            <v-card flat>
-              <div class="px-3 mb-5 d-flex align-center justify-space-between">
-                <div class="font-weight-bold">CryptoKitties</div>
-                <div>Total 3</div>
-              </div>
-              <div>
-                <v-card
-                  v-for="(kitty, key) in CryptoKitties"
-                  :key="key"
-                  flat
-                  color="superPrimary"
-                >
-                  <div
-                    class="d-flex align-center mb-2 px-4 justify-space-between"
-                  >
-                    <div class="d-flex align-center">
-                      <img class="mr-5" :src="kitty.image" height="90" />
-                      <div>#{{ kitty.number }}</div>
-                    </div>
-                    <mew-button
-                      :has-full-width="false"
-                      btn-style="outline"
-                      title="Send"
-                      button-size="large"
-                    />
-                  </div>
-                </v-card>
-              </div>
-            </v-card>
-          </v-tab-item>
-
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text>
-                <p>
-                  Sed aliquam ultrices mauris. Donec posuere vulputate arcu.
-                  Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
-                </p>
-
-                <p>
-                  Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
-                  lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
-                  non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
-                  nisl sit amet velit hendrerit rutrum.
-                </p>
-
-                <p class="mb-0">
-                  Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
-                  Pellentesque libero tortor, tincidunt et, tincidunt eget,
-                  semper nec, quam. Phasellus blandit leo ut odio.
-                </p>
-              </v-card-text>
-            </v-card>
-          </v-tab-item>
-
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text>
-                <p>
-                  Sed aliquam ultrices mauris. Donec posuere vulputate arcu.
-                  Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
-                </p>
-
-                <p>
-                  Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
-                  lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
-                  non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
-                  nisl sit amet velit hendrerit rutrum.
-                </p>
-
-                <p class="mb-0">
-                  Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
-                  Pellentesque libero tortor, tincidunt et, tincidunt eget,
-                  semper nec, quam. Phasellus blandit leo ut odio.
-                </p>
-              </v-card-text>
-            </v-card>
-          </v-tab-item>
-
-          <v-tab-item>
-            <v-card flat>
-              <v-card-text>
-                <p>
-                  Sed aliquam ultrices mauris. Donec posuere vulputate arcu.
-                  Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
-                </p>
-
-                <p>
-                  Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
-                  lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
-                  non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
-                  nisl sit amet velit hendrerit rutrum.
-                </p>
-
-                <p class="mb-0">
-                  Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
-                  Pellentesque libero tortor, tincidunt et, tincidunt eget,
-                  semper nec, quam. Phasellus blandit leo ut odio.
-                </p>
-              </v-card-text>
-            </v-card>
-          </v-tab-item>
-        </v-tabs>
+        <mew6-vertical-tabs :tabs="tabs" />
       </InterfaceWrap>
     </mew6-white-sheet>
   </div>
@@ -137,19 +20,20 @@
 
 <script>
 import InterfaceWrap from '@/web/components/InterfaceWraps/InterfaceWrap1';
-import Kitty1 from '@/assets/images/temp/cryptokitties1.svg';
-import Kitty2 from '@/assets/images/temp/cryptokitties2.svg';
-import Kitty3 from '@/assets/images/temp/cryptokitties3.svg';
+import CryptoFlowers from './components/CryptoFlowers';
+import CryptoKitties from './components/CryptoKitties';
+import GodsUnchained from './components/GodsUnchained';
+import MyCryptoHeros from './components/MyCryptoHeros';
 
 export default {
   components: { InterfaceWrap },
   data() {
     return {
-      openTxConfirmation: false,
-      CryptoKitties: [
-        { image: Kitty1, number: '4534' },
-        { image: Kitty2, number: '4537' },
-        { image: Kitty3, number: '4589' }
+      tabs: [
+        { label: 'CryptoKitties (3)', content: CryptoKitties },
+        { label: 'Gods Unchained (2)', content: GodsUnchained },
+        { label: 'CryptoFlowers (1)', content: CryptoFlowers },
+        { label: 'MyCryptoHeros (1)', content: MyCryptoHeros }
       ]
     };
   },
@@ -161,8 +45,4 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.v-tab {
-  justify-content: flex-start;
-}
-</style>
+<style lang="scss" scoped></style>
