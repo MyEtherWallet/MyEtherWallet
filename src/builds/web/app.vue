df609210bafce12968107e46b5a490fa3acd9fdf
David Hong <hsurf22@gmail.com>
2020-07-16 06:57:24 -0700

    Add more mew components

--- src/builds/web/app.vue
+++ src/builds/web/app.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-content>
+    <v-main>
       <router-view />
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
--- src/web/containers/DappsContainer/MakerDAO/components/WalletBalance/WalletBalance.vue
+++ src/web/containers/DappsContainer/MakerDAO/components/WalletBalance/WalletBalance.vue
@@ -1,85 +1,53 @@
 <template>
-  <div class="more-actions--table pb-5">
-    <v-data-table
-      :headers="headers"
-      :items="addresses"
-      :items-per-page="10"
-      item-key="id"
-      sort-by="calories"
-    >
-      <template v-slot:item.icon="{ item }">
-        <div class="d-flex align-center justify-center">
-          <img :src="item.icon" :alt="item.token" height="24" class="pl-4" />
-        </div>
-      </template>
-      <template v-slot:item.change="{ item }"> {{ item.change }}% </template>
-    </v-data-table>
-  </div>
+  <mew-table :table-headers="tableHeaders" :table-data="tableData" />
 </template>
 
 <script>
 export default {
   components: {},
   data: () => ({
-    headers: [
+    tableHeaders: [
+      {
+        text: 'Asset',
+        value: 'asset',
+        sortable: true,
+        filterable: false,
+        containsLink: false,
+        width: '100%'
+      },
+      {
+        text: 'Balance',
+        value: 'balance',
+        sortable: true,
+        filterable: false,
+        containsLink: false,
+        width: '100%'
+      },
+      {
+        text: 'USD Value',
+        value: 'value',
+        sortable: true,
+        filterable: false,
+        containsLink: false,
+        width: '100%'
+      },
       {
         text: '',
-        value: 'icon',
-        sortable: false
-      },
-      {
-        text: 'Token Name',
-        value: 'token'
-      },
-      { text: 'Price', value: 'price' },
-      { text: 'Market Cap', value: 'marketCap' },
-      { text: 'Change(24h)', value: 'change' },
-      { text: 'Amount', value: 'amount' },
-      { text: 'My value', value: 'myvalue' }
+        value: 'btn',
+        sortable: false,
+        filterable: false,
+        containsLink: false,
+        width: '100%'
+      }
     ],
-    addresses: []
-  }),
-  created() {
-    this.initialize();
-  },
-  methods: {
-    initialize() {
-      this.addresses = [
-        {
-          icon: require('@/assets/images/currencies/icon-btc-gold.svg'),
-          token: 'Monero(XMR)',
-          price: '$8.29',
-          marketCap: '$1.56B',
-          change: 0.23,
-          amount: 27.54,
-          myvalue: '$228.42'
-        },
-        {
-          icon: require('@/assets/images/currencies/icon-eth-blue.svg'),
-          token: 'AMIS(AMIS)',
-          price: '$10.72',
-          marketCap: '$1.56B',
-          change: 0.23,
-          amount: 27.54,
-          myvalue: '$228.42'
-        }
-      ];
-    }
-  }
+    tableData: [
+      {
+        asset: 'DAI',
+        balance: '15.231234 DAI',
+        value: '$12.23',
+        btn: 'Send'
+      }
+    ]
+  })
 };
 </script>
-
-<style lang="scss">
-.more-actions--table {
-  .v-data-table-header {
-    background-color: var(--v-bg_table-base);
-  }
-  .v-data-table thead tr th {
-    text-transform: uppercase;
-    font-weight: 500;
-    &:not(:hover) {
-      color: var(--v-gray_text-base) !important;
-    }
-  }
-}
-</style>
