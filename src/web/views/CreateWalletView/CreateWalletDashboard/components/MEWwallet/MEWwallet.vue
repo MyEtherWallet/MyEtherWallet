435c1daaab3a3553f29376970f47ab75aca0744d
Jessica <jessicaspeng@gmail.com>
2020-07-21 16:01:22 -0700

    Update create wallet page

--- src/web/views/CreateWalletView/CreateWalletDashboard/components/MEWwallet/MEWwallet.vue
+++ src/web/views/CreateWalletView/CreateWalletDashboard/components/MEWwallet/MEWwallet.vue
@@ -5,14 +5,23 @@
     class="display--block text-decoration--none"
   >
     <mew-super-button
+      class="mew-wallet-btn"
       color-theme="basic"
       title="Get MEWwallet"
       subtitle="MEW Brings a hardware wallet style security to your iOS or Android
             smartphone, helping you to secure your funds as never before."
       title-icon="mdi-shield-check"
-      icon-type="mdi"
-      :right-icon="require('@/assets/images/snippets/bg-mew-wallet.png')"
-    />
+      title-icon-type="mdi"
+      right-icon-type="img"
+      title-icon-class="primary--text"
+    >
+      <template v-slot:rightSlot>
+        <img
+          class="mew-wallet-img"
+          :src="require('@/assets/images/snippets/bg-mew-wallet.png')"
+        />
+      </template>
+    </mew-super-button>
   </a>
 </template>
 
@@ -22,3 +31,12 @@ export default {
   data: () => ({})
 };
 </script>
+<style lang="scss" scoped>
+.mew-wallet-btn {
+  img {
+    height: 140px;
+    position: absolute;
+    bottom: 0;
+  }
+}
+</style>
