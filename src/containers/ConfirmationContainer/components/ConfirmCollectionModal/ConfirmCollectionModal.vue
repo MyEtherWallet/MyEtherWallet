515d6a1c4e098f23dd41cbd98be7b4c72d566591
gamalielhere <gamalielgad1234@gmail.com>
2020-07-23 15:46:29 -0700

    Move gas price warning outside of tx container

--- src/containers/ConfirmationContainer/components/ConfirmCollectionModal/ConfirmCollectionModal.vue
+++ src/containers/ConfirmationContainer/components/ConfirmCollectionModal/ConfirmCollectionModal.vue
@@ -28,10 +28,10 @@
             {{ network.type.currencyName }}
           </p>
         </div>
+        <div v-if="showGasWarning" class="gas-price-warning">
+          {{ $t('errorsGlobal.high-gas-limit-warning') }}
+        </div>
         <div class="modal-content-body">
-          <div v-if="showGasWarning" class="gas-price-warning">
-            {{ $t('errorsGlobal.high-gas-limit-warning') }}
-          </div>
           <div
             v-for="(item, idx) in unSignedArray"
             :key="item.to + idx + item.value"
