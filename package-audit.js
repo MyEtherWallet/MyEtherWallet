a14225a4ae7a928fe3eb494d707d9d694da43f64
gamalielhere <gamalielgad1234@gmail.com>
2020-07-27 12:10:19 -0700

    Add stuff

--- package-audit.js
+++ package-audit.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-const AUDIT_EXCEPTIONS = ['handlebars', 'decompress', 'http-proxy'];
+const AUDIT_EXCEPTIONS = [];
 
 const execute = (command, callback) => {
   exec(
