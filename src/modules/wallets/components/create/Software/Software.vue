8b82a0cfb64fd18652da85cdbb4d1d3ae3a2e858
gamalielhere <gamalielgad1234@gmail.com>
2020-07-31 12:28:19 -0700

    Remove nested folders from create wallet, add missing imports

--- src/modules/wallets/components/create/Software/Software.vue
+++ src/modules/wallets/components/create/Software/Software.vue
@@ -1,71 +1,69 @@
 <template>
   <div>
-    <BaseOverlay :open="open">
-      <BlockTitle
-        :data="
-          type === ''
-            ? softwareTitle
-            : type === 'keystore'
-            ? keyStoreTitle
-            : mnemonicTtitle
-        "
-      />
-      <v-sheet
-        v-if="type === '' && step === 0"
-        color="transparent"
-        max-width="650px"
-        class="mx-auto"
-      >
-        <div class="mb-5">
-          <mew-super-button
-            color-theme="basic"
-            title="Keystore File"
-            subtitle="Keystore file contains all the sensitive information of your wallet.
+    <mew-overlay :show-overlay="open" :title="typeTitle">
+      <template v-slot:mewOverlayBody>
+        <v-sheet
+          v-if="type === '' && step === 0"
+          color="transparent"
+          max-width="650px"
+          class="mx-auto"
+        >
+          <div class="mb-5">
+            <mew-super-button
+              color-theme="basic"
+              title="Keystore File"
+              subtitle="Keystore file contains all the sensitive information of your wallet.
                   We don't recommand using this method to create your wallet."
-            icon-type="img"
-            :right-icon="
-              require('@/assets/images/icons/icon-keystore-file.svg')
-            "
-            @click.native="
-              () => {
-                createType('keystore');
-                updateStep(1);
-              }
-            "
-          />
-        </div>
-        <div class="mb-5">
-          <mew-super-button
-            class="mb-1"
-            color-theme="basic"
-            title="Mnemonic phrase"
-            subtitle="Mnemonic Phrase can be lost or stolen by someone else. We don't
+              icon-type="img"
+              :right-icon="
+                require('@/assets/images/icons/icon-keystore-file.svg')
+              "
+              @click.native="
+                () => {
+                  createType('keystore');
+                }
+              "
+            />
+          </div>
+          <div class="mb-5">
+            <mew-super-button
+              class="mb-1"
+              color-theme="basic"
+              title="Mnemonic phrase"
+              subtitle="Mnemonic Phrase can be lost or stolen by someone else. We don't
                   recommand using this method to create your wallet."
-            icon-type="img"
-            :right-icon="require('@/assets/images/icons/icon-mnemonic.svg')"
+              icon-type="img"
+              :right-icon="require('@/assets/images/icons/icon-mnemonic.svg')"
+              @click.native="
+                () => {
+                  createType('mnemonic');
+                }
+              "
+            />
+          </div>
+          <warning-sheet
+            title="NOT RECOMMENDED"
+            description='This information is sensitive, and these options should only be used in offline settings by experienced crypto users. And MEW "CAN NOT" change your password. Please "DO NOT FORGET" to save your password, and it is your private key. You will need this "Password + Keystore file" to access your wallet.'
           />
+        </v-sheet>
+        <div v-else-if="type === 'keystore'">
+          <keystore :update-step="updateStep" :step="step" />
         </div>
-        <warning-sheet
-          title="NOT RECOMMENDED"
-          description='This information is sensitive, and these options should only be used in offline settings by experienced crypto users. And MEW "CAN NOT" change your password. Please "DO NOT FORGET" to save your password, and it is your private key. You will need this "Password + Keystore file" to access your wallet.'
-        />
-      </v-sheet>
-      <div v-else-if="type === 'keystore'">
-        <Keystore :update-step="updateStep" :step="step" />
-      </div>
-      <div v-else-if="type === 'mnem'"></div>
-    </BaseOverlay>
+        <div v-else-if="type === 'mnemonic'">
+          <mnemonic-phrase :update-step="updateStep" :step="step" />
+        </div>
+      </template>
+    </mew-overlay>
   </div>
 </template>
 
 <script>
-import BlockTitle from '@/components/BlockTitle';
-import BaseOverlay from '@/components/Overlays/BaseOverlay';
 import Keystore from '../Keystore';
+import MnemonicPhrase from '../MnemonicPhrase';
 
 export default {
   name: 'Software',
-  components: { BlockTitle, BaseOverlay, Keystore },
+  components: { keystore: Keystore, 'mnemonic-phrase': MnemonicPhrase },
   props: {
     open: {
       type: Boolean,
@@ -73,31 +71,30 @@ export default {
     }
   },
   data: () => ({
-    softwareTitle: {
-      textProps: 'black--text',
-      title: 'Software',
-      description: 'This would be most simplest way to create your wallet!',
-      descriptionMaxWidth: '400px',
-      centered: true
-    },
-    keyStoreTitle: {
-      textProps: 'black--text',
-      title: 'Keystore file',
-      description:
-        'An official, free companion App for MyEtherWallet that helps you secure your funds as never before.',
-      descriptionMaxWidth: '400px',
-      centered: true
-    },
-    mnemonicTtitle: {
-      textProps: 'black--text',
-      title: 'Mnemonic Phrase',
-      description: 'This would be most simplest way to create your wallet!',
-      descriptionMaxWidth: '400px',
-      centered: true
-    },
     type: '',
     step: 0
   }),
+  computed: {
+    typeTitle() {
+      return this.type === ''
+        ? 'Software'
+        : this.type === 'keystore'
+        ? 'Keystore File'
+        : 'Mnemonic Phrase';
+    }
+  },
+  watch: {
+    type(newVal) {
+      if (newVal === '') {
+        this.step = 0;
+      } else {
+        this.step = 1;
+      }
+    },
+    step(newVal) {
+      console.log(newVal);
+    }
+  },
   methods: {
     createType(type) {
       this.type = type ? type : '';
