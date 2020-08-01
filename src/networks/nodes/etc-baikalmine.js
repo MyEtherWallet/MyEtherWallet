2ea63703716f199665eb305d6a983e57e06f710c
BaikalMine <31639973+BaikalMine@users.noreply.github.com>
2020-08-01 20:58:11 +0800

    Add BaikalMine ETC nodes for MEW

---
+++ src/networks/nodes/etc-baikalmine.js
@@ -0,0 +1,10 @@
+import { ETC } from '../types';
+export default {
+  type: ETC,
+  service: 'BaikalMine.com',
+  url: 'https://etc-mew.baikalmine.com',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
