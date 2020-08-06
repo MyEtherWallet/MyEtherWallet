297df1e4a2674452133c4841ad6efe3c7a93df68
gamalielhere <gamalielgad1234@gmail.com>
2020-08-06 15:38:11 -0700

    More cleanup

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -1,31 +1,20 @@
 <template>
-  <div>
-    <landing />
-    <features class="pt-12 pb-6" />
-    <why-mew class="py-6" />
-    <join class="py-5" />
-    <partners class="py-11" />
-    <get-started class="py-9" />
-  </div>
+  <v-sheet>
+    <header-home />
+    <router-view />
+    <footer-home />
+  </v-sheet>
 </template>
 
 <script>
-import landing from './components/landing/Landing';
-import features from './components/features/Features';
-import whyMew from './components/why-mew/WhyMew';
-import join from '@/components/join/Join';
-import getStarted from '@/components/get-started/GetStarted';
-import partners from '@/components/partners/Partners';
+import headerHome from './components/header/Header';
+import footerHome from './components/footer/Footer';
 
 export default {
-  name: 'Home',
+  name: 'Navigation',
   components: {
-    landing,
-    features,
-    whyMew,
-    join,
-    getStarted,
-    partners
+    headerHome,
+    footerHome
   }
 };
 </script>
