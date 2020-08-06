9a5142a56073455cb5b6fcc2011e1f23e169d598
gamalielhere <gamalielgad1234@gmail.com>
2020-08-06 15:38:11 -0700

    More cleanup

---
+++ src/modules/home/pages/landing-page/LandingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <landing />
+    <features class="pt-12 pb-6" />
+    <why-mew class="py-6" />
+    <join class="py-5" />
+    <partners class="py-11" />
+    <get-started class="py-9" />
+  </div>
+</template>
+
+<script>
+import landing from '../../components/landing/Landing';
+import features from '../../components/features/Features';
+import whyMew from '../../components/why-mew/WhyMew';
+import join from '@/components/join/Join';
+import getStarted from '@/components/get-started/GetStarted';
+import partners from '@/components/partners/Partners';
+
+export default {
+  name: 'Home',
+  components: {
+    landing,
+    features,
+    whyMew,
+    join,
+    getStarted,
+    partners
+  }
+};
+</script>
--- src/modules/how-it-works/HowItWorks.vue
+++ src/modules/how-it-works/HowItWorks.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <home-header title="How it works" />
-    <tab-content class="pb-8" />
-    <swap class="pt-8" />
-    <dapps-center />
-    <tokens class="pb-3" />
-    <more class="py-3" />
-    <get-started class="pt-3" />
+    <mew-tabs :items="mewTabs">
+      <template v-slot:tabContent1>
+        <send />
+      </template>
+      <template v-slot:tabContent2>
+        <swap />
+      </template>
+      <template v-slot:tabContent3>
+        <dapps-center />
+      </template>
+      <template v-slot:tabContent4>
+        <tokens />
+      </template>
+      <template v-slot:tabContent5>
+        <more />
+      </template>
+    </mew-tabs>
+    <get-started />
   </div>
 </template>
 
 <script>
 import homeHeader from '@/components/home-header/HomeHeader';
-import tabContent from './components/tab-content/TabContent';
 import swap from './components/swap/Swap';
+import send from './components/send/Send';
 import dappsCenter from './components/dapps-center/DappsCenter';
 import tokens from './components/tokens/Tokens';
 import more from './components/more/More';
@@ -23,12 +35,33 @@ export default {
   name: 'HowItWorks',
   components: {
     homeHeader,
-    tabContent,
     swap,
+    send,
     dappsCenter,
     tokens,
     more,
     getStarted
+  },
+  data() {
+    return {
+      mewTabs: [
+        {
+          name: 'send'
+        },
+        {
+          name: 'swap'
+        },
+        {
+          name: 'dapps'
+        },
+        {
+          name: 'tokens'
+        },
+        {
+          name: 'more'
+        }
+      ]
+    };
   }
 };
 </script>
