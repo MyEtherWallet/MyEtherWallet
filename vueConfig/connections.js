6a0e2bb9c0143fe9ec831dc349dbff3127a189c6
gamalielhere <gamalielgad1234@gmail.com>
2020-08-12 10:57:24 -0700

    Fix ewt network

--- vueConfig/connections.js
+++ vueConfig/connections.js
@@ -70,7 +70,7 @@ module.exports = [
   'https://dispatch.pokt.network:443',
   'https://service.gcp.pokt.network:443',
   'https://rpc.fuse.io:443',
-  'https://rpc.energyweb.org/:443',
+  'https://rpc.energyweb.org:443',
   'https://s3.us-west-2.amazonaws.com:443/mew-ipfs-bucket',
   'wss://www.walletlink.org:443',
   'https://estimategas.mewapi.io:443'
