d7265c32fc425bd5f5c444391fcf069275d96f14
David Hong <hsurf22@gmail.com>
2020-08-12 19:55:47 -0700

    Add input to Watch only address

--- src/modules/tools/pages/watch-only/WatchOnly.vue
+++ src/modules/tools/pages/watch-only/WatchOnly.vue
@@ -2,6 +2,15 @@
   <div>
     <block-title no-page-title :data="title" class="mb-7" />
     <table-container />
+    <div class="py-3">
+      <address-select label="To address" :items="addresses"></address-select>
+      <mew-input label="Address name" placeholder=" " />
+      <mew-button
+        title="Add"
+        button-size="xlarge"
+        class="mt-3 mx-auto display--block"
+      />
+    </div>
   </div>
 </template>
 
@@ -12,6 +21,20 @@ import tableContainer from '../../components/table-container/TableContainer';
 export default {
   components: { blockTitle, tableContainer },
   data: () => ({
+    addresses: [
+      {
+        address: '0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D',
+        currency: 'ETH',
+        nickname: 'My Address',
+        resolverAddr: '0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D'
+      },
+      {
+        address: '0x43689531907482BEE7e650D18411E284A7337A66',
+        currency: 'ETH',
+        nickname: 'nickname',
+        resolverAddr: '0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D'
+      }
+    ],
     title: {
       title: 'Watch only address',
       description:
