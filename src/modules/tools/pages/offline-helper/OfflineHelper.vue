cbf57d2160ca5e9292c3a6d5c4875d9bcda5c71a
David Hong <hsurf22@gmail.com>
2020-08-12 20:10:25 -0700

    Update Convert units

--- src/modules/tools/pages/offline-helper/OfflineHelper.vue
+++ src/modules/tools/pages/offline-helper/OfflineHelper.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-    SendOfflineHelper
+    <mew-stepper :items="items" :on-step="2">
+      <template v-slot:outsideStepContent0>
+        111
+      </template>
+      <template v-slot:outsideStepContent0>
+        222
+      </template>
+      <template v-slot:outsideStepContent0>
+        333
+      </template>
+    </mew-stepper>
   </div>
 </template>
 
 <script>
 export default {
   components: {},
-  data: () => ({})
+  data: () => ({
+    items: [
+      {
+        step: 1,
+        name: 'STEP 1. Create password'
+      },
+      {
+        step: 2,
+        name: 'STEP 2. Download keystore file'
+      },
+      {
+        step: 3,
+        name: 'STEP 3. Well done'
+      }
+    ]
+  })
 };
 </script>
 
