679840f97760da8010dd37a3b84c3e4860f9ad42
David Hong <hsurf22@gmail.com>
2020-08-20 14:48:44 -0700

    update ambrpay

---
+++ src/modules/wallets/components/ambrpay-my-subscription-overlay/AmbrpayMySubscriptionOverlay.vue
@@ -0,0 +1,27 @@
+<template>
+  <mew-overlay :show-overlay="open">
+    <template v-slot:mewOverlayBody>
+      <div>My Subscription Overlay</div>
+    </template>
+  </mew-overlay>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    open: { default: false, type: Boolean },
+    close: {
+      default: function () {
+        return {};
+      },
+      type: Function
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
