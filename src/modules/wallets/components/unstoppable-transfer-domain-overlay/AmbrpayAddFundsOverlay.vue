e31b058afde9f6fe5f3519e8f0067222fd59902a
David Hong <hsurf22@gmail.com>
2020-08-20 20:49:39 -0700

    Update unstoppable domain

---
+++ src/modules/wallets/components/unstoppable-transfer-domain-overlay/AmbrpayAddFundsOverlay.vue
@@ -0,0 +1,27 @@
+<template>
+  <mew-overlay :show-overlay="open">
+    <template v-slot:mewOverlayBody>
+      <div>Add Funds Overlay</div>
+    </template>
+  </mew-overlay>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    open: { default: false, type: Boolean },
+    close: {
+      default: function () {
+        return {};
+      },
+      type: Function
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
