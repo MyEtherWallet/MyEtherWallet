4a9f35201386619ae16041e8b642226d0a1779f6
gamalielhere <gamalielgad1234@gmail.com>
2020-08-26 10:27:47 -0700

    Final changes

--- src/modules/name-manager/services/getHashFromFile.js
+++ src/modules/name-manager/services/getHashFromFile.js
@@ -1,28 +1,18 @@
 export default hash => {
-  return new Promise((resolve, reject) => {
-    try {
-      const ipfsHash = fetch('https://swap.mewapi.io/ipfs', {
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({
-          method: 'uploadComplete',
-          hash: hash
-        })
-      }).then(response => {
-        return response.json();
-      });
-
-      ipfsHash.then(response => {
-        if (response.error) {
-          return reject(new Error('Error getting IPFS Hash!'));
-        }
-
-        return resolve(response);
-      });
-    } catch (e) {
-      reject(e);
+  return fetch('https://swap.mewapi.io/ipfs', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      method: 'uploadComplete',
+      hash: hash
+    })
+  }).then(response => {
+    if (response.error) {
+      return new Error('Error getting IPFS Hash!');
     }
+
+    return response.json();
   });
 };
