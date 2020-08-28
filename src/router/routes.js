04a8045fcbba2fd1a4d712428ee67fbb3e29d052
gamalielhere <gamalielgad1234@gmail.com>
2020-08-28 11:26:37 -0700

    Finalize routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,19 +4,9 @@ import TeamPage from '@/modules/team/Team';
 import BuyHardwareWallet from '@/modules/hardware/Hardware';
 import ToolsPage from '@/modules/tools/Tools';
 import LandingPage from '@/modules/home/pages/landing-page/LandingPage';
-
 import CreateWallet from '@/modules/wallets/pages/create/CreateWallet';
-
 import AccessWallet from '@/modules/wallets/pages/access/Access';
-// import AccessWalletHardwareWallets from '@/modules/wallets/pages/access/hardware-wallets/HardwareWallets';
-// import AccessWalletMobileApps from '@/modules/wallets/pages/access/mobile-apps/MobileApps';
-// import AccessWalletBrowserExtension from '@/modules/wallets/pages/access/browser-extension/BrowserExtension';
-
-// import AccessWalletBrowserExtensionInstall from '@/modules/wallets/components/install-extension/InstallExtension';
-// import AccessWalletBrowserExtensionAccess from '@/modules/wallets/components/access-extension/AccessExtension';
-
 import HowItWorks from '@/modules/how-it-works/HowItWorks';
-
 import WalletRoutes from '@/modules/wallets/routes.js';
 
 const routes = [
@@ -88,46 +78,6 @@ const routes = [
           requiresAuth: false
         }
       }
-      // {
-      //   path: 'access-wallet/hardware',
-      //   name: 'AccessWalletHardwareWallets',
-      //   component: AccessWalletHardwareWallets,
-      //   meta: {
-      //     requiresAuth: false
-      //   }
-      // },
-      // {
-      //   path: 'access-wallet/browser-extension',
-      //   name: 'AccessWalletBrowserExtension',
-      //   component: AccessWalletBrowserExtension,
-      //   meta: {
-      //     requiresAuth: false
-      //   }
-      // },
-      // {
-      //   path: 'access-wallet/browser-extension/install',
-      //   name: 'AccessWalletBrowserExtensionInstall',
-      //   component: AccessWalletBrowserExtensionInstall,
-      //   meta: {
-      //     requiresAuth: false
-      //   }
-      // },
-      // {
-      //   path: 'access-wallet/browser-extension/access',
-      //   name: 'AccessWalletBrowserExtensionAccess',
-      //   component: AccessWalletBrowserExtensionAccess,
-      //   meta: {
-      //     requiresAuth: false
-      //   }
-      // },
-      // {
-      //   path: 'access-wallet/mobile-apps',
-      //   name: 'AccessWalletMobileApps',
-      //   component: AccessWalletMobileApps,
-      //   meta: {
-      //     requiresAuth: false
-      //   }
-      // }
     ]
   },
   WalletRoutes
