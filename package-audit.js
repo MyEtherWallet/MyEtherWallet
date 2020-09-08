773680da47913b198b4e44d02ec6a8da3ee78906
gamalielhere <gamalielgad1234@gmail.com>
2020-09-08 13:57:30 -0700

    Remove audits and resolve everything for testing

--- package-audit.js
+++ package-audit.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-const AUDIT_EXCEPTIONS = ['decompress', 'http-proxy', 'findings', 'elliptic', 'dot-prop', 'serialize-javascript', 'handlebars', 'cryptiles'];
+const AUDIT_EXCEPTIONS = [];
 
 const execute = (command, callback) => {
   exec(
