d1dee29baf2b038d5b7a47d33ac5978441beeadf
SteveM <SteveM@myEtherWallet.com>
2020-09-15 11:06:25 -0700

    fix typo

--- src/containers/ConfirmationContainer/components/SuccessModal/SuccessModal.vue
+++ src/containers/ConfirmationContainer/components/SuccessModal/SuccessModal.vue
@@ -83,7 +83,7 @@ export default {
       return {
         // eslint-disable-next-line
         title: this.$t('sendTx.success.button-check-explorer', {
-          explorrerName: 'EthVm'
+          explorrerName: this.$t('footer.ethvm')
         }),
         buttonStyle: 'green-border',
         fullWidth: true
