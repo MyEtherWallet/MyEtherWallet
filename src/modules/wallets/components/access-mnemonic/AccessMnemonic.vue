a57ef9ee232dd8fd9b2a2b2145a73a8f87dd1d99
gamalielhere <gamalielgad1234@gmail.com>
2020-09-17 13:21:24 -0700

    Polish network address part

--- src/modules/wallets/components/access-mnemonic/AccessMnemonic.vue
+++ src/modules/wallets/components/access-mnemonic/AccessMnemonic.vue
@@ -3,7 +3,7 @@
     <div class="sheet-container">
       <v-sheet
         :outlined="true"
-        :color="'white'"
+        :color="sheetColor"
         :rounded="true"
         :max-width="740"
         :min-width="475"
@@ -139,61 +139,105 @@
                   <div>
                     <v-radio-group v-model="selectedAddress">
                       <table width="100%">
-                        <tr class="table-header">
-                          <th width="50%" class="align-center">Address</th>
-                          <th width="25%" class="align-center">Eth Balance</th>
-                          <th width="25%" class="align-center"># of Tokens</th>
-                        </tr>
-                        <tr
-                          v-for="acc in accounts"
-                          v-show="accounts.length > 0"
-                          :key="acc.address"
-                        >
-                          <td>
-                            <v-row justify="space-around">
-                              <v-col cols="1">
-                                <v-radio label="" :value="acc.address" />
-                              </v-col>
-                              <v-col cols="8" class="text-truncate">
-                                <span>{{ acc.address }}</span>
-                                <input
-                                  :ref="acc.address"
-                                  :value="acc.address"
-                                  class="address-copy-input"
-                                />
-                              </v-col>
-                              <v-col cols="2">
-                                <v-row>
-                                  <v-icon
-                                    small
-                                    class="cursor--pointer"
-                                    @click="copy(acc.address)"
-                                    >mdi-content-copy</v-icon
-                                  >
-                                  <v-icon
-                                    small
-                                    class="cursor--pointer"
-                                    @click="launchExplorrer(acc.address)"
-                                    >mdi-launch</v-icon
-                                  >
-                                </v-row>
-                              </v-col>
-                            </v-row>
-                          </td>
-                          <td>
-                            {{
-                              acc.balance === 'Loading..'
-                                ? acc.balance
-                                : `${acc.balance} ${network.type.name}`
-                            }}
-                          </td>
-                          <td>{{ acc.tokens }}</td>
-                        </tr>
-                        <tr v-show="accounts.length === 0">
-                          Loading...
-                        </tr>
+                        <thead>
+                          <tr class="table-header">
+                            <th width="50%" class="align-center">Address</th>
+                            <th width="25%" class="align-center">
+                              Eth Balance
+                            </th>
+                            <th width="25%" class="align-center">
+                              # of Tokens
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody class="table-row-class">
+                          <tr
+                            v-for="acc in accounts"
+                            v-show="accounts.length > 0"
+                            :key="acc.address"
+                          >
+                            <td>
+                              <v-row justify="space-around">
+                                <v-col cols="1">
+                                  <v-radio label="" :value="acc.address" />
+                                </v-col>
+                                <v-col cols="8" class="text-truncate">
+                                  <v-row justify="space-around">
+                                    <blockie
+                                      width="25px"
+                                      height="25px"
+                                      :address="acc.address"
+                                    />
+                                    <span>{{
+                                      acc.address | concatAddress
+                                    }}</span>
+                                  </v-row>
+                                  <input
+                                    :ref="acc.address"
+                                    :value="acc.address"
+                                    class="address-copy-input"
+                                  />
+                                </v-col>
+                                <v-col cols="2">
+                                  <v-row>
+                                    <v-icon
+                                      small
+                                      class="cursor--pointer"
+                                      @click="copy(acc.address)"
+                                      >mdi-content-copy</v-icon
+                                    >
+                                    <v-icon
+                                      small
+                                      class="cursor--pointer"
+                                      @click="launchExplorrer(acc.address)"
+                                      >mdi-launch</v-icon
+                                    >
+                                  </v-row>
+                                </v-col>
+                              </v-row>
+                            </td>
+                            <td>
+                              {{
+                                acc.balance === 'Loading..'
+                                  ? acc.balance
+                                  : `${acc.balance} ${network.type.name}`
+                              }}
+                            </td>
+                            <td>{{ acc.tokens }}</td>
+                          </tr>
+                          <tr v-show="accounts.length === 0">
+                            Loading...
+                          </tr>
+                        </tbody>
                       </table>
                     </v-radio-group>
+                    <br />
+                    <v-row align="center" justify="center">
+                      <div>
+                        <mew-button
+                          title="Previous"
+                          color-theme="basic"
+                          icon="mdi-chevron-left"
+                          icon-type="mdi"
+                          :has-full-width="false"
+                          button-size="small"
+                          icon-align="left"
+                          btn-style="transparent"
+                          @click.native="previousAddressSet"
+                        />
+                        <mew-button
+                          title="Next"
+                          color-theme="basic"
+                          icon="mdi-chevron-right"
+                          icon-type="mdi"
+                          :has-full-width="false"
+                          button-size="small"
+                          icon-align="right"
+                          btn-style="transparent"
+                          @click.native="nextAddressSet"
+                        />
+                      </div>
+                    </v-row>
                   </div>
                 </template>
               </mew-expand-panel>
@@ -234,6 +278,14 @@ const MAX_ADDRESSES = 5;
 
 export default {
   name: 'AccessMnemonic',
+  filters: {
+    concatAddress(val) {
+      return `${val.substring(0, 11)}...${val.substring(
+        val.length - 4,
+        val.length
+      )}`;
+    }
+  },
   props: {
     open: {
       type: Boolean,
@@ -398,9 +450,7 @@ export default {
       }
 
       this.currentIdx += MAX_ADDRESSES;
-      this.selectedAddress = this.accounts[
-        this.currentIdx - MAX_ADDRESSES
-      ].address;
+      this.selectedAddress = this.accounts[0].address;
     },
     unlockBtn() {
       this.unlockMnemonicWallet(this.parsedPhrase, this.extraWord);
@@ -426,6 +476,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+table {
+  border-spacing: 0;
+}
+
 .table-header {
   text-align: center;
   background-color: #f0f0f0;
@@ -433,6 +487,12 @@ export default {
     color: #96a8b6;
     font-weight: bold;
     text-transform: uppercase;
+  }
+}
+
+.table-row-class {
+  tr:nth-child(odd) {
+    background-color: #f9f9f9;
   }
 }
 
