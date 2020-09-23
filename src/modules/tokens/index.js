4b0cb0a0b0cdd41b6ae826b8ef281ad353760f71
Jessica <jessicaspeng@gmail.com>
2020-09-23 16:58:35 -0700

    Add getOwnersERC20Tokens query

--- src/modules/tokens/index.js
+++ src/modules/tokens/index.js
@@ -1,22 +1,53 @@
-import { getLatestPrices } from './getLatestPrices.graphql';
+import { getLatestPrices, getOwnersERC20Tokens } from './tokens.graphql';
 
 export default class Tokenslist {
-  constructor(network) {
-    this.network = network;
-    console.error("this", this.$apollo)
-    this.getLatestPrices();
+  constructor(apollo) {
+    this.apollo = apollo;
   }
-
   getLatestPrices() {
-    this.$apollo
+    this.apollo
       .query({
         query: getLatestPrices
       })
       .then(response => {
-        console.error('response', response)
+        return response;
       })
       .catch(error => {
-        throw error;
+        return error;
       });
+  }
+  getOwnersERC20Tokens(hash) {
+    return this.apollo
+      .query({
+        query: getOwnersERC20Tokens,
+        variables: {
+          hash: hash
+        }
+      })
+      .then(response => {
+        console.error('response', response);
+        if (response && response.data) {
+          return this.formatOwnersERC20Tokens(
+            response.data.getOwnersERC20Tokens.owners
+          );
+        }
+      })
+      .catch(error => {
+        return error;
+      });
+  }
+  formatOwnersERC20Tokens(tokens) {
+    const formattedList = [];
+    console.error('tokens', tokens);
+    tokens.forEach(token => {
+      formattedList.push({
+        name: token.tokenInfo.symbol,
+        subtext: token.tokenInfo.name,
+        value: token.tokenInfo.name,
+        balance: token.balance,
+        contract: token.tokenInfo.contract
+      });
+    });
+    return formattedList;
   }
 }
