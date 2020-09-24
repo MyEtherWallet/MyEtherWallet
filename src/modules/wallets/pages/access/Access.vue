8dbf8b51f53b20a846eb4ab9e9a4375848533ff9
gamalielhere <gamalielgad1234@gmail.com>
2020-09-24 16:19:37 -0700

    Setup mew wallet

--- src/modules/wallets/pages/access/Access.vue
+++ src/modules/wallets/pages/access/Access.vue
@@ -15,53 +15,20 @@
       </h5>
     </block-title>
     <v-sheet color="transparent" max-width="650px" class="mx-auto">
-      <div class="mb-5">
+      <div v-for="btn in buttons" :key="btn.title" class="mb-5">
         <mew-super-button
-          color-theme="basic"
-          title="Browser Extension"
-          subtitle="Use Browser Extension to access my wallet"
-          icon-type="img"
-          :right-icon="require('@/assets/images/icons/icon-mew-cx.png')"
-          title-icon="mdi-shield-check"
-          @click.native="showBrowser = !showBrowser"
-        />
-      </div>
-      <div class="mb-5">
-        <mew-super-button
-          color-theme="basic"
-          title="Hardware wallets"
-          subtitle="Ledge, Trezor, Keep key, FINNEY, BitBox, Secalot"
-          right-icon-type="img"
+          :color-theme="btn.color"
+          :title="btn.title"
+          :subtitle="btn.subtitle"
           title-icon-type="mdi"
-          :right-icon="
-            require('@/assets/images/icons/icon-hardware-wallet.png')
-          "
-          title-icon="mdi-shield-check"
-          @click.native="showHardware = !showHardware"
-        />
-      </div>
-      <div class="mb-5">
-        <mew-super-button
-          color-theme="basic"
-          title="Mobile Apps"
-          subtitle="MEW Wallet, WalletConnect, Wallet Link"
-          icon-type="img"
-          :right-icon="require('@/assets/images/icons/icon-wallet-connect.svg')"
-          :right-icon2="require('@/assets/images/icons/icon-wallet-link.png')"
-          title-icon="mdi-shield-check"
-          @click.native="showMobile = !showMobile"
-        />
-      </div>
-      <div>
-        <mew-super-button
-          color-theme="outline"
-          title="Software"
-          subtitle="Keystore files, Mnemonic phrase, Private key"
-          icon-type="img"
-          title-icon="mdi-alert"
-          note="NOT RECOMMANDED"
-          @click.native="showSoftware = !showSoftware"
-        />
+          :title-icon="btn.titleIcon"
+          :note="btn.note"
+          @click.native="btn.fn"
+        >
+          <template v-slot:contentSlot>
+            <img :src="btn.rightIcon" width="100px" />
+          </template>
+        </mew-super-button>
       </div>
     </v-sheet>
     <div class="spacer-y-medium" />
@@ -78,7 +45,6 @@ import browserExtensionOverlay from '@/modules/wallets/components/browser-extens
 import hardwareAccessOverlay from '@/modules/wallets/components/hardware-access-overlay/HardwareAccessOverlay.';
 import mobileAccessOverlay from '@/modules/wallets/components/mobile-access-overlay/MobileAccessOverlay';
 import softwareAccessOverlay from '@/modules/wallets/components/software-access-overlay/SoftwareAccessOverlay';
-
 export default {
   name: 'CreateNewWallet',
   components: {
@@ -97,6 +63,61 @@ export default {
         description: '',
         centered: true
       },
+      buttons: [
+        {
+          color: 'basic',
+          title: 'MEW wallet',
+          subtitle: 'Use MEWwallet to access my wallet',
+          note: '',
+          rightIcon: require('@/assets/images/snippets/mobile/logo-mew-wallet.png'),
+          titleIcon: '',
+          fn: () => {}
+        },
+        {
+          color: 'basic',
+          title: 'Browser Extension',
+          subtitle: 'Use Browser Extension to access my wallet',
+          note: '',
+          rightIcon: require('@/assets/images/icons/icon-mew-cx.png'),
+          titleIcon: 'mdi-shield-check',
+          fn: () => {
+            this.open('showBrowser');
+          }
+        },
+        {
+          color: 'basic',
+          title: 'Hardware wallets',
+          subtitle: 'Ledge, Trezor, Keep key, FINNEY, BitBox, Secalot',
+          note: '',
+          rightIcon: require('@/assets/images/icons/icon-hardware-wallet.png'),
+          titleIcon: 'mdi-shield-check',
+          fn: () => {
+            this.open('showHardware');
+          }
+        },
+        {
+          color: 'basic',
+          title: 'Mobile Apps',
+          subtitle: 'WalletConnect, Wallet Link',
+          note: '',
+          rightIcon: require('@/assets/images/icons/icon-wallet-connect.svg'),
+          titleIcon: 'mdi-shield-check',
+          fn: () => {
+            this.open('showMobile');
+          }
+        },
+        {
+          color: 'outline',
+          title: 'Software',
+          subtitle: 'Keystore files, Mnemonic phrase, Private key',
+          note: 'NOT RECOMMANDED',
+          rightIcon: '',
+          titleIcon: 'mdi-alert-circle',
+          fn: () => {
+            this.open('showSoftware');
+          }
+        }
+      ],
       showBrowser: false,
       showHardware: false,
       showMobile: false,
@@ -106,6 +127,9 @@ export default {
   methods: {
     close(type) {
       this[type] = false;
+    },
+    open(type) {
+      this[type] = true;
     }
   }
 };
