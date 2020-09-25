4258c066a88a0250671341fcacd3855557027e73
Jessica <jessicaspeng@gmail.com>
2020-09-25 13:59:27 -0700

    Fix layout to use mew ocomponents

--- src/modules/wallets/pages/send/send-tx/SendTx.vue
+++ src/modules/wallets/pages/send/send-tx/SendTx.vue
@@ -1,108 +1,113 @@
 <template>
-  <v-container class="d-flex">
-    <div class="flex-grow-1">
-      <mew6-white-sheet>
-        <interface-wrap :title="$t('sendTx.send-tx')">
+  <mew-module
+    class="d-flex flex-grow-1 pt-6"
+    :title="$t('sendTx.send-tx')"
+    :has-elevation="true"
+    :has-indicator="true"
+  >
+    <template v-slot:moduleBody>
+      <div class="full-width px-16 pt-3">
+        <div class="d-flex justify-end mr-3 entire-bal">
+          <mew-button
+            :title="$t('sendTx.entire-bal')"
+            btn-style="transparent"
+            @click.native="sendEntireBal"
+          />
+        </div>
+        <div class="d-flex">
+          <mew-select
+            :items="ownersTokens"
+            :label="$t('sendTx.type')"
+            class="mr-3"
+          />
+          <div class="position--relative flex-grow-1">
+            <mew-input
+              :label="$t('sendTx.amount')"
+              placeholder=" "
+              :right-label="fixedBal"
+              :value="amount"
+            />
+          </div>
+        </div>
+        <address-select
+          :copy-tooltip="$t('common.copy')"
+          :save-tooltip="$t('common.save')"
+          :enable-save-address="true"
+          :label="$t('sendTx.to-addr')"
+          :items="addresses"
+          :placeholder="$t('sendTx.enter-addr')"
+          :success-toast="$t('sendTx.success.title')"
+          :is-valid-address="true"
+          @emitSelectedValue="getSelectedValue"
+        />
+      </div>
+
+      <mew-expand-panel
+        is-toggle
+        has-dividers
+        :panel-items="expandPanel"
+        class="px-16"
+      >
+        <template v-slot:panelBody1>
           <div>
-            <div class="d-flex justify-end mr-3 entire-bal">
-              <mew-button
-                :title="$t('sendTx.entire-bal')"
-                btn-style="transparent"
-                @click.native="sendEntireBal"
-              />
+            <mew-input
+              :label="$t('common.gas.price')"
+              placeholder=" "
+              value="40"
+            />
+            <mew-input
+              :label="$t('common.gas.limit')"
+              placeholder=" "
+              value="21000"
+            />
+          </div>
+
+          <div class="d-flex justify-space-between px-5">
+            <div class="mew-body font-weight-medium d-flex align-center">
+              {{ $t('sendTx.tx-fee') }}
+              <info-tooltip class="ml-1" text="Tx fees" />
             </div>
-            <div class="d-flex">
-              <mew-select
-                :items="ownersTokens"
-                :label="$t('sendTx.type')"
-                class="mr-3"
-              />
-              <div class="position--relative flex-grow-1">
-                <mew-input
-                  :label="$t('sendTx.amount')"
-                  placeholder=" "
-                  :right-label="fixedBal"
-                  :value="amount"
-                />
-              </div>
-            </div>
-            <address-select
-              :copy-tooltip="$t('common.copy')"
-              :save-tooltip="$t('common.save')"
-              :enable-save-address="true"
-              :label="$t('sendTx.to-addr')"
-              :items="addresses"
-              :placeholder="$t('sendTx.enter-addr')"
-              :success-toast="$t('sendTx.success.title')"
-              :is-valid-address="true"
-              @emitSelectedValue="getSelectedValue"
-            />
+            <div>$0.177</div>
           </div>
+          <divider dot class="mt-5" />
+          <mew-input
+            :label="$t('sendTx.add-data')"
+            placeholder=" "
+            value
+            class="mt-10 mb-n5"
+          />
+        </template>
+      </mew-expand-panel>
 
-          <mew-expand-panel is-toggle has-dividers :panel-items="expandPanel">
-            <template v-slot:panelBody1>
-              <div>
-                <mew-input
-                  :label="$t('common.gas.price')"
-                  placeholder=" "
-                  value="40"
-                />
-                <mew-input
-                  :label="$t('common.gas.limit')"
-                  placeholder=" "
-                  value="21000"
-                />
-              </div>
-
-              <div class="d-flex justify-space-between px-5">
-                <div class="mew-body font-weight-medium d-flex align-center">
-                  {{ $t('sendTx.tx-fee') }}
-                  <info-tooltip class="ml-1" text="Tx fees" />
-                </div>
-                <div>$0.177</div>
-              </div>
-              <divider dot class="mt-5" />
-              <mew-input
-                :label="$t('sendTx.add-data')"
-                placeholder=" "
-                value
-                class="mt-10 mb-n5"
-              />
-            </template>
-          </mew-expand-panel>
-
-          <div class="text-center mt-12">
-            <mew-button
-              :title="$t('sendTx.send')"
-              :has-full-width="false"
-              button-size="xlarge"
-            />
-          </div>
-          <div class="text-center mt-4">
-            <mew-button
-              :title="$t('common.clear-all')"
-              :has-full-width="false"
-              button-size="small"
-              btn-style="transparent"
-            />
-          </div>
-        </interface-wrap>
-      </mew6-white-sheet>
-    </div>
-    <div class="pa-4"></div>
-  </v-container>
+      <div class="d-flex flex-column mt-12">
+        <div class="text-center">
+          <mew-button
+            :title="$t('sendTx.send')"
+            :has-full-width="false"
+            button-size="xlarge"
+          />
+        </div>
+        <div class="text-center mt-4">
+          <mew-button
+            :title="$t('common.clear-all')"
+            :has-full-width="false"
+            button-size="small"
+            btn-style="transparent"
+          />
+        </div>
+      </div>
+    </template>
+  </mew-module>
 </template>
 
 <script>
-import interfaceWrap from '@/components/interface-wrap/InterfaceWrap';
 // import eth from '@/assets/images/currencies/icon-eth-blue.svg';
 import divider from '@/components/dividerx/DividerX';
 import SendTransaction from './index';
 
 export default {
   components: {
-    divider,
-    interfaceWrap
+    divider
   },
   props: {
     ownersTokens: {
@@ -148,7 +153,7 @@ export default {
   mounted() {
     this.sendTx = new SendTransaction(this.account.balance, this.$apollo);
     this.fixedBal = this.sendTx.getFixedBal();
-    console.error('ownersToken', this.ownersTokens)
+    console.error('ownersToken', this.ownersTokens);
   },
   methods: {
     getSelectedValue(value) {
