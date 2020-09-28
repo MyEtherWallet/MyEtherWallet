b7877c3796e36af53995c9f5897647a5e82191a4
David Hong <hsurf22@gmail.com>
2020-09-28 15:21:28 -0700

    Update Mobile Secalot

--- src/modules/wallets/components/access-wallet-overlay/HardwareWallets/Secalot/Secalot.vue
+++ src/modules/wallets/components/access-wallet-overlay/HardwareWallets/Secalot/Secalot.vue
@@ -1,56 +1,151 @@
 <template>
-  <mew-overlay
-    :show-overlay="open"
-    title="1. Connect with Secalot"
-    right-btn-text="Cancel"
-    @closeOverlay="$emit('close')"
-  >
-    <template v-slot:mewComponent>
-      <mew-tabs :items="tabs" is-block>
-        <template v-slot:tabContent1>
-          <mew6-white-sheet>
-            <div class="overlay-content pa-8">
-              <div class="text-center mb-8">
-                <img
-                  src="@/assets/images/currencies/icon-eth-blue.svg"
-                  alt="Eth icon"
-                  height="60"
-                />
-              </div>
-              <div>
-                <mew-select label="App opened in Secalot" />
-                <mew-select label="HD derivation path" />
-              </div>
+  <div class="mew-component--access-wallet-bitbox">
+    <mew-overlay
+      :show-overlay="open"
+      right-btn-text="Cancel"
+      @closeOverlay="$emit('close')"
+    >
+      <template v-slot:mewOverlayBody>
+        <div v-if="step === 1" class="full-width">
+          <h2 class="text-center mb-10">1. Enter your password</h2>
+          <mew6-white-sheet max-width="600px" class="mx-auto pa-4 pa-sm-12">
+            <div class="mb-6 font-weight-bold">
+              Enter your Secalot password.
+            </div>
+            <v-text-field
+              label="Password"
+              :type="showPassword ? 'text' : 'password'"
+              placeholder="Enter keystore file password"
+              outlined
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+            />
+            <mew-checkbox
+              class="justify-center"
+              label="To access my wallet, I accept"
+              :link="linkToTerms"
+            />
+
+            <div class="d-flex justify-center mt-2">
               <mew-button
+                title="Access my wallet"
                 button-size="xlarge"
-                title="Connect with Secalot"
-                has-full-width
-                @click.native="activeTab = 1"
+                :has-full-width="false"
+                @click.native="step += 1"
               />
             </div>
           </mew6-white-sheet>
-        </template>
-        <template v-slot:tabContent2>
-          <mew6-white-sheet>
-            <GroupRadioButtons :buttons="networkButtons" />
-            <address-table />
+          <page-indicator-dot class="mt-4" :items="3" :current-item="1" />
+        </div>
+
+        <div v-if="step === 2" class="full-width">
+          <h2 class="text-center mb-10">2. Connect with BitBox</h2>
+          <mew6-white-sheet max-width="600px" class="mx-auto pa-4 pa-sm-12">
+            <div class="text-center mb-10">
+              <img
+                src="@/assets/images/currencies/icon-eth-blue.svg"
+                alt="App"
+                height="80"
+              />
+            </div>
+
+            <mew-select :items="apps" />
+            <mew-select :items="derivationPath" />
+
+            <div class="d-flex justify-center mt-2">
+              <mew-button
+                title="Connect with BitBox"
+                button-size="xlarge"
+                :has-full-width="false"
+                @click.native="step += 1"
+              />
+            </div>
           </mew6-white-sheet>
-        </template>
-      </mew-tabs>
-    </template>
-  </mew-overlay>
+          <page-indicator-dot class="mt-4" :items="3" :current-item="2" />
+        </div>
+
+        <v-sheet
+          v-if="step === 3"
+          color="transparent"
+          max-width="700px"
+          width="100%"
+        >
+          <h2 class="text-center mb-10">3. Confirm network & address</h2>
+
+          <mew-expand-panel :panel-items="changeNetwork" class="mb-2">
+            <template v-slot:panelBody1>
+              <v-radio-group v-model="networkSelected">
+                <div v-for="(network, i) in networks" :key="i">
+                  <div class="text-uppercase font-weight-bold subtitle-1 mb-1">
+                    {{ network.label }}
+                  </div>
+
+                  <v-row no-gutters>
+                    <v-col
+                      v-for="button in network.buttons"
+                      :key="button.value"
+                      cols="12"
+                      sm="6"
+                      class="mt-2"
+                    >
+                      <v-radio
+                        :label="button.name"
+                        :value="button.value"
+                      ></v-radio>
+                    </v-col>
+                  </v-row>
+
+                  <div>{{ network.id }}</div>
+                  <divider-line v-if="networks.length != i + 1" class="my-5" />
+                </div>
+              </v-radio-group>
+            </template>
+          </mew-expand-panel>
+
+          <mew-expand-panel :panel-items="addressToInteract">
+            <template v-slot:panelBody1>
+              <mew-table
+                style="margin: 0 -24px"
+                :table-headers="tableHeaders"
+                :table-data="tableData"
+                has-select
+              />
+              <div class="d-flex align-center justify-center mt-5">
+                <div class="cursor--pointer mx-4">&lt; Previous</div>
+                <div class="cursor--pointer mx-4">Next &gt;</div>
+              </div>
+            </template>
+          </mew-expand-panel>
+
+          <mew-checkbox
+            class="mt-5 justify-center"
+            label="To access my wallet, I accept"
+            :link="linkToTerms"
+          />
+
+          <div class="d-flex justify-center mt-2">
+            <mew-button
+              title="Access my wallet"
+              button-size="xlarge"
+              :has-full-width="false"
+              @click.native="step = 1"
+            />
+          </div>
+          <page-indicator-dot class="mt-4" :items="3" :current-item="3" />
+        </v-sheet>
+      </template>
+    </mew-overlay>
+  </div>
 </template>
 
 <script>
-// TODO: add component in mew components
-// import GroupRadioButtons from '@/components/Buttons/GroupRadioButtons';
-import addressTable from './components/AddressTable/AddressTable';
+import pageIndicatorDot from '@/components/page-indicator-dot/PageIndicatorDot';
+import dividerLine from '@/components/divider-line/DividerLine';
 
 export default {
   components: {
-    // GroupRadioButtons,
-
-    addressTable
+    pageIndicatorDot,
+    dividerLine
   },
   props: {
     open: { default: false, type: Boolean },
@@ -63,61 +158,121 @@ export default {
   },
   data() {
     return {
-      tabs: [
+      step: 1,
+      showPassword: false,
+      linkToTerms: {
+        title: 'Terms.',
+        url: 'https://www.myetherwallet.com/terms-of-service'
+      },
+      pairingCode: ['AUNEF', 'NE237', 'ALUC3', 'A86N0'],
+      tableHeaders: [
         {
-          name: ''
+          text: 'Address',
+          value: 'address',
+          sortable: false,
+          filterable: false,
+          width: '100%'
         },
         {
-          name: ''
+          text: 'Eth Balance',
+          value: 'ethBalance',
+          sortable: false,
+          filterable: false,
+          containsLink: true,
+          width: '100%'
+        },
+        {
+          text: '#Token',
+          value: 'token',
+          sortable: false,
+          filterable: false,
+          containsLink: true,
+          width: '100%'
         }
       ],
-      networkButtons: [
+      tableData: [
         {
-          groupName: 'ETH',
-          btns: [
-            { label: 'myetherapi.com', value: 'eth-myetherapi' },
-            { label: 'infura.io', value: 'eth-infura' },
-            { label: 'giveth.io', value: 'eth-giveth' },
-            { label: 'therscan.io', value: 'eth-therscan' }
+          address: '9834759283750173071305',
+          ethBalance: '0.0001 ETH',
+          token: '21'
+        },
+        {
+          address: '9834759283750173071305ergerthtr',
+          ethBalance: '2.23 ETH',
+          token: '10'
+        },
+        {
+          address: '9834759283750173071erger305',
+          ethBalance: '0.23 ETH',
+          token: '8'
+        }
+      ],
+      changeNetwork: [
+        {
+          name: 'Network',
+          subtext: 'ETH - myetherapi.com'
+        }
+      ],
+      addressToInteract: [
+        {
+          name: 'Address to interact with'
+        }
+      ],
+      networkSelected: null,
+      networks: [
+        {
+          label: 'eth',
+          buttons: [
+            { name: 'myetherapi.com', value: 'myetherapi' },
+            { name: 'infura.io', value: 'infura' },
+            { name: 'giveth.io', value: 'giveth' },
+            { name: 'therscan.io', value: 'therscan' }
           ]
         },
         {
-          groupName: 'ROP',
-          btns: [
-            { label: 'etherscan.io', value: 'rop-etherscan' },
-            { label: 'infura.io', value: 'rop-infura' },
-            { label: 'giveth.io', value: 'rop-giveth' },
-            { label: 'therscan.io', value: 'rop-therscan' }
+          label: 'rop',
+          buttons: [
+            { name: 'myetherapi.com', value: 'myetherapi1' },
+            { name: 'infura.io', value: 'infura1' },
+            { name: 'giveth.io', value: 'giveth1' },
+            { name: 'therscan.io', value: 'therscan1' }
           ]
         },
         {
-          groupName: 'RIN',
-          btns: [
-            { label: 'etherscan.io', value: 'rin-etherscan' },
-            { label: 'infura.io', value: 'rin-infura' },
-            { label: 'giveth.io', value: 'rin-giveth' },
-            { label: 'therscan.io', value: 'rin-therscan' }
+          label: 'rin',
+          buttons: [
+            { name: 'myetherapi.com', value: 'myetherapi2' },
+            { name: 'infura.io', value: 'infura2' },
+            { name: 'giveth.io', value: 'giveth2' },
+            { name: 'therscan.io', value: 'therscan2' }
           ]
         }
       ],
-      appSelect: 'eth',
-      derivationPathSelect: '1',
-      activeTab: 0,
       apps: [
-        { label: 'Ethereum', value: 'eth' },
-        { label: 'SometingElse', value: 'smt' }
+        {
+          name: 'Ethereum',
+          value: 'ethereum'
+        }
       ],
       derivationPath: [
-        { label: `m/44'/60'/0'`, value: '1' },
-        { label: `m/44'/60'/0'`, value: '2' }
-      ]
+        {
+          name: "m/44'/60'/0",
+          value: "m/44'/60'/0"
+        },
+        {
+          name: "m/44'/60'/0",
+          value: "m/44'/60'/0"
+        }
+      ],
+      bitBoxSelected: null
     };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.overlay-content {
-  width: 500px;
+.v-input--selection-controls {
+  padding: 0;
+  margin: 0;
 }
 </style>
