10545707b766c570c83c512052a2666583564844
debasis360 <72032387+debasis360@users.noreply.github.com>
2020-09-28 11:40:15 -0400

    Create sapcoin.js

---
+++ src/networks/nodes/sapcoin.js
@@ -0,0 +1,10 @@
+import { SAP } from '../types';
+export default {
+  type: SAP,
+  service: '0xinfra.com',
+  url: 'https://sap-geth.0xinfra.com/',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
