aa2875265fb403af71a99f4794d360615078b5b3
gamalielhere <gamalielgad1234@gmail.com>
2020-09-28 13:25:51 -0700

    start work on mnemonic

--- src/modules/wallets/components/mnemonic-phrase/MnemonicPhrase.vue
+++ src/modules/wallets/components/mnemonic-phrase/MnemonicPhrase.vue
@@ -1,177 +1,170 @@
 <template>
-  <div class="expandHeader">
-    <v-sheet color="transparent" max-width="800px" class="mx-auto">
-      <mew-stepper :items="steppers" :on-step="1">
-        <template v-slot:outsideStepContent0>
-          <div class="mnemonic-step1">
-            <v-sheet>
-              <div class="subtitle-1 font-weight-bold grey--text">STEP 1.</div>
-              <div class="headline font-weight-bold mb-5">
-                Write down the words
-              </div>
-
-              <div class="d-flex align-center justify-end pb-4">
-                <mew-button
-                  button-size="medium"
-                  icon="mdi-sync"
-                  icon-type="mdi"
-                  icon-align="left"
-                  title="Random"
-                  btn-style="transparent"
-                  color-theme="basic"
-                  @click.native="setPhrases"
-                />
-
-                <div class="selector ml-3">
-                  <mew-select :items="mnemonicOptions" />
-                </div>
-              </div>
-              <phrase-block class="mb-8">
-                <mnemonic-phrase-table :data="phrases" />
-              </phrase-block>
-
-              <mew-expand-panel is-toggle has-dividers :panel-items="exPanel">
-                <template v-slot:panelBody1> Input needs to be added </template>
-              </mew-expand-panel>
-
-              <div class="d-flex justify-center mt-6">
-                <mew-button
-                  title="I wrote them down"
-                  button-size="xlarge"
-                  :has-full-width="false"
-                  @click.native="linkToStep(2)"
-                />
-              </div>
-            </v-sheet>
-            <warning-sheet
-              class="mt-5"
-              title="Caution"
-              description="DO NOT take a screenshot or
-            share this phrase with anyone. This phrase acts as the PRIVATE KEY to access
-            your wallet."
-              :link-obj="link"
-            />
-          </div>
-        </template>
-
-        <template v-slot:outsideStepContent1>
-          <div>
-            <v-sheet>
-              <div class="subtitle-1 font-weight-bold grey--text">STEP 2.</div>
-              <div class="mb-10">
-                <div class="headline font-weight-bold">Verification</div>
-                <div>
-                  Please select the correct words based on their numbers, and
-                  enter the extra word if you have one.
-                </div>
-              </div>
-
-              <v-sheet max-width="600px" class="mx-auto">
-                <phrase-block class="mb-2 d-flex align-center">
-                  <div style="min-width: 60px">5.</div>
-                  <v-radio-group
-                    v-model="radioGroup1"
-                    hide-details
-                    class="full-width-percent"
-                  >
-                    <v-row>
-                      <v-col v-for="n in 3" :key="n" lg="4">
-                        <v-radio :label="`Radio ${n}`" :value="n"></v-radio>
-                      </v-col>
-                    </v-row>
-                  </v-radio-group>
-                </phrase-block>
-                <phrase-block class="mb-2 d-flex align-center">
-                  <div style="min-width: 60px">5.</div>
-                  <v-radio-group
-                    v-model="radioGroup2"
-                    hide-details
-                    class="full-width-percent"
-                  >
-                    <v-row>
-                      <v-col v-for="n in 3" :key="n" lg="4">
-                        <v-radio :label="`Radiaaaao ${n}`" :value="n"></v-radio>
-                      </v-col>
-                    </v-row>
-                  </v-radio-group>
-                </phrase-block>
-                <phrase-block class="mb-2 d-flex align-center">
-                  <div style="min-width: 60px">5.</div>
-                  <v-radio-group
-                    v-model="radioGroup3"
-                    hide-details
-                    class="full-width-percent"
-                  >
-                    <v-row>
-                      <v-col v-for="n in 3" :key="n" lg="4">
-                        <v-radio :label="`Radiaaaao ${n}`" :value="n"></v-radio>
-                      </v-col>
-                    </v-row>
-                  </v-radio-group>
-                </phrase-block>
-                <mew-input
-                  label="Extra word"
-                  placeholder="Please confirm your extra word"
-                  class="mt-10 mb-3"
-                />
-              </v-sheet>
-              <div class="d-flex justify-center mb-3">
-                <mew-button
-                  title="Verify"
-                  button-size="xlarge"
-                  :has-full-width="false"
-                  @click.native="linkToStep(3)"
-                />
-              </div>
-            </v-sheet>
-
-            <warning-sheet
-              title="NOT RECOMMENDED"
-              description="Not recommanded"
-            />
-          </div>
-        </template>
-
-        <template v-slot:outsideStepContent2>
+  <v-sheet color="transparent">
+    <mew-stepper :items="steppers" :on-step="3">
+      <template v-slot:outsideStepContent1>
+        <div class="mnemonic-step1">
           <v-sheet>
-            <div class="d-flex align-center">
-              <div class="mr-8">
-                <div class="subtitle-1 font-weight-bold grey--text">
-                  STEP 3.
-                </div>
-                <div class="headline font-weight-bold mb-3">You are done!</div>
-                <p class="mb-6">
-                  Congratulation! Please use the MEWconnect App to scan this QR
-                  code in order to access your new wallet. And you are done!
-                </p>
+            <div class="subtitle-1 font-weight-bold grey--text">STEP 1.</div>
+            <div class="headline font-weight-bold mb-5">
+              Write down the words
+            </div>
 
-                <mew-button
-                  title="Access my wallet"
-                  button-size="xlarge"
-                  :has-full-width="false"
-                  @click.native="linkToStep(3)"
-                />
+            <div class="d-flex align-center justify-end pb-4">
+              <mew-button
+                button-size="medium"
+                icon="mdi-sync"
+                icon-type="mdi"
+                icon-align="left"
+                title="Random"
+                btn-style="transparent"
+                color-theme="basic"
+                @click.native="setPhrases"
+              />
 
-                <p class="mt-4 mb-0">
-                  Need help?
-                  <router-link
-                    class="primary--text text-decoration--none font-weight-bold"
-                    to="/"
-                    >Get helps from MEWconnect</router-link
-                  >
-                </p>
+              <div class="selector ml-3">
+                <mew-select :items="mnemonicOptions" />
               </div>
-              <v-img
-                max-width="250px"
-                src="@/assets/images/icons/icon-keystore-mew.png"
+            </div>
+            <phrase-block class="mb-8">
+              <mnemonic-phrase-table :data="phrases" />
+            </phrase-block>
+
+            <mew-expand-panel is-toggle has-dividers :panel-items="exPanel">
+              <template v-slot:panelBody1> Input needs to be added </template>
+            </mew-expand-panel>
+
+            <div class="d-flex justify-center mt-6">
+              <mew-button
+                title="I wrote them down"
+                button-size="xlarge"
+                :has-full-width="false"
+                @click.native="linkToStep(2)"
               />
             </div>
           </v-sheet>
-        </template>
-      </mew-stepper>
-    </v-sheet>
-    <div class="spacer-y-medium" />
-  </div>
+          <warning-sheet
+            class="mt-5"
+            title="Caution"
+            description="DO NOT take a screenshot or
+            share this phrase with anyone. This phrase acts as the PRIVATE KEY to access
+            your wallet."
+            :link-obj="link"
+          />
+        </div>
+      </template>
+
+      <template v-slot:outsideStepContent2>
+        <div>
+          <v-sheet>
+            <div class="subtitle-1 font-weight-bold grey--text">STEP 2.</div>
+            <div class="mb-10">
+              <div class="headline font-weight-bold">Verification</div>
+              <div>
+                Please select the correct words based on their numbers, and
+                enter the extra word if you have one.
+              </div>
+            </div>
+
+            <v-sheet max-width="600px" class="mx-auto">
+              <phrase-block class="mb-2 d-flex align-center">
+                <div style="min-width: 60px">5.</div>
+                <v-radio-group
+                  v-model="radioGroup1"
+                  hide-details
+                  class="full-width-percent"
+                >
+                  <v-row>
+                    <v-col v-for="n in 3" :key="n" lg="4">
+                      <v-radio :label="`Radio ${n}`" :value="n"></v-radio>
+                    </v-col>
+                  </v-row>
+                </v-radio-group>
+              </phrase-block>
+              <phrase-block class="mb-2 d-flex align-center">
+                <div style="min-width: 60px">5.</div>
+                <v-radio-group
+                  v-model="radioGroup2"
+                  hide-details
+                  class="full-width-percent"
+                >
+                  <v-row>
+                    <v-col v-for="n in 3" :key="n" lg="4">
+                      <v-radio :label="`Radiaaaao ${n}`" :value="n"></v-radio>
+                    </v-col>
+                  </v-row>
+                </v-radio-group>
+              </phrase-block>
+              <phrase-block class="mb-2 d-flex align-center">
+                <div style="min-width: 60px">5.</div>
+                <v-radio-group
+                  v-model="radioGroup3"
+                  hide-details
+                  class="full-width-percent"
+                >
+                  <v-row>
+                    <v-col v-for="n in 3" :key="n" lg="4">
+                      <v-radio :label="`Radiaaaao ${n}`" :value="n"></v-radio>
+                    </v-col>
+                  </v-row>
+                </v-radio-group>
+              </phrase-block>
+              <mew-input
+                label="Extra word"
+                placeholder="Please confirm your extra word"
+                class="mt-10 mb-3"
+              />
+            </v-sheet>
+            <div class="d-flex justify-center mb-3">
+              <mew-button
+                title="Verify"
+                button-size="xlarge"
+                :has-full-width="false"
+                @click.native="linkToStep(3)"
+              />
+            </div>
+          </v-sheet>
+
+          <warning-sheet
+            title="NOT RECOMMENDED"
+            description="Not recommanded"
+          />
+        </div>
+      </template>
+
+      <template v-slot:outsideStepContent3>
+        <v-sheet>
+          <div class="d-flex align-center">
+            <div class="mr-8">
+              <div class="subtitle-1 font-weight-bold grey--text">STEP 3.</div>
+              <div class="headline font-weight-bold mb-3">Well done!</div>
+              <p class="mb-6">
+                Congratulations! You have created a new wallet successfully.
+              </p>
+
+              <mew-button
+                title="Access my wallet"
+                button-size="xlarge"
+                :has-full-width="false"
+                @click.native="linkToStep(3)"
+              />
+
+              <p class="mt-4 mb-0">
+                <router-link
+                  class="primary--text text-decoration--none font-weight-bold"
+                  to="/"
+                  >Back to homepages</router-link
+                >
+              </p>
+            </div>
+            <v-img
+              max-width="250px"
+              src="@/assets/images/icons/icon-keystore-mew.png"
+            />
+          </div>
+        </v-sheet>
+      </template>
+    </mew-stepper>
+  </v-sheet>
 </template>
 
 <script>
