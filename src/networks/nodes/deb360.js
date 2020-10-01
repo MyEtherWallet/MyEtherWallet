839c616e17a256c16c6f5dcc7a98d2a752314f70
debasis360 <72032387+debasis360@users.noreply.github.com>
2020-09-30 20:02:16 -0400

    Create deb360.js

---
+++ src/networks/nodes/deb360.js
@@ -0,0 +1,10 @@
+import { DEB } from '../types';
+export default {
+  type: CLO,
+  service: '0xinfra.com',
+  url: 'https://deb360-geth.0xinfra.com/',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
