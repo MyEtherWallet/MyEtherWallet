e8203dc47b985bf8455eb114c74035d36fa757b6
gamalielhere <gamalielgad1234@gmail.com>
2020-10-15 15:36:20 -0700

    Stopping point

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,31 @@
 import Router from 'vue-router';
 import store from '@/store';
+// import { getMode, getRoutes } from '@/builds/configs';
+// import { ExtensionHelpers } from '@/helpers';
+// import { isAddress } from '@/helpers/addressUtils';
+// import { Misc } from '@/helpers';
+// import { MEW_CX } from '@/builds/configs/types';
+import langShortCodes from '@/translations/getShortCodes';
 import routes from './routes';
+// const storeQuery = query => {
+//   const queryKeys = Object.keys(query);
+//   if (queryKeys.length > 0) {
+//     const blankObj = {};
+//     for (const key in query) {
+//       blankObj[key] = Misc.stripTags(query[key]);
+//     }
 
+//     store.dispatch('main/saveQueryVal', blankObj);
+//   }
+// };
+const getLangBasePath = () => {
+  if (ROUTER_MODE === 'hash') return undefined;
+  const locale = window.location.pathname.replace(/^\/([^/]+).*/i, '$1').trim();
+  if (Object.keys(langShortCodes).includes(locale)) return '/' + locale;
+  return undefined;
+};
 const router = new Router({
+  base: getLangBasePath(),
   mode: ROUTER_MODE || 'hash',
   routes: routes,
   scrollBehavior(to) {
@@ -14,28 +37,17 @@ const router = new Router({
     window.scrollTo(0, 0);
   }
 });
-
-router.beforeResolve((to, ___, next) => {
-  if (
-    to.hasOwnProperty('meta') &&
-    to.meta.hasOwnProperty('requiresAuth') &&
-    to.meta.requiresAuth === false
-  ) {
+router.beforeResolve((to, from, next) => {
+  // storeQuery(to.query);
+  if (to.meta.hasOwnProperty('requiresAuth')) {
     next();
   } else {
-    const queryKeys = Object.keys(to.query);
-    if (queryKeys.length > 0) {
-      const blankObj = {};
-      store.dispatch('saveQueryVal', blankObj);
-    }
-    if (store.state.wallet === null) {
-      console.log('WHERE?????', store.state.wallet);
+    if (store.state.main.wallet === null) {
       store.dispatch('setLastPath', to.path);
-      next({ name: 'AccessWallet' });
+      next({ name: 'AccessWalletLayout' });
     } else {
-      console.log('WHERE????? 1', store.state.wallet);
-      if (store.state.path !== '') {
-        const localPath = store.state.path;
+      if (store.state.main.path !== '') {
+        const localPath = store.state.main.path;
         store.dispatch('setLastPath', '');
         next({ path: localPath });
       } else {
