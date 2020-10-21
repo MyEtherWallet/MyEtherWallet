061e970bf7f7a80bb29e8e8e9bd64789093b7111
gamalielhere <gamalielgad1234@gmail.com>
2020-10-21 14:10:21 -0700

    Add component to hold transaction modals

---
+++ src/modules/wallets/components/confirmation/Confirmation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import EventNames from '@/utils/web3-provider/events.js';
+export default {
+  name: 'Confirmation Container',
+  created() {
+    this.$eventHub.$on(EventNames.SHOW_TX_CONFIRM_MODAL, (tx, resolve) => {});
+  }
+};
+</script>
