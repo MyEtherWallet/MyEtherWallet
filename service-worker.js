07ca51c8791ecdb0eb26c95807dc91012d9cce08
gamalielhere <gamalielhere@users.noreply.github.com>
2020-10-22 18:44:08 +0000

    Deploying to gh-pages-history from  @ 264b3125b12b0cef63087ed85cf722e1572c4c72 🚀

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 
 importScripts(
-  "/precache-manifest.92d831205953e67d345dc22315ac9b57.js"
+  "/precache-manifest.1ecd66fcf03f6da935f4a15fb70cfdd5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "myetherwallet"});
