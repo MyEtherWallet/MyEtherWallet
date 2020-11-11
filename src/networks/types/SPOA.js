5157d6755fc2015fad1ddecccb82a20de7e6295e
l0kol <66525457+l0kol@users.noreply.github.com>
2020-11-11 18:58:17 +0100

    Create SPOA.js

---
+++ src/networks/types/SPOA.js
@@ -0,0 +1,19 @@
+export default {
+  name: ‘SPOA’,
+  name_long: ‘Sokol Testnet’,
+  homePage: ‘
+https://sokol.poa.network'
+,
+  blockExplorerTX: ‘
+https://explorer.callisto.network/tx/[[txHash]]'
+,
+  blockExplorerAddr: ‘
+https://blockscout.com/poa/core/api'
+,
+  chainID: 77,
+  tokens: tokens,
+  contracts: contracts,
+  icon: SPOA,
+  currencyName: ‘SPOA’
+};
+
