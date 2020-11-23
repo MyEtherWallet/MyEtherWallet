24454d647d52a854263860a96a811d5ccf0950e0
SteveM <SteveM@myEtherWallet.com>
2020-11-23 12:03:35 -0800

    update token amount from contract when tx mined

--- src/dapps/GntMigrator/GntMigrator.vue
+++ src/dapps/GntMigrator/GntMigrator.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="lend-migrator-container">
-    <back-button/>
+    <back-button />
     <b-container class="text-center">
       <div class="pt-4 lend-title">{{ $t('dappsMisc.gnt-title') }}</div>
       <div class="d-flex mt-4 mb-1 total-container entire-bal">
         <p>
           GNT:
           <span class="balance"
-          ><span v-if="!loading">{{ lendBalance }} </span>
-            <i v-if="loading" class="fa fa-spin fa-spinner fa-lg"/>
+            ><span v-if="!loading">{{ lendBalance }} </span>
+            <i v-if="loading" class="fa fa-spin fa-spinner fa-lg" />
           </span>
         </p>
         <button class="button-link" @click="setEntireBalance">
@@ -38,8 +38,9 @@
 import BackButton from '@/layouts/InterfaceLayout/components/BackButton';
 import OLD_GNT from './abi/oldToken';
 import BigNumber from 'bignumber.js';
-import {mapState} from 'vuex';
-import {Toast} from '@/helpers';
+import { mapState } from 'vuex';
+import { Toast } from '@/helpers';
+import utils from 'web3-utils';
 
 const OLD_GNT_ADDRESS = '0xa74476443119A942dE498590Fe1f2454d7D4aC0d';
 // const NEW_GNT_ADDRESS = '0x7DD9c5Cba05E151C895FDe1CF355C9A1D5DA6429';
@@ -57,38 +58,49 @@ export default {
       }
     }
   },
-  data () {
+  data() {
     return {
       amount: 0,
       hasEnoughRatio: false,
       lendMigratorContract: '',
-      loading: false
+      loading: false,
+      balanceUpdate: false,
+      updatedBalance: 0
     };
   },
   computed: {
     ...mapState('main', ['web3', 'account']),
-    lendBalance () {
-      const lendToken = this.tokensWithBalance.find(item => {
-        return item.symbol === SYMBOL;
-      });
-      return lendToken ? new BigNumber(lendToken.balance).toFixed() : 0;
+    lendBalance() {
+      if (!this.balanceUpdate) {
+        const lendToken = this.tokensWithBalance.find(item => {
+          return item.symbol === SYMBOL;
+        });
+        return lendToken ? new BigNumber(lendToken.balance).toFixed() : 0;
+      } else {
+        return this.updatedBalance;
+      }
     },
-    disabled () {
+    disabled() {
       if (this.amount > 0 && this.amount <= this.lendBalance) {
         return false;
       }
       return true;
     }
   },
-  mounted () {
-  },
+  mounted() {},
   methods: {
-    async checkAllowance () {
+    async checkAllowance() {
       const lendContract = new this.web3.eth.Contract(OLD_GNT, OLD_GNT_ADDRESS);
       this.migrate(lendContract);
       this.loading = true;
     },
-    async migrate (lendContract) {
+    getLendBalance() {
+      const lendToken = this.tokensWithBalance.find(item => {
+        return item.symbol === SYMBOL;
+      });
+      return lendToken ? new BigNumber(lendToken.balance).toFixed() : 0;
+    },
+    async migrate(lendContract) {
       const estimatedAmount = new BigNumber(this.amount)
         .times(new BigNumber(10).pow(18))
         .toFixed();
@@ -113,9 +125,9 @@ export default {
             data: migrateData
           })
           .then(() => {
-            this.lendBalance = this.lendBalance - this.amount;
             this.amount = 0;
             this.loading = false;
+            this.getUpdatedBalance();
           })
           .catch(error => {
             this.loading = false;
@@ -123,8 +135,39 @@ export default {
           });
       });
     },
-    setEntireBalance () {
+    setEntireBalance() {
       this.amount = this.lendBalance;
+    },
+    getUpdatedBalance() {
+      const abi = [
+        {
+          constant: true,
+          inputs: [
+            {
+              name: '_owner',
+              type: 'address'
+            }
+          ],
+          name: 'balanceOf',
+          outputs: [
+            {
+              name: '',
+              type: 'uint256'
+            }
+          ],
+          payable: false,
+          type: 'function'
+        }
+      ];
+      const contract = new this.web3.eth.Contract(abi, OLD_GNT_ADDRESS);
+      this.balanceUpdate = false;
+      contract.methods
+        .balanceOf(this.account.address)
+        .call()
+        .then(res => {
+          this.updatedBalance = utils.fromWei(res, 'Ether');
+          this.balanceUpdate = true;
+        });
     }
   }
 };
