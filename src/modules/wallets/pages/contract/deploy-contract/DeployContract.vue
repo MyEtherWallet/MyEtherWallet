fe08c1c35e6fa20403496a4191875b5adbb1df8a
David Hong <hsurf22@gmail.com>
2020-12-01 15:21:19 -0800

    Update mobile deploy contract

--- src/modules/wallets/pages/contract/deploy-contract/DeployContract.vue
+++ src/modules/wallets/pages/contract/deploy-contract/DeployContract.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <div class="d-flex">
+    <div class="d-block d-lg-none">
+      <network mobile class="mb-4" />
+      <myEthBalance mobile class="mb-4" />
+    </div>
+
+    <div class="d-flex mt-4 mt-lg-0">
       <div class="flex-grow-1">
         <mew6-white-sheet>
           <interface-wrap title="Deploy contract">
@@ -17,7 +22,7 @@
 
             <mew-input label="Contract name" placeholder=" " />
 
-            <div class="text-center mt-3">
+            <div class="text-center mt-lg-3">
               <mew-button
                 title="Sign Transaction"
                 :has-full-width="false"
@@ -27,27 +32,41 @@
           </interface-wrap>
         </mew6-white-sheet>
       </div>
-      <div class="pa-4"></div>
-      <div>
+
+      <div class="pa-4 d-none d-lg-block"></div>
+
+      <div class="d-none d-lg-block">
         <network />
+        <div class="pa-4"></div>
+        <tx-history title="Transaction history" />
+        <div class="pa-4"></div>
+        <myEthBalance />
         <div class="pa-4"></div>
         <swap />
       </div>
+    </div>
+
+    <div class="d-block d-lg-none">
+      <tx-history class="mt-4" title="Transaction history" mobile />
+      <swap class="mt-4" mobile />
     </div>
   </div>
 </template>
 
 <script>
-import InterfaceWrap from '@/components/interface-wrap/InterfaceWrap';
-
-import Network from '@/modules/wallets/components/network/Network';
-import Swap from '@/modules/wallets/components/swap/Swap';
+import interfaceWrap from '@/components/interface-wrap/InterfaceWrap';
+import txHistory from '@/modules/wallets/components/transaction-history/TransactionHistory';
+import network from '@/modules/wallets/components/network/Network';
+import swap from '@/modules/wallets/components/swap/Swap';
+import myEthBalance from '@/modules/wallets/components/my-eth-balance/MyEthBalance';
 
 export default {
   components: {
-    'interface-wrap': InterfaceWrap,
-    network: Network,
-    swap: Swap
+    interfaceWrap,
+    swap,
+    txHistory,
+    network,
+    myEthBalance
   }
 };
 </script>
