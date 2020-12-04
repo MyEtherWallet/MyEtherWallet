3d801e24455add42fd5add7e339b9ac5c766a26a
gamalielhere <gamalielgad1234@gmail.com>
2020-12-04 14:25:08 -0800

    feat: show explorrer buttons

--- src/dapps/Staked/components/Stepper/steps/Done/Done.vue
+++ src/dapps/Staked/components/Stepper/steps/Done/Done.vue
@@ -4,13 +4,25 @@
     <h2 class="title">{{ $t('confirmation.success') }}</h2>
     <span>{{ $t('dappsStaked.done-desc') }}</span>
     <div class="explorrer-container">
-      <div></div>
+      <a :href="explorrer" target="_blank">{{
+        $t('sendTx.success.button-check-explorer', {
+          explorrerName: explorrerName
+        })
+      }}</a>
+      <a v-if="showEthvm" :href="ethvm" target="_blank">
+        {{
+          $t('sendTx.success.button-check-explorer', {
+            explorrerName: $t('footer.ethvm')
+          })
+        }}
+      </a>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import { Misc } from '@/helpers';
 export default {
   props: {
     hash: {
@@ -19,7 +31,19 @@ export default {
     }
   },
   computed: {
-    ...mapState('main', ['network'])
+    ...mapState('main', ['network']),
+    explorrer() {
+      return this.network.type.blockExplorerTX.replace('[[txHash]]', this.hash);
+    },
+    ethvm() {
+      return `https://www.ethvm.com/tx/${this.hash}`;
+    },
+    showEthvm() {
+      return this.network.type.name === 'ETH';
+    },
+    explorrerName() {
+      return Misc.getService(this.network.type.blockExplorerTX);
+    }
   },
   mounted() {
     this.$emit('completed', true, { key: 'done', value: true });
