aea60be40b25f7bcefb2ec10248abed940fe6d80
kvhnuke <10602065+kvhnuke@users.noreply.github.com>
2020-12-08 14:07:44 +0000

    Deploying to gh-pages-history from  @ c74f2b35ac1adbd755b28957592cb5c6943518d2 🚀

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 
 importScripts(
-  "/precache-manifest.d38caefb3d7961958ca5bed65d956ff5.js"
+  "/precache-manifest.511243886a8c337e4a9bd197ff305828.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "myetherwallet"});
