e83c41f6ac53e1b099c5e002a28631a371812fc7
SteveM <SteveM@myEtherWallet.com>
2021-02-09 13:02:20 -0800

    fix lint

--- src/modules/wallets/components/access-wallet-overlay/AccessWalletOverlay.vue
+++ src/modules/wallets/components/access-wallet-overlay/AccessWalletOverlay.vue
@@ -1,13 +1,10 @@
 <template>
-  <div>
-      </div>
+  <div></div>
 </template>
 
 <script>
-
 export default {
-  components: {
-  },
+  components: {},
   props: {
     open: {
       default: '',
