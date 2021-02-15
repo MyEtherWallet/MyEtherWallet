ee232ecfc78981995f928f05213f015c92fa0a1d
Jessica <jessicaspeng@gmail.com>
2021-02-15 13:44:07 -0800

    Add mew component styles

--- src/app.vue
+++ src/app.vue
@@ -37,5 +37,5 @@ export default {
 
 <style lang="scss">
 @import '@/assets/styles/GlobalStyles.scss';
-// @import '@myetherwallet/mew-components/src/assets/styles/global.scss';
+@import '@myetherwallet/mew-components/src/assets/styles/global.scss';
 </style>
