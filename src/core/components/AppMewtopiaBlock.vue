d232fc23869396d9a6afd2a398491b773191c28f
David Hong <hsurf22@gmail.com>
2021-02-15 18:21:33 -0800

    Fix Get started ad

--- src/core/components/AppMewtopiaBlock.vue
+++ src/core/components/AppMewtopiaBlock.vue
@@ -1,49 +1,79 @@
 <template>
-  <div class="mew-component--landing">
-    <v-container>
-      <v-row
-        class="border-radius--10px card-content align-center justify-space-between pa-7 pa-md-16"
-      >
-        <v-col cols="12" sm="6" md="8" class="pa-0">
-          <h2 class="mew-subtitle white--text mb-5">
-            <span class="newbie-title-content pr-2">Newbie in crypto?</span
-            ><span class="newbie-title-content">Explore MEWtopia!</span>
-          </h2>
-          <p class="white--text">
-            Our media platform where you can learn about crypto, share ideas,
-            and become part of a supportive community.
-          </p>
-        </v-col>
-        <v-spacer />
-        <mew-button
-          pa-xs-10
-          pa-md-16
-          btn-link="https://www.mewtopia.com/"
-          :has-full-width="false"
-          title="Get Started"
-          btn-style="outline"
-          color-theme="white"
-          btn-size="xlarge"
-        />
-      </v-row>
-    </v-container>
+  <div class="mew-component--ad-get-started">
+    <div class="desktop-content d-none d-lg-block">
+      <v-container>
+        <div
+          class="border-radius--10px card-content d-flex align-center justify-space-between"
+        >
+          <div>
+            <h2 class="font-weight--700 white--text">
+              Newbie in crypto? Explore MEWtopia!
+            </h2>
+            <p class="white--text mt-3">
+              Our media platform where you can learn about crypto, share ideas,
+              and become part of a supportive community.
+            </p>
+          </div>
+          <mew-button
+            btn-link="https://www.mewtopia.com/"
+            :has-full-width="false"
+            title="Get Started"
+            btn-style="outline"
+            color-theme="white"
+            btn-size="xlarge"
+          />
+        </div>
+      </v-container>
+    </div>
+
+    <div class="mobile-content d-block d-lg-none">
+      <div class="card-content">
+        <v-sheet color="transparent" max-width="500px" class="mx-auto">
+          <div class="mb-10">
+            <h1 class="font-weight--700 white--text">Newbie in crypto?</h1>
+            <h1 class="font-weight--700 white--text">Explore MEWtopia!</h1>
+            <p class="white--text mt-3">
+              Our media platform where you can learn about crypto, share ideas,
+              and become part of a supportive community.
+            </p>
+          </div>
+          <mew-button
+            btn-link="https://www.mewtopia.com/"
+            :has-full-width="false"
+            title="Get Started"
+            btn-style="outline"
+            color-theme="white"
+            btn-size="xlarge"
+          />
+        </v-sheet>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'AppMewtopiaBlock',
+  name: 'AdGetStarted',
   components: {}
 };
 </script>
 
 <style lang="scss" scoped>
-.card-content {
-  background-image: url('~@/assets/images/backgrounds/bg-light.jpg');
-  background-position: 0px 0px;
-  background-size: 100%;
+.desktop-content {
+  .card-content {
+    padding: 80px 90px;
+    background-image: url('~@/assets/images/backgrounds/bg-light.jpg');
+    background-position: 0px 0px;
+    background-size: 100%;
+  }
 }
-.newbie-title-content {
-  display: inline-block;
+
+.mobile-content {
+  .card-content {
+    padding: 4rem 2rem;
+    background-image: url('~@/assets/images/backgrounds/bg-light.jpg');
+    background-position: center right -30px;
+    background-size: cover;
+  }
 }
 </style>
