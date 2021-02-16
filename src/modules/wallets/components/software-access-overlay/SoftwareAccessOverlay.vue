8de1548d0866ea07355e8b00b8d8668363db7ecd
David Hong <hsurf22@gmail.com>
2021-02-15 16:35:45 -0800

    Fix Access wallet > software overlay buttons and layouts

--- src/modules/wallets/components/software-access-overlay/SoftwareAccessOverlay.vue
+++ src/modules/wallets/components/software-access-overlay/SoftwareAccessOverlay.vue
@@ -9,9 +9,13 @@
     @closeOverlay="overlayClose"
   >
     <template #mewOverlayBody>
-      <v-sheet color="transparent" max-width="650px" class="mx-auto">
-        <v-row v-if="!step">
-          <v-col v-for="(btn, key) in buttons" :key="key" cols="12" sm="12">
+      <v-sheet
+        color="transparent"
+        max-width="650px"
+        class="mx-auto mew--software-access-overlay"
+      >
+        <template v-if="!step">
+          <div v-for="(btn, key) in buttons" :key="key">
             <mew-super-button
               btn-mode="small-right-image"
               :title="btn.label"
@@ -19,10 +23,11 @@
               :right-icon="btn.icon"
               icon-type="img"
               color-theme="basic"
+              class="mb-5 mew-super-btn-shadow"
               @click.native="btn.fn"
             />
-          </v-col>
-        </v-row>
+          </div>
+        </template>
         <access-keystore
           v-else-if="showKeystore"
           :btn-call="btnCall"
@@ -44,13 +49,12 @@
           v-else-if="showPrivKey"
           :unlock-private-key-wallet="unlockPrivateKeyWallet"
         />
-        <v-col cols="12" sm="12">
-          <mew-warning-sheet
-            title="Not Recommended"
-            description="This information is sensetive, and these options should only be used in offline settings by experienced crypto users."
-            :link-obj="warningSheetObj"
-          />
-        </v-col>
+        <mew-warning-sheet
+          class="pt-4"
+          title="Not Recommended"
+          description="This information is sensetive, and these options should only be used in offline settings by experienced crypto users."
+          :link-obj="warningSheetObj"
+        />
       </v-sheet>
       <div class="spacer-y-medium" />
     </template>
@@ -135,7 +139,7 @@ export default {
         {
           label: 'Private Key',
           description: 'Access via Private Key',
-          icon: require('@/assets/images/icons/icon-private-key-grey.svg'),
+          icon: require('@/assets/images/icons/icon-private-key-grey.png'),
           fn: () => {
             this.btnCall('privateKey');
           }
@@ -279,3 +283,11 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.mew--software-access-overlay {
+  .warning-sheet {
+    padding-top: 30px !important;
+  }
+}
+</style>
