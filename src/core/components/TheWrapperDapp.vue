6ec4842a4ceb1b29ab0cfa6347caf4ec7802cf8a
Jessica <jessicaspeng@gmail.com>
2021-03-02 15:56:27 -0800

    devop: add in wrapper dapp

---
+++ src/core/components/TheWrapperDapp.vue
@@ -0,0 +1,72 @@
+<template>
+  <!--
+    =====================================================================================
+      DAPP WRAPPER:
+    =====================================================================================
+    -->
+  <v-sheet class="rounded-lg">
+    <!--
+    =====================================================================================
+      Mew Banner - props: bannerText, bannerImg
+      TODO: we currently do not have all the custom dapp banner images, so will use the default for
+      now if there is none available.
+    =====================================================================================
+    -->
+    <mew-banner :text-obj="bannerText" :banner-img="bannerImg" />
+    <!--
+    =====================================================================================
+      Mew Tabs - props: tabItems, activeTab; takes in a slot for each 
+      tab content (tabContent + tab number )
+    =====================================================================================
+    -->
+    <mew-tabs
+      v-if="tabItems.length > 0"
+      class="pt-5"
+      :items="tabItems"
+      :is-centered="true"
+      :active-tab="activeTab"
+      has-underline
+    >
+      <template
+        v-for="(item, idx) in tabItems"
+        :slot="'tabContent' + (idx + 1)"
+      >
+        <slot :name="'tabContent' + (idx + 1)" />
+      </template>
+    </mew-tabs>
+    <!--
+    =====================================================================================
+     Slot: content, used to place body content if not using tabs.
+    =====================================================================================
+    -->
+    <div v-if="tabItems.length === 0" class="pa-8">
+      <slot name="content" />
+    </div>
+  </v-sheet>
+</template>
+
+<script>
+import bannerImage from '@/assets/images/backgrounds/bg-dapps-center.png';
+
+export default {
+  components: {},
+  props: {
+    bannerImg: {
+      default: bannerImage,
+      type: String
+    },
+    bannerText: {
+      default: () => {},
+      type: Object
+    },
+    tabItems: {
+      default: () => [],
+      type: Array
+    },
+    activeTab: {
+      default: 0,
+      type: Number
+    }
+  }
+};
+</script>
--- src/modules/dapps-center/ModuleDappsCenter.vue
+++ src/modules/dapps-center/ModuleDappsCenter.vue
@@ -1,123 +1,54 @@
 <template>
-  <div class="d-block">
-    <mew6-white-sheet>
-      <mew-banner :text-obj="title" :banner-img="bannerImage" />
-      <div class="pa-8">
-        <div class="mew-heading-1 px-4 mb-2">MEW Dapps</div>
-        <v-row>
-          <v-col
-            v-for="(dapp, key) in mewDapps"
-            :key="key"
-            cols="12"
-            md="6"
-            lg="4"
-          >
-            <mew-super-button
-              :cols-num="8"
-              :title="dapp.title"
-              :subtitle="dapp.subtitle"
-              :tag="dapp.tag"
-              :is-new="dapp.isNew"
-              :title-icon-type="dapp.titleIconType"
-              :title-icon="dapp.titleIcon"
-              :right-icon-type="dapp.rightIconType"
-              :right-icon="dapp.rightIcon"
-              @click.native="routeTo(dapp.path)"
-            />
-          </v-col>
-        </v-row>
-      </div>
-      <div class="pa-8">
-        <div class="mew-heading-1 px-4 mb-2">Recent</div>
-        <v-row>
-          <v-col
-            v-for="(dapp, key) in recent"
-            :key="key"
-            cols="12"
-            sm="6"
-            lg="4"
-          >
-            <mew-super-button
-              :title="dapp.title"
-              :subtitle="dapp.subtitle"
-              :tag="dapp.tag"
-              :is-new="dapp.isNew"
-              :title-icon-type="dapp.titleIconType"
-              :title-icon="dapp.titleIcon"
-              :right-icon-type="dapp.rightIconType"
-              :right-icon="dapp.rightIcon"
-            />
-          </v-col>
-        </v-row>
-      </div>
-    </mew6-white-sheet>
-  </div>
+  <!--
+    =====================================================================================
+      Dapp Center Module
+    =====================================================================================
+    -->
+  <the-wrapper-dapp :banner-img="bannerImage" :banner-text="title">
+    <template #content>
+      <div class="mew-heading-1 px-4 mb-4">MEW Dapps</div>
+      <v-row>
+        <v-col v-for="(dapp, key) in dapps" :key="key" cols="12" md="6" lg="4">
+          <mew-super-button
+            :cols-num="8"
+            :title="dapp.title"
+            :subtitle="dapp.subtitle"
+            :tag="dapp.tag"
+            :is-new="dapp.isNew"
+            :title-icon-type="dapp.titleIconType"
+            :title-icon="dapp.titleIcon"
+            :right-icon-type="dapp.rightIconType"
+            :right-icon="dapp.rightIcon"
+            @click.native="routeTo(dapp.path)"
+          />
+        </v-col>
+      </v-row>
+    </template>
+  </the-wrapper-dapp>
 </template>
 
 <script>
+import TheWrapperDapp from '@/core/components/TheWrapperDapp';
 import bannerImage from '@/assets/images/backgrounds/bg-dapps-center.png';
 export default {
-  components: {},
+  components: { TheWrapperDapp },
   data() {
     return {
       bannerImage: bannerImage,
       title: {
         title: 'Explore MEW Dapps'
       },
-      dapps: {
-        MakerDAO: {
-          title: 'MakerDAO',
-          subtitle: 'Collateralize & Generate DAI',
-          tag: '#Exchanges',
-          isNew: true,
-          titleIconType: 'mdi',
-          titleIcon: 'mdi-thumb-up',
-          rightIconType: 'mew',
-          rightIcon: 'makerdao'
-        },
-        ENSManager: {
+      dapps: [
+        {
           title: 'ENS manager',
           subtitle: 'Migrate or register ENS domain / subdomain',
           tag: '#Property',
           rightIconType: 'mew',
           rightIcon: 'ensManager',
           path: 'ENSManager'
-        },
-        SafeSendTX: {
-          title: 'SafeSend transaction',
-          subtitle: 'Migrate or register ENS domain / subdomain',
-          tag: '#Security',
-          rightIconType: 'mew',
-          rightIcon: 'lock'
-        },
-        ScheduleTX: {
-          title: 'Schedule a transaction',
-          subtitle: 'Using the decentralized ETH alarm clock prototype',
-          tag: '#Exchange',
-          rightIconType: 'mew',
-          rightIcon: 'calendar'
         }
-      }
+      ]
     };
-  },
-  computed: {
-    mewDapps() {
-      return [
-        this.dapps.MakerDAO,
-        this.dapps.ENSManager,
-        this.dapps.ENSManager,
-        this.dapps.SafeSendTX,
-        this.dapps.ScheduleTX,
-        this.dapps.ENSManager
-      ];
-    },
-    recent() {
-      return [
-        this.dapps.MakerDAO,
-        this.dapps.ENSManager,
-        this.dapps.ENSManager
-      ];
-    }
   },
   methods: {
     routeTo(path) {
