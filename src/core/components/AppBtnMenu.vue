6f8c43130c9fed851aa9f37ec1184f6002b5bb12
olgakup <olga.kupchevskaya.812@my.csun.edu>
2021-03-11 12:44:19 -0800

    devop: navigation bar implementation for the wallet on smal screens

---
+++ src/core/components/AppBtnMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn icon @click="menuMethod">
+    <img src="@/assets/images/icons/icon-menu.svg" />
+  </v-btn>
+</template>
+<script>
+export default {
+  name: 'AppBtnMenu',
+  props: {
+    menuMethod: {
+      type: Function,
+      default: function () {
+        return {};
+      }
+    }
+  }
+};
+</script>
