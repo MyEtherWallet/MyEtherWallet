de133bfa8ddfe758aa49dbea08e543527af40675
aerth <aerth@riseup.net>
2021-03-29 16:00:06 +0000

    add aqua.js

---
+++ src/networks/nodes/aqua-conical.js
@@ -0,0 +1,10 @@
+import { AQUA } from '../types';
+export default {
+  type: AQUA,
+  service: 'c.onical',
+  url: 'https://c.onical.org',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
