4e34b08cfde6609079865591ba6ff7c34230a754
olgakup <16910687+olgakup@users.noreply.github.com>
2021-03-31 15:33:05 -0700

    chore: lint code

--- src/core/store/wallet/getters.js
+++ src/core/store/wallet/getters.js
@@ -11,19 +11,19 @@ const tokensList = function (state) {
   const tokens = state.tokens;
   return tokens.length > 0
     ? tokens.map(item => {
-      if (!item.hasOwnProperty('balance')) {
-        item.balance = toBN(0);
-      } else {
-        item.balance = toBN(item.balance);
-      }
-      return item;
-    })
+        if (!item.hasOwnProperty('balance')) {
+          item.balance = toBN(0);
+        } else {
+          item.balance = toBN(item.balance);
+        }
+        return item;
+      })
     : [];
 };
 
 const initialLoad = function (state) {
-  return state.initialLoadTokens || state.initialLoadBalance
-}
+  return state.initialLoadTokens || state.initialLoadBalance;
+};
 
 export default {
   balanceInETH,
--- src/core/store/wallet/mutations.js
+++ src/core/store/wallet/mutations.js
@@ -25,7 +25,7 @@ const SET_WALLET = function (state, wallet) {
 const SET_BALANCE = function (state, balance) {
   state.balance = balance;
   if (state.initialLoadBalance) {
-    state.initialLoadBalance = false
+    state.initialLoadBalance = false;
   }
 };
 
@@ -44,10 +44,9 @@ const SET_OWNED_DOMAINS = function (state, ensDomains) {
 const SET_TOKENS = function (state, tokens) {
   state.tokens = tokens;
   if (state.initialLoadTokens) {
-    state.initialLoadTokens = false
+    state.initialLoadTokens = false;
   }
 };
-
 
 export default {
   REMOVE_WALLET,
