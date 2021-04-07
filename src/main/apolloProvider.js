e6d0d44b0a56537fa76cd67817632010cb8aea91
Jessica <jessicaspeng@gmail.com>
2021-04-07 15:08:08 -0700

    devop: remove separated apollo client

--- src/main/apolloProvider.js
+++ src/main/apolloProvider.js
@@ -2,7 +2,12 @@ import VueApollo from 'vue-apollo';
 import Vue from 'vue';
 
 import { aave } from '@/dapps/apollo-dapps.js';
-import main from '@/apollo/mainApolloClient';
+import { createApolloClient } from '@/core/helpers/createApolloClient';
+
+const main = createApolloClient(
+  'https://api.ethvm.com',
+  'wss://apiws.ethvm.com'
+);
 
 const apolloProvider = new VueApollo({
   clients: {
