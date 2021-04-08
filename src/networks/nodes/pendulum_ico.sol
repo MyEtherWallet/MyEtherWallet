e9dd65960bf2fcc6ff969f9b503eb502b64c2ceb
charlie83xt <65869775+charlie83xt@users.noreply.github.com>
2021-04-08 16:25:49 +0100

    Create pendulum_ico.sol

---
+++ src/networks/nodes/pendulum_ico.sol
@@ -0,0 +1,10 @@
+import { CLO } from '../types';
+export default {
+  type: CLO,
+  service: '0xinfra.com',
+  url: 'HTTP://127.0.0.1',
+  port: 7545,
+  auth: false,
+  username: '',
+  password: ''
+};
