b42d04d2cd810c514e22dbd120d84ab526bdfb80
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-05-05 17:05:02 -0700

    devop: add tokens value

--- src/views/layouts-wallet/TheSwapLayout.vue
+++ src/views/layouts-wallet/TheSwapLayout.vue
@@ -7,17 +7,21 @@
         :amount="amount"
       />
     </template>
+    <template #rightColItem1>
+      <module-tokens-value />
+    </template>
   </the-wrapper-wallet>
 </template>
 
 <script>
 import TheWrapperWallet from '@/core/components/TheWrapperWallet';
 import ModuleSwap from '@/modules/swap/ModuleSwap';
+import ModuleTokensValue from '@/modules/tokens-value/ModuleTokensValue';
 const ETH_TOKEN = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 const DAI_TOKEN = '0x6b175474e89094c44da98b954eedeac495271d0f';
 
 export default {
-  components: { TheWrapperWallet, ModuleSwap },
+  components: { TheWrapperWallet, ModuleSwap, ModuleTokensValue },
   props: {
     fromToken: {
       type: String,
