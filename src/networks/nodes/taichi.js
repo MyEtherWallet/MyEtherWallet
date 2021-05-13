236055868cf6a4d6c03596d97d0f1923551f79eb
Macd77 <84126486+Macd77@users.noreply.github.com>
2021-05-13 14:02:31 +0300

    Update taichi.js

--- src/networks/nodes/taichi.js
+++ src/networks/nodes/taichi.js
@@ -2,7 +2,7 @@ import { TAICHI } from '../types';
 export default {
   type: TAICHI,
   service: 'taichi.network',
-  url: 'https://api.taichi.network:10001/rpc/private','https://api-eu.taichi.network:10001/rpc/private','https://api-us.taichi.network:10001/rpc/private',
+  url: 'https://api.taichi.network:10001/rpc/private',
   port: 1,
   auth: false,
   username: '',
