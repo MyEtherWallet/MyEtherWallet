ca547938866deeb6972ddded131a91546258c4da
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-05-17 14:49:33 -0700

    lint: changes

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -300,8 +300,7 @@ export default {
         {
           contract_address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
           decimals: 18,
-          img:
-            'https://img.mewapi.io/?image=https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/ETH-0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.svg',
+          img: 'https://img.mewapi.io/?image=https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/ETH-0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee.svg',
           isEth: true,
           name: 'Ethereum',
           subtext: 'ETH',
@@ -312,8 +311,7 @@ export default {
         {
           contract_address: '0xbtc',
           decimals: 18,
-          img:
-            'https://img.mewapi.io/?image=https://web-api.changelly.com/api/coins/btc.png',
+          img: 'https://img.mewapi.io/?image=https://web-api.changelly.com/api/coins/btc.png',
           isEth: false,
           name: 'Bitcoin',
           subtext: 'BTC',
@@ -334,8 +332,7 @@ export default {
         {
           contract_address: '0xe41d2489571d322189246dafa5ebde1f4699f498',
           decimals: 18,
-          img:
-            'https://img.mewapi.io/?image=https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/ZRX-0xe41d2489571d322189246dafa5ebde1f4699f498.svg',
+          img: 'https://img.mewapi.io/?image=https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/src/icons/ZRX-0xe41d2489571d322189246dafa5ebde1f4699f498.svg',
           isEth: true,
           name: '0x',
           subtext: 'ZRX',
