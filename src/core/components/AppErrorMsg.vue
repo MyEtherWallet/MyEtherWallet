6a95d25870e60c2c073af2e1307f90ac05282fa0
David Hong <hsurf22@gmail.com>
2021-05-24 13:41:29 -0700

    devop: add user error message component

---
+++ src/core/components/AppErrorMsg.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog> </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    close: {
+      type: Function,
+      default: () => {}
+    },
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
