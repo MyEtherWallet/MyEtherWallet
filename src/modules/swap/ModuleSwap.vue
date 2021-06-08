46a681bf6a969dc7443ddad40469f6898c865774
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-06-08 16:54:24 -0700

    devop: handle swap network fee when clicking outside

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -1049,6 +1049,7 @@ export default {
       this.gasPriceModal = false;
     },
     closeGasPrice() {
+      console.log('close gasPrice');
       this.gasPriceModal = false;
     },
     setWrappedBtc(symbol) {
