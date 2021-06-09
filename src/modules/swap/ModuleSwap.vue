edb82d6df74462baf89281f082b7a8d3da29c17e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-06-09 10:10:02 -0700

    chore: remove console

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -1049,7 +1049,6 @@ export default {
       this.gasPriceModal = false;
     },
     closeGasPrice() {
-      console.log('close gasPrice');
       this.gasPriceModal = false;
     },
     setWrappedBtc(symbol) {
