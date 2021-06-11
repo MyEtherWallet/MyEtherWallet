62ad198d6c457a51af2b3b4c4056b0245c7fc455
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-06-10 18:18:21 -0700

    devop: add coingecko lists

--- fetchLists/index.js
+++ fetchLists/index.js
@@ -6,16 +6,17 @@ const IMAGE_PROXY = 'https://img.mewapi.io/?image=';
 if (!fs.existsSync(configs.GENERATED_FOLDER_PATH)) {
   fs.mkdirSync(configs.GENERATED_FOLDER_PATH);
 }
-const getOneInchFormattedList = async (url, network) => {
+const getFormattedList = async (url, network) => {
   const tokens = await fetch(url)
     .then(res => res.json())
     .then(tokens => {
       tokens = Object.values(tokens.tokens).map(t => {
-        t.contract_address = t.address;
+        t.contract_address = t.address.toLowerCase();
         t.icon = IMAGE_PROXY + t.logoURI;
         t.icon_png = IMAGE_PROXY + t.logoURI;
         t.network = network;
         delete t.logoURI;
+        delete t.chainId;
         return t;
       });
       tokens = tokens.filter(
@@ -26,20 +27,34 @@ const getOneInchFormattedList = async (url, network) => {
   return tokens;
 };
 const fetchOneInchLists = async () => {
-  const ethTokens = await getOneInchFormattedList(
+  const ethTokens = await getFormattedList(
     'https://api.1inch.exchange/v3.0/1/tokens',
     'eth'
   );
-  const bscTokens = await getOneInchFormattedList(
+  const bscTokens = await getFormattedList(
     'https://api.1inch.exchange/v3.0/56/tokens',
     'bsc'
   );
-  const maticTokens = await getOneInchFormattedList(
+  const maticTokens = await getFormattedList(
     'https://api.1inch.exchange/v3.0/137/tokens',
     'matic'
   );
   return {
     eth: ethTokens,
+    bsc: bscTokens,
+    matic: maticTokens
+  };
+};
+const fetchCGtokenList = async () => {
+  const bscTokens = await getFormattedList(
+    'https://tokens.coingecko.com/binance-smart-chain/all.json',
+    'bsc'
+  );
+  const maticTokens = await getFormattedList(
+    'https://tokens.coingecko.com/polygon-pos/all.json',
+    'matic'
+  );
+  return {
     bsc: bscTokens,
     matic: maticTokens
   };
@@ -88,13 +103,13 @@ const fetchMasterFile = async () => {
       fs.mkdirSync(configs.MASTER_FILE_PATH);
     }
     const oneInchTokens = await fetchOneInchLists();
+    const CGTokens = await fetchCGtokenList();
     const response = await fetch(
       'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/master-file.json'
     )
       .then(res => res.json())
       .then(tokens => {
         const networkTokens = {};
-        const networkTokenAddresses = {};
         tokens.forEach(token => {
           token.address = token.contract_address;
           if (token.icon !== '') {
@@ -102,34 +117,35 @@ const fetchMasterFile = async () => {
             token.icon_png = IMAGE_PROXY + token.icon_png;
           }
           delete token.link;
-          if (networkTokens[token.network]) {
-            networkTokens[token.network].push(token);
-            networkTokenAddresses[token.network].push(token.address);
-          } else {
-            networkTokens[token.network] = [token];
-            networkTokenAddresses[token.network] = [token.address];
-          }
+          if (!networkTokens[token.network]) networkTokens[token.network] = {};
+          networkTokens[token.network][token.address] = token;
         });
         const oneInchNetworks = Object.keys(oneInchTokens);
         for (const network of oneInchNetworks) {
-          if (!networkTokens[network]) {
-            networkTokens[network] = oneInchTokens[network];
-            tokens = tokens.concat(oneInchTokens[network]);
-          } else {
-            oneInchTokens[network].forEach(token => {
-              if (!networkTokenAddresses[network].includes(token.address)) {
-                networkTokens[network].push(token);
-                tokens.push(token);
-              }
-            });
-          }
+          if (!networkTokens[network]) networkTokens[network] = {};
+          oneInchTokens[network].forEach(t => {
+            if (!networkTokens[network][t.address]) {
+              networkTokens[network][t.address] = t;
+              tokens.push(t);
+            }
+          });
+        }
+        const CGNetworks = Object.keys(CGTokens);
+        for (const network of CGNetworks) {
+          if (!networkTokens[network]) networkTokens[network] = {};
+          CGTokens[network].forEach(t => {
+            if (!networkTokens[network][t.address]) {
+              networkTokens[network][t.address] = t;
+              tokens.push(t);
+            }
+          });
         }
         const networks = Object.keys(networkTokens);
         for (const network of networks) {
           console.log('Writing tokens for the network: ' + network);
           fs.writeFileSync(
             `${configs.TOKENS_PATH}/tokens-${network}.json`,
-            JSON.stringify(networkTokens[network])
+            JSON.stringify(Object.values(networkTokens[network]))
           );
         }
         return tokens;
