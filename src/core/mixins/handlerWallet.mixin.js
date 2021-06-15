253d92e0e8cb2fc996438b016a93b430f763d4bf
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-06-15 15:30:50 -0700

    devop: cleanup

--- src/core/mixins/handlerWallet.mixin.js
+++ src/core/mixins/handlerWallet.mixin.js
@@ -3,8 +3,6 @@
  */
 import { getLatestPrices } from '@/apollo/queries/wallets/wallets.graphql';
 import { Toast, ERROR } from '@/modules/toast/handler/handlerToast';
-import BigNumber from 'bignumber.js';
-import { formatFloatingPointValue } from '@/core/helpers/numberFormatHelper';
 
 const tokens = {
   eth: 'ethereum'
@@ -52,84 +50,6 @@ export default {
       error(error) {
         Toast(error.message, {}, ERROR);
       }
-    }
-    /**
-    //  * Apollo query to fetch owners erc20 tokens and set the state
-    //  */
-    // getOwnersERC20Tokens: {
-    //   query: getOwnersERC20Tokens,
-    //   variables() {
-    //     return {
-    //       hash: this.address
-    //     };
-    //   },
-    //   skip() {
-    //     return (
-    //       this.address === null || !this.isEthNetwork || this.address === ''
-    //     );
-    //   },
-    //   result({ data }) {
-    //     if (data && data.getOwnersERC20Tokens) {
-    //       const formattedList = [];
-    //       const ownedTokens = data.getOwnersERC20Tokens.owners;
-    //       ownedTokens.forEach(token => {
-    //         let foundToken;
-    //         if (this.tokensData && this.tokensData.get) {
-    //           foundToken = this.tokensData.get(
-    //             token.tokenInfo.contract.toLowerCase()
-    //           );
-    //         }
-    //         const denominator = new BigNumber(10).pow(token.tokenInfo.decimals);
-    //         const usdBalance = foundToken
-    //           ? new BigNumber(token.balance)
-    //               .div(denominator)
-    //               .times(foundToken.current_price)
-    //               .toString()
-    //           : null;
-    //         const price = foundToken ? foundToken.current_price : null;
-    //         // need to eventually change image to check tokens network rather than just use eth network (if theres no image from coingecko)
-    //         formattedList.push({
-    //           name: token.tokenInfo.symbol,
-    //           symbol: token.tokenInfo.symbol,
-    //           subtext: token.tokenInfo.name,
-    //           value: token.tokenInfo.name,
-    //           balance: token.balance,
-    //           contract: token.tokenInfo.contract,
-    //           img: foundToken ? foundToken.image : ethImg,
-    //           decimals: token.tokenInfo.decimals,
-    //           market_cap: foundToken ? foundToken.market_cap : null,
-    //           price_change_percentage_24h: foundToken
-    //             ? foundToken.price_change_percentage_24h
-    //             : null,
-    //           totalBalanceRaw: usdBalance,
-    //           totalBalance: formatFiatValue(usdBalance).value,
-    //           priceRaw: price,
-    //           price: formatFiatValue(price).value,
-    //           tokenBalance: this._getTokenBalance(
-    //             token.balance,
-    //             token.tokenInfo.decimals
-    //           ).value
-    //         });
-    //       });
-    //       this.setTokens(formattedList);
-    //     }
-    //   },
-    //   error(error) {
-    //     Toast(error.message, {}, ERROR);
-    //   }
-    // }
-  },
-  methods: {
-    /**
-     * Get token balance
-     */
-    _getTokenBalance(balance, decimals) {
-      let n = new BigNumber(balance);
-      if (decimals) {
-        n = n.div(new BigNumber(10).pow(decimals));
-        n = formatFloatingPointValue(n);
-      }
-      return n;
     }
   }
 };
