a61580a34467f7e127dc5b72f27860822c54c570
olgakup <olga.kupchevskaya.812@my.csun.edu>
2021-06-30 22:09:13 -0700

    ui: new network switch ui implementation

--- src/modules/network/components/NetworkSwitch.vue
+++ src/modules/network/components/NetworkSwitch.vue
@@ -1,48 +1,115 @@
 <template>
   <mew-overlay
     :show-overlay="open"
+    title="Select Network"
     left-btn-text=""
     right-btn-text="Close"
     @closeOverlay="$emit('close')"
   >
     <template #mewOverlayBody>
-      <h2 class="text-center mb-10">Network</h2>
+      <v-sheet color="white" max-width="740px" width="100%" class="mx-auto">
+        <!-- <h2 class="text-center mt-13 mb-8">Select Network</h2> -->
 
-      <v-sheet
-        color="transparent"
-        max-width="700px"
-        width="100%"
-        class="mx-auto"
-      >
-        <mew6-white-sheet full-width class="mt-7">
-          <div class="pa-8">
-            <v-radio-group v-model="networkSelected">
-              <div v-for="(item, i) in typeNames" :key="i">
-                <div class="text-uppercase font-weight-bold subtitle-1 mb-1">
-                  {{ item }}
+        <v-sheet
+          color="transparent"
+          max-width="516px"
+          width="100%"
+          class="mx-auto pb-2 pb-am-8 px-3 pt-3 px-sm-0"
+        >
+          <v-row
+            class="
+              align-end
+              justify-center justify-sm-space-between
+              px-0
+              pt-5 pt-3
+            "
+          >
+            <!--
+            =====================================================================================
+              Toggle: Main/Test/All
+            =====================================================================================
+            -->
+            <div
+              class="
+                align-center align-sm-end
+                justify-center
+                pr-sm-3
+                pb-sm-3
+                order-sm-2
+              "
+            >
+              <v-btn-toggle
+                v-model="toggleType"
+                mandatory
+                active-class="titlePrimary white--text alig-end"
+              >
+                <v-btn small>Main</v-btn>
+                <v-btn small>Test</v-btn>
+                <v-btn small>All</v-btn>
+              </v-btn-toggle>
+            </div>
+            <!--
+            =====================================================================================
+              Search Data
+            =====================================================================================
+            -->
+            <v-col cols="12" sm="7" class="order-sm-1">
+              <mew-search placeholder="Find Network" :value="searchInput" />
+            </v-col>
+          </v-row>
+          <!--
+          =====================================================================================
+            Networks
+          =====================================================================================
+          -->
+          <v-radio-group v-model="networkSelected" class="networks-container">
+            <v-container
+              v-for="(network, i) in networks"
+              :key="network.name"
+              :class="[
+                { 'network-border-bottom': i + 1 < networks.length },
+                'py-4 px-5'
+              ]"
+            >
+              <v-row class="pa-0 mew-body align-center justify-start">
+                <!--
+                =====================================================================================
+                  Incon
+                =====================================================================================
+                -->
+                <v-img
+                  :src="network.icon"
+                  :lazy-src="
+                    require('@/assets/images/currencies/icon-eth-grey.svg')
+                  "
+                  contain
+                  max-height="24px"
+                  max-width="24px"
+                />
+                <!--
+                =====================================================================================
+                  Symbol/Namte
+                =====================================================================================
+                -->
+                <div class="titlePrimary--text Capitalize pl-3">
+                  {{ network.name }}
                 </div>
-
-                <v-row no-gutters>
-                  <v-col
-                    v-for="network in networks[item]"
-                    :key="network.value"
-                    cols="12"
-                    md="6"
-                    class="mt-2"
-                  >
-                    <v-radio
-                      :label="network.name"
-                      :value="network.value"
-                    ></v-radio>
-                  </v-col>
-                </v-row>
-
-                <div>{{ item.id }}</div>
-                <v-divider v-if="typeNames.length != i + 1" class="my-5" />
-              </div>
-            </v-radio-group>
-          </div>
-        </mew6-white-sheet>
+                <div class="px-2 textSecondary--text">-</div>
+                <div class="textSecondary--text">
+                  {{ network.name_long }}
+                </div>
+                <v-spacer />
+                <!--
+                =====================================================================================
+                  Radio
+                =====================================================================================
+                -->
+                <v-radio :value="network.name" :class="['py-2 mb-0']">
+                </v-radio>
+              </v-row>
+            </v-container>
+          </v-radio-group>
+        </v-sheet>
       </v-sheet>
     </template>
   </mew-overlay>
@@ -63,7 +130,9 @@ export default {
     return {
       networkSelected: null,
       types: types,
-      nodes: nodes
+      nodes: nodes,
+      toggleType: 0,
+      searchInput: ''
     };
   },
   computed: {
@@ -74,22 +143,25 @@ export default {
       typeNames.unshift('ETH');
       return typeNames;
     },
+
     networks() {
-      const newObj = {};
-      Object.keys(this.types).forEach(item => {
-        newObj[item] = [];
+      const allNetworks = [];
+      this.typeNames.forEach(item => {
+        allNetworks.push(types[item]);
       });
+      console.log(allNetworks);
+      return allNetworks;
 
-      Object.keys(this.nodes).forEach(item => {
-        if (this.nodes[item]) {
-          newObj[this.nodes[item].type.name].push({
-            name: this.nodes[item].service,
-            value: this.nodes[item].url
-          });
-        }
-      });
+      // Object.keys(this.nodes).forEach(item => {
+      //   if (this.nodes[item]) {
+      //     newObj[this.nodes[item].type.name].push({
+      //       name: this.nodes[item].service,
+      //       value: this.nodes[item].url
+      //     });
+      //   }
+      // });
 
-      return newObj;
+      // return newObj;
     }
   },
   watch: {
@@ -125,3 +197,22 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+.networks-container {
+  max-width: 516px;
+}
+
+.network-border-bottom {
+  border-bottom: 1px solid #ececec;
+}
+
+.v-radio label {
+  left: -8px !important;
+}
+
+.v-input--radio-group__input {
+  border: 1px solid #ececec;
+  box-sizing: border-box;
+  border-radius: 4px;
+}
+</style>
