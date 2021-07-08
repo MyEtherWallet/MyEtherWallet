ba463abf5fbb3f22c4abfbe0bbb9dc925f635518
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-07-08 14:12:05 -0700

    devop: update the api for provider class

--- checkBranch.sh
+++ checkBranch.sh
@@ -3,7 +3,7 @@ LC_ALL=C
 
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 acceptable_branch_names="feature, feat, bug, chore, devop, release, and hotfix"
-valid_branch_regex="^(feature|feat|fix|bug|chore|devop|release|hotfix|ui|v6)\/[a-z0-9._-]+$"
+valid_branch_regex="^(feature|feat|fix|bug|chore|devop|release|hotfix|ui)\/[a-z0-9._-]+$"
 
 message="Commit rejected! Please make sure your branch name starts with any of the following: $acceptable_branch_names."
 
