a4fb8808bb52e638489006fadeb52ac3e3c80e83
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-07-08 02:12:00 -0700

    chore: 🔧 change eth test node

--- src/modules/name-resolver/tests/name-resolver.spec.js
+++ src/modules/name-resolver/tests/name-resolver.spec.js
@@ -5,7 +5,7 @@ const nameResolver = new NameResolver({
       registry: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
     }
   },
-  url: 'https://main-light.eth.linkpool.io/',
+  url: 'https://eth.getblock.io/mainnet/',
   port: 443
 });
 
@@ -20,7 +20,7 @@ describe('Ethereum Name Resolver', () => {
       expect(addr).toBe('0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D');
     });
   });
-  xtest('it should cns name: cofounding.zil', () => {
+  test('it should cns name: cofounding.zil', () => {
     return nameResolver.resolveName('cofounding.zil').then(addr => {
       expect(addr).toBe('0xaA91734f90795e80751C96e682A321bB3C1A4186');
     });
