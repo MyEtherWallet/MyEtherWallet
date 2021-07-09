de370b3f2d2581288f51a73dc2ad79676d61fb5d
David Hong <hsurf22@gmail.com>
2021-07-09 12:37:37 -0700

    fix: fix tools layout

--- src/modules/message/ModuleMessageVerify.vue
+++ src/modules/message/ModuleMessageVerify.vue
@@ -1,39 +1,32 @@
 <template>
-  <mew-module
-    class="d-flex flex-grow-1 pt-6"
-    :title="title"
-    :has-elevation="true"
-    :has-indicator="true"
-  >
-    <template #moduleBody>
-      <div>
-        <v-textarea
-          v-model="message"
-          outlined
-          label="Signature"
-          :value="message"
-        ></v-textarea>
+  <div>
+    <div class="mew-heading-2 mb-12">{{ title }}</div>
 
-        <div v-if="signResult">
-          <span v-if="didSign"> {{ signer }} Did Sign</span>
-          <span v-if="didntSign">{{ signer }} Didn't sign</span>
-        </div>
-        <mew-button
-          title="Verify"
-          btn-size="xlarge"
-          class="display--block mx-auto mt-5"
-          @click.native="verifyMessage"
-        />
-        <mew-button
-          btn-style="transparent"
-          title="Clear All"
-          btn-size="small"
-          class="display--block mx-auto mt-5"
-          @click.native="clearAll"
-        />
-      </div>
-    </template>
-  </mew-module>
+    <v-textarea
+      v-model="message"
+      outlined
+      label="Signature"
+      :value="message"
+    ></v-textarea>
+
+    <div v-if="signResult">
+      <span v-if="didSign"> {{ signer }} Did Sign</span>
+      <span v-if="didntSign">{{ signer }} Didn't sign</span>
+    </div>
+    <mew-button
+      title="Verify"
+      btn-size="xlarge"
+      class="display--block mx-auto mt-5"
+      @click.native="verifyMessage"
+    />
+    <mew-button
+      btn-style="transparent"
+      title="Clear All"
+      btn-size="small"
+      class="display--block mx-auto mt-5"
+      @click.native="clearAll"
+    />
+  </div>
 </template>
 
 <script>
