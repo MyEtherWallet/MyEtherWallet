a71d2acb44b1c6a4fb4569d99396c7e15369c8ba
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-07-12 15:01:32 -0700

    devop: 🔧 allow translation as branch name and commit subject

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-      'type-enum': [2, 'always', ['build', 'ui', 'feature', 'ci', 'devop', 'chore', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'lint', 'test', 'translations']]
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ui',
+        'feature',
+        'ci',
+        'devop',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'revert',
+        'style',
+        'lint',
+        'test',
+        'translation'
+      ]
+    ]
   }
-}
+};
