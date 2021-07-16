e1fe3b62eabe04dcbe077eb8fb28a977931b710e
Jessica <jessicaspeng@gmail.com>
2021-07-16 09:54:59 -0700

    feat: ✨ mew wallet banner

--- src/views/TheDefaultView.vue
+++ src/views/TheDefaultView.vue
@@ -5,17 +5,21 @@
       <router-view />
     </v-main>
     <the-default-footer />
+    <the-default-wallet-banner />
   </v-sheet>
 </template>
 
 <script>
 import TheDefaultHeader from './components-default/TheDefaultHeader';
 import TheDefaultFooter from './components-default/TheDefaultFooter';
+import TheDefaultWalletBanner from './components-default/TheDefaultWalletBanner';
+
 export default {
   name: 'TheDefaultView',
   components: {
     TheDefaultHeader,
-    TheDefaultFooter
+    TheDefaultFooter,
+    TheDefaultWalletBanner
   }
 };
 </script>
