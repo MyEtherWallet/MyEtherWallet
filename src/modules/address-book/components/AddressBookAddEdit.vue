65c303e5b7cff067251972406fdbb827ad39da87
Jessica <jessicaspeng@gmail.com>
2021-07-20 11:53:34 -0700

    fix: 🐛 valid address check

--- src/modules/address-book/components/AddressBookAddEdit.vue
+++ src/modules/address-book/components/AddressBookAddEdit.vue
@@ -150,8 +150,10 @@ export default {
     },
     validAddress() {
       return this.resolvedAddr.length > 0
-        ? utils.isAddress(this.resolvedAddr)
-        : utils.isAddress(this.addressToAdd);
+        ? utils.isAddress(this.resolvedAddr) &&
+            utils.isHexStrict(this.resolvedAddr)
+        : utils.isAddress(this.addressToAdd) &&
+            utils.isHexStrict(this.addressToAdd);
     },
     editMode() {
       return this.mode === modes[1];
