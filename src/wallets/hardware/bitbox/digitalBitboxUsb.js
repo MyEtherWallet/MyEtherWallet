9b0f27cee2cde3ca8cb9ef3d6aeafe848cf12b56
Marko Bencun <mbencun+pgp@gmail.com>
2021-07-26 15:42:00 +0200

    bitbox01: hotfix on v5: make BitBox01 work with WebHID, drop U2F-hijack
    BitBox01 is currently not functional on v6.0.3 and it does not seem
    trivial to make it work.
    
    The previous version is hosted at https://v5.myetherwallet.com/, but
    there, BitBox01 support also broke.
    
    This patch is a hot-fix for the v5 deployment that makes the BitBox01
    integration functional again, using WebHID (Chrome only).
    
    This will help BitBox01 users migrate their coins to a new wallet.

--- src/wallets/hardware/bitbox/digitalBitboxUsb.js
+++ src/wallets/hardware/bitbox/digitalBitboxUsb.js
@@ -6,76 +6,81 @@
 
 'use strict';
 
-import u2f from 'u2f-api';
-
 const DigitalBitboxUsb = function () {};
 
-// Convert from normal to web-safe, strip trailing "="s
-DigitalBitboxUsb.webSafe64 = function (base64) {
-  return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-};
+let _callback = null;
+let _hidDevice = null;
+let _readBuffer = null;
+let _dataLen = null;
 
-// Convert from web-safe to normal, add trailing "="s
-DigitalBitboxUsb.normal64 = function (base64) {
-  return (
-    // eslint-disable-next-line
-    base64.replace(/\-/g, '+').replace(/_/g, '/') +
-    '=='.substring(0, (3 * base64.length) % 4)
-  );
-};
-
-DigitalBitboxUsb.prototype.u2fCallback = function (response, callback) {
-  if ('signatureData' in response) {
-    const data = new Buffer(
-      DigitalBitboxUsb.normal64(response.signatureData),
-      'base64'
-    );
-    callback(data.slice(5));
-  } else {
-    callback(undefined, response);
+async function connectWebHID() {
+  if (_hidDevice !== null) {
+    return;
   }
-};
+  const vendorID = 0x03eb;
+  const productID = 0x2402;
+  _hidDevice = (
+    await navigator.hid.requestDevice({ filters: [{ vendorID, productID }] })
+  )[0];
+  await _hidDevice.open();
+  _hidDevice.addEventListener('inputreport', event => {
+    if (!_callback) {
+      return;
+    }
+    const data = Buffer.from(event.data.buffer);
+    if (_readBuffer === null) {
+      if (data.length < 7) {
+        throw 'unexpected response';
+      }
+      _dataLen = data[5] * 256 + data[6];
+      _readBuffer = data.slice(7);
+    } else {
+      if (data.length < 5) {
+        throw 'unexpected response';
+      }
+      _readBuffer = Buffer.concat([_readBuffer, data.slice(5)]);
+    }
+    if (_readBuffer.length >= _dataLen) {
+      const x = _readBuffer.slice(0, _dataLen);
+      const cb = _callback;
+      _callback = null;
+      _readBuffer = null;
+      _dataLen = null;
+      cb(x);
+    }
+  });
+}
 
+const cid = 0xff000000;
+const cmd = 0x80 + 0x40 + 0x01;
 DigitalBitboxUsb.prototype.exchange = function (msg, callback) {
+  _callback = callback;
   msg = Buffer.from(msg, 'ascii');
-  const kh_max_len = 128 - 2; // Subtract 2 bytes for `index` and `total` header
-  // Challenge is needed for U2F but unused in the hijack procedure
-  const challenge = new Buffer(
-    'dbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdbdb',
-    'hex'
-  );
-  const total = Math.ceil(msg.length / kh_max_len);
-  const self = this;
-  const localCallback = function (result) {
-    self.u2fCallback(result, callback);
-  };
-  for (let index = 0; index < total; index++) {
-    const kh = Buffer.concat([
-      Buffer.from([total]),
-      Buffer.from([index]),
-      msg.slice(index * kh_max_len, (index + 1) * kh_max_len)
-    ]);
-    const key = {
-      appId: location.origin,
-      challenge: DigitalBitboxUsb.webSafe64(challenge.toString('base64')),
-      version: 'U2F_V2',
-      keyHandle: DigitalBitboxUsb.webSafe64(kh.toString('base64'))
-    };
-    // Set timeout to 35 seconds for Windows 10 to wait for user confirmation
-    // Keep 3 seconds for other plaforms so that polling is fast enough
-    let timeout;
-    navigator.platform.indexOf('Win') >= 0 &&
-    (navigator.userAgent.indexOf('Windows NT 10.0') != -1 ||
-      navigator.userAgent.indexOf('Windows 10.0') != -1)
-      ? (timeout = 35)
-      : (timeout = 3);
-    u2f
-      .sign(key, timeout)
-      .then(localCallback)
-      .catch(err => {
-        callback(undefined, err);
-      });
-  }
+  connectWebHID().then(() => {
+    const kh_max_len = 64;
+    let index = 0;
+    while (msg.length > 0) {
+      const tmp = Buffer.alloc(4);
+      tmp.writeUInt32BE(cid);
+      let header = null;
+      if (index === 0) {
+        const tmp2 = Buffer.alloc(2);
+        tmp2.writeUInt16BE(msg.length & 0xffff);
+        header = Buffer.concat([tmp, Buffer.from([cmd]), tmp2]);
+      } else {
+        const seq = index - 1;
+        header = Buffer.concat([tmp, Buffer.from([seq])]);
+      }
+      const chunk = msg.slice(0, kh_max_len - header.length);
+      msg = msg.slice(kh_max_len - header.length);
+      let kh = Buffer.concat([header, chunk]);
+      if (kh.length < kh_max_len) {
+        kh = Buffer.concat([kh, Buffer.alloc(kh_max_len - kh.length, 0xee)]);
+      }
+      _hidDevice.sendReport(0, Uint8Array.from(kh));
+      index++;
+    }
+  });
 };
 
 export default DigitalBitboxUsb;
