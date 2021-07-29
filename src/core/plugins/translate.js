db53b94be0df982dcc44ea1bb3e74de5209226db
andrewdahn <andrewahnjr@gmail.com>
2021-07-28 19:11:43 -0700

    translation: 🌐 env variable for project id

--- src/core/plugins/translate.js
+++
@@ -1,9 +0,0 @@
-// document.dispatchEvent(new Event('lokalise-update-elements'));
-
-// document.addEventListener('lokalise-on-save', function (event) {
-//   console.log('!!!!');
-//   console.log(event.detail.updatedTranslations);
-//   console.log(event.detail.skippedTranslations);
-//   console.log(event.detail.dismissedTranslations);
-//   console.log(event.detail.errorMessage);
-// });
