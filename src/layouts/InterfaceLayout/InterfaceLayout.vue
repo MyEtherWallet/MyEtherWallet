5fd63c7dc0412cceea013210dee75ee398092e1e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-08-02 11:14:32 -0700

    chore: pull in bitbox fix, update packages

--- src/layouts/InterfaceLayout/InterfaceLayout.vue
+++ src/layouts/InterfaceLayout/InterfaceLayout.vue
@@ -594,6 +594,7 @@ export default {
         ) {
           try {
             getAddressTokens(this.account.address).then(res => {
+              console.log(res);
               const tokens = [];
               const apiTokens = res.data.getOwnersERC20Tokens.owners;
               const parsedApiTokens = apiTokens.map(apiT => {
