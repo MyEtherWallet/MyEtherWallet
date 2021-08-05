b2e6ad2a4c6cc7b5ffaa4ba2669c68577909a6f8
Jessica <jessicaspeng@gmail.com>
2021-08-04 18:53:46 -0700

    chore: 👷 update packages

--- package-test.js
+++ package-test.js
@@ -19,7 +19,12 @@ const EXCEPTIONS = [
   'husky',
   '@aave/protocol-js',
   'sass',
-  'scryptsy'
+  'scryptsy',
+  'web3',
+  'web3-core-helpers',
+  'web3-core-method',
+  'web3-core-requestmanager',
+  'web3-utils'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
