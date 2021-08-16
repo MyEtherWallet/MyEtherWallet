c90bd8f09edb5156945f18b56d22ec5f7b2e051a
Ryan <ryans@myetherwallet.com>
2021-08-16 13:15:19 -0700

    fix: 🐛 move error to global handler for software wallets

--- src/modules/access-wallet/software/handlers/web3wallet/errorHandler.js
+++ src/modules/access-wallet/software/handlers/web3wallet/errorHandler.js
@@ -9,9 +9,7 @@ const ERRORS = {
     'Returned error: Error: MetaMask Tx Signature: User denied transaction signature.',
   'User rejected action!': 'User rejected action!',
   "Can't sign messages from a burner account!":
-    "Can't sign messages from a burner account!",
-  "Returned values aren't valid, did it run Out of Gas? You might also see this error if you are not using the correct ABI for the contract you are retrieving data from, requesting data from a block number that does not exist, or querying a node which is not fully synced.":
-    "Returned values aren't valid, did it run Out of Gas? You might also see this error if you are not using the correct ABI for the contract you are retrieving data from, requesting data from a block number that does not exist, or querying a node which is not fully synced."
+    "Can't sign messages from a burner account!"
 };
 const WARNINGS = {};
 
