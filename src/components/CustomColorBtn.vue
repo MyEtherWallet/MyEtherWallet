249e1bf8104e85667c5ce6c9269b4a267f5788e5
David Hong <hsurf22@gmail.com>
2021-08-19 17:17:40 -0700

    feat: custom color button - test only before apply this to mew-components

---
+++ src/components/CustomColorBtn.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-btn
+    style="text-transform: initial"
+    :style="hoverColor ? hoverColorStyle : ''"
+    depressed
+    :color="btnColor"
+    :class="`${textColor}--text`"
+    :ripple="{ class: `${rippleColor}--text` }"
+    :x-large="xLarge"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    btnColor: {
+      type: String,
+      default: ''
+    },
+    textColor: {
+      type: String,
+      default: ''
+    },
+    hoverColor: {
+      type: String,
+      default: ''
+    },
+    rippleColor: {
+      type: String,
+      default: ''
+    },
+    xLarge: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    hoverColorStyle() {
+      return {
+        '--hover-color': this.hoverColor
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-btn:not(.v-btn--round).v-size--x-large {
+  height: initial;
+  min-height: 52px;
+}
+.v-btn:before {
+  background-color: var(--hover-color, currentColor);
+}
+</style>
--- src/views/layouts-default/TheHomeLayout.vue
+++ src/views/layouts-default/TheHomeLayout.vue
@@ -1,5 +1,55 @@
 <template>
   <div>
+    <v-container class="py-15">
+      <custom-color-btn
+        class="ma-1"
+        btn-color="primary"
+        text-color=""
+        hover-color="blue"
+        ripple-color="red"
+        x-large
+      >
+        <div class="pa-3">
+          <div>Button Color: primary</div>
+          <div>Text Color:</div>
+          <div>Hover Color: blue</div>
+          <div>Ripple Color: red</div>
+        </div>
+      </custom-color-btn>
+
+      <custom-color-btn
+        class="ma-1"
+        btn-color="pink"
+        text-color="black"
+        hover-color="yellow"
+        ripple-color="white"
+        x-large
+      >
+        <div class="pa-3">
+          <div>Button Color: pink</div>
+          <div>Text Color: black</div>
+          <div>Hover Color: yellow</div>
+          <div>Ripple Color: white</div>
+        </div>
+      </custom-color-btn>
+
+      <custom-color-btn
+        class="ma-1"
+        btn-color="orange"
+        text-color="blue"
+        hover-color="white"
+        ripple-color="yellow"
+        x-large
+      >
+        <div class="pa-3">
+          <div>Button Color: orange</div>
+          <div>Text Color: blue</div>
+          <div>Hover Color: white</div>
+          <div>Ripple Color: yellow</div>
+        </div>
+      </custom-color-btn>
+    </v-container>
+
     <home-landing />
     <home-features />
     <home-socials />
@@ -11,6 +61,7 @@
 </template>
 
 <script>
+import CustomColorBtn from '@/components/CustomColorBtn';
 import HomeLanding from '../components-default/HomeLanding';
 import HomeFeatures from '../components-default/HomeFeatures';
 import HomeSocials from '../components-default/HomeSocials.vue';
@@ -22,6 +73,7 @@ import AppPartnersBlock from '@/core/components/AppPartnersBlock';
 export default {
   name: 'TheHomeLayout',
   components: {
+    CustomColorBtn,
     HomeLanding,
     HomeFeatures,
     HomeSocials,
