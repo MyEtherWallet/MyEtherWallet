f0a3b0eac1a3c12e51fc82b33a979fe05f0da64d
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-08-19 17:16:32 -0700

    fix: 🐛 eth_subscription issue

--- src/dapps/ens-manager-dapp/handlers/handlerPermanentName.js
+++ src/dapps/ens-manager-dapp/handlers/handlerPermanentName.js
@@ -142,19 +142,9 @@ export default class PermanentNameModule extends ENSManagerInterface {
           .on('transactionHash', hash =>
             promiEvent.emit('transactionHash', hash)
           )
-          .once('receipt', receipt => {
-            return promiEvent.emit('receipt', receipt);
-          })
-          .on('receipt', receipt => {
-            return promiEvent.emit('receipt', receipt);
-          })
-          .on('confirmation', (confNumber, receipt) => {
-            return promiEvent.emit('receipt', receipt);
-          })
+          .once('receipt', receipt => promiEvent.emit('receipt', receipt))
           .on('error', err => promiEvent.emit('error', err))
-          .then(receipt => {
-            promiEvent.emit('receipt', receipt);
-          });
+          .then(receipt => promiEvent.emit('receipt', receipt));
       });
     return promiEvent;
   }
