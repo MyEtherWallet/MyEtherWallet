ea41c7bc8c169e932721b5c5f807f89e70f508d9
Ryan <ryans@myetherwallet.com>
2021-09-02 16:39:29 -0700

    fix: 🐛 add mneumonic error to global errors

--- src/modules/toast/handler/handlerToast.js
+++ src/modules/toast/handler/handlerToast.js
@@ -11,7 +11,9 @@ const GLOBAL_ERRORS = {
     "Returned values aren't valid, did it run Out of Gas? You might also see this error if you are not using the correct ABI for the contract you are retrieving data from, requesting data from a block number that does not exist, or querying a node which is not fully synced.",
   'Invalid message type!': 'Invalid message type!',
   'Device is used in another window': 'Device is used in another window',
-  'wrong previous session': 'Wrong previous session'
+  'Wrong previous session': 'Wrong previous session',
+  'Something went wrong in mnemonic wallet access':
+    'Something went wrong in mnemonic wallet access'
 };
 const foundGlobalError = text => {
   const errorValues = Object.values(GLOBAL_ERRORS);
