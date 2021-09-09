1085acff26c8e8a59b7212a54a6051c890183a14
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-09-08 19:33:04 -0700

    devop: 🔧 update wallet connect library

--- connections.js
+++ connections.js
@@ -33,16 +33,18 @@ module.exports = [
   'https://api.wallet.coinbase.com:443',
   'data:',
   'https://mainnet.infura.io:443',
+  'https://registry.walletconnect.org:443',
+  'https://cdn.live.ledger.com:443',
+  'https://app.lokalise.com:443',
+  'https://registry.walletconnect.org:443/data/wallets.json',
+  'wss://*.bridge.walletconnect.org:443',
+  // intercom stuff
+  'api-iam.intercom.io:443',
   'https://api.zilliqa.com:443',
   'https://widget.intercom.io:443',
   'https://api-iam.intercom.io:443',
   'https://js.intercomcdn.com:443',
   'wss://nexus-websocket-a.intercom.io:443',
-  'api-iam.intercom.io:443',
-  'https://registry.walletconnect.org:443',
-  'https://cdn.live.ledger.com:443',
-  'https://app.lokalise.com:443',
-  'https://registry.walletconnect.org:443/data/wallets.json',
   'https://js.intercomcdn.com:443',
   'https://uploads.intercomcdn.com:443'
 ];
