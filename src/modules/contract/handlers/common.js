856df645141765fd489f9fc01c5a822e6e4c7c33
Ryan <ryans@myetherwallet.com>
2021-09-23 14:42:22 -0700

    fix: 🐛 contract interaction input

--- src/modules/contract/handlers/common.js
+++ src/modules/contract/handlers/common.js
@@ -21,13 +21,13 @@ const isContractArgValid = (value, solidityType) => {
       }
       return true;
     }
-    if (solidityType.includes(uint) || solidityType.includes(int))
+    if (solidityType.includes(uint) || solidityType.includes(int)) {
       return value !== '' && !isNaN(value) && isInt(value);
-    if (solidityType === address) return isAddress(value);
-    if (solidityType === string) return true;
-    if (solidityType.includes(bytes))
+    } else if (solidityType === address) return isAddress(value);
+    else if (solidityType === string) return true;
+    else if (solidityType.includes(bytes))
       return value.substr(0, 2) === '0x' && validateHexString(value);
-    if (solidityType === bool)
+    else if (solidityType === bool)
       return typeof value === typeof true || typeof value === typeof false;
     return false;
   } catch (e) {
@@ -39,12 +39,16 @@ const getType = inputType => {
   if (inputType.includes('[]')) {
     return { type: 'string', solidityType: `${inputType}` };
   }
-  if (inputType.includes(uint)) return { type: 'number', solidityType: uint };
-  if (inputType.includes(address))
+  if (inputType.includes(uint) || inputType.includes(int))
+    return { type: 'number', solidityType: uint };
+  else if (inputType.includes(address))
     return { type: 'text', solidityType: address };
-  if (inputType.includes(string)) return { type: 'text', solidityType: string };
-  if (inputType.includes(bytes)) return { type: 'text', solidityType: bytes };
-  if (inputType.includes(bool)) return { type: 'radio', solidityType: bool };
+  else if (inputType.includes(string))
+    return { type: 'text', solidityType: string };
+  else if (inputType.includes(bytes))
+    return { type: 'text', solidityType: bytes };
+  else if (inputType.includes(bool))
+    return { type: 'radio', solidityType: bool };
   return { type: 'text', solidityType: string };
 };
 const parseABI = json => {
