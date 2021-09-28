7a1b717d2999bf0bd2286f564582ea393d8c0301
andrewdahn <andrewahnjr@gmail.com>
2021-09-28 13:43:02 -0700

    refactor: ♻️ refactor token images

--- src/modules/balance/ModuleTokensValue.vue
+++ src/modules/balance/ModuleTokensValue.vue
@@ -73,12 +73,12 @@ export default {
       return formatFiatValue(this.totalTokenFiatValue).value;
     },
     tokenImages() {
-      const filterNoImageTokens = this.tokensList.filter(token => token.img);
-      const firstFive = filterNoImageTokens.slice(0, 5);
-
-      return firstFive.map(item => {
-        return item.img;
-      });
+      return this.tokensList
+        .filter(token => token.img)
+        .slice(0, 5)
+        .map(item => {
+          return item.img;
+        });
     },
     moreTokensCount() {
       return this.tokensList.length - this.tokenImages.length;
