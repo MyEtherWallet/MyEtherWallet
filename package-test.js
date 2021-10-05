e4d6bbf8a19a1770abffc0b72d139d7a2240cb8d
David Hong <hsurf22@gmail.com>
2021-10-05 08:15:48 -0700

    devop: add qrcode.vue in package test exceptions

--- package-test.js
+++ package-test.js
@@ -25,7 +25,8 @@ const EXCEPTIONS = [
   'web3-core-requestmanager',
   'web3-utils',
   'remark-cli',
-  'node-fetch'
+  'node-fetch',
+  'qrcode.vue'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
