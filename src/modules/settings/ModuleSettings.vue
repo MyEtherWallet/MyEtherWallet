b2e36bc41aa170f47e1f263cab0f8f6c4e6a256f
andrewdahn <andrewahnjr@gmail.com>
2021-10-06 16:16:29 -0700

    fix: 🐛 add new settings

--- src/modules/settings/ModuleSettings.vue
+++ src/modules/settings/ModuleSettings.vue
@@ -11,98 +11,76 @@
     content-size="xlarge"
     :close="close"
   >
-    <template #mewOverlayBody>
-      <mew6-white-sheet
-        style="max-width: 744px"
-        class="pt-1 pb-12 px-12 module--settings-module-settings"
-      >
-        <div class="mew-subtitle text-center mb-7">{{ title }}</div>
-
-        <div v-if="!editMode && !addMode">
-          <mew-expand-panel
-            :panel-items="panelItems"
-            :idx-to-expand="idxToExpand"
-            has-dividers
-          >
-            <template #panelBody1>
-              <settings-gas-price
-                class="px-6 mb-10"
-                :buttons="gasButtons"
-                :selected="gasPriceType"
-                :set-selected="setSelected"
-                :gas-price="gasPrice"
-                :global="true"
-              />
-            </template>
-            <template #panelBody2>
-              <settings-import-config :import-config="settingsHandler" />
-            </template>
-            <template #panelBody3>
-              <settings-export-config :export-config="exportStore" />
-            </template>
-            <template #panelBody4>
-              <div class="pa-6">
-                <div class="mb-4">
-                  {{ $t('interface.address-book.add-up-to') }}
-                </div>
-                <mew-table
-                  :table-headers="tableHeaders"
-                  :table-data="tableData"
-                  has-color
-                  :success-toast="$t('common.copied')"
-                  @onClick="onEdit"
-                />
-
-                <div class="d-flex justify-center mt-5">
-                  <mew-button
-                    :disabled="addressBook.length > 10"
-                    title="+ Add"
-                    btn-size="xlarge"
-                    @click.native="addMode = !addMode"
-                  />
-                </div>
-              </div>
-            </template>
-            <!-- <template #panelBody5>
-            <notifications />
-          </template> -->
-          </mew-expand-panel>
-        </div>
-        <!--
-        =====================================================================================
-        Add / Edit Address Book overlay
-        =====================================================================================
-        -->
-        <address-book-add-edit
-          v-if="addMode || editMode"
-          :item="itemToEdit"
-          :mode="onMode"
-          @back="back"
+    <mew-expand-panel
+      v-if="!editMode && !addMode"
+      :panel-items="panelItems"
+      :idx-to-expand="idxToExpand"
+    >
+      <template #panelBody1>
+        <settings-gas-price
+          :buttons="gasButtons"
+          :selected="gasPriceType"
+          :set-selected="setSelected"
+          :gas-price="gasPrice"
+          :set-custom-gas-price="setCustomGasPrice"
+          :global="true"
         />
-      </mew6-white-sheet>
+      </template>
+      <template #panelBody2>
+        <settings-import-config :import-config="settingsHandler" />
+      </template>
+      <template #panelBody3>
+        <settings-export-config :export-config="exportStore" />
+      </template>
+      <template #panelBody4>
+        <div class="pa-6">
+          <div class="mb-4">
+            {{ $t('interface.address-book.add-up-to') }}
+          </div>
+          <mew-table
+            :table-headers="tableHeaders"
+            :table-data="tableData"
+            has-color
+            :success-toast="$t('common.copied')"
+            @onClick="onEdit"
+          />
 
-      <div class="mt-10">
-        Need help?
-        <a
-          href="mailto:support@myetherwallet.com"
-          target="_blank"
-          class="font-weight-medium"
-        >
-          Contact support
-        </a>
-      </div>
-    </template>
+          <div class="d-flex justify-center mt-5">
+            <mew-button
+              :disabled="addressBook.length > 10"
+              title="+ Add"
+              btn-size="xlarge"
+              @click.native="addMode = !addMode"
+            />
+          </div>
+        </div>
+      </template>
+      <!-- <template #panelBody5>
+        <notifications />
+      </template> -->
+    </mew-expand-panel>
+    <!--
+  =====================================================================================
+    Add / Edit Address Book overlay
+  =====================================================================================
+  -->
+    <address-book-add-edit
+      v-if="addMode || editMode"
+      :item="itemToEdit"
+      :mode="onMode"
+      @back="back"
+    />
   </mew-overlay>
 </template>
 
 <script>
-import { gasPriceTypes } from '@/core/helpers/gasPriceHelper';
 import SettingsImportConfig from './components/SettingsImportConfig';
 import SettingsExportConfig from './components/SettingsExportConfig';
 import SettingsGasPrice from './components/SettingsGasPrice';
 import AddressBookAddEdit from '@/modules/address-book/components/AddressBookAddEdit';
 import handlerSettings from './handler/handlerSettings';
 import { mapState } from 'vuex';
+import { fromWei } from 'web3-utils';
 import gasPriceMixin from './handler/gasPriceMixin';
 import { ROUTES_HOME, ROUTES_WALLET } from '@/core/configs/configRoutes';
 const modes = ['add', 'edit'];
@@ -172,15 +150,10 @@ export default {
     panelItems() {
       return [
         {
-          name: 'Default transaction priority',
-          subtext: `
-          ${
-            this.gasPriceType === gasPriceTypes.ECONOMY ? 'Normal priority' : ''
-          }
-          ${
-            this.gasPriceType === gasPriceTypes.REGULAR ? 'Higher priority' : ''
-          }
-          ${this.gasPriceType === gasPriceTypes.FAST ? 'Highest priority' : ''}`
+          name: 'Gas price',
+          subtext: `${fromWei(this.gasPrice, 'gwei')} Gwei (${
+            this.gasPriceType
+          })`
         },
         {
           name: 'Import configurations'
@@ -263,12 +236,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss">
-.module--settings-module-settings {
-  .v-expansion-panel {
-    margin-top: 0px !important;
-    margin-bottom: -1px !important;
-  }
-}
-</style>
