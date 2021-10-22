e3932de9fe4ba352e1ae35ba4d8bad440bf541c8
Ryan <ryans@myetherwallet.com>
2021-10-22 14:29:23 -0700

    fix: 🐛 show select token placeholder

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -1077,9 +1077,7 @@ export default {
     setDefaults() {
       setTimeout(() => {
         this.fromTokenType = this.getDefaultFromToken();
-        if (this.defaults.toToken) {
-          this.toTokenType = this.getDefaultToToken();
-        }
+        this.toTokenType = this.getDefaultToToken();
         this.setTokenInValue(this.tokenInValue);
       }, 500);
     },
