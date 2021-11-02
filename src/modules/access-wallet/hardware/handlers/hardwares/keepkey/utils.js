ecfae681cba1dd2feac002aa66eab7805fc373a9
Gamaliel Padillo <gamalielgad1234@gmail.com>
2021-11-02 15:39:39 -0700

    chore: 👷 use toBN

--- src/modules/access-wallet/hardware/handlers/hardwares/keepkey/utils.js
+++ src/modules/access-wallet/hardware/handlers/hardwares/keepkey/utils.js
@@ -1,5 +1,4 @@
-import BigNumber from 'bignumber.js';
-import { isHex } from 'web3-utils';
+import { isHex, toHex, toBN } from 'web3-utils';
 
 const getUint8Tx = tx => {
   return {
@@ -19,7 +18,7 @@ const getHexTx = tx => {
       : '0x' + tx.to.toString('hex'),
     value: '0x' + tx.value.toString('hex'),
     data: '0x' + tx.data.toString('hex'),
-    chainId: '0x' + BigNumber(tx.common.chainId()).toString(16), // this might not actually be needed
+    chainId: '0x' + toHex(toBN(tx.common.chainId())), // this might not actually be needed
     nonce: '0x' + tx.nonce.toString('hex'),
     gasLimit: tx.gasLimit
       ? '0x' + tx.gasLimit.toString('hex')
