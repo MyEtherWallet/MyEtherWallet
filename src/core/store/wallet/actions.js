a8849b800d352682fa8b7cebfdf84683c2266088
Ryan <32647967+rsteubs@users.noreply.github.com>
2021-11-10 13:07:39 -0800

    Update actions.js

--- src/core/store/wallet/actions.js
+++ src/core/store/wallet/actions.js
@@ -11,7 +11,7 @@ const removeWallet = function ({ commit, state }) {
     state.identifier === WALLET_TYPES.WALLET_LINK ||
     state.identifier === WALLET_TYPES.MEW_CONNECT
   ) {
-    let connection = state.instance.getConnection()
+    const connection = state.instance.getConnection()
     if (connection) {
       connection.disconnect();
     }
