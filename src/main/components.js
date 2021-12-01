77ac28df7cd5db4cf37adddbd20bbc4fed0db968
David Hong <hsurf22@gmail.com>
2021-11-30 22:27:48 -0800

    devop: remove unused files

--- src/main/components.js
+++ src/main/components.js
@@ -2,11 +2,9 @@ import Vue from 'vue';
 import mewComponents from '@myetherwallet/mew-components';
 import whiteSheet from '@/components/white-sheet/WhiteSheet.vue';
 import QrCode from '@/core/components/AppQrCode.vue';
-import MewToast from '@/core/MewToast.vue';
 
 Object.keys(mewComponents).forEach(name => {
   Vue.component(name, mewComponents[name]);
 });
 Vue.component('Mew6WhiteSheet', whiteSheet);
 Vue.component('QrCode', QrCode);
-Vue.component('MewToast', MewToast);
