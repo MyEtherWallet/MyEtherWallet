MyEtherWallet/MyEtherWallet

commit 1681be07d2bbf156eb96d83dd99a9fd782f7ad7f

chore: 👷 fix bitbox

src/modules/access-wallet/hardware/handlers/hardwares/bitbox02/index.js
@@ -12,6 +12,7 @@ import errorHandler from './errorHandler';
 import store from '@/core/store';
 import commonGenerator from '@/core/helpers/commonGenerator';
 import toBuffer from '@/core/helpers/toBuffer';
+import { BN } from 'web3-utils';
 
 const NEED_PASSWORD = false;
 
@@ -83,18 +84,36 @@ class BitBox02Wallet {
       const tx = new Transaction.fromTxData(txParams, {
         common: commonGenerator(store.getters['global/network'])
       });
+      // const obj = Object.assign(
+      //   {},
+      //   {
+      //     nonce: tx.nonce.toArrayLike(Buffer, 'be', 32),
+      //     gasPrice: tx.gasPrice.toArrayLike(Buffer, 'be', 32),
+      //     gasLimit: tx.gasLimit.toArrayLike(Buffer, 'be', 32),
+      //     to: BN(tx.to.toBuffer()).toArrayLike(Buffer, 'be', 32),
+      //     value: tx.value.toArrayLike(Buffer, 'be', 32),
+      //     data: tx.data
+      //   }
+      // );
       const networkId = tx.common.chainId();
       const signingData = {
         keypath: this.basePath + '/' + idx,
         chainId: networkId,
-        tx: tx
+        tx: {
+          nonce: BN(tx.nonce).toArrayLike(Buffer, 'be', 32),
+          gasPrice: BN(tx.gasPrice).toArrayLike(Buffer, 'be', 32),
+          gasLimit: BN(tx.gasLimit).toArrayLike(Buffer, 'be', 32),
+          to: tx.to.toBuffer(),
+          value: BN(tx.value).toArrayLike(Buffer, 'be', 32),
+          data: tx.data
+        }
       };
       const result = await this.BitBox02.ethSignTransaction(signingData);
-      txParams.r = Buffer.from(result.r);
-      txParams.s = Buffer.from(result.s);
-      txParams.v = Buffer.from(result.v);
+      tx.r = Buffer.from(result.r);
+      tx.s = Buffer.from(result.s);
+      tx.v = Buffer.from(result.v);
 
-      const signedChainId = calculateChainIdFromV(txParams.v);
+      const signedChainId = calculateChainIdFromV(tx.v);
       if (signedChainId !== networkId)
         throw new Error(
           'Invalid networkId signature returned. Expected: ' +
@@ -103,7 +122,7 @@ class BitBox02Wallet {
             signedChainId,
           'InvalidNetworkId'
         );
-      return getSignTransactionObject(Transaction.fromTxData(txParams));
+      return getSignTransactionObject(Transaction.fromTxData(tx));
     };
 
     const msgSigner = async msg => {

commit 233901a8b0930214e57fadbcde7078bd793f177f

fix: 🐛 BitBox02 transaction signing, assign to read only property ‘r’
Fixes 'Cannot assign to read only property ‘r’ of object'.

Regression seems to be from 1681be07d2bbf156eb96d83dd99a9fd782f7ad7f.

src/modules/access-wallet/hardware/handlers/hardwares/bitbox02/index.js
@@ -98,11 +98,11 @@ class BitBox02Wallet {
         }
       };
       const result = await this.BitBox02.ethSignTransaction(signingData);
-      tx.r = Buffer.from(result.r);
-      tx.s = Buffer.from(result.s);
-      tx.v = Buffer.from(result.v);
+      txParams.r = Buffer.from(result.r);
+      txParams.s = Buffer.from(result.s);
+      txParams.v = Buffer.from(result.v);
 
-      const signedChainId = calculateChainIdFromV(tx.v);
+      const signedChainId = calculateChainIdFromV(txParams.v);
       if (signedChainId !== networkId)
         throw new Error(
           'Invalid networkId signature returned. Expected: ' +
@@ -111,7 +111,7 @@ class BitBox02Wallet {
             signedChainId,
           'InvalidNetworkId'
         );
-      return getSignTransactionObject(Transaction.fromTxData(tx));
+      return getSignTransactionObject(Transaction.fromTxData(txParams));
     };
 
     const msgSigner = async msg => {