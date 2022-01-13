cfaf498fb97aa6d7e88d69f0e06ca022cd6c4bae
David Hong <hsurf22@gmail.com>
2022-01-13 14:58:21 -0800

    devop: add more loaders

--- src/modules/balance/ModuleBalanceLoader.vue
+++ src/modules/balance/ModuleBalanceLoader.vue
@@ -1,15 +1,29 @@
 <template>
-  <mew6-white-sheet class="pa-6">
-    <div class="d-flex align-center">
-      <div class="d-flex align-center">
+  <mew6-white-sheet class="mew-component--module-balance-loader pa-6">
+    <v-row>
+      <v-col cols="12" sm="6" class="d-flex flex-grow-1 align-center">
         <v-skeleton-loader type="avatar" />
-        <div class="ml-2">
-          <v-skeleton-loader type="text" width="200px" />
-          <v-skeleton-loader type="heading" width="300px" />
-          <v-skeleton-loader type="text" width="200px" class="mt-2" />
+        <div class="ml-2 flex-grow-1">
+          <v-skeleton-loader
+            type="image"
+            max-width="150px"
+            height="15px"
+            class="mb-2"
+          />
+          <v-skeleton-loader
+            type="image"
+            max-width="180px"
+            height="30px"
+            class="mb-2"
+          />
+          <v-skeleton-loader type="image" max-width="100px" height="15px" />
         </div>
-      </div>
-      <div class="d-flex flex-grow-1 justify-end">
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        class="d-flex flex-grow-1 justify-sm-end align-center"
+      >
         <v-skeleton-loader
           type="button"
           width="35px"
@@ -29,8 +43,45 @@
           class="mr-2"
         />
         <v-skeleton-loader type="button" width="35px" height="30px" />
-      </div>
-    </div>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-6">
+      <v-col cols="2">
+        <v-skeleton-loader type="text" class="my-6" />
+        <v-skeleton-loader type="text" class="my-6" />
+        <v-skeleton-loader type="text" class="my-6" />
+        <v-skeleton-loader type="text" class="my-6" />
+        <v-skeleton-loader type="text" class="my-6" />
+      </v-col>
+      <v-col cols="10">
+        <v-skeleton-loader type="image" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        class="d-flex align-center justify-center justify-sm-start flex-column flex-sm-row"
+      >
+        <v-skeleton-loader
+          type="image"
+          width="130px"
+          height="20px"
+          class="ma-1"
+        />
+        <v-skeleton-loader
+          type="image"
+          width="130px"
+          height="20px"
+          class="ma-1"
+        />
+      </v-col>
+      <v-col cols="12" sm="6" class="d-flex justify-center justify-sm-end">
+        <v-skeleton-loader type="image" width="200px" height="50px" />
+      </v-col>
+    </v-row>
   </mew6-white-sheet>
 </template>
 
@@ -48,3 +99,9 @@ export default {
 </script>
 
 <style lang="scss" scoped></style>
+
+<style lang="scss">
+.mew-component--module-balance-loader .v-skeleton-loader__text {
+  margin-bottom: 0px !important;
+}
+</style>
