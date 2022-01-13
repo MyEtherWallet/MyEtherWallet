94a826e41f8e20e5e3acfbf26081d0e532f3bb11
David Hong <hsurf22@gmail.com>
2022-01-13 02:17:35 -0800

    devop: add loaders

--- src/modules/balance/ModuleBalanceLoader.vue
+++ src/modules/balance/ModuleBalanceLoader.vue
@@ -1,8 +1,35 @@
 <template>
   <mew6-white-sheet class="pa-6">
-    <div class="d-flex">
-      <v-skeleton-loader type="avatar" />
-      <v-skeleton-loader type="text" width="200px" />
+    <div class="d-flex align-center">
+      <div class="d-flex align-center">
+        <v-skeleton-loader type="avatar" />
+        <div class="ml-2">
+          <v-skeleton-loader type="text" width="200px" />
+          <v-skeleton-loader type="heading" width="300px" />
+          <v-skeleton-loader type="text" width="200px" class="mt-2" />
+        </div>
+      </div>
+      <div class="d-flex flex-grow-1 justify-end">
+        <v-skeleton-loader
+          type="button"
+          width="35px"
+          height="30px"
+          class="mr-2"
+        />
+        <v-skeleton-loader
+          type="button"
+          width="35px"
+          height="30px"
+          class="mr-2"
+        />
+        <v-skeleton-loader
+          type="button"
+          width="35px"
+          height="30px"
+          class="mr-2"
+        />
+        <v-skeleton-loader type="button" width="35px" height="30px" />
+      </div>
     </div>
   </mew6-white-sheet>
 </template>
