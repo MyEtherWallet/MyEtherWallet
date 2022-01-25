ada82486610611a00fd754d9866f09bcc6d9198b
denlev <levadnyuk66@gmail.com>
2022-01-25 22:50:04 +0200

    import mintme-mew-ws

--- src/utils/networks/nodes/index.js
+++ src/utils/networks/nodes/index.js
@@ -7,6 +7,7 @@ import ropmewws from './rop-mew-ws';
 import bscmewws from './bsc-mew-ws';
 import maticmewws from './matic-mew-ws';
 import gochain from './gochain-ws';
+import mintmemewws from './mintme-mew-ws';
 
 export {
   etcethereumclassiccooperativeparityws,
@@ -17,5 +18,6 @@ export {
   ropmewws,
   bscmewws,
   maticmewws,
-  gochain
+  gochain,
+  mintmemewws
 };
