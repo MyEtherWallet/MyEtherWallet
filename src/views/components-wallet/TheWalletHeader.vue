fa9389b18c1c9f4d8c9288fc2fadcb44f5d4a8fd
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-02-09 13:51:52 -0800

    chore: 👷 add update for the header

--- src/views/components-wallet/TheWalletHeader.vue
+++ src/views/components-wallet/TheWalletHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="d-flex align-center py-4 px-1">
-    <div class="d-flex align-center">
+  <v-row class="d-flex align-center py-4 px-1">
+    <!-- <div class="d-flex align-center">
       <img class="ml-2 mr-3" src="@/assets/images/icons/icon-puppy-mew.svg" />
       <div>
         <div class="font-weight-bold">MEWtopia</div>
@@ -14,13 +14,36 @@
           >
         </div>
       </div>
-    </div>
-    <div class="ml-auto d-flex align-center">
+    </div> -->
+    <v-col
+      cols="8"
+      no-gutters
+      dense
+      class="d-flex align-center justify-space-between"
+    >
+      <div class="d-flex align-center">
+        <div class="party-popper-container ml-2 mr-3 d-flex pa-3">
+          <img
+            src="@/assets/images/icons/icon-party-popper.png"
+            width="20px"
+            height="20px"
+          />
+        </div>
+        <div class="d-flex align-center">
+          <span class="mr-2 textMedium--text font-weight-bold">
+            Buy & sell crypto with 0% fees
+          </span>
+          <span class="mew-label textMedium--text mr-2">Promo ends in:</span>
+        </div>
+      </div>
+      <mew-button title="Buy crypto now" btn-size="medium" />
+    </v-col>
+    <v-col cols="4" class="ml-auto d-flex align-center justify-end">
       <div class="align-center d-none d-lg-block">
         <notification-overlay />
       </div>
-    </div>
-  </div>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -51,6 +74,11 @@ a {
   font-weight: 500;
   color: var(--v-primary-base);
 }
-</style>
 
-<style lang="scss"></style>
+.party-popper-container {
+  background-color: var(--v-whiteBackground-base);
+  box-shadow: 0px 5px 30px rgba(166, 173, 201, 0.22),
+    0px 5px 5px rgba(154, 160, 185, 0.05);
+  border-radius: 100px;
+}
+</style>
