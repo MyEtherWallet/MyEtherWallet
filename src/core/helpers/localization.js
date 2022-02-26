7e154ff70c56f7eac67261942b43ea32384852a1
Jesus Morales Jr <jmjles@gmail.com>
2022-02-25 21:05:36 -0800

    chore: 👷 comments, handeled verysmall numbers

--- src/core/helpers/localization.js
+++ src/core/helpers/localization.js
@@ -1,6 +1,10 @@
 import BigNumber from 'bignumber.js';
 import { isBigNumber } from 'web3-utils';
 
+/***********************************************
+ * Localizes numbers to its specified currency *
+ * Returns String                              *
+ ***********************************************/
 export const localizeCurrency = ({
   currency = 'USD',
   number = '0.00',
@@ -21,7 +25,9 @@ export const localizeCurrency = ({
         maximumFractionDigits: 6
       }
     : {};
+
   rate = typeof rate === 'string' ? currencyToNumber(rate) : rate;
+
   number =
     typeof number === 'string'
       ? currencyToNumber(number)
@@ -30,24 +36,36 @@ export const localizeCurrency = ({
       : isBigNumber(number)
       ? currencyToNumber(number.toString())
       : number;
+
   const locale = locales[currency] ? locales[currency] : 'en-US';
 
   if (isNaN(number)) {
     return convertNumber({ locale, currency, options, convertedPrice: 0.0 });
   }
+
   const convertedPrice = small
     ? new BigNumber(number).times(rate).toFixed(6)
     : verySmall
-    ? new BigNumber(number).times(rate).toFixed(5)
+    ? new BigNumber(number).times(rate).toFixed(7)
     : new BigNumber(number).times(rate);
+
   return convertNumber({ locale, currency, options, convertedPrice });
 };
 
+/****************************
+ * Expects String           *
+ * Converts String to Float *
+ * Returns Float            *
+ ****************************/
 export const currencyToNumber = currency =>
   typeof currency === 'string'
-    ? parseFloat(currency.replace(/[,$₽]/g, ''))
+    ? parseFloat(currency.replace(/[,$₽<\s]/g, ''))
     : currency;
 
+/*************************************
+ * Converts Number to Local Currency *
+ * Returns String                    *
+ *************************************/
 const convertNumber = ({ locale, currency, options, convertedPrice }) =>
   new Intl.NumberFormat(locale, {
     style: 'currency',
@@ -56,6 +74,9 @@ const convertNumber = ({ locale, currency, options, convertedPrice }) =>
     ...options
   }).format(convertedPrice);
 
+/******************************
+ * Locale Codes From Currency *
+ ******************************/
 const locales = {
   USD: 'en-US',
   RUB: 'ru-RU',
