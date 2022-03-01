22e83e18bf1f19151b480c549c0ac8f36303c3e0
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-03-01 14:48:53 -0800

    chore: 👷 switch moonpay to live api

--- src/modules/moon-pay/handlers/moonpayHandler.js
+++ src/modules/moon-pay/handlers/moonpayHandler.js
@@ -1,6 +1,6 @@
 import { sha3 } from 'web3-utils';
 import axios from 'axios';
-const API = 'https://staging.mewwallet.dev';
+const API = 'https://mainnet.mewwallet.dev';
 const MOONPAY = 'MOONPAY';
 
 export default class MoonPayHandler {
