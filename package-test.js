ca065a875349ade4f2acb267d1386462ebfb9451
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-03-17 12:43:19 -0700

    chore: 👷 exclude packages for now

--- package-test.js
+++ package-test.js
@@ -40,7 +40,19 @@ const EXCEPTIONS = [
   '@vue/cli-plugin-unit-jest',
   '@vue/cli-service',
   'vue-i18n',
-  'vue'
+  'vue',
+  // update after hotfix
+  '@ensdomains/ens-contracts',
+  '@lokalise/node-api',
+  '@myetherwallet/mew-components',
+  '@sentry/browser',
+  '@sentry/tracing',
+  '@sentry/vue',
+  'highcharts',
+  'lottie-web',
+  'merkletreejs',
+  'axios',
+  'xss'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
