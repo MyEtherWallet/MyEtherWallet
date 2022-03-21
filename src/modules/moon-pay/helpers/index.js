d0424e356f92b3442f9dc8aa754fa5ac0e78d4bc
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-03-21 13:17:49 -0700

    chore: 👷 remove unused helpers

--- src/modules/moon-pay/helpers/index.js
+++ src/modules/moon-pay/helpers/index.js
@@ -1,8 +1,3 @@
-import crypto from 'crypto';
-import { isObject } from 'lodash';
-import { sha3 } from 'web3-utils';
-
-const ENCRYPTION_TYPE = 'aes-256-cbc';
 const MOONPAY_EVENT = 'openMoonPay';
 const MOONPAY_OFFER_END = new Date('3/30/22 12:00 am');
 const LOCALE = {
@@ -13,55 +8,4 @@ const LOCALE = {
   CAD: 'en-CA',
   GBP: 'en-GB'
 };
-const decrypt = (value, id, address) => {
-  // convert value to string
-  const valueBuffer = Buffer.from(value, 'hex');
-
-  // generate iv using first half of sha3 of address
-  const iv = Buffer.from(
-    sha3(address.toLowerCase()).replace('0x', '').substring(0, 32),
-    'hex'
-  );
-
-  // generate key using sha3 of id
-  const key = Buffer.from(sha3(id.toLowerCase()).replace('0x', ''), 'hex');
-
-  // create decipher and decrypt value
-  const decipher = crypto.createDecipheriv(ENCRYPTION_TYPE, key, iv);
-  const decryptedValue = decipher.update(valueBuffer);
-  const decryptedBuffer = Buffer.concat([decryptedValue, decipher.final()]);
-  const decryptedString = decryptedBuffer.toString();
-
-  try {
-    const decryptedObject = JSON.parse(decryptedString);
-    return decryptedObject;
-  } catch (e) {
-    return decryptedString;
-  }
-};
-
-const encrypt = (value, id, address) => {
-  // convert value to string
-  const valueString = isObject(value) ? JSON.stringify(value) : value;
-
-  // generate iv using first half of sha3 of address
-
-  // generate iv using first half of sha3 of address
-  const iv = Buffer.from(
-    sha3(address.toLowerCase()).replace('0x', '').substring(0, 32),
-    'hex'
-  );
-
-  // generate key using sha3 of id
-  const key = Buffer.from(sha3(id.toLowerCase()).replace('0x', ''), 'hex');
-
-  // create cipher and encrypt value
-  const cipher = crypto.createCipheriv(ENCRYPTION_TYPE, key, iv);
-  const encryptedValue = cipher.update(valueString);
-  const encryptedBuffer = Buffer.concat([encryptedValue, cipher.final()]);
-  const encryptedString = encryptedBuffer.toString();
-
-  return encryptedString;
-};
-
-export { encrypt, decrypt, LOCALE, MOONPAY_EVENT, MOONPAY_OFFER_END };
+export { LOCALE, MOONPAY_EVENT, MOONPAY_OFFER_END };
