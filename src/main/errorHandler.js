bfeba72906154b14a8ac66c796376221ee965040
Jesus Morales Jr <jmjles@gmail.com>
2022-04-07 14:51:14 -0700

    fix: 🐛 infinte loop

--- src/main/errorHandler.js
+++ src/main/errorHandler.js
@@ -1,9 +1,25 @@
-import WalletErrorHandler from '@/modules/access-wallet/common/WalletErrorHandler';
+import { isObject } from 'lodash';
 
 const mewApiError = 'Websocket connection failed';
+
 export const knownErrors = {
   'Can\'t assign to property "request"': mewApiError,
   "Cannot create property 'request'": mewApiError
 };
 
-export default WalletErrorHandler(knownErrors, {});
+const handleError = err => {
+  const errorValues = Object.keys(knownErrors);
+  const foundError = errorValues.find(item => {
+    const message =
+      err && err.message
+        ? isObject(err.message)
+          ? err.message.message
+          : err.message
+        : err;
+    if (!message) return false;
+    return message.includes(item);
+  });
+  return foundError ? true : false;
+};
+
+export default handleError;
