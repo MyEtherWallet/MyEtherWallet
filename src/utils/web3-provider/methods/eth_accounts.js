28a1e8624e9057ae65b4e44b0ff351ac206cbd19
Abyscuit <marcus14530@gmail.com>
2022-06-01 13:18:23 -0700

    refactor: ♻️ use res for error

--- src/utils/web3-provider/methods/eth_accounts.js
+++ src/utils/web3-provider/methods/eth_accounts.js
@@ -1,13 +1,12 @@
 import { toPayload } from '../jsonrpc';
 export default async ({ payload, store }, res, next) => {
   if (payload.method !== 'eth_accounts') return next();
-  if (!store.state.wallet) return next();
   try {
     res(
       null,
       toPayload(payload.id, [store.state.wallet.instance.getAddressString()])
     );
   } catch (err) {
-    return next(err);
+    res(err);
   }
 };
