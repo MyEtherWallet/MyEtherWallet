033e279b0b944d2b3425a0ccd4647229766ceedb
radenlmantuano <newcodeondablock@gmail.com>
2022-06-21 12:27:04 -0700

    chore: 👷 fix typo

--- src/main/errorHandler.js
+++ src/main/errorHandler.js
@@ -30,7 +30,7 @@ export const knownErrors = {
   'ENS is not supported': 'ENS is not supported on network private',
   '未能完成该操作。无效的自变量':
     'The operation could not be completed. Invalid argument.',
-  'TypeError Failed to fetch': 'Failes to fetch incoming data'
+  'TypeError Failed to fetch': 'Failed to fetch incoming data'
 };
 
 const handleError = err => {
