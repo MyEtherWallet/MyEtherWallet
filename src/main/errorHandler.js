498f76a31dd5230654e8524064bdeec00d80d9cf
radenlmantuano <newcodeondablock@gmail.com>
2022-06-21 11:14:31 -0700

    fix: 🐛 sentry gatt operation unknown

--- src/main/errorHandler.js
+++ src/main/errorHandler.js
@@ -36,7 +36,9 @@ export const knownErrors = {
     'Unexpected response from request. Please try again.',
   'Error: TypeError: Origin https://www.myetherwallet.com is not allowed by Access-Control-Allow-Origin.':
     'CORS Request cannot be made due to security reasons',
-  "Cannot read properties of undefined (reading 'click')": ''
+  "Cannot read properties of undefined (reading 'click')": '',
+  'GATT operation failed for unknown reason.':
+    'Please reset/disconnect then reconnect bluetooth pairings on device and on connected devices'
 };
 
 const handleError = err => {
