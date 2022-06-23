6c6dedfce3c1f9e00ab80c2ee67efb8cd89d13ba
radenlmantuano <newcodeondablock@gmail.com>
2022-06-23 08:01:53 -0700

    fix: 🐛 sentry error maxblock tobn

--- src/main/errorHandler.js
+++ src/main/errorHandler.js
@@ -36,7 +36,9 @@ export const knownErrors = {
     'Unexpected response from request. Please try again.',
   'Error: TypeError: Origin https://www.myetherwallet.com is not allowed by Access-Control-Allow-Origin.':
     'CORS Request cannot be made due to security reasons',
-  "Cannot read properties of undefined (reading 'click')": ''
+  "Cannot read properties of undefined (reading 'click')": '',
+  'Error: Error: [number-to-bn] while converting number undefined to BN.js instance, error: invalid number value.':
+    ''
 };
 
 const handleError = err => {
