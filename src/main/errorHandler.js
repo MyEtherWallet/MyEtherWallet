95b3c5615d56b4019409e68617586bad4257b56f
Abyscuit <marcus14530@gmail.com>
2022-06-23 11:14:52 -0700

    fix: 🐛 permission denied error

--- src/main/errorHandler.js
+++ src/main/errorHandler.js
@@ -36,7 +36,8 @@ export const knownErrors = {
     'Unexpected response from request. Please try again.',
   'Error: TypeError: Origin https://www.myetherwallet.com is not allowed by Access-Control-Allow-Origin.':
     'CORS Request cannot be made due to security reasons',
-  "Cannot read properties of undefined (reading 'click')": ''
+  "Cannot read properties of undefined (reading 'click')": '',
+  'Permission denied.': 'Permission denied. Please try again.'
 };
 
 const handleError = err => {
