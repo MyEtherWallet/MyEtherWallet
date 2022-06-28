364c33e7b3bf6a8c8a7db878c27aa34e7895c181
Abyscuit <marcus14530@gmail.com>
2022-06-28 16:55:45 -0700

    translation: 🌐 add about us

--- src/translations/en_US.js
+++ src/translations/en_US.js
@@ -4,6 +4,7 @@ import dappsMaker from './dapps-maker/en_US';
 import dappsSubmission from './dapps-submission/en_US';
 import home from './home/en_US';
 import howItWorks from './how-it-works/en_US.json';
+import aboutUs from './about-us/en_US.json';
 import footer from './footer/en_US';
 import gettingStarted from './getting-started/en_US';
 import privacyPolicy from './privacy-policy/en_US';
@@ -39,6 +40,7 @@ const en_US = {
   common: common,
   home: home,
   howItWorks: howItWorks,
+  aboutUs: aboutUs,
   footer: footer,
   gettingStarted: gettingStarted,
   privacyPol: privacyPolicy,
