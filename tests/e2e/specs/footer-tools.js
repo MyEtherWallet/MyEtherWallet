a4a6b90b4e88f337fcf7c3910747fee0e42c9db5
unknown <jmjles@gmail.com>
2022-07-14 15:14:55 -0700

    devop: 🔧 completed e2e tests

--- tests/e2e/specs/footer-tools.js
+++ tests/e2e/specs/footer-tools.js
@@ -4,6 +4,14 @@ const css = 'css selector';
 const signature =
   '0xf88380018203339407a565b7ed7d7a678680a4c162885bedbb695fe080a44401a6e4000000000000000000000000000000000000000000000000000000000000001226a0223a7c9bcf5531c99be5ea7082183816eb20cfe0bbc322e97cc5c7f71ab8b20ea02aadee6b34b45bb15bc42d9c09de4a6754e7000908da72d48cc7704971491663';
 const address = '0x9ccCb2B82f8F607db3F5D50BF4083665c3002f83';
+const verifySignature = `{
+  "address": "0xd0d919cb7e8285fe76e278a7e4c4b687a274176c",
+  "msg": "0x74657374696e67",
+  "sig": "ac0bf2cf6e4a9c91c91476dfc80319ae5f3e2bf6bf2f59e6e87f8d0e2fcffe2145d53173e81d7893b8e75813712ed9fbdd58e424ed5ffa19c2701f073c98cccc1b",
+  "version": "3",
+  "signer": "MEW"
+}`;
+const testPW = '1234password';
 module.exports = {
   before: function (browser) {
     browser.globals.waitForConditionTimeout = 15000;
@@ -54,6 +62,15 @@ module.exports = {
 
     // check if url is correct
     browser.assert.urlContains('verify');
+
+    // input message
+    browser.click(css, '.VerifyInput').keys(verifySignature);
+
+    // click button
+    browser.click(css, '.VerifyButton');
+
+    // check if message is viewable
+    browser.waitForElementVisible(css, '.VerifyMessage');
   },
   'Convert Unit test': async browser => {
     // open browser
@@ -66,6 +83,23 @@ module.exports = {
 
     // check if url is correct
     browser.assert.urlContains('convert');
+
+    // check wei, gwei, ether
+    browser.expect
+      .element('.CurrencyLeftInput input')
+      .to.have.value.that.equals('1000000000000000000');
+
+    browser
+      .click('.CurrencyRightSelect')
+      .click('.menuable__content__active div :first-child')
+      .expect.element('.CurrencyLeftInput input')
+      .to.have.value.that.equals('1');
+
+    browser
+      .click('.CurrencyRightSelect')
+      .click('.menuable__content__active div :nth-child(4)')
+      .expect.element('.CurrencyLeftInput input')
+      .to.have.value.that.equals('1000000000');
   },
   'Generate Keystore test': async browser => {
     // open browser
@@ -78,6 +112,21 @@ module.exports = {
 
     // check if url is correct
     browser.assert.urlContains('keystore');
+
+    // select download
+    browser
+      .moveToElement('.KeystoreDownloadButton', 10, 10)
+      .click(css, '.KeystoreDownloadButton');
+
+    // enter test password
+    browser.click(css, '.KeystorePassword').keys(testPW);
+    browser.click(css, '.KeystoreConfirmPassword').keys(testPW);
+
+    // accept terms
+    browser.click(css, '.KeystoreConfirm');
+
+    // check if download is enabled
+    browser.expect.element('.KeystoreDownloadFile').to.be.enabled;
   },
   'Send Offline Helper test': async browser => {
     // open browser
