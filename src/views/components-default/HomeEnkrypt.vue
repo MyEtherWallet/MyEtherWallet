22311d990b95a81790d23f68e0cb058a4833d188
David Hong <hsurf22@gmail.com>
2022-07-16 17:24:46 -0700

    devop: add left block

--- src/views/components-default/HomeEnkrypt.vue
+++ src/views/components-default/HomeEnkrypt.vue
@@ -1,6 +1,36 @@
 <template>
-  <div class="home--enkrypt">
-    <v-container> aaa </v-container>
+  <div class="home--enkrypt" style="padding: 100px 0">
+    <v-container>
+      <div style="max-width: 400px">
+        <img
+          src="@/assets/images/icons/icon-enkrypt-full-logo.svg"
+          alt="Enkrypt"
+          height="28"
+          class="mb-4"
+        />
+        <div class="text-title mb-5">
+          Enkrypt - MEW's mulitchain browser extension
+        </div>
+        <div class="text-subtitle mb-10">
+          Use thousands of web3 apps across multiple networks, buy crypto, swap
+          tokens, and manage your NFTs. Welcome to the multichain future.
+        </div>
+        <mew-button
+          class="download-button"
+          color-theme="#7E44F2"
+          style="border-radius: 100px !important"
+          btn-size="large"
+        >
+          <img
+            src="@/assets/images/icons/icon-chrome.svg"
+            alt="Chrome"
+            height="25"
+            class="mr-2"
+          />
+          <span class="font-weight-bold"> Get the extension </span>
+        </mew-button>
+      </div>
+    </v-container>
   </div>
 </template>
 
@@ -12,4 +42,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.text-title {
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 64px;
+  letter-spacing: 0px;
+}
+.text-subtitle {
+  font-size: 18px;
+}
+.download-button {
+  img {
+    border: 2px solid white;
+    border-radius: 50%;
+  }
+}
+</style>
