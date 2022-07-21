eb770ef57f7d14d8e1a7f388e1a048066678d104
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-07-20 17:52:24 -0700

    chore: 👷 remove skale

--- src/views/TheDefaultView.vue
+++ src/views/TheDefaultView.vue
@@ -5,20 +5,17 @@
       <router-view />
     </v-main>
     <the-default-footer />
-    <the-default-wallet-banner />
   </div>
 </template>
 
 <script>
 import TheDefaultHeader from './components-default/TheDefaultHeader';
 import TheDefaultFooter from './components-default/TheDefaultFooter';
-import TheDefaultWalletBanner from './components-default/TheDefaultWalletBanner';
 export default {
   name: 'TheDefaultView',
   components: {
     TheDefaultHeader,
-    TheDefaultFooter,
-    TheDefaultWalletBanner
+    TheDefaultFooter
   }
 };
 </script>
