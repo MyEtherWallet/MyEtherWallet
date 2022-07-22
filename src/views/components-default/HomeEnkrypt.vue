16111301f38a406682c8e3080dfb5aab328f1c29
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-07-21 18:11:16 -0700

    devop: 🔧 turn button style global

--- src/views/components-default/HomeEnkrypt.vue
+++ src/views/components-default/HomeEnkrypt.vue
@@ -31,7 +31,7 @@
             target="_blank"
           >
             <mew-button
-              class="download-button"
+              class="extension-btn"
               color-theme="#7E44F2"
               style="border-radius: 100px !important"
               btn-size="large"
@@ -68,6 +68,23 @@ export default {
   data: () => ({})
 };
 </script>
+
+<style lang="css">
+.extension-btn {
+  background: conic-gradient(
+    from 186.32deg at 112.5% -13.75%,
+    #c549ff -284.63deg,
+    #704bff 0deg,
+    #c549ff 75.37deg,
+    #704bff 360deg
+  ) !important;
+
+  box-shadow: 0px 25px 10px rgba(89, 0, 255, 0.01),
+    0px 14px 8px rgba(89, 0, 255, 0.05), 0px 6px 6px rgba(89, 0, 255, 0.09),
+    0px 2px 3px rgba(89, 0, 255, 0.1), 0px 0px 0px rgba(89, 0, 255, 0.1);
+  border-radius: 32px !important;
+}
+</style>
 
 <style lang="scss" scoped>
 .home--enkrypt {
