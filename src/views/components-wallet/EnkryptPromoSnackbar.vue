de2809f17f16ebb91eb7b7e728e0954d13d41d6e
David Hong <hsurf22@gmail.com>
2022-07-26 21:01:43 -0700

    devop: update mobile layout for enkrypt promo snackbar

--- src/views/components-wallet/EnkryptPromoSnackbar.vue
+++ src/views/components-wallet/EnkryptPromoSnackbar.vue
@@ -2,40 +2,56 @@
   <div class="enkrypt-promo-snackbar">
     <v-snackbar
       :value="show"
-      width="380px"
-      height="200px"
+      max-width="380px"
+      min-width="auto"
       :timeout="-1"
       light
       left
       rounded
       transition="scale-transition"
-      class="pl-5"
+      class="pl-sm-5"
     >
-      <div class="d-flex flex-column pa-6 justify-space-between height--full">
-        <div class="d-flex justify-space-between align-center">
-          <img
-            alt="enkrypt logo"
-            src="@/assets/images/icons/enkrypt/icon-enkrypt-colored.svg"
-            height="20px"
-          />
-          <v-icon class="cursor--pointer" medium @click="closeSnackbar"
-            >mdi-close</v-icon
-          >
-        </div>
-        <div class="d-flex justify-space-between mew-body">
+      <!-- ============================================================================== -->
+      <!-- Close button -->
+      <!-- ============================================================================== -->
+      <v-btn class="close-button" icon>
+        <v-icon
+          class="cursor--pointer"
+          medium
+          color="greyMedium"
+          @click="closeSnackbar"
+        >
+          mdi-close
+        </v-icon>
+      </v-btn>
+
+      <div class="pa-6">
+        <img
+          class="mb-6"
+          alt="enkrypt logo"
+          src="@/assets/images/icons/enkrypt/icon-enkrypt-colored.svg"
+          height="20px"
+        />
+
+        <div class="mew-body mb-6 mb-sm-7">
           Access your wallet and switch accounts in seconds with our official
           browser extension
         </div>
-        <div class="btn-container d-flex justify-space-between align-center">
+
+        <div
+          class="btn-container d-flex flex-column-reverse flex-sm-row justify-space-between align-center"
+        >
           <mew-button
+            class="ml-sm-n4 mt-1 mt-sm-0"
             color-theme="#939fb9"
             btn-style="transparent"
             @click.native="openHelpCenter"
             >Learn More</mew-button
           >
           <mew-button
-            color-theme="#7E44F2"
+            class="extension-btn"
             style="border-radius: 40px !important"
+            color-theme="#7E44F2"
             @click.native="openEnkrypt"
           >
             <img
@@ -45,7 +61,7 @@
               width="25px"
               height="25px"
             />
-            <span class="font-weight-bold"> Install Extension </span>
+            <span class="font-weight-bold"> {{ text }} </span>
           </mew-button>
         </div>
       </div>
@@ -109,18 +125,25 @@ export default {
   }
 };
 </script>
+
 <style lang="scss">
 .enkrypt-promo-snackbar {
+  position: relative;
   .v-snack__content {
     padding: 0 !important;
-    width: 100% !important;
-    height: 100% !important;
   }
 }
 </style>
+
 <style lang="scss" scoped>
 .btn-container {
   width: 100%;
-  height: 40px;
+  height: 100%;
+}
+
+.close-button {
+  position: absolute;
+  top: 15px;
+  right: 15px;
 }
 </style>
