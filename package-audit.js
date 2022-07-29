9880819ee8e75cfa0a894a770b055f130e8a2d9e
radenlmantuano <newcodeondablock@gmail.com>
2022-07-29 00:45:38 -0700

    chore: 👷 add exceptions

--- package-audit.js
+++ package-audit.js
@@ -18,7 +18,13 @@ const AUDIT_EXCEPTIONS = [
   'scss-tokenizer',
   // no package found
   'codecov',
-  'scss-tokenizer'
+  'scss-tokenizer',
+  'high',
+  'socket.io-parser',
+  'xmlhttprequest-ssl',
+  'node-forge',
+  'engine.io',
+  'parse-path'
 ];
 const execute = (command, callback) => {
   exec(
