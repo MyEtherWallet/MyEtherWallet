175b482f8c18c3389b490590ed71bc8b6f841071
Jesus Morales Jr <jmjles@gmail.com>
2022-08-11 15:40:10 -0700

    devop: 🔧 added checks for undefined

--- src/modules/swap/handlers/handlerSwap.js
+++ src/modules/swap/handlers/handlerSwap.js
@@ -17,7 +17,7 @@ class Swap {
   getAllTokens() {
     const allTokens = {};
     return this.providers[0].getSupportedTokens().then(baseList => {
-      baseList.forEach(t => (allTokens[t.contract] = t));
+      baseList?.forEach(t => (allTokens[t.contract] = t));
       return Promise.all(
         this.providers.slice(3).map(p => {
           if (!p.isSupportedNetwork(this.chain)) return Promise.resolve();
