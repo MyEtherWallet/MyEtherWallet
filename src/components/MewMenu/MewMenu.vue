105985dba47febff26ec8bf0927ef2562b59e800
David Hong <hsurf22@gmail.com>
2022-08-11 17:25:19 -0700

    devop: fix position of menu popup

--- src/components/MewMenu/MewMenu.vue
+++ src/components/MewMenu/MewMenu.vue
@@ -31,45 +31,47 @@
       </div>
     </template>
 
-    <!-- ================================================================== -->
-    <!-- Top arrow for content window -->
-    <!-- ================================================================== -->
-    <div v-if="topArrow" class="top-arrow" :style="topArrowStyle" />
+    <div class="content-container">
+      <!-- ================================================================== -->
+      <!-- Top arrow for content window -->
+      <!-- ================================================================== -->
+      <div v-if="topArrow" class="top-arrow" :style="topArrowStyle" />
 
-    <!-- ===================================================================================== -->
-    <!-- Menu List Content -->
-    <!-- ===================================================================================== -->
-    <div class="list-content">
-      <v-list v-for="(item, index) in listObj.items" :key="index">
-        <!-- ===================================================================================== -->
-        <!-- slot: 'mewMenuItem' + menu item number -->
-        <!-- ===================================================================================== -->
-        <slot :name="'mewMenuItem' + (item + 1)" />
-        <v-list-item v-if="item.title">
-          <v-list-item-title class="basic--text mew-heading-3 titleItem">
-            {{ item.title }}
-          </v-list-item-title>
-        </v-list-item>
-        <v-list-item
-          v-for="(subItem, idx) in item.items"
-          :key="subItem + idx"
-          class="cursor-pointer"
-          @click="goTo(subItem.to)"
-        >
-          <v-list-item-title
-            class="mew-body basic--text subItem d-flex align-center"
+      <!-- ===================================================================================== -->
+      <!-- Menu List Content -->
+      <!-- ===================================================================================== -->
+      <div class="list-content">
+        <v-list v-for="(item, index) in listObj.items" :key="index">
+          <!-- ===================================================================================== -->
+          <!-- slot: 'mewMenuItem' + menu item number -->
+          <!-- ===================================================================================== -->
+          <slot :name="'mewMenuItem' + (item + 1)" />
+          <v-list-item v-if="item.title">
+            <v-list-item-title class="basic--text mew-heading-3 titleItem">
+              {{ item.title }}
+            </v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            v-for="(subItem, idx) in item.items"
+            :key="subItem + idx"
+            class="cursor-pointer"
+            @click="goTo(subItem.to)"
           >
-            <v-icon
-              v-if="subItem.iconName"
-              class="mr-1 basic--text"
-              size="14px"
+            <v-list-item-title
+              class="mew-body basic--text subItem d-flex align-center"
             >
-              {{ subItem.iconName }}
-            </v-icon>
-            {{ subItem.title }}
-          </v-list-item-title>
-        </v-list-item>
-      </v-list>
+              <v-icon
+                v-if="subItem.iconName"
+                class="mr-1 basic--text"
+                size="14px"
+              >
+                {{ subItem.iconName }}
+              </v-icon>
+              {{ subItem.title }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </div>
     </div>
   </v-menu>
 </template>
@@ -139,7 +141,9 @@ export default {
   padding: 12px 10px 10px 10px;
   margin-left: -10px;
   border: none !important;
-  position: relative;
+  .content-container {
+    position: relative;
+  }
   .list-content {
     border-radius: 4px;
     overflow: hidden;
