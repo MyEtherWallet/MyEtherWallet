8db34d0ba4b67ec31da465c12c0ddb6ad5222458
Jesus Morales Jr <jmjles@gmail.com>
2022-08-15 17:43:25 -0700

    fix: 🐛 added missing info

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -976,11 +976,11 @@ export default {
         }
         const foundToken = this.contractToToken(token.contract);
         if (foundToken) {
-          const name = foundToken.name;
+          const name = foundToken.name || foundToken.subtext;
           foundToken.contract = token.contract;
           foundToken.price = this.getFiatValue(foundToken.pricef);
           foundToken.isEth = token.isEth;
-          foundToken.name = token.symbol;
+          foundToken.name = token.symbol || foundToken.symbol;
           foundToken.value = foundToken.contract;
           foundToken.subtext = name;
           localContractToToken[token.contract] = foundToken;
@@ -990,7 +990,7 @@ export default {
         token.price = '';
         token.subtext = name;
         token.value = token.contract;
-        token.name = token.symbol;
+        token.name = token.symbol || token.subtext;
         localContractToToken[token.contract] = token;
       });
     },
