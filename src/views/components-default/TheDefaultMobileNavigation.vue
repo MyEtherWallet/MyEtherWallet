99fefad1e782c0e927510f77f22d47f0dfcade8f
David Hong <hsurf22@gmail.com>
2022-08-26 00:59:22 -0700

    devop: update mobile menu

--- src/views/components-default/TheDefaultMobileNavigation.vue
+++ src/views/components-default/TheDefaultMobileNavigation.vue
@@ -4,26 +4,29 @@
 
     <v-navigation-drawer
       v-model="isOpen"
+      width="280px"
       absolute
       temporary
-      color="expandHeader"
+      color="white"
     >
-      <div class="text-center mt-7 mb-8" style="position: relative">
+      <div class="pa-5" style="position: relative">
         <v-btn class="close-button" large icon light @click="isOpen = false">
-          <v-icon color="white" large>mdi-window-close</v-icon>
+          <v-icon color="textDark">mdi-window-close</v-icon>
         </v-btn>
         <img
-          src="@/assets/images/icons/logo-mew.svg"
+          src="@/assets/images/icons/icon-mew-dark.svg"
           height="36"
           @click="pushRoute({ name: 'Home' })"
         />
       </div>
 
-      <v-list color="expandHeader" dark class="px-2">
+      <v-list color="white" class="px-2">
         <template v-for="(item, index) in menu">
-          <v-list-item v-if="!item.sub" :key="index" class="mb-3">
+          <v-list-item v-if="!item.sub" :key="index">
             <v-list-item-content v-if="item.to" @click="pushRoute(item.to)">
-              <div class="mew-heading-2">{{ item.label }}</div>
+              <div class="mew-heading-3 font-weight-medium textDark--text">
+                {{ item.label }}
+              </div>
             </v-list-item-content>
             <a
               v-if="item.url"
@@ -31,35 +34,32 @@
               target="_blanks"
               @click="isOpen = false"
             >
-              <v-list-item-content class="white--text">
-                <div class="mew-heading-2">{{ item.label }}</div>
+              <v-list-item-content class="textDark--text">
+                <div class="mew-heading-3 font-weight-medium textDark--text">
+                  {{ item.label }}
+                </div>
               </v-list-item-content>
             </a>
           </v-list-item>
 
-          <v-list-group
-            v-if="item.sub"
-            :key="index"
-            prepend-icon=""
-            color="white"
-            class="mb-3"
-          >
+          <v-list-group v-if="item.sub" :key="index" prepend-icon="">
             <template #activator>
               <v-list-item-content>
-                <div class="mew-heading-2">{{ item.label }}</div>
+                <div class="mew-heading-3 font-weight-medium textDark--text">
+                  {{ item.label }}
+                </div>
               </v-list-item-content>
             </template>
             <v-list-item
               v-for="(child, ckey) in item.sub"
               :key="ckey"
-              style="background-color: var(--v-expandHeader-base) !important"
               dense
               class="pl-4"
             >
               <v-list-item-content>
                 <v-list-item-title
                   v-if="child.to"
-                  class="pl-4 white--text font-weight-regular mew-body"
+                  class="pl-4 textDark--text font-weight-regular mew-body"
                   @click="pushRoute(child.to)"
                 >
                   {{ child.label }}
@@ -71,7 +71,7 @@
                   @click="isOpen = false"
                 >
                   <v-list-item-title
-                    class="pl-13 white--text font-weight-regular mew-body"
+                    class="pl-13 textDark--text font-weight-regular mew-body"
                   >
                     {{ child.label }}
                   </v-list-item-title>
@@ -80,12 +80,64 @@
             </v-list-item>
           </v-list-group>
         </template>
-        <v-list-item class="mb-3">
+        <v-list-item>
           <v-list-item-content @click="openMoonpay">
-            <div class="mew-heading-2">Buy ETH</div>
+            <div class="mew-heading-3 font-weight-medium textDark--text">
+              Buy ETH
+            </div>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-content>
+            <div class="mew-heading-3 font-weight-medium textDark--text mb-6">
+              Products
+            </div>
+            <a
+              v-for="(product, keyProduct) in mewProducts"
+              :key="keyProduct"
+              :href="product.link"
+              target="_blank"
+              class="d-flex align-center mb-5 ml-5"
+            >
+              <img
+                width="35"
+                :src="product.img"
+                :alt="product.label"
+                class="mr-3"
+              />
+              <div class="mew-heading-3 font-weight-medium textDark--text">
+                {{ product.label }}
+              </div>
+            </a>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <div class="pa-4 bottom-buttons">
+        <mew-button
+          btn-size="large"
+          has-full-width
+          title="Create a new wallet"
+          class="mb-2"
+          @click.native="
+            $router.push({
+              name: ROUTES_HOME.CREATE_WALLET.NAME,
+              params: {}
+            })
+          "
+        ></mew-button>
+        <mew-button
+          btn-style="outline"
+          btn-size="large"
+          has-full-width
+          title="Access my wallet"
+          @click.native="
+            $router.push({
+              name: ROUTES_HOME.ACCESS_WALLET.NAME,
+              params: {}
+            })
+          "
+        ></mew-button>
+      </div>
     </v-navigation-drawer>
   </div>
 </template>
@@ -100,7 +152,56 @@ export default {
   components: { AppBtnMenu },
   mixins: [buyMore],
   data: () => ({
-    isOpen: false
+    ROUTES_HOME: ROUTES_HOME,
+    isOpen: false,
+    mewProducts: [
+      {
+        label: 'MEW web',
+        description: 'Ethereum desktop wallet',
+        img: require('@/assets/images/icons/icon-mew-logo.svg'),
+        link: 'https://www.myetherwallet.com/'
+      },
+      {
+        label: 'MEW wallet',
+        imgDescription: {
+          icons: [
+            require('@/assets/images/icons/icon-apple.svg'),
+            require('@/assets/images/icons/icon-google.svg')
+          ],
+          label: 'Get the app'
+        },
+        img: require('@/assets/images/icons/icon-mew-wallet.png'),
+        link: 'https://www.mewwallet.com/'
+      },
+      {
+        label: 'Enkrypt',
+        imgDescription: {
+          icons: [require('@/assets/images/icons/icon-chrome.svg')],
+          label: 'Get the extension'
+        },
+        img: require('@/assets/images/icons/icon-enkrypt-block.svg'),
+        link: 'https://www.enkrypt.com'
+      },
+
+      {
+        label: 'EthVM',
+        description: 'Blockchain explorer',
+        img: require('@/assets/images/icons/icon-ethvm.svg'),
+        link: 'https://www.ethvm.com/'
+      },
+      {
+        label: 'MEWtopia',
+        description: 'Education Blog',
+        img: require('@/assets/images/icons/icon-puppy-mew.svg'),
+        link: 'https://www.mewtopia.com/'
+      },
+      {
+        label: 'Help Center',
+        description: 'How to use MEW products',
+        img: require('@/assets/images/icons/icon-customer-support.svg'),
+        link: 'https://help.myetherwallet.com/'
+      }
+    ]
   }),
   computed: {
     ...mapGetters('global', ['swapLink']),
@@ -162,13 +263,26 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.v-navigation-drawer {
-  width: 100% !important;
-  max-width: 500px;
-}
 .close-button {
   position: absolute;
-  left: 10px;
-  top: -3px;
+  right: 10px;
+  top: 16px;
+}
+.bottom-buttons {
+  background-color: var(--v-greyLight-base);
+}
+</style>
+
+<style lang="scss">
+.mew-component--landing-page-menu-mobile {
+  .theme--light.v-icon {
+    color: var(--v-textDark-base) !important;
+  }
+  .v-list-item--dense .v-list-item__title {
+    line-height: 18px !important;
+  }
+  .v-navigation-drawer__content {
+    height: 100vh;
+  }
 }
 </style>
