29390deeff0ecc0a2cba6e8e04233c204c38cf86
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-09-02 23:56:45 -0700

    devop: 🔧 cointype in address book for future use

--- src/modules/address-book/components/AddressBookAddEdit.vue
+++ src/modules/address-book/components/AddressBookAddEdit.vue
@@ -171,13 +171,13 @@ export default {
     },
     coin() {
       if (!this.validAddress) return '';
-      return (
-        'Valid ' +
-        (this.resolvedAddr.length > 0 && !this.resolvedAddr?.includes('.')
-          ? isValidCoinAddress(this.resolvedAddr).coin
-          : isValidCoinAddress(this.lowercaseAddressToAdd).coin) +
-        ' address'
-      );
+      return `Valid ${this.coinType} address`;
+    },
+    coinType() {
+      return this.resolvedAddr.length > 0 && !this.resolvedAddr?.includes('.')
+        ? isValidCoinAddress(this.resolvedAddr).coin
+        : isValidCoinAddress(this.lowercaseAddressToAdd).coin ||
+            isValidCoinAddress(this.addressToAdd).coin;
     },
     editMode() {
       return this.mode === modes[1];
@@ -330,6 +330,8 @@ export default {
     update() {
       this.addressBookStore[this.currentIdx].address =
         this.checksumAddressToAdd;
+      this.addressBookStore[this.currentIdx].coinType =
+        this.coinType.toLowerCase();
       this.addressBookStore[this.currentIdx].nickname = this.nickname;
       this.setAddressBook(this.addressBookStore);
       this.$emit('back', [3]);
@@ -348,6 +350,7 @@ export default {
       this.addressBookStore.push({
         address: this.checksumAddressToAdd,
         resolvedAddr: this.resolvedAddress,
+        coinType: this.coinType.toLowerCase(),
         nickname: this.nickname || (this.addressBookStore.length + 1).toString()
       });
       this.setAddressBook(this.addressBookStore);
