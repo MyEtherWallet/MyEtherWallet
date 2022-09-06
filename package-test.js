5b339ea61ddf70bda7fdf20c215ad7b7a9e8d913
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-09-06 10:46:09 -0700

    chore: 👷 update packages, fix conflicts

--- package-test.js
+++ package-test.js
@@ -55,23 +55,7 @@ const EXCEPTIONS = [
   'package-json',
   'codecov',
   'node-polyfill-webpack-plugin',
-  // check if fixable after hotfix
-  '@ensdomains/ensjs',
-  '@ensdomains/ens-contracts',
-  'protobufjs',
-  '@formatjs/intl-numberformat',
-  'babel-jest',
-  'qrcode',
-  'vue-template-compiler',
-  '@ledgerhq/hw-transport-web-ble',
-  '@ledgerhq/hw-app-eth',
-  '@ledgerhq/hw-transport-webusb',
-  '@lokalise/node-api',
-  '@unstoppabledomains/resolution',
-  '@walletconnect/client',
-  '@walletconnect/qrcode-modal',
-  'chromedriver',
-  'graphql'
+  'vue-template-compiler'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
