fcc9b98724060e887a2ac3690956a9f9ab04742a
Gamaliel Padillo <gamalielgad1234@gmail.com>
2022-09-22 11:17:50 -0700

    fix: 🐛 show a warning when nonEth

--- src/modules/confirmation/ModuleConfirmation.vue
+++ src/modules/confirmation/ModuleConfirmation.vue
@@ -32,7 +32,10 @@
     >
       <template #dialogBody>
         <v-card-text ref="scrollableContent" class="py-0 px-4 px-md-0">
-          <div class="px-4 py-6 pr-6 textBlack2--text border-radius--5px mb-5">
+          <div
+            v-if="toNonEth"
+            class="px-4 py-6 pr-6 textBlack2--text border-radius--5px mb-5"
+          >
             <b>Please double check everything.</b> MEW team will not be able to
             reverse your transaction once its submitted. You will still be
             charged gas fee even if the transaction fails.
@@ -75,12 +78,10 @@
             :to-currency="swapInfo.toType"
             :to-address="swapInfo.to"
           />
-          <!-- Warning Sheet -->
-          <!-- =========================================================================== -->
-          <!-- Commented to hide swap warning message -->
-          <!-- =========================================================================== -->
-          <!--
-          <div class="px-4 py-6 pr-6 textBlack2--text border-radius--5px mb-5">
+          <div
+            v-if="!toNonEth"
+            class="px-4 py-6 pr-6 textBlack2--text border-radius--5px mb-5"
+          >
             <b>Make sure all the information is correct.</b> Cancelling or
             reversing a transaction cannot be guaranteed. You will still be
             charged gas fee even if transaction fails.
@@ -91,7 +92,6 @@
               >Learn more.</a
             >
           </div>
-          -->
 
           <!-- Ledger Warning Sheet -->
           <div
