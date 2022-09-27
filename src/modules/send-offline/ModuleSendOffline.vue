89cb1e19c89896f0420900162d05ddf11bbe0508
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-09-27 11:08:00 -0700

    fix: 🐛 undefined reading symbol

--- src/modules/send-offline/ModuleSendOffline.vue
+++ src/modules/send-offline/ModuleSendOffline.vue
@@ -278,7 +278,7 @@ export default {
       return '';
     },
     disableData() {
-      return this.selectedCurrency.symbol !== this.network.type.currencyName;
+      return this.selectedCurrency?.symbol !== this.network.type.currencyName;
     },
     validAddress() {
       return this.toAddress !== '' && isValidAddress(this.toAddress);
