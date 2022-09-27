b7659baad7ca8f7a50b056b210f2e8ba25b144e6
andrewdahn <andrewahnjr@gmail.com>
2022-09-27 12:55:35 -0700

    fix: 🐛 fix moonbeam switch

--- src/views/TheWalletView.vue
+++ src/views/TheWalletView.vue
@@ -19,7 +19,7 @@ import { mapActions, mapState, mapGetters } from 'vuex';
 import { toBN } from 'web3-utils';
 import Web3 from 'web3';
 import moment from 'moment';
-
+import { debounce } from 'lodash';
 import handlerWallet from '@/core/mixins/handlerWallet.mixin';
 import nodeList from '@/utils/networks';
 import {
@@ -162,7 +162,7 @@ export default {
       }
       this.updateGasPrice();
     },
-    subscribeToBlockNumber() {
+    subscribeToBlockNumber: debounce(function () {
       this.web3.eth.getBlockNumber().then(bNumber => {
         this.setBlockNumber(bNumber);
         this.web3.eth.getBlock(bNumber).then(block => {
@@ -188,7 +188,7 @@ export default {
             });
         });
       });
-    },
+    }, 1000),
     /**
      * Checks Metamask chainID on load, switches current network if it doesn't match
      * and setup listeners for metamask changes
