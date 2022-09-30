61c4fe9f00279887c60204454c4c09210bfeb2fd
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-09-29 18:38:34 -0700

    devop: 🔧 create send transaction e2e test

---
+++ tests/e2e/specs/sending-test.js
@@ -0,0 +1,36 @@
+const css = 'css selector';
+const address = '0x9ccCb2B82f8F607db3F5D50BF4083665c3002f83';
+const amount = '0.0000001';
+
+module.exports = {
+  before: function (browser) {
+    browser.globals.waitForConditionTimeout = 15000;
+  },
+  'Send Transaction test': async browser => {
+    // Assuming on wallet dashboard
+    browser
+      .waitForElementVisible(css, '.SendTransaction')
+      .click(css, '.SendTransaction');
+
+    // input amount
+    browser.assert
+      .urlContains('send-tx')
+      .waitForElementVisible(css, '.AmountInput')
+      .click(css, '.AmountInput')
+      .keys(amount);
+
+    // input address
+    browser
+      .waitForElementVisible(css, '.AddressInput')
+      .click(css, '.AddressInput')
+      .keys(address);
+
+    // click send when enabled
+    browser.ensure.elementIsEnabled('.SendButton').click(css, '.SendButton');
+
+    // click confirm and send
+    browser
+      .waitForElementVisible(css, '.ConfirmButton')
+      .click(css, '.ConfirmButtom');
+  }
+};
