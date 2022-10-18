ed4106db4c90800b70280cb21b43aeb2108a579f
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-10-17 17:49:17 -0700

    fix: 🐛 unselected token when searching

--- src/modules/moon-pay/components/MoonPaySellComponent.vue
+++ src/modules/moon-pay/components/MoonPaySellComponent.vue
@@ -4,6 +4,7 @@
     <!-- Currency select -->
     <!-- ============================================================== -->
     <mew-select
+      ref="selectedCurrency"
       label="Currency"
       :items="currencyItems"
       :value="selectedCurrency"
@@ -426,6 +427,25 @@ export default {
     }
   },
   mounted() {
+    // Watch for currency menu isActive
+    const selectedCurrencyRef = this.$refs.selectedCurrency;
+    const menuRef = selectedCurrencyRef.$children[0].$refs.menu;
+    this.$watch(
+      () => {
+        return menuRef.isActive;
+      },
+      val => {
+        // If menu is closed make sure search is cleared
+        if (!val) {
+          if (
+            selectedCurrencyRef.search !== '' ||
+            selectedCurrencyRef.selectItems.length === 0
+          )
+            selectedCurrencyRef.search = '';
+        }
+      }
+    );
+
     if (!this.inWallet) this.$refs.addressInput.$refs.addressSelect.clear();
     this.sendHandler = new handlerSend();
     this.fetchSellInfo();
