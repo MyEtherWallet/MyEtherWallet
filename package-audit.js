6b47cfff35a7a37ad45dc31d6c97cfcaa007c90f
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-11-22 11:05:27 -0800

    chore: 👷 update package-audit

--- package-audit.js
+++ package-audit.js
@@ -17,7 +17,8 @@ const AUDIT_EXCEPTIONS = [
   'semver-regex',
   'scss-tokenizer',
   'codecov', // no package found
-  'loader-utils'
+  'loader-utils',
+  'engine.io'
 ];
 const execute = (command, callback) => {
   exec(
