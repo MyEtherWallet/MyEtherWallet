8f54a75336d6057d3fb74cc2939209bbf9dc23d5
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2022-12-19 11:22:28 -0800

    chore: 👷 update packages, tokens and exceptions

--- package-test.js
+++ package-test.js
@@ -75,7 +75,9 @@ const EXCEPTIONS = [
   'web3-eth-contract',
   'is-ipfs',
   'axios',
-  'prettier' // creates more warnings
+  'prettier', // creates more warnings
+  '@ledgerhq/hw-app-eth',
+  '@ledgerhq/live-common'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
