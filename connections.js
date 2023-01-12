b50dd3bbb433a864a4abbd2abf5757dcf4de6952
Abyscuit <36307949+Abyscuit@users.noreply.github.com>
2023-01-12 13:27:25 -0800

    devop: 🔧 add mewtopia to connections

--- connections.js
+++ connections.js
@@ -31,6 +31,7 @@ module.exports = [
   'https://raw.githubusercontent.com:443/MyEtherWallet/dynamic-data/main/articles.json',
   'https://formspree.io:443/f/mqjndkkx',
   'https://broadcast.mewapi.io:443',
+  'https://www.mewtopia.com:443',
   // nodes
   'https://nodes.mewapi.io:443',
   'wss://nodes.mewapi.io:443',
