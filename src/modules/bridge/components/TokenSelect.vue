104d53adbd41870296a1d4c155d9fe53c236a659
David Hong <davidh@myetherwallet.com>
2023-01-11 18:24:16 -0800

    devop: fix token selector popup issue

--- src/modules/bridge/components/TokenSelect.vue
+++ src/modules/bridge/components/TokenSelect.vue
@@ -169,7 +169,7 @@ export default {
     }
   },
   mounted() {
-    this.tokenSelected = this.validNetwork ? this.network.type.name : '';
+    //this.tokenSelected = this.validNetwork ? this.network.type.name : '';
   },
   methods: {
     /**
