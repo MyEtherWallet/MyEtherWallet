29ecace8b3b786be3bc8f736d3179b49475d55c6
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-01-20 09:52:04 -0800

    fix: 🐛 staging build

--- package-test.js
+++ package-test.js
@@ -78,7 +78,10 @@ const EXCEPTIONS = [
   'axios',
   'prettier', // creates more warnings
   '@ledgerhq/hw-app-eth', // issue with imports
-  '@ledgerhq/live-common' // issue with imports
+  '@ledgerhq/live-common', // issue with imports
+  '@commitlint/config-conventional',
+  'commitlint',
+  'eslint-plugin-security'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
