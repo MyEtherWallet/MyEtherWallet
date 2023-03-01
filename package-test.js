1dc90fdb71399d8d24b8fb87d0db27ef5c8eb0e8
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-03-01 10:55:44 -0800

    fix: 🐛 build

--- package-test.js
+++ package-test.js
@@ -79,7 +79,12 @@ const EXCEPTIONS = [
   'prettier', // creates more warnings
   '@ledgerhq/hw-app-eth', // issue with imports
   '@ledgerhq/live-common', // issue with imports
-  'stylelint-config-prettier' // throws an error when committing
+  'stylelint-config-prettier', // throws an error when committing
+  '@coolwallet/eth',
+  '@ensdomains/address-encoder',
+  '@formatjs/intl-numberformat',
+  'commitlint',
+  '@commitlint/config-conventional'
 ];
 const CUSTOM_DIST = {
   ['babel-core']: 'bridge'
