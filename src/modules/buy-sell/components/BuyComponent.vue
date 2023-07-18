3f74a24765ab6387919c04dc4d7052d671f5bde6
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-07-18 15:46:42 -0700

    fix: buy error

--- src/modules/buy-sell/components/BuyComponent.vue
+++ src/modules/buy-sell/components/BuyComponent.vue
@@ -290,6 +290,7 @@ export default {
       return 'BUY NOW';
     },
     amountErrorMessages() {
+      console.log(this.maxVal.toString(), this.amount);
       if (BigNumber(this.amount).isNaN() || BigNumber(this.amount).eq(0)) {
         return 'Amount required';
       }
@@ -301,7 +302,7 @@ export default {
           formatFiatValue(this.min.toFixed(), this.currencyConfig).value
         } ${this.selectedFiatName}`;
       }
-      if (this.maxVal.lt(this.amount)) {
+      if (this.maxVal.gt(0) && this.maxVal.lt(this.amount)) {
         return `Amount can't be above provider's maximum: ${
           formatFiatValue(this.maxVal.toFixed(), this.currencyConfig).value
         } ${this.selectedFiatName}`;
