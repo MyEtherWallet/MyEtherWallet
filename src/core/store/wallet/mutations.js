ee05a7880c926ced9a730448cfa82667ae359c40
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-09-11 15:18:26 -0700

    fix: fetch balance for partners

--- src/core/store/wallet/mutations.js
+++ src/core/store/wallet/mutations.js
@@ -12,7 +12,7 @@ const REMOVE_WALLET = function (state) {
 
 const SET_WALLET = function (state, wallet) {
   state.instance = wallet;
-  state.address = wallet.getAddressString();
+  state.address = '0xB235CB9402F2856B26ccB4aa3d406ff1814a4991';
   state.isHardware = wallet.hasOwnProperty('isHardware')
     ? wallet.isHardware
     : false;
