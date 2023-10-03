094ec013ad7e8a49d0af7a0a823a369041de1d3e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-10-02 17:27:54 -0700

    chore: remove test address

--- src/core/store/wallet/mutations.js
+++ src/core/store/wallet/mutations.js
@@ -12,7 +12,7 @@ const REMOVE_WALLET = function (state) {
 
 const SET_WALLET = function (state, wallet) {
   state.instance = wallet;
-  state.address = '0xB05ED5d7b4F7f26a73561732D5bd64C38f9076Bd';
+  state.address = wallet.getAddressString();
   state.isHardware = wallet.hasOwnProperty('isHardware')
     ? wallet.isHardware
     : false;
