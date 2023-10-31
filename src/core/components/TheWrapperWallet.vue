c488fd1c791e4487d58129cc2ce2525e4069c9cd
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-10-31 15:01:16 -0700

    fix: show module tokens

--- src/core/components/TheWrapperWallet.vue
+++ src/core/components/TheWrapperWallet.vue
@@ -65,7 +65,7 @@
         </v-col>
         <v-col
           v-for="n in totalRightColItems"
-          v-show="totalRightColItems > 1"
+          v-show="totalRightColItems >= 1"
           :key="n"
           cols="12"
           class="pa-2 pa-md-3 pb-0"
