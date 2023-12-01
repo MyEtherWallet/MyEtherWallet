8de7e85febdaed3bab70a339f41965dcc2c8d9dd
Gamaliel Padillo <gamalielgad1234@gmail.com>
2023-11-30 16:55:08 -0800

    devop: move anchor to better cut off

--- src/views/components-default/HowItWorksDappsCenter.vue
+++ src/views/components-default/HowItWorksDappsCenter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="dapps" class="component-container">
-    <div class="py-3 py-md-12" />
+  <div class="component-container">
+    <div id="dapps" class="py-3 py-md-12" />
     <v-container>
       <v-row class="flex-column-reverse flex-lg-row">
         <v-col
