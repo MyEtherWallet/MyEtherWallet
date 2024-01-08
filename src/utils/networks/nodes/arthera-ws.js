b9f67aad324a4db0be9b7722db9c4ede7b3fd2c5
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-01-08 13:43:43 -0800

    chore: remove slash

--- src/utils/networks/nodes/arthera-ws.js
+++ src/utils/networks/nodes/arthera-ws.js
@@ -2,7 +2,7 @@ import { AA } from '../types';
 export default {
   type: AA,
   service: 'arthera-ws',
-  url: 'wss://ws.arthera.net/',
+  url: 'wss://ws.arthera.net',
   port: 443,
   auth: false,
   username: '',
