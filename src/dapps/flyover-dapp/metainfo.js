ea27af01496aae43b85f36f2cd7da5aa7f328263
ahsan-javaiid <ahsan.javaid@iovlabs.org>
2024-01-31 22:21:11 +0500

    fix: flyover dapp validations

--- src/dapps/flyover-dapp/metainfo.js
+++ src/dapps/flyover-dapp/metainfo.js
@@ -1,6 +1,6 @@
 import layout from './TheFlyoverLayout.vue';
 import Module1 from './modules/ModulePegin.vue';
-// import { ROOTSTOCK } from '@/utils/networks/types';
+import { ROOTSTOCK } from '@/utils/networks/types';
 import { FLYOVER_ROUTE, flyoverRouterGuard } from './routes';
 
 export default {
@@ -14,7 +14,7 @@ export default {
   path: FLYOVER_ROUTE.FLYOVER.PATH,
   name: FLYOVER_ROUTE.FLYOVER.NAME,
   defaultName: FLYOVER_ROUTE.FLYOVER.NAME,
-  networks: [],
+  networks: [ROOTSTOCK],
   layout,
   release: '01/22/2024',
   meta: {
