e17675d2a411c1948632fad1f8c9f96c82e57dbb
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-02-01 13:00:10 -0800

    fix: better bitcoin validation

--- src/dapps/flyover-dapp/handlers/helpers/validations.js
+++ src/dapps/flyover-dapp/handlers/helpers/validations.js
@@ -1,5 +1,5 @@
 import { isAddress } from '@/core/helpers/addressUtils.js';
-
+import MultiCoinValidator from 'multicoin-address-validator';
 export function isAmountValid(amount, min, max) {
   const val = parseFloat(amount);
 
@@ -14,10 +14,8 @@ export function isRootstockAddress(address) {
   return isAddress(address);
 }
 
-const LEGACY_MAINNET_REGEX = /^[1]([a-km-zA-HJ-NP-Z1-9]{25,34})$/;
-
 export function isLegacyBtcAddress(address) {
   if (!address) false;
 
-  return LEGACY_MAINNET_REGEX.test(address);
+  return MultiCoinValidator.validate(address, 'Bitcoin');
 }
