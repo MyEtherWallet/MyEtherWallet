38eff122701b982ef3d2f684c18e9fd2b503574e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-03-07 11:43:22 -0800

    chore: revert to mainnet for now

--- src/modules/buy-sell/handlers/handlerOrder.js
+++ src/modules/buy-sell/handlers/handlerOrder.js
@@ -1,6 +1,6 @@
 import { sha3 } from 'web3-utils';
 import axios from 'axios';
-const API = 'https://staging.mewwallet.dev';
+const API = 'https://mainnet.mewwallet.dev';
 
 export default class OrderHandler {
   constructor() {}
