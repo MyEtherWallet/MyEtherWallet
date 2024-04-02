4497ac89f393f781a94677048181ddc7b7335243
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-04-02 10:41:43 -0700

    chore: change to setup:

--- src/modules/swap/ModuleSwap.vue
+++ src/modules/swap/ModuleSwap.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="mew-component--swap">
     <white-sheet class="px-10 py-10">
-      {{ sample }}
+      <div class="mew-heading-1">Swap</div>
     </white-sheet>
   </div>
 </template>
 
 <script setup>
 import EnkryptSwap from '@enkryptcom/swap';
-const sample = 'HELLO';
 </script>
 
 <style lang="scss" scoped></style>
