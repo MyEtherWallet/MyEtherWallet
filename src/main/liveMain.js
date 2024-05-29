f364c5b7d6a6b359caaa33501465800305ddaf78
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-05-29 15:13:01 -0700

    devop: more changes

--- src/main/liveMain.js
+++ src/main/liveMain.js
@@ -13,6 +13,7 @@ import * as nameHashPckg from 'eth-ens-namehash';
 import VueIntercom from '@mathieustan/vue-intercom';
 import VueSocialSharing from 'vue-social-sharing';
 import * as amplitude from '@amplitude/analytics-browser';
+// import * as sessionReplay from '@amplitude/session-replay-browser';
 import { sessionReplayPlugin } from '@amplitude/plugin-session-replay-browser';
 
 /**Dapps Store */
@@ -66,13 +67,16 @@ Vue.config.productionTip = false;
 // fake generative 32 hex character
 const popupStore = locStore.get('popups-store') || { consentToTrack: false };
 
-const sessionReplayTracking = sessionReplayPlugin({
-  configEndpointUrl:
-    process.env.NODE_ENV === 'production'
-      ? 'https://analytics-web.mewwallet.dev/record'
-      : 'https://analytics-web-development.mewwallet.dev/record'
-});
-amplitude.add(sessionReplayTracking).promise.then(() => {
+const main = async () => {
+  const sessionReplayTracking = sessionReplayPlugin({
+    sampleRate: 1,
+    debugMode: true
+    // configEndpointUrl:
+    //   process.env.NODE_ENV === 'production'
+    //     ? 'https://analytics-web.mewwallet.dev/record'
+    //     : 'https://analytics-web-development.mewwallet.dev/record'
+  });
+  await amplitude.add(sessionReplayTracking).promise;
   amplitude.init(nameHashPckg.hash(VERSION), {
     instanceName:
       process.env.NODE_ENV === 'production' ? 'mew-web-prod' : 'mew-web-dev',
@@ -89,40 +93,49 @@ amplitude.add(sessionReplayTracking).promise.then(() => {
     logLevel: amplitude.Types.LogLevel.None,
     defaultTracking: {
       formInteractions: false,
-      pageViews: false
+      pageViews: false,
+      sessions: true
     }
   });
   Vue.prototype.$amplitude = amplitude;
-});
 
-// Lazy Loader
-Vue.use(VueLazyLoad);
+  // Lazy Loader
+  Vue.use(VueLazyLoad);
 
-new Vue({
-  el: '#app',
-  i18n,
-  router,
-  store,
-  apolloProvider,
-  vuetify,
-  beforeCreate() {
-    const userId = this.$route.query.intercomid
-      ? this.$route.query.intercomid
-      : uuidv4();
-    this.$intercom.boot({ user_id: userId });
+  new Vue({
+    el: '#app',
+    i18n,
+    router,
+    store,
+    apolloProvider,
+    vuetify,
+    beforeCreate() {
+      const userId = this.$route.query.intercomid
+        ? this.$route.query.intercomid
+        : uuidv4();
+      this.$intercom.boot({ user_id: userId });
 
-    if (locStore.get('mew-testing') === undefined) {
-      locStore.set('mew-testing', false);
-    }
-    this.$store.commit('custom/INIT_STORE');
-    this.$store.commit('global/INIT_STORE');
-    this.$store.commit('notifications/INIT_STORE');
-    this.$store.commit('addressBook/INIT_STORE');
-    this.$store.commit('article/INIT_STORE');
-    this.$store.commit('popups/INIT_STORE');
-    dappStoreBeforeCreate(this.$store);
+      if (locStore.get('mew-testing') === undefined) {
+        locStore.set('mew-testing', false);
+      }
+      this.$store.commit('custom/INIT_STORE');
+      this.$store.commit('global/INIT_STORE');
+      this.$store.commit('notifications/INIT_STORE');
+      this.$store.commit('addressBook/INIT_STORE');
+      this.$store.commit('article/INIT_STORE');
+      this.$store.commit('popups/INIT_STORE');
+      dappStoreBeforeCreate(this.$store);
+      let counter = 0;
+      const x = setInterval(() => {
+        console.log(sessionReplayTracking);
+        counter++;
+        if (counter === 5) clearInterval(x);
+      }, 5000);
 
-    this.$amplitude?.setOptOut(!this.$store.state.popups.consentToTrack);
-  },
-  render: h => h(app)
-});
+      this.$amplitude?.setOptOut(!this.$store.state.popups.consentToTrack);
+      // this.$amplitude.add('sessionReplayTracking');
+    },
+    render: h => h(app)
+  });
+};
+main();
