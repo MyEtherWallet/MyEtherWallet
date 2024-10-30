190235d3bc2d6b8a081a0382992b8121faf65573
olgakup <olga.kupchevskaya.812@my.csun.edu>
2024-10-30 13:40:08 -0700

    feat: ✨ fonts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import './assets/main.css'
+import '@assets/main.css'
 import i18n from './i18n/index'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import '@assets/fonts/DMSans/DMSans.css';
 import App from './App.vue'
 import router from './router'
 
