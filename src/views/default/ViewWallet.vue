c54604fd6cb763b6318d4e678cc59171889fa618
Gamaliel Padillo <gamalielgad1234@gmail.com>
2024-12-03 13:47:36 -0800

    devop: start

---
+++ src/views/default/ViewWallet.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <!-- sidemenu -->
+    <div></div>
+    <!-- main content -->
+    <div></div>
+    <!-- right items -->
+    <div></div>
+  </main>
+</template>
+<script setup lang="ts"></script>
