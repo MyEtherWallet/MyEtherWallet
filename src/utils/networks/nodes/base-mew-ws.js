845843c6acc51ecc31c4f2d03414be8743223b0e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-04-01 10:31:05 -0700

    feat: add Base network

---
+++ src/utils/networks/nodes/base-mew-ws.js
@@ -0,0 +1,10 @@
+import { BASE } from '../types';
+export default {
+  type: BASE,
+  service: 'myetherwallet.com-ws',
+  url: 'wss://nodes.mewapi.io/ws/base',
+  port: 443,
+  auth: false,
+  username: '',
+  password: ''
+};
