b155efadbbadd18f61cfafa3114f3a3f1781812f
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-04-16 12:10:01 -0700

    fix: names changed

--- src/modules/nft-manager/handlers/config/configNft.js
+++ src/modules/nft-manager/handlers/config/configNft.js
@@ -12,14 +12,14 @@ export const chains = {
   1: `${API}eth-mainnet/address/`,
   137: `${API}matic-mainnet/address/`,
   56: `${API}bsc-mainnet/address/`,
-  42161: `${API}arb-mainnet/address/`,
+  42161: `${API}arbitrum-mainnet/address/`,
   1313161554: `${API}aurora-mainnet/address/`,
   43114: `${API}avalanche-mainnet/address/`,
-  250: `${API}ftm-mainnet/address/`,
+  250: `${API}fantom-mainnet/address/`,
   100: `${API}gnosis-mainnet/address/`,
   1284: `${API}moonbeam-mainnet/address/`,
   1285: `${API}moonbeam-moonriver/address/`,
-  10: `${API}op-mainnet/address/`,
+  10: `${API}optimism-mainnet/address/`,
   196: `${API}x1-mainnet/address/`
 };
 
