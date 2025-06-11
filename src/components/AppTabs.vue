835372fde2225777abb08193df1c459489c25b32
olgakup <16910687+olgakup@users.noreply.github.com>
2025-06-11 15:12:52 -0700

    devop: app tabs  logic as composable

--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -34,7 +34,7 @@
             : activeTabIndex
         "
         :id="tab.id"
-        @click="() => (model = index)"
+        @click="setActiveTab(index)"
       >
         <slot name="tab-content">
           <span class="mx-3"> {{ tab.name }}</span>
@@ -60,10 +60,9 @@
   </div>
 </template>
 <script setup lang="ts">
-import { computed, nextTick, onBeforeMount, type PropType } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { type PropType } from 'vue'
 import { type Tab, type Tab_Panel } from '@/types/components/appTabs'
-
+import { useAppTabs } from '@/composables/useAppTabs'
 const props = defineProps({
   /**
    * @tabs - An array of tab objects.
@@ -100,75 +99,20 @@ const props = defineProps({
  * @model - The v-model for the active tab index.
  * use <v-model:activeTabIndex> to bind the active tab index to a parent component.
  */
-const model = defineModel('activeTabIndex', {
+const activeTabIndex = defineModel('activeTabIndex', {
   type: Number,
   required: true,
   default: 0,
 })
-const route = useRoute()
-const currentRouteName = computed(() => route.name)
 
-const currActivePanel = computed(() => {
-  return props.panel[model.value]
-})
+const { handleKeyDown, currentRouteName, currActivePanel } = useAppTabs(
+  activeTabIndex,
+  props.panel,
+  props.tabs,
+  props.useRouteLink,
+)
 
-const router = useRouter()
-
-const navigateToTab = async (index: number) => {
-  try {
-    if (props.useRouteLink) {
-      await nextTick()
-      await router.push({ name: props.tabs[index].routeName })
-    }
-    await nextTick()
-    document.getElementById(props.tabs[index].id)?.focus()
-  } catch (err) {
-    //Todo: add sentry logging
-    console.error('Failed to navigate to tab:', err)
-  }
+const setActiveTab = (index: number) => {
+  activeTabIndex.value = index
 }
-
-/**
- * Handles keyboard navigation for tab switching.
- *
- * @param {KeyboardEvent} event - The keyboard event triggered by user interaction.
- *
- * If the left arrow key is pressed:
- * - Decrements the model value if it's greater than 0.
- * - If `useRouteLink` prop is true, navigates to the previous tab's route and focuses on its element.
- *
- * If the right arrow key is pressed:
- * - Increments the model value if it's less than the total number of tabs minus one.
- * - If `useRouteLink` prop is true, navigates to the next tab's route and focuses on its element.
- */
-const handleKeyDown = (event: KeyboardEvent) => {
-  if (event.key === 'ArrowLeft') {
-    if (model.value === 0) {
-      return
-    }
-    model.value -= 1
-    navigateToTab(model.value)
-  } else if (event.key === 'ArrowRight') {
-    if (model.value === props.tabs.length - 1) return
-    model.value += 1
-    navigateToTab(model.value)
-  }
-}
-
-/**
- * Ensure corret active tab is set if using routerlink
- */
-onBeforeMount(() => {
-  if (props.useRouteLink) {
-    const index = props.tabs.findIndex(
-      tab => tab.routeName === currentRouteName.value,
-    )
-    if (index !== -1) {
-      model.value = index
-    } else {
-      model.value = 0
-      navigateToTab(0)
-    }
-  }
-})
 </script>
---
+++ src/composables/useAppTabs.ts
@@ -0,0 +1,85 @@
+import { computed, nextTick, onBeforeMount, type Ref } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { type Tab, type Tab_Panel } from '@/types/components/appTabs'
+
+export const useAppTabs = (
+  activeTabIndex: Ref<number>,
+  panels: Tab_Panel[],
+  tabs: Tab[],
+  useRouteLink: boolean = false,
+) => {
+  const router = useRouter()
+  const route = useRoute()
+  const currentRouteName = computed(() => route.name)
+
+  const currActivePanel = computed(() => {
+    return panels[activeTabIndex.value]
+  })
+
+  const navigateToTab = async (index: number) => {
+    try {
+      if (useRouteLink) {
+        await nextTick()
+        await router.push({ name: tabs[index].routeName })
+      }
+      await nextTick()
+      document.getElementById(tabs[index].id)?.focus()
+    } catch (err) {
+      //Todo: add sentry logging
+      console.error('Failed to navigate to tab:', err)
+    }
+  }
+
+  /**
+   * Handles keyboard navigation for tab switching.
+   *
+   * @param {KeyboardEvent} event - The keyboard event triggered by user interaction.
+   *
+   * If the left arrow key is pressed:
+   * - Decrements the activeTabIndex value if it's greater than 0.
+   * - If `useRouteLink` prop is true, navigates to the previous tab's route and focuses on its element.
+   *
+   * If the right arrow key is pressed:
+   * - Increments the activeTabIndex value if it's less than the total number of tabs minus one.
+   * - If `useRouteLink` prop is true, navigates to the next tab's route and focuses on its element.
+   */
+  const handleKeyDown = (event: KeyboardEvent) => {
+    let next = 0
+    if (event.key === 'ArrowLeft') {
+      if (activeTabIndex.value === 0) {
+        return
+      }
+      next = activeTabIndex.value - 1
+    } else if (event.key === 'ArrowRight') {
+      if (activeTabIndex.value === tabs.length - 1) return
+      next = activeTabIndex.value + 1
+    }
+    activeTabIndex.value = next
+    navigateToTab(next)
+  }
+
+  /**
+   * Ensure corret active tab is set if using routerlink
+   */
+  onBeforeMount(() => {
+    if (useRouteLink) {
+      const index = tabs.findIndex(
+        tab => tab.routeName === currentRouteName.value,
+      )
+      if (index !== -1) {
+        activeTabIndex.value = index
+      } else {
+        activeTabIndex.value = 0
+        navigateToTab(0)
+      }
+    }
+  })
+
+  return {
+    activeTabIndex,
+    currActivePanel,
+    navigateToTab,
+    handleKeyDown,
+    currentRouteName,
+  }
+}
