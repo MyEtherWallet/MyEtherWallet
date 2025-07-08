2831bccf49819617768d8f8fb529bd5fc1e7b3cb
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2025-07-08 11:25:10 -0700

    fix: 🐛 random wallet connect popup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <the-app-layout />
+    <the-app-layout v-if="isLoadingComplete" />
     <module-toast />
   </div>
 </template>
@@ -12,7 +12,7 @@ import { useFetchMewApi } from '@/composables/useFetchMewApi'
 import { type ChainsRaw } from '@/mew_api/types'
 import { useChainsStore } from '@/stores/chainsStore'
 import { useProviderStore } from '@/stores/providerStore'
-import { onMounted, watch } from 'vue'
+import { onMounted, watch, ref } from 'vue'
 import { useWalletStore } from '@/stores/walletStore'
 import { storeToRefs } from 'pinia'
 import { type TokenBalancesRaw } from '@/mew_api/types'
@@ -20,6 +20,7 @@ import { type TokenBalancesRaw } from '@/mew_api/types'
 const store = useWalletStore()
 const { wallet, walletAddress } = storeToRefs(store)
 const { setTokens, setIsLoadingBalances } = store
+const isLoadingComplete = ref(false)
 
 watch(
   () => walletAddress.value,
@@ -48,6 +49,7 @@ const { data, onFetchResponse } = useMEWFetch<ChainsRaw>('/chains').get().json()
 
 onFetchResponse(() => {
   setChainData(data.value.result || [])
+  isLoadingComplete.value = true
   return data.value.result
 })
 
