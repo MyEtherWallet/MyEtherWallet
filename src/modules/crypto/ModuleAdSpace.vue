b0a6c63310e85082ddafdb3c1dfafe545ccbba6e
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-08-21 14:06:58 -0700

    devop: some cleanup

--- src/modules/crypto/ModuleAdSpace.vue
+++ src/modules/crypto/ModuleAdSpace.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="rounded-2xl border border-slate-200 bg-gradient-to-br from-indigo-500 to-fuchsia-500 text-white p-6 text-center shadow-sm min-h-[150px]"
+    class="rounded-2xl border text-white p-6 text-center shadow-sm min-h-[150px]"
   >
     <h3 class="font-semibold">Optional MEW Marketing Space</h3>
   </section>
--- src/modules/crypto/ModuleEducationSpace.vue
+++ src/modules/crypto/ModuleEducationSpace.vue
@@ -1,7 +1,5 @@
 <template>
-  <section
-    class="rounded-2xl border border-slate-200 bg-gradient-to-br from-indigo-500 to-fuchsia-500 text-white p-6 text-center shadow-sm"
-  >
-    <h3 class="font-semibold">Katya Space<br />educational</h3>
+  <section class="rounded-2xl border text-white p-6 text-center shadow-sm">
+    <h3 class="font-semibold">Educational Space</h3>
   </section>
 </template>
