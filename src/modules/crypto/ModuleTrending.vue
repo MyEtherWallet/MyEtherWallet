9153bb633852f6440aeda5f4d666a21bf37e4eb2
Gamaliel Padillo <gamalielgad1234@gmail.com>
2025-08-22 11:01:35 -0700

    chore: 🔧 loading trending

--- src/modules/crypto/ModuleTrending.vue
+++ src/modules/crypto/ModuleTrending.vue
@@ -1,9 +1,14 @@
 <template>
   <!-- Top: Trending -->
-  <header class="bg-white border rounded-2xl p-4 shadow-sm">
-    <h2 class="font-semibold mb-3">Trending</h2>
+  <header class="rounded-2xl pb-4">
+    <h2 class="text-s-32 mb-3">Trending</h2>
     <div class="grid grid-flow-col auto-cols-fr gap-3">
-      <span v-for="n in 6" :key="n" class="h-7 rounded-full bg-indigo-100" />
+      <div
+        v-for="n in 6"
+        :key="n"
+        class="h-7 bg-white border rounded-full bg-grey-8 cursor-pointer"
+        :class="{ 'animate-pulse': true }"
+      ></div>
     </div>
   </header>
 </template>
