2b53e262a89805410e02469b89955952c4f7c44a
olgakup <16910687+olgakup@users.noreply.github.com>
2025-09-03 11:04:01 -0700

    devop: add panels logic

--- src/components/core_layouts/LayoutWallet.vue
+++ src/components/core_layouts/LayoutWallet.vue
@@ -2,13 +2,13 @@
   <div
     :class="[
       isOpenSideMenu ? 'min-w-[400px] max-w-[400px] w-[400px]' : 'min-w-[60px]',
-      'absolute lg:sticky z-20  mt-[1px] top-0 right-0  lg:h-screen transition-all duration-300 ease-out',
+      'absolute lg:sticky z-20  mt-[1px] top-0 right-0  lg:h-screen transition-all duration-200 ease-out',
     ]"
   >
     <div
       :class="[
         isOpenSideMenu ? 'p-4' : 'p-2',
-        'bg-white min-h-[calc(100vh-69px)] sm:h-[calc(100vh-200px)]  shadow-[0px_3px_12px_-6px_rgba(0,0,0,0.32);]  overflow-y-scroll',
+        'bg-white min-h-[calc(100vh-69px)] sm:h-[calc(100vh-200px)]  shadow-[0pxx_3px_12px_-6px_rgba(0,0,0,0.32);]  overflow-y-scroll',
       ]"
     >
       <app-btn-icon
@@ -18,10 +18,12 @@
       >
         <ChevronDoubleLeftIcon class="w-5 h-5" />
       </app-btn-icon>
-      <app-wallet-card
-        v-if="isWalletConnected && isOpenSideMenu"
-        class="px-4"
-      />
+      <transition name="fade" mode="out-in">
+        <app-wallet-card
+          v-if="isWalletConnected && isOpenSideMenu"
+          class="mb-4"
+        />
+      </transition>
 
       <!-- Actions -->
       <div
@@ -40,7 +42,7 @@
           @click="openDeposit"
         >
           <QrCodeIcon
-            :class="[{ 'mb-1': isOpenSideMenu }, 'w-7 h-7 text-primary']"
+            :class="[{ 'mb-1': isOpenSideMenu }, 'w-6 h-6 text-primary']"
           />
           <p v-if="isOpenSideMenu" class="text-s-12">{{ $t('deposit') }}</p>
         </button>
@@ -53,7 +55,7 @@
           @click.stop.prevent="closeDialog"
         >
           <icon-buy
-            :class="[{ 'mb-1': isOpenSideMenu }, 'w-7 h-7 text-primary']"
+            :class="[{ 'mb-1': isOpenSideMenu }, 'w-6 h-6 text-primary']"
           />
           <p v-if="isOpenSideMenu" class="text-s-12">{{ $t('buy-sell') }}</p>
         </router-link>
@@ -67,7 +69,7 @@
           @click.stop.prevent="closeDialog"
         >
           <icon-send
-            :class="[{ 'mb-1': isOpenSideMenu }, 'w-7 h-7 text-primary']"
+            :class="[{ 'mb-1': isOpenSideMenu }, 'w-6 h-6 text-primary']"
           />
           <p v-if="isOpenSideMenu" class="text-s-12">{{ $t('common.send') }}</p>
         </div>
@@ -80,28 +82,28 @@
           @click.stop.prevent="closeDialog"
         >
           <icon-swap
-            :class="[{ 'mb-1': isOpenSideMenu }, 'w-7 h-7 text-primary']"
+            :class="[{ 'mb-1': isOpenSideMenu }, 'w-6 h-6 text-primary']"
           />
           <p v-if="isOpenSideMenu" class="text-s-12 capitalize">
             {{ $t('swap') }}
           </p>
         </div>
       </div>
-      <div v-if="isOpenSideMenu">
-        <p class="font-semibold text-s-24 mt-5 mb-1 ml-3">{{ panelTitle }}</p>
-        <transition name="fade" mode="out-in">
-          <!-- Modules -->
-          <ModuleSend v-if="activePanel === 0" key="send" />
-        </transition>
-      </div>
+      <transition name="fade" mode="out-in">
+        <div v-if="isOpenSideMenu">
+          <transition name="fade" mode="out-in">
+            <!-- Modules -->
+            <ModuleSend v-if="walletPanel === 0" key="send" />
+          </transition>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { ref, computed, onMounted, watch } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import AppWalletCard from '@/components/AppWalletCard.vue'
-// import TableTokens from './components/TableTokens.vue'
 import { useWalletStore } from '@/stores/walletStore'
 import { useWalletMenu } from '@/composables/useWalletMenu'
 import { useAppBreakpoints } from '@/composables/useAppBreakpoints'
@@ -114,14 +116,13 @@ import { ROUTES_MAIN } from '@/router/routeNames'
 import AppBtnIcon from '../AppBtnIcon.vue'
 
 const walletMenu = useWalletMenu()
-const { isOpenSideMenu, setIsOpenSideMenu } = walletMenu
+const { isOpenSideMenu, walletPanel, setIsOpenSideMenu, setWalletPanel } =
+  walletMenu
 
 const breakpoints = useAppBreakpoints()
 const { isDesktopAndUp } = breakpoints
 const walletStore = useWalletStore()
 const { isWalletConnected } = storeToRefs(walletStore)
-
-const activePanel = ref(0)
 
 const openDialog = ref(false)
 const closeDialog = () => {
@@ -131,17 +132,6 @@ const closeDialog = () => {
 const openDeposit = () => {
   openDialog.value = true
 }
-
-const panelTitle = computed(() => {
-  switch (activePanel.value) {
-    case 0:
-      return 'Send'
-    case 1:
-      return 'Swap'
-    default:
-      return ''
-  }
-})
 
 onMounted(() => {
   if (isDesktopAndUp.value) {
@@ -163,6 +153,6 @@ const openPanel = (panel: number) => {
   if (!isOpenSideMenu.value) {
     setIsOpenSideMenu(true)
   }
-  activePanel.value = panel
+  setWalletPanel(panel)
 }
 </script>
--- src/composables/useWalletMenu.ts
+++ src/composables/useWalletMenu.ts
@@ -2,13 +2,20 @@ import { ref } from 'vue'
 
 export const useWalletMenu = () => {
   const isOpenSideMenu = ref(false)
+  const walletPanel = ref(0)
 
   const setIsOpenSideMenu = (value: boolean) => {
     isOpenSideMenu.value = value
   }
 
+  const setWalletPanel = (value: number) => {
+    walletPanel.value = value
+  }
+
   return {
     isOpenSideMenu,
     setIsOpenSideMenu,
+    walletPanel,
+    setWalletPanel,
   }
 }
