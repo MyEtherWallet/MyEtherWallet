1bc0a8531f0764b73219a994438c9424d776bbdd
olgakup <16910687+olgakup@users.noreply.github.com>
2025-09-17 13:15:32 -0700

    devop: type guards

--- src/stores/walletMenuStore.ts
+++ src/stores/walletMenuStore.ts
@@ -3,6 +3,8 @@ import { defineStore } from 'pinia'
 
 export const useWalletMenuStore = defineStore('walletMenuStore', () => {
   const isOpenSideMenu = ref(false)
+  type WalletPanel = 0 | 1 | 2 | 3
+
   /**
    * Current Active panel in the wallet menu
    * 0 - Buy
@@ -10,14 +12,17 @@ export const useWalletMenuStore = defineStore('walletMenuStore', () => {
    * 2 - Send
    * 3 - Swap
    */
-  const walletPanel = ref(3)
+  const walletPanel = ref<WalletPanel>(3)
 
   const setIsOpenSideMenu = (value: boolean) => {
     isOpenSideMenu.value = value
   }
 
   const setWalletPanel = (value: number) => {
-    walletPanel.value = value
+    if (value < 0 || value > 3) {
+      throw new Error('Invalid wallet panel')
+    }
+    walletPanel.value = value as WalletPanel
   }
 
   return {
