e84af7b82e454a6162a161586fa3d0993937514b
olgakup <16910687+olgakup@users.noreply.github.com>
2025-10-14 12:05:05 -0700

    devop: change views file structure

--- src/views/ViewPortfolio.vue
+++ src/views/ViewPortfolio.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="flex items-center flex-col">
+  <div class="flex flex-col gap-6">
+    <div class="grid grid-cols-12 w-full gap-6">
+      <AppWalletCard class="cols-span-12 lg:col-span-5" />
+      <div class="col-span-12 lg:col-span-7">
+        <module-trending />
+      </div>
+    </div>
     <h1 class="mt-10 title3 pt-10 text-center">Coming soon</h1>
     <p class="text-s-16 mt-6 text-center">
       This is an Alpha Version of MyEtherWallet V7.
@@ -14,4 +20,5 @@
 </template>
 <script setup lang="ts">
 import AppSubscribeToUpdates from '@/components/AppSubscribeToUpdates.vue'
+import AppWalletCard from '@/components/AppWalletCard.vue'
 </script>
