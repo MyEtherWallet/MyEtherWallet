c4b2f51d6feca9bb554b0a51d27f45a5e4416cb3
olgakup <16910687+olgakup@users.noreply.github.com>
2026-01-27 15:47:42 -0800

    fix: 🐛 typo

--- src/views/ViewTemp.vue
+++ src/views/ViewTemp.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center flex-col">
     <h1 class="mt-10 title3 pt-10 text-center">Coming soon</h1>
     <p class="text-s-16 mt-6 text-center">
-      This is an Beta version of MyEtherWallet.
+      This is a Beta version of MyEtherWallet.
     </p>
     <p class="text-s-16 mt-1 text-center">
       We are working hard to bring you the best experience possible.
