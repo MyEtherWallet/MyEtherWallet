43ae4ed5ea72d1bd413d6f46f3cc225ece2ecb01
Nicolas Irigoitia <nicolasirigoitia@Nicolass-MacBook-Pro.local>
2026-03-20 15:30:42 -0300

    fix: 🐛 beta mentions removed

--- src/views/ViewTemp.vue
+++ src/views/ViewTemp.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex items-center flex-col">
     <h1 class="mt-10 title3 pt-10 text-center">Coming soon</h1>
-    <p class="text-s-16 mt-6 text-center">
-      This is a Beta version of MyEtherWallet.
-    </p>
+    <p class="text-s-16 mt-6 text-center">Pardon our crypto dust!</p>
     <p class="text-s-16 mt-1 text-center">
-      We are working hard to bring you the best experience possible.
+      We are currently working on even more staking and earning options for our
+      amazing users (you)!
     </p>
-    <p class="text-s-16 mt-1 mb-10 text-center">Stay tuned for updates!</p>
+    <p class="text-s-16 mt-1 mb-10 text-center">
+      To access your staked assets please visit the previous version of MEW
+      <a href="#" target="_blank" class="underline text-info">here</a>.
+    </p>
     <AppSubscribeToUpdates />
   </div>
 </template>
