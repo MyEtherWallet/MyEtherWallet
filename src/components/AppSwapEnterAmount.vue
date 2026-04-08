4abaab3600bdb370f58790604adb14e1ecdf72ec
Gamaliel Padillo <gamalielgad1234@gmail.com>
2026-04-08 11:55:37 -0700

    feat: ✨ add dropdown, add trending and stablecoins

--- src/components/AppSwapEnterAmount.vue
+++ src/components/AppSwapEnterAmount.vue
@@ -56,6 +56,7 @@
         v-model:selected-token="selectedToken"
         :external-loading="isLoading"
         :chain-tokens="tokens || []"
+        :trending-addresses="trendingAddresses"
         :is-from-view="isFromView"
         :network-name="networkName"
         @open:select-token="setIsOpenSelectToken"
@@ -155,6 +156,10 @@ const props = defineProps({
   networkName: {
     type: String,
     required: false,
+  },
+  trendingAddresses: {
+    type: Array as () => string[],
+    default: () => [],
   },
   isPristine: {
     type: Boolean,
