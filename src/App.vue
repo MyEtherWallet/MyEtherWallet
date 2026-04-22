928957ab28393986f97662f164845629299d36f2
Gamaliel Padillo <gamalielgad1234@gmail.com>
2026-04-22 14:37:47 -0700

    feat: ✨ release wallet banner tomorrow

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,9 @@
     <module-access-wallet v-if="isLoadingComplete" :aria-selected="true" />
     <module-create-wallet v-if="isLoadingComplete" :aria-selected="true" />
     <the-gdpr-banner v-if="isLoadingComplete" :aria-hidden="isAreaHidden" />
-    <!-- <app-mew-wallet-banner
+    <app-mew-wallet-banner
       v-if="isLoadingComplete && !isTradingRestrictedInRegion"
-    /> -->
-    <app-mew-wallet-banner />
+    />
   </div>
 </template>
 
@@ -43,8 +42,7 @@ import { useRewardsStore } from '@/stores/rewardsStore'
 import Intercom from '@intercom/messenger-js-sdk'
 import { useMarketStatus } from './modules/trade/composables'
 
-const { fetchMarketStatus } = useMarketStatus()
-// const { isTradingRestrictedInRegion, fetchMarketStatus } = useMarketStatus()
+const { isTradingRestrictedInRegion, fetchMarketStatus } = useMarketStatus()
 
 const dialogStore = useDialogStore()
 const { isAreaHidden } = storeToRefs(dialogStore)
