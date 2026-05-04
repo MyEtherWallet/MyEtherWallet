79668b46939e55df9b3d53d0429050f7b3ec8c4f
olgakup <16910687+olgakup@users.noreply.github.com>
2026-05-04 14:32:54 -0700

    fix: 🐛 ui comments

--- src/modules/perps/components/PerpsPortfolioSummary.vue
+++ src/modules/perps/components/PerpsPortfolioSummary.vue
@@ -9,22 +9,13 @@
           {{ walletBalance }}
         </p>
         <p
-          class="text-info font-bold tracking-sp-06 uppercase text-s-11 mt-3 mb-2"
+          class="text-info font-bold tracking-sp-06 uppercase text-s-11 mt-2 mb-2"
         >
           Realized PnL
         </p>
         <p class="text-s-16 leading-none" :class="pnlColorClass">
           {{ formattedRealizedPnl }}
           <span class="text-s-13 ml-2 leading-[16px]">({{ pnlPercent }})</span>
-        </p>
-        <p
-          class="text-info font-bold tracking-sp-06 uppercase text-s-11 mt-5 mb-2"
-        >
-          Unrealized PnL
-        </p>
-        <p class="text-s-16 leading-none" :class="uPnlColorClass">
-          {{ formattedUPnl }}
-          <span class="text-s-13 ml-2 leading-[16px]">({{ uPnlPercent }})</span>
         </p>
         <div class="flex items-center gap-3 justify-start mt-5 -mx-1">
           <AppBaseButton @click="$emit('deposit')" class="w-full" size="medium">
@@ -67,6 +58,22 @@
           <p :class="marginRatioColorClass" class="ml-2 font-medium">
             {{ marginRatio }}
           </p>
+        </div>
+        <div class="flex items-start justify-between gap-4 w-full">
+          <p class="text-info font-bold tracking-sp-06 uppercase text-s-11">
+            Unrealized PnL
+          </p>
+          <div class="flex justify-end flex-col items-start">
+            <p :class="uPnlColorClass" class="font-medium text-right">
+              {{ formattedUPnl }}
+            </p>
+            <p
+              :class="uPnlColorClass"
+              class="text-s-13 leading-[16px] text-right ml-auto"
+            >
+              {{ uPnlPercent }}
+            </p>
+          </div>
         </div>
         <app-btn-text
           class="mt-1 text-s-13 text-primary mr-auto -ml-3"
