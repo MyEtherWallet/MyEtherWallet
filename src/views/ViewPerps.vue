84e1ab05f4c55e096e128b90c92425df68a3f972
Gamaliel Padillo <gamalielgad1234@gmail.com>
2026-05-05 18:07:15 -0700

    feat: ✨ if has watch only, auto request sign on perps page

--- src/views/ViewPerps.vue
+++ src/views/ViewPerps.vue
@@ -71,7 +71,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { PERP_INFO_ROUTE_NAME } from '@/router/routeNames'
@@ -90,12 +90,21 @@ import { useAppBreakpoints } from '@/composables/useAppBreakpoints'
 const router = useRouter()
 const walletMenu = useWalletMenuStore()
 const walletStore = useWalletStore()
-const { isWatchOnly } = storeToRefs(walletStore)
+const { isWatchOnly, wallet } = storeToRefs(walletStore)
 const accessStore = useAccessStore()
 const { token, isWalletConnected, isAuthenticating, authError, login, logout } =
   usePerpsAuth()
 const { isDesktopAndUp } = useAppBreakpoints()
 const connectWallet = () => accessStore.openAccessDialog()
+
+watch(
+  () => wallet.value,
+  (newVal, oldVal) => {
+    if (newVal && !isWatchOnly.value && oldVal) {
+      login()
+    }
+  },
+)
 const showDeposit = ref(false)
 const showWithdraw = ref(false)
 
