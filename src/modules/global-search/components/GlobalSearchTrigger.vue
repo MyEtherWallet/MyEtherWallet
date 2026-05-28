786d5954ef4dcce9612991c9a5e7fbb6b685a9b6
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2026-05-28 14:48:12 -0300

    feat: [MEW-1766] add GlobalSearchTrigger mobile icon button

---
+++ src/modules/global-search/components/GlobalSearchTrigger.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="relative">
+    <button
+      type="button"
+      class="w-9 h-9 flex items-center justify-center rounded-full hover:bg-mewBg"
+      @click="open"
+    >
+      <magnifying-glass-icon class="w-5 h-5" />
+    </button>
+    <div
+      v-if="isOpen"
+      class="fixed inset-x-0 top-0 z-30 bg-white px-4 py-3 flex items-center gap-2 shadow"
+    >
+      <input
+        v-model="query"
+        type="text"
+        :placeholder="$t('search.placeholder')"
+        class="flex-1 bg-mewBg rounded-full px-4 py-2 outline-none text-s-14"
+        autofocus
+      />
+      <button class="text-s-13 font-medium text-primary" @click="close">
+        Cancel
+      </button>
+    </div>
+    <global-search-popover />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+import { useGlobalSearch } from '../composables/useGlobalSearch'
+import GlobalSearchPopover from './GlobalSearchPopover.vue'
+
+const { isOpen, query, open, close } = useGlobalSearch()
+</script>
