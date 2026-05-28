bad29e7d76709c0446e2d4f1616a2a39f6636cc8
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2026-05-28 14:48:35 -0300

    feat: [MEW-1766] add ModuleGlobalSearch root

---
+++ src/modules/global-search/ModuleGlobalSearch.vue
@@ -0,0 +1,14 @@
+<template>
+  <global-search-input v-if="isDesktop" />
+  <global-search-trigger v-else />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useAppBreakpoints } from '@/composables/useAppBreakpoints'
+import GlobalSearchInput from './components/GlobalSearchInput.vue'
+import GlobalSearchTrigger from './components/GlobalSearchTrigger.vue'
+
+const { isXLMinAndUp } = useAppBreakpoints()
+const isDesktop = computed(() => isXLMinAndUp.value)
+</script>
