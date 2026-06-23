d3a83f40ee8a9c3e8a92ba82afff8b354d750a95
Gamaliel Padillo <gamalielgad1234@gmail.com>
2026-06-23 13:12:02 -0700

    chore: 🔧 switch to prod

--- src/configs.ts
+++ src/configs.ts
@@ -17,8 +17,7 @@ const configs = {
     'GNOSIS',
     'ROOTSTOCK',
   ],
-  // MEW_REWARDS_API_URL: 'https://mew-rewards-prod.ethvm.dev',
-  MEW_REWARDS_API_URL: 'http://localhost:3000',
+  MEW_REWARDS_API_URL: 'https://mew-rewards-prod.ethvm.dev',
   MEW_LIVE_URLS: ['app.beta.myetherwallet.com', 'app.myetherwallet.com'],
   MEW_SENTRY_DSN:
     import.meta.env.VITE_SENTRY_DSN ||
