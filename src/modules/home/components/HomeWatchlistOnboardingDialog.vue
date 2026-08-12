24b422cbee3cb6bb5b3cec98494519c7cb62d9e7
René Ricardo <32307287+renericardoperez@users.noreply.github.com>
2026-08-12 10:42:27 -0300

    feat: add watchlist onboarding dialog wiring the 3 steps + bucket adds

---
+++ src/modules/home/components/HomeWatchlistOnboardingDialog.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import AppDialog from '@/components/AppDialog.vue'
+import { useWatchlistStore } from '@/stores/watchlistTableStore'
+import { useRecommendedWatchlist } from '@/modules/home/composables/useRecommendedWatchlist'
+import WatchlistStepMarkets from './WatchlistStepMarkets.vue'
+import WatchlistStepIndustries from './WatchlistStepIndustries.vue'
+import WatchlistStepAssets from './WatchlistStepAssets.vue'
+
+const isOpen = defineModel<boolean>('isOpen', { required: true })
+
+const watchlistStore = useWatchlistStore()
+const { assets, isLoading, fetchRecommendations } = useRecommendedWatchlist()
+
+const activeStep = ref(0)
+const selectedMarkets = ref<string[]>([])
+const selectedIndustries = ref<string[]>([])
+const selectedAssetIds = ref<string[]>([])
+
+const reset = () => {
+  activeStep.value = 0
+  selectedMarkets.value = []
+  selectedIndustries.value = []
+  selectedAssetIds.value = []
+}
+
+const goToIndustries = () => {
+  activeStep.value = 1
+}
+
+const goToAssets = () => {
+  activeStep.value = 2
+  fetchRecommendations(selectedMarkets.value, selectedIndustries.value)
+}
+
+const finish = () => {
+  for (const id of selectedAssetIds.value) {
+    const asset = assets.value.find(a => a.id === id)
+    if (!asset) continue
+    if (asset.type === 'perp') {
+      if (!watchlistStore.isWatchListedPerp(asset.watchlistId)) {
+        watchlistStore.setWatchlistPerp(asset.watchlistId)
+      }
+    } else if (!watchlistStore.isWatchListed(asset.watchlistId)) {
+      watchlistStore.setWatchlistItem(asset.watchlistId, asset.type === 'stock')
+    }
+  }
+  isOpen.value = false
+}
+
+// Start every run fresh once the dialog closes (no back button by design).
+watch(isOpen, open => {
+  if (!open) reset()
+})
+</script>
+
+<template>
+  <AppDialog
+    v-model:is-open="isOpen"
+    has-content-gutter
+    class="sm:mx-auto sm:w-full sm:max-w-[480px]"
+    data-test="watchlist-onboarding-dialog"
+  >
+    <template #content>
+      <div class="pb-6 pt-2">
+        <WatchlistStepMarkets
+          v-if="activeStep === 0"
+          v-model="selectedMarkets"
+          @continue="goToIndustries"
+        />
+        <WatchlistStepIndustries
+          v-else-if="activeStep === 1"
+          v-model="selectedIndustries"
+          @continue="goToAssets"
+        />
+        <WatchlistStepAssets
+          v-else
+          v-model="selectedAssetIds"
+          :assets="assets"
+          :is-loading="isLoading"
+          @done="finish"
+        />
+      </div>
+    </template>
+  </AppDialog>
+</template>
